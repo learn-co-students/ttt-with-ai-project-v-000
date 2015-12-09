6bfddc3f9de75a94b529c42bf04df831bdcefb2c
ed-karabinus <ejkarabin@gmail.com>
2015-12-09 03:47:58 -0600

    Removed redundant definition of constant WIN_COMBINATIONS.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -27,17 +27,6 @@ class Player::Computer < Player
     end
   end
 
-  WIN_COMBINATIONS = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [2,4,6],
-    [0,4,8]
-  ]
-
   def won?(board)
     WIN_COMBINATIONS.each do |win_combination|
       win_index_1 = win_combination[0]
