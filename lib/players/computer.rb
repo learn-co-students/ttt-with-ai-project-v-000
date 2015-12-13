1e1568772abd07fa9ba6dead08d5e27fc0fe6692
Justin Thomas <jthomas0209@gmail.com>
2015-12-13 16:02:24 -0500

    Working on minmax, still issues

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,45 +2,70 @@ require_relative '../player.rb'
 require 'pry'
 
 class Player::Computer < Player
-  attr_accessor :scores, :moves
+  attr_accessor :scores
 
   def initialize(token)
     @token = token
     @scores = Array.new(9,0)
-    @moves = []
   end
   
   def valid_moves(board)
-    @moves.clear
+    moves = []
     for i in 1..9 do 
-      @moves << i if board.valid_move?(i.to_s)
-    end
-  end
-
-  def score_options(board)
-    @scores = Array.new(9,0)
-    char = ["X","O"]
-    char.delete(@token)
-    child_board = board.cells.dup
-    @moves.each do |move|
-      options = Game::WIN_COMBINATIONS.find_all{|combo| combo.include?(move-1)}
-      options.each do |element|
-        child_board[move-1] = @token
-        @scores[move-1] = 10 if child_board[element[0]] == child_board[element[1]] && child_board[element[0]] == child_board[element[2]]
-        child_board[move-1] = " "
-        child_board[move-1] = char[0]
-        @scores[move-1] = -10 if child_board[element[0]] == child_board[element[1]] && child_board[element[0]] == child_board[element[2]]
-        child_board[move-1] = " "
+      if board.valid_move?(i.to_s)
+        moves << i 
+      else
+        @scores[i-1] = nil
       end
     end
+    moves
+  end
+
+  def score_options(board,current_token,initial_moves,depth=0)
+    if board.full?
+      return nil
+    else
+      initial_moves.each do |move|
+        minmax(board,current_token,depth,move,initial_moves.dup)
+      end
+    end 
   end
 
   def move(board)
-    valid_moves(board)
-    score_options(board)
-    input = @moves.sample
-    input = @scores.find_index(-10)+1 if @scores.find_index(-10) != nil
-    input = @scores.find_index(10)+1 if @scores.find_index(10) != nil
+    @scores = Array.new(9,0)
+    child_board = board.dup
+    initial_moves = valid_moves(child_board)
+    if initial_moves.include?(5)
+      input = 5
+    else
+      score_options(child_board,@token,initial_moves)
+      if @scores.compact.max > @scores.compact.min
+        input = @scores.find_index{|element| element == @scores.compact.max} + 1
+      else
+        input = @scores.find_index{|element| element == @scores.compact.min} + 1
+      end
+    end
     input.to_s
+  end
+
+  def minmax(board,current_token,depth,move,available_moves)
+    board.cells[move-1] = current_token
+    options = Game::WIN_COMBINATIONS.find_all{|combo| combo.include?(move-1)}
+    options.each do |element|
+      if board.cells[element[0]] == board.cells[element[1]] && board.cells[element[0]] == board.cells[element[2]]
+        board.cells[move-1] = " "
+        if current_token == @token  
+          @scores[move-1] = (10-depth) if (10-depth) > @scores[move-1] 
+        else 
+         @scores[move-1] = (depth-10) if (depth-10).abs > @scores[move-1].abs
+        end
+        return @scores[move-1]
+      else
+        current_token == @token ? current_token = "O" : current_token = "X"
+        available_moves.delete(move)
+        score_options(board,current_token,available_moves,depth+1)
+        board.cells[move-1] = " "
+      end
+    end
   end
 end
