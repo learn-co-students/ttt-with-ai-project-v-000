d03e008aebd4350c836e1d96cfb7de53ea18bea1
authorbeard <msnolan@gmail.com>
2015-12-24 14:11:09 -0500

    computer player specs passing

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,12 +1,11 @@
 
 class Computer < Player
+  attr_accessor :move
+  attr_reader :token
 
   def move(board)
-    move = (1..9).detect{|i| board.valid_move(i)}
-
-  def move
-    computer_move = (1..9).detect{|i| valid_move?(i)}
-    game
+    computer_move = (1..9).detect{|i| board.valid_move?(i)}.to_s
   end
+
 
 end
