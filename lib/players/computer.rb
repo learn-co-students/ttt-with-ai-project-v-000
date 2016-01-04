c73faf4d5d4a6dd76f65a88e9649523d054390b3
rluftw <luxh93@gmail.com>
2016-01-04 15:34:36 -0500

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,13 +1,6 @@
 require_relative "../player.rb"
 
 class Player::Computer < Player
-	def move(board)
-		move = rand(7)+1
-		move.to_s
-	end
-end
-
-class Player::Computer < Player
 
 	def move(board)
 		return "5" if !board.taken?("5")
