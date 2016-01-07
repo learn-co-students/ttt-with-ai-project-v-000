30f7433bebb36ce57e1b3aa3ae1377bad0f36fa0
eric-an <erican@me.com>
2016-01-06 22:41:27 -0800

    define complete Game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,7 @@
+require 'pry'
+require_relative "./players/human.rb"
+require_relative "./board.rb"
+
 class Game
   attr_accessor :board, :player_1, :player_2
 
@@ -12,10 +16,63 @@ class Game
     [6,4,2]
   ]
 
-  def initialize(player_1, player_2, board)
+ def initialize(player_1 = Human.new("X"), player_2 = Human.new("O"), board = Board.new)
     @player_1 = player_1
     @player_2 = player_2
     @board = board
   end
 
+  def current_player
+    ( board.turn_count % 2 == 0 ? @player_1 : @player_2 )
+  end
+
+  def over?
+    board.full? || won? || draw?
+  end
+
+  def won?
+    WIN_COMBINATIONS.each do |win_combination|
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
+
+      position_1 = board.cells[win_index_1]
+      position_2 = board.cells[win_index_2]
+      position_3 = board.cells[win_index_3]
+      
+      return win_combination if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+    end
+    return false
+  end
+
+  def draw?
+    board.full? && !won?
+  end
+
+  def winner
+    if won?
+      board.cells[won?[0]]
+    end
+  end
+
+  def turn
+    input = current_player.move(board)
+    if board.valid_move?(input)
+      board.update(input, current_player)
+    else
+      turn
+    end
+  end
+
+  def play
+    until over?
+      turn
+    end
+
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cats Game!"
+    end
+  end
 end
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,7 +1,10 @@
+require_relative "../player.rb"
+
 class Human < Player
 
   def move(board)
     puts "Please choose a move by choosing a number."
     input = gets.strip
   end
+
 end
