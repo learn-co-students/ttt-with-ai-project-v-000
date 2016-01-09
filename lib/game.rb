3da595bf3673de79b07b635380559cd87d6994d9
eric-an <erican@me.com>
2016-01-09 04:43:20 -0800

    complete unbeatable AI class and implement wargame method

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,8 @@
 class Game
   attr_accessor :board, :player_1, :player_2
 
+  @@draws = 0
+
   WIN_COMBINATIONS = [
     [0,1,2],
     [3,4,5],
@@ -55,6 +57,7 @@ class Game
     puts " "
     puts "It's your turn, Player '#{current_player.token}':"
     input = current_player.move(board)
+    puts input
     if board.valid_move?(input)
       board.update(input, current_player)
       board.display
@@ -72,6 +75,7 @@ class Game
     if won?
       puts "Congratulations #{winner}!"
     elsif draw?
+      @@draws += 1
       puts "Cats Game!"
     end
   end
@@ -83,33 +87,41 @@ class Game
 
   def human_vs_computer
     response = ""
-      until response == 'human' || response == 'computer'
-      puts "Who goes first? 'Human' or 'Computer'?"
-      response = gets.strip.downcase          
-        case response 
-        when "human"  
-          game = Game.new(Human.new("X"), Computer.new("O"), board = Board.new)
-          puts "You will be 'X'."
-        when "computer"
-          game = Game.new(Computer.new("X"), Human.new("O"), board = Board.new)
-          puts "The computer will be 'X'."
-        else
-          puts "Please choose 'human' or 'computer'."
-        end
+    until response == 'human' || response == 'computer'
+    puts "Who goes first? 'Human' or 'Computer'?"
+    response = gets.strip.downcase          
+      case response 
+      when "human"  
+        game = Game.new(Human.new("X"), Computer.new("O"), board = Board.new)
+        puts "You will be 'X'."
+      when "computer"
+        game = Game.new(Computer.new("X"), Human.new("O"), board = Board.new)
+        puts "The computer will be 'X'."
+      else
+        puts "Please choose 'human' or 'computer'."
       end
-      game.play
-      play_again?
+    end
+    game.play
+    play_again?
+  end
+
+  def wargame
+    game = Game.new(Computer.new("X"), Computer.new("O"), board = Board.new)
+    game.play
   end
 
   def start
     input = nil
-    while input != 4
+    while input != 5
       puts "How would you like to play? Please select a number:"
       puts "1. computer vs. computer"
       puts "2. human vs. computer"
       puts "3. human vs. human"
+      puts "4. wargame mode"
+      puts "5. quit"
+
       puts " "
-      puts "4. quit"
+      puts "Make a selection:"
       input = gets.strip.to_i
       case input
         when 1
@@ -124,8 +136,14 @@ class Game
           puts "Player 2 will be 'O'."
           game.play
           play_again?
-        when 4
+        when 5
           puts "Thanks for playing!"
+        when 4
+          puts "How many wargames would you like to simulate? Choose a number."
+          number = gets.strip.to_i
+          number.times { wargame }
+          puts "**Games played: #{number} ** Games drawn: #{@@draws} ** Games won: #{number-@@draws} **"
+          puts " "
         else
           puts "That's an invalid choice."
       end
--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,7 +1,47 @@
+require 'pry'
+require_relative '../game.rb'
+require_relative '../player.rb'
+require_relative '../board.rb'
 class Computer < Player
+  attr_accessor :board
 
   def move(board)
-    move = 1 + rand(10)
-    move.to_s
+    ( !board.taken?("5") ? 5 : possible_move(board) + 1)
   end
-end 
+
+  def possible_move(board)
+    win_or_block(board) || corner_move(board) || random_move(board) 
+  end
+
+  def corner_move(board)
+    corners = [0,2,6,8]
+    corners.shuffle.detect { |spot| !board.taken?(spot+1) }
+  end
+
+  def open_spot(board, token)  #returns winning combination that must be blocked or won
+    Game::WIN_COMBINATIONS.detect do |spot|
+      ((board.cells[spot[0]] == token && board.cells[spot[1]] == token) && !board.taken?(spot[2]+1)) ||
+      ((board.cells[spot[1]] == token && board.cells[spot[2]] == token) && !board.taken?(spot[0]+1)) ||
+      ((board.cells[spot[2]] == token && board.cells[spot[0]] == token) && !board.taken?(spot[1]+1))
+    end
+  end
+
+  def opp_token
+    ( self.token == "X" ? "O" : "X")
+  end
+
+# if there is a winning combination with one spot not taken, the next move is to pick this spot - 
+# whether it's to block the opponent or to try to take it for the win
+  def win_or_block(board)
+    if open_spot(board, self.token)
+      open_spot(board, self.token).detect { |index| !board.taken?(index+1) }
+    elsif open_spot(board, self.opp_token)
+      open_spot(board, self.opp_token).detect { |index| !board.taken?(index+1) }
+    end 
+  end
+
+  def random_move(board)
+    [0,1,2,3,4,5,6,7,8].shuffle.detect { |spot| !board.taken?(spot+1) }
+  end
+
+end
