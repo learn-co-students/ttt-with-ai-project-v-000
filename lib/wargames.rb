53c0a77b4643c7faa43cbb3c99a0fc2155c4033e
authorbeard <msnolan@gmail.com>
2016-01-10 01:03:30 -0500

    refactored d_z?, #strategery, #get_schwifty, still problem with block_move.

--- lib/wargames.rb
+++ lib/wargames.rb
@@ -22,6 +22,7 @@ class Wargames < Game
       @games_played += 1
       endgame("won") if won?
       endgame("draw") if draw?
+  # binding.pry
       play
     end
     puts "\e[H\e[2J"
@@ -31,97 +32,149 @@ class Wargames < Game
   def turn
     @board.display
     @move = ai_move(@board)
-# binding.pry
     @board.ai_update(@move, current_player)
     # sleep 1
     
   end
 
-  def ai_move(board)
-
+  def ai_move(board) ##FIRST MOVE IS RANDOM, SECOND IS CENTER OR, IF THAT'S FILLED, A CORNER
     if board.turn_count == 0
-    # binding.pry
+# binding.pry
       current_player.move(board).to_i - 1
-    elsif center_open? 
-      # binding.pry
-      4
+    # elsif center_open? 
+  
+    #   4
     else
       danger_zone? || strategery(current_player)
-# binding.pry
     end
     
   end
 
-  def danger_zone?
-    # binding.pry
-    @dz_array = @wins_available.detect{|a| 
-      a.select{|i| @board.cells[i] !=" "}.size==2}
-    if @dz_array == nil
+  def danger_zone? ## LOOKS THROUGH WINNING ARRAYS FOR ANY WITH TWO SPOTS MARKED
+                   ## --IF AN IMMEDIATE WIN IS AVAILABLE, TAKES THAT
+                   ## --IF OPPONENT WIN IMMINENT, TAKES THAT
+                   ## --IF NEITHER OF THOSE THINGS, DELETES THAT ARRAY, LOOKS FOR ANOTHER
+                   ## --IF THERE ARE NO POTENTIAL WINNERS WITH TWO SPOTS MARKET, HANDS OFF TO
+                   ##   #STRATEGERY
+
+  # binding.pry 
+    @check = @wins_available.select{|a| 
+      a.select{|i| @board.cells[i] !=" "}.size==2} ##<--ARRAY FROM W_C CONSTANT W/2 SPOTS FILLED
+    if @check.empty?
       return false
     else 
+      @dz_array = @check.detect{|a| a.select{|i| @board.cells[i] == current_player.token}}
       case @dz_array.select{|i| @board.cells[i] == current_player.token}.size
       when 2
-        # binding.pry
+  binding.pry  
         win_move 
-      when 1 
+      when 0   ##<--CHECKS FOR OPPONENT WIN IMMINENT
+  binding.pry       
         @wins_available.delete(@dz_array)
-        # binding.pry
-        danger_zone?
-      when 0
-        @wins_available.delete(@dz_array)
-        # binding.pry
+    
         block_win
+      when 1 
+binding.pry  
+        @wins_available.delete(@dz_array) ##<--NOBODY CAN WIN THAT WAY, DELETE IT, LOOK FOR ANOTHER
+    
+        danger_zone?
       end
+  binding.pry
     end
   end
 
-  def strategery(player)
-    # get_schwifty
-    if @wins_available.empty? && corner_open?
-      corner(CORNERS) 
+  def strategery(player) ## RUNS ON @WINS_AVAIALABLE, WHICH SHOULD HAVE BEEN TRIMMED BY D_Z?
+                         ## --IF THE BOARD'S OPEN & THERE AREN'T WINS AVAILABLE, LOOKS FOR A CORNER
+                         ##   FIRST, THEN AND ONLY THEN PICKS AT RANDOM
+                         ##   --THIS IS EXTRANEOUS, ULTIMATELY (IF NOBODY CAN WIN, GAME SHOULD)
+                         ##     END), BUT FOR NOW I WANT TO SEE GAMES PLAY THROUGH TO HELP 
+                         ##     ME FIND BUGS
+                         ## --IF A WIN IS STILL POSSIBLE, FIRST LOOKS TO BLOCK OPPONENT
+                         ##   --CREATES SUB-ARRAY OF WINS_AVAILABLE CONTAINING JUST OPP. MARKER
+                         ##     --CHECKS THROUGH THAT FOR AN OPEN CORNER SPACE & TAKES IT OR
+                         ##     --TAKES FIRST OPEN BLOCKING SPACE
+                         ## --IF SUB-ARRAY IS EMPTY, THAT MEANS IT'S THE ONLY PLAYER WITH A 
+                         ##   POSSIBLE WIN AVAILABLE
+                         ##   --CHECKS WINS_AVAILABLE FOR CORNER & TAKES ONE OR
+                         ##   --IF NO CORNERS OPEN, TAKES NEXT AVAIL SPACE FROM WINS_AVAIL 
+
+                         ##   # get_schwifty
+  # binding.pry
+    if @wins_available.empty? ##<--NOBODY CAN WIN THIS GAME
+      if corner_open?
+
+        corner(CORNERS) ##<--ULT UNNECESSARY, BUT HERE TO MAKE SURE THE REST WORKS
+      else
+        current_player.move(board).to_i - 1 
+      end
       # else
       #   player.move(board).to_i - 1
       # end
     # elsif schwifty.collect{|a| a & CORNERS}.flatten.empty? || !corner_open?
     #   schwifty.flatten.detect{|i| @board.cells[i] = " "}
+   
     else
-      get_schwifty(player)
+      get_schwifty(player) ##<--NO IMMINENT WINS, BUT WINS STILL POSSIBLE
     end
-# binding.pry
   end
 
-  def get_schwifty(player)
-    @schwifty = @wins_available.select{|a| 
-      a.detect{|i| @board.cells[i] == player.token}}.flatten.uniq
-    @unschwifty = @wins_available.select{|a| 
-        a.detect{|i| @board.cells[i] != player.token && " "}}.flatten.uniq
-    if @schwifty.empty? 
-      if corner_open?
-        corner(CORNERS)
-      elsif !@unschwifty.empty?
-  # binding.pry
-        @unschwifty.detect{|i| @board.cells[i] == " "}
-  # binding.pry
-      else
-        player.move(board).to_i - 1
+  def get_schwifty(player)  
+    @poss_danger = @wins_available.select{|a| 
+      a.none?{|i| @board.cells[i] == player.token}} ##<--W_C ARRAY W/O CURR_PLAY TOKEN
+                                                   ## --COULD BE ENTIRELY EMPTY, THOUGH
+    @opp_wins = @poss_danger.select{|a| 
+        a.select{|i| @board.cells[i] == " "}.size==2} ##<--IN CASE NEED IT LATER; COULD BE EMPTY
+   
+# binding.pry
+    if @poss_danger.empty? ##<--IF ANYBODY WINS, IT WON'T BE THE OTHER GUY
+      @good_moves = @wins_available.select{|a| a.select{|i| @board.cells[i] == " "}.size==2}
+      if @good_moves.size > 0 ##<--CHECK TO SEE IF OTHER WINS_AVAIL ARRAYS ARE OPEN OR OURS
+        if center_open ##<--CENTER IS VALUABLE
+          4
+        elsif corner_open?
+          corner(@good_moves.flatten & CORNERS)
+        else
+          @good_moves.detect{|i| @board.cells[i] = " "} ##<--NO WINNING CORNERS OPEN, JUST PICK ONE POT WINNER
+        end
+      else ##<--WINS_AVAIL INCLUDES AT LEAST ONE EMPTY ARRAY
+        if center_open? ##<--THIS PROB NEEDS TO BE ABSTRACTED OUT
+          4
+        elsif corner_open?
+          corner(@wins_available.flatten & CORNERS)
+        else
+          @wins_available.flatten.sort_by{rand}
+        end
       end
 
-  # binding.pry
-      # player.move(board).to_i - 1 ###THIS LIKELY LOSES THE GAME
-    elsif (@schwifty & CORNERS).empty?
-      @schwifty.sort_by{rand}.detect{|i| @board.cells[i] == " "}
-    else
-      corner(@schwifty & CORNERS)
+          
+
+    elsif @opp_wins.size > 0 ##<--OTHER GUY HAS WINS AVAILABLE, LET'S CHECK THAT
+        if corner_open? 
+          corner(@opp_wins.flatten & CORNERS)
+        else 
+          @opp_wins.flatten.sort_by{rand}.detect{|i| @board.cells[i]==" "}  
+        end
+    else  ##<--THERE ARE WINS OUT THERE, BUT NOT THE OTHER GUY'S, SO LET'S PICK ONE
+        if center_open?
+          4
+        elsif corner_open?
+          corner(@poss_danger.flatten & CORNERS)
+        else
+          @poss_danger.flatten.sort_by{rand}.detect{|i| @board.cells[i] == " "}
+        end
+      
+    
+  # binding.pry  
     end
   end
 
-  def possible_wins
+  def possible_wins ## CREATES COPY OF WIN_COMBINATIONS, IS RUN ONLY ONCE PER GAME, AFTER BOARD RESET
     @wins_available = WIN_COMBINATIONS.select{|a|
       a.select{|i| @board.cells[i] == " "}.size >= 2}
   end
   
   def win_move
-    # binding.pry
+
     @dz_array.detect{|i| @board.cells[i] == " "} 
   end
 
@@ -130,19 +183,20 @@ class Wargames < Game
   end
 
   def corner_open?
-    CORNERS.collect{|i| @board.cells[i] == " "}.empty? ? false : true 
-# binding.pry
+    CORNERS.select{|i| @board.cells[i] == " "}.empty? ? false : true 
   end
 
   def corner(moves)
     moves.sort_by{rand}.detect{|i| @board.cells[i] == " "}
-# binding.pry
   end
 
   def center_open?
     @board.cells[4] == " "
   end
 
+  def space(index)
+    @board.cells[index]
+  end
   
 
   def endgame(result)
@@ -151,7 +205,7 @@ class Wargames < Game
       when "won"
         [player_2, player_1].detect{|p| p.token == winner}.wins += 1
         # current_player.wins += 1
-        # binding.pry
+    
         scoreboard
         puts "Congratulations #{winner}!"
         
@@ -172,7 +226,7 @@ class Wargames < Game
     puts "WINS: "
     puts "X: #{player_1.wins}, O: #{player_2.wins} \n---------\n"
     puts "CAT: #{cat_counter}\n\n"
-    # binding.pry
+
   end
 
 end
