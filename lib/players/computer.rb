cf4da999f27ea43bc0dfaedd2eff3ecdc3405d64
Mitul Mistry <mitul.mistry1@gmail.com>
2016-01-12 22:05:10 +0000

    basic AI stubs

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,14 +1,52 @@
 class Player::Computer < Player
   def move(board)
-    random_move(board)
+    #1. Make winning move
+    location = make_winning_move(board)
+
+    #2. Block winning move
+    location = block_winning_move(board) if location.nil?
+
+    #3. Center
+    location = choose_center(board) if location.nil?
+
+    #4. Opposite corner
+    location = choose_opposite_corner(board) if location.nil?
+
+    #5. Empty corner
+    location = choose_empty_corner(board) if location.nil?
+
+    #6. Random move
+    location = make_random_move(board) if location.nil?
+
+    location.to_s
   end
-  
-  def random_move(board)
+
+  def make_winning_move(board)
+
+  end
+
+  def block_winning_move(board)
+
+  end
+
+  def choose_center(board)
+    5 if !board.taken?(5)
+  end
+
+  def choose_opposite_corner(board)
+
+  end
+
+  def choose_empty_corner(board)
+
+  end
+
+  def make_random_move(board)
     num = 0
     loop do
       num = rand(1...10) #random number 1-9
       break if !board.taken?(num)
     end
-    num.to_s
+    num
   end
 end
