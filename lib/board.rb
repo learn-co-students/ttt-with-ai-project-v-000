de9063c418e8b0310c5d60c2f80d2d9982d4b605
Mitul Mistry <mitul.mistry1@gmail.com>
2016-01-12 04:10:06 +0000

    board methods

---
+++ lib/board.rb
@@ -0,0 +1,34 @@
+class Board
+  attr_accessor :cells
+
+  def initialize
+    @cells = []
+    9.times { @cells << " " }
+  end
+
+  def reset!
+    @cells.clear
+    9.times { @cells << " " }
+  end
+
+  def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
+
+  def position(input)
+    @cells[input.to_i - 1]
+  end
+
+  def full?
+    @cells.all? { |cell| cell == "X" || cell == "O"}
+  end
+
+  def turn_count
+    @cells.select { |cell| cell == "X" || cell == "O"}.count
+  end
+
+end
