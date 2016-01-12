ae3e03a411494a40e7659d4222fcabc58d60502c
Mitul Mistry <mitul.mistry1@gmail.com>
2016-01-12 21:37:47 +0000

    all tests passing

--- lib/game.rb
+++ lib/game.rb
@@ -1,17 +1,17 @@
 class Game
   attr_accessor :player_1, :player_2, :board
   WIN_COMBINATIONS = [
-  [0,1,2], # Top row
-  [3,4,5],  # Middle row
-  [6,7,8],
+    [0,1,2], # Top row
+    [3,4,5],  # Middle row
+    [6,7,8],
 
-  [0,3,6], #verticals
-  [1,4,7],
-  [2,5,8],
+    [0,3,6], #verticals
+    [1,4,7],
+    [2,5,8],
 
-  [0,4,8], #diagonals
-  [6,4,2]
-  ]
+    [0,4,8], #diagonals
+    [6,4,2]
+    ]
 
   def initialize(player_1 = Player::Human.new("X"), player_2 = Player::Human.new("O"), board = Board.new)
     @player_1 = player_1
@@ -29,7 +29,7 @@ class Game
 
   def won?
     WIN_COMBINATIONS.detect do |combo|
-      @board.cells[combo[0]] == @board.cells[combo[1]] && @board.cells[combo[1]] == @board.cells[combo[2]] && @board.taken?(combo[0]) #@board.taken?(combo[0]) to make sure it == "X" or "O" and not empty spaces
+      @board.cells[combo[0]] == @board.cells[combo[1]] && @board.cells[combo[1]] == @board.cells[combo[2]] && @board.taken?(combo[0] + 1) #@board.taken?(combo[0] + 1) to make sure it == "X" or "O" and not empty spaces, +1 for offsetting to user input
     end
   end
 
@@ -42,13 +42,31 @@ class Game
   end
 
   def turn
-    #while !over?
+    player = current_player
+    current_move = player.move(@board)
 
-    #end
+    if !@board.valid_move?(current_move)
+      turn #recurse
+    else
+      puts "#{player.token} moved #{current_move}"
+      @board.update(current_move, player)
+      @board.display
+      puts "\n"
+    end
   end
 
   def play
+    @board.display
+    puts "\n"
+    while !over?
+      turn
+    end
 
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cats Game!"
+    end
   end
 
 end
