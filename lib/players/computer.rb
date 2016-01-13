1662df18f4f470445a8e2fadb2abdda35000ff22
ryuichi7 <brettrheenan@gmail.com>
2016-01-13 13:31:42 +0100

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,25 +1,45 @@
+require 'pry'
 class Computer < Player
 
   def move(board = nil)
-    input = [*1..9].sample.to_s
-    input    
+    if x = win_or_block(board) || center(board) || corner_move(board) || side_move(board)
+      ( x + 1 ).to_s
+    end
+  end
+
+  def center(board)
+    4 if !board.taken?(5)
+  end
+
+  def corner_move(board)
+    [0,2,6,8].shuffle.detect { |corner| !board.taken?(corner + 1) }
+  end
+
+  def side_move(board)
+    [1,3,5,7].shuffle.detect { |side| !board.taken?(side + 1) }
+  end
+
+  def opp_token
+    token == "X" ? "O" : "X"
+  end
+
+  def end_move(board, token)
+    Game::WIN_COMBINATIONS.detect do |win|
+      win.map { |move| board.cells[move] }.grep(/\s/).size == 1 &&
+      ((board.cells[win[0]] == token && board.cells[win[1]] == token) ||
+      (board.cells[win[1]] == token && board.cells[win[2]] == token) ||
+      (board.cells[win[2]] == token && board.cells[win[0]] == token))
+    end
+  end
+
+  def win_or_block(board)
+    if end_move(board, token)
+      (end_move(board, token).detect { |move| board.cells[move] != token } )
+    elsif end_move(board, opp_token)
+      (end_move(board, opp_token).detect { |move| board.cells[move] != opp_token } )
+    end
   end
 end
-
-#beginnings of implementing strategy
-
-# if board == Array.new(9, " ")
-    #   input = [1,3,7,9].sample.to_s
-    #   input
-    # elsif 
-    #   board.cells.each
-
-
-
-# def minmax(board)
-#   game = Game.new
-#   game.board = board
-#   if game.won?
 
 
 
