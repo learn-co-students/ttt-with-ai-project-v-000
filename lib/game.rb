4cc1a13d527fadf4d95aff5f1147b80af56b6a13
azheng249 <andy.zheng249@gmail.com>
2016-01-13 16:47:43 -0500

    Added AI and completed CLI

--- lib/game.rb
+++ lib/game.rb
@@ -1,20 +1,15 @@
 require_relative 'board.rb'
 require_relative 'player.rb'
 require_relative 'players/human.rb'
-require 'pry'
+require_relative 'players/computer.rb'
 
 class Game
 	attr_accessor :board, :player_1, :player_2
 
 	WIN_COMBINATIONS = [
-		[0,1,2], 
-		[3,4,5], 
-		[6,7,8], 
-		[0,3,6], 
-		[1,4,7], 
-		[2,5,8], 
-		[0,4,8], 
-		[6,4,2]
+		[0,1,2], [3,4,5], [6,7,8],	# horizontal wins
+		[0,3,6], [1,4,7], [2,5,8],	# vertical wins
+		[0,4,8], [6,4,2]			# diagonal wins
 	]
 
 	def initialize(player_1 = Player::Human.new("X"), player_2 = Player::Human.new("O"), board = Board.new)
@@ -28,24 +23,9 @@ class Game
 	end
 
 	def won?
-		# Finds indexes of X and O to find 
-	    x_indexes, o_indexes = [], []
-	    for i in 0...9
-	    	x_indexes.push(i) if board.cells[i] == "X"
-	    	o_indexes.push(i) if board.cells[i] == "O"
-	    end
-
-      	# Checks each win_combination and checks the X or O indexes to see if either has winning combination
-		# If the matches include a win_combination, that combination is returned, otherwise false is returned.
-
-		WIN_COMBINATIONS.each { |win_combination|
-		  if win_combination.all?{|i| x_indexes.include?(i)} || win_combination.all? {|i| o_indexes.include?(i)}
-		    return win_combination
-		  end
+		WIN_COMBINATIONS.detect { |combo| 
+			@board.cells[combo[0]] == @board.cells[combo[1]] && @board.cells[combo[1]] == @board.cells[combo[2]] && @board.cells[combo[0]] != " "
 		}
-
-		# Returns false if it did not find a win_combination
-		return false
 	end
 
 	def over?
@@ -63,27 +43,29 @@ class Game
 	end
 
 	def turn
-		@board.update(current_player.move(@board), current_player)
+		move = current_player.move(@board)
+
+		until @board.valid_move?(move)
+			move = current_player.move(@board)
+		end
+
+		@board.update(move, current_player)
 	end
 
 	def play
 
 		until over?
-			puts "#{current_player.token}'s turn."
+			puts "\n#{current_player.token}'s turn.\n"
 			@board.display
+
 			turn
 			break if won? || draw?
 		end
 
-		puts "Congratulations #{winner}!" if won?
-		puts "Cats Game!" if draw?
-		
+		puts "", "Congratulations #{winner}!" if won?
+		puts "", "Cats Game!" if draw?
+		@board.display
+		puts
 	end
 
 end
-
-
-
-
-
-
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,13 +1,8 @@
+require_relative '../board.rb'
+
 class Player::Human < Player
 	def move(board)
-		puts "Where do you want to move? "
+		puts "\nEnter a valid position 1-9 that is not taken: "
 		user_input = gets.strip
-
-		until user_input.to_i.between?(1,9) && board.valid_move?(user_input)
-			puts "Please enter a valid position (1-9) that isn't taken: "
-			user_input = gets.strip
-		end
-
-		user_input
 	end
 end
