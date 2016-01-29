04d61ea7d11986253c398942bb10df468f2b6c51
Adam King <adam.king0126@gmail.com>
2016-01-29 12:48:08 -0600

    completed spec 04:197

---
+++ lib/board.rb
@@ -0,0 +1,48 @@
+require 'pry'
+class Board
+  attr_accessor :cells
+
+  def initialize
+    @cells = Array.new(9, " ") 
+  end
+
+  def reset!
+    @cells = Array.new(9, " ") 
+  end
+
+  def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts " ------------ "
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts " ------------ "
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
+
+  def position(pos)
+    @cells[pos.to_i - 1]
+  end
+
+  def full?
+    @cells.include?(" ") ? false : true
+  end
+
+  def turn_count
+    9 - @cells.count(" ")
+  end
+
+  def taken?(position)
+    @cells[position.to_i - 1] == " " ? false : true
+  end
+
+  def valid_move?(position)
+    position.to_i.between?(1,9) && !(taken?(position))
+  end
+
+  def update(position, player)
+    @cells[position.to_i - 1] = player.token
+  end
+
+  def get_cells(array)
+    array.map{ |x| x = @cells[x] }
+  end
+end
---
+++ lib/game.rb
@@ -0,0 +1,55 @@
+require 'pry'
+class Game
+  WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+  attr_accessor :player_1, :player_2, :board
+
+  def initialize(player_1 = Human.new('X'), player_2 = Human.new('O'), board = Board.new)
+    @player_1 = player_1
+    @player_2 = player_2
+    @board = board
+  end
+
+  def current_player
+    @board.turn_count % 2 == 0 ? @player_1 : @player_2 
+  end
+
+  def over?
+    (@board.full? || won? || draw?) ? true : false
+  end
+
+  def won?
+    WIN_COMBINATIONS.each do |combination| 
+      if @board.get_cells(combination).uniq.length == 1
+        @winner = @board.cells[combination[0]] 
+        return true
+      end
+    end
+    false
+  end
+
+  def draw?
+    return true if @board.full? && (self.won? == false)
+    false
+  end
+
+  def winner
+    self.won?
+    @winner == " " ? nil : @winner
+  end
+
+  def turn
+    loop do
+      move = current_player.move(board)
+      if @board.valid_move?(move)
+        @board.update(move, current_player)
+        break
+      end
+      puts "Invalid move.  Please try again."
+    end
+  end
+
+  def play
+    self.turn
+  end
+
+end
