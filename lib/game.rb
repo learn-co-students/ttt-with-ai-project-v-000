378d21f73ec548c318634fd2d5a08127d4afcfaf
corya0687 <corya0687@gmail.com>
2016-01-30 09:20:17 -0500

    updated game.rb with helper methods for ai

--- lib/game.rb
+++ lib/game.rb
@@ -1,93 +1,102 @@
 require 'pry'
 class Game
-
   attr_accessor :board, :player_1, :player_2, :last_turn
   WIN_COMBINATIONS = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [6,4,2]
+   [0,1,2],
+   [3,4,5],
+   [6,7,8],
+   [0,3,6],
+   [1,4,7],
+   [2,5,8],
+   [0,4,8],
+   [6,4,2]
   ]
 
   def initialize(player_1 = Player::Human.new("X"), player_2 = Player::Human.new("O"), board = Board.new)
-    @board = board
-    @player_1 = player_1
-    @player_2 = player_2
-
+   @board = board
+   @player_1 = player_1
+   @player_2 = player_2
   end
 
-  def ai_battle
+  def ai_fight
     @player_1 = Player::Computer.new("X")
     @player_2 = Player::Computer.new("O")
   end
 
-  def man_vs_machine
-
-   # if place_holder = 1
-     # @player_2 = Player::Computer.new("O")
-   # end
-
+  def against_ai
+    if place_holder == 1
+      @player_2 = Player::Computer.new("O")
+    
+    end
   end
 
-
   def over?
-    won? || draw?
+   won? || draw?
   end
 
   def current_player
-    @board.turn_count % 2 == 0 ? @player_1 : @player_2
+   @board.turn_count % 2 == 0 ? @player_1 : @player_2
   end
 
   def winner
-    if winning_combo = won?
-      @winner = @board.cells[winning_combo.first]
-    end
+   if winning_combo = won?
+     @winner = @board.cells[winning_combo.first]
+   end
   end
 
   def turn
-    player = current_player
-    current_move = player.move(@board)
-    if !@board.valid_move?(current_move)
-      turn
-    else
-      puts "Turn: #{@board.turn_count+1}\n"
-      @board.display
-      @board.update(current_move, player)
-      puts "#{player.token} moved #{current_move}"
-      @board.display
-      @last_turn = current_move
-      puts "\n\n"
-     
-    end
+   player = current_player
+   current_move = player.move(@board)
+   if !@board.valid_move?(current_move)
+     turn
+   else
+     puts "Turn: #{@board.turn_count+1}\n"
+     @board.display
+     @board.update(current_move, player)
+     self.last_turn = current_move
+     puts "#{player.token} moved #{current_move}"
+     @board.display
+     puts "\n\n"
+   end
   end
 
   def play
-    while !over?
-      turn
-    end
-    if won?
-      puts "Congratulations #{winner}!"
-    elsif draw?
-      puts "Cats Game!"
-    end
+   while !over?
+     turn
+   end
+   if won?
+     puts "Congratulations #{winner}!"
+   elsif draw?
+     puts "Cats Game!"
+   end
   end
 
-  def won?
+  def can_win?
     WIN_COMBINATIONS.detect do |combo|
-      @board.cells[combo[0]] == @board.cells[combo[1]] &&
-      @board.cells[combo[1]] == @board.cells[combo[2]] &&
-      @board.taken?(combo[0]+1)
+      if @board.cells[combo[0]] == @board.cells[combo[1]]
+        if board.valid_move(combo[2])
+          return combo[2]
+        elsif @board.cells[combo[1]] == @board.cells[combo[2]]
+          if board.valid_move(combo[0])
+            return combo[0]
+          elsif @board.cells[combo[0]] == @board.cells[combo[2]]
+            board.valid_move(combo[1])
+            return combo[1]
+          end
+        end
+      end
     end
+  end
+  
+  def won?
+   WIN_COMBINATIONS.detect do |combo|
+     @board.cells[combo[0]] == @board.cells[combo[1]] &&
+     @board.cells[combo[1]] == @board.cells[combo[2]] &&
+     @board.taken?(combo[0]+1)
+   end
   end
 
   def draw?
-    @board.full? && !won?
+   @board.full? && !won?
   end
 end
-
-
-
