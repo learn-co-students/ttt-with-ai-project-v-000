73b6d720bec2b91f4a41bb6dcd0b4db6d6bfcf96
Joshua Azemoh <buzzme46@gmail.com>
2016-01-31 18:00:28 +0100

    Basic AI with random moves

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,6 +1,12 @@
 class Computer < Player
 
   def move(board)
-    "5"
+    puts "Com #{token} is thinking..."
+    @board = board
+    possible_moves.sample
+  end
+
+  def possible_moves
+    (1..9).select {|i| !@board.taken?(i) }
   end
 end
--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,6 +4,28 @@ def help
   puts "1: 0 Player (Com vs Com).\n2: 1 Player (Human vs Com).\n3: 2 Players (Human vs Human).\nhelp: to display this menu.\nexit: to quit.\n\n"
 end
 
+# Game between a Human and Computer
+def one_player
+  puts "Would you like to go first? (y/n)"
+  input = gets.strip
+
+  player_1, player_2 = nil
+
+  if ["y", "yes"].include?(input.downcase)
+    player_1 = Human.new("X")
+    player_2 = Computer.new("O")
+  elsif ["n", "no"].include?(input.downcase)
+    player_1 = Computer.new("X")
+    player_2 = Human.new("O")
+  else
+    puts "Invalid input, quiting..."
+    return
+  end
+
+  Game.new(player_1, player_2).play
+end
+
+# Game between two Humans
 def two_players
   player_1 = Human.new("X")
   player_2 = Human.new("O")
