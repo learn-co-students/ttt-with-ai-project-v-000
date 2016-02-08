b2456062e47c974a9c65122ba4f8baeb7cb76d61
LKWLaLa <lkwlala@gmail.com>
2016-02-07 22:45:02 -0500

    add methods to Game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,24 +1,66 @@
+require 'pry'
+
+
 class Game
 
-  attr_accessor :board, :player_1, :player_2
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
 
-  def initialize(board = Array.new(9, ""), player_1, player_2)
-    self.board = board
-    self.player_1 = player_1
-    self.player_2 = player_2
+  attr_accessor :board, :player_1, :player_2
+ 
+
+  def initialize(player_1=Human.new("X"),player_2=Human.new("O"),board =Board.new)
+    @player_1 = player_1
+    @player_2 = player_2
+    @board = board
   end
 
-  def start
+  def current_player
+    counter = self.board.turn_count
+    if counter %2 == 0
+      player_1
+    else
+      player_2
+    end
+    #binding.pry
+  end
+
+  def over?
+    self.draw? ||  self.won?
+  end
+
+      
+  def draw?
+    !(self.won?) && self.board.full?
+    #binding.pry
+  end    
+ 
+
+  def won?
+    WIN_COMBINATIONS.detect do |line|
+      line.all?{|i| self.board.cells[i] == "X" } || line.all?{|i| self.board.cells[i] == "O"}
+  end
+
+
+  def winner
+    if won?
+      line = won?
+      self.board.cells[line[0]]
+    end
   end
 
   def turn
+    puts ""
   end
 
-  def play
-  end
 
-  
-
-
-end
+end  
 
