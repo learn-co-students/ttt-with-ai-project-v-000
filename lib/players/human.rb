d6205a2b3bde55580542ab42c5f652c9f8080355
Tracy <tracetrace@gmail.com>
2016-02-11 23:52:52 +0000

    Done.

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -2,6 +2,7 @@ class Player::Human < Player
 
   def move(board)
     puts "Please choose a move, 1 - 9."
+    puts "The cell numbers go top to bottom, left to right."
     move = gets.chomp
   end
 
