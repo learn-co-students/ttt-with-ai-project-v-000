9fd59b2bfe6f91b98f49263a5c49b0a2d14f1d52
Lady Codington <myemailforcoding@gmail.com>
2016-02-12 14:00:18 -0500

    adjusted won?

--- lib/game.rb
+++ lib/game.rb
@@ -27,9 +27,26 @@ class Game
   end
 
   def won?
-    WIN_COMBINATIONS.detect do |combo|
-      board.cells[combo[0]] == board.cells[combo[1]] && board.cells[combo[0]] == board.cells[combo[2]]
+    WIN_COMBINATIONS.each do |win_combination|
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
+
+      # load the value of board at specified index
+      position_1 = board.cells[win_index_1]
+      position_2 = board.cells[win_index_2]
+      position_3 = board.cells[win_index_3]
+
+      if position_1 == 'X' && position_2 == 'X' && position_3 == 'X'
+        return win_combination
+      elsif position_1 == 'O' && position_2 == 'O' && position_3 == 'O'
+        return win_combination
+      end
     end
+    false
+    #WIN_COMBINATIONS.detect do |combo|
+    #  board.cells[combo[0]] == board.cells[combo[1]] && board.cells[combo[0]] == board.cells[combo[2]]
+    #end
   end
 
   def draw?
@@ -58,5 +75,4 @@ class Game
       turn
     end
   end
-
 end
