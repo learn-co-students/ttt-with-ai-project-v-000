10f828e8bb9f7fecdc040b08f00f05e90fe8c3cf
Lady Codington <myemailforcoding@gmail.com>
2016-02-12 14:30:51 -0500

    Computer#move still unfinished even though tests pass

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,3 +1,9 @@
-class Computer < Player
+require 'pry'
 
+class Computer < Player
+  def move(board)
+    valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    valid_moves[rand(1..9)]
+    #binding.pry
+  end
 end
