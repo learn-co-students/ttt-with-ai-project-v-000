c7b6d92ef48c025d5e545f23dc425ceae71937b4
shinyrachel <rachel.kory@gmail.com>
2016-02-18 15:03:35 -0500

    most of 01 board passing

---
+++ lib/board.rb
@@ -0,0 +1,45 @@
+class Board
+  attr_accessor :cells
+
+  def initialize
+    self.reset!
+  end
+
+  def reset!
+    self.cells = Array.new(9, " ")
+  end
+
+  def display
+    puts " #{self.cells[0]} | #{self.cells[1]} | #{self.cells[2]} "
+    puts "-----------"
+    puts " #{self.cells[3]} | #{self.cells[4]} | #{self.cells[5]} "
+    puts "-----------"
+    puts " #{self.cells[6]} | #{self.cells[7]} | #{self.cells[8]} "
+  end
+
+  def position(number)
+    self.cells[number.to_i - 1]
+  end
+
+  def full?
+    self.cells.none? { |cell| cell == " "}
+  end
+
+  def turn_count
+    self.cells.count { |cell| cell != " " }
+  end
+
+  def taken?(position_number)
+    self.position(position_number) != " "
+  end
+
+  def valid_move?(position_number)
+    position_number.to_i.between?(1,9) && !self.taken?(position_number)
+  end
+
+  def update(position_number, player)
+    self.position(position_number) = player.token
+  end
+
+
+end
