9f5cfe0ccb97143da24204f0855ff0ba69d13d8f
Matthew <mattpreiser@gmail.com>
2016-02-20 17:41:10 -0500

    Board, Game rspec passing

--- lib/game.rb
+++ lib/game.rb
@@ -1,18 +1,16 @@
 class Game
+  attr_accessor :board, :player_1, :player_2, :cells
 
-attr_accessor :board, :player_1, :player_2, :cells
-
-WIN_COMBINATIONS = [
-   [0,1,2], # top row
-   [3,4,5], # middle row
-   [6,7,8], # bottom row
-   [0,3,6], # left column
-   [1,4,7], # middle column
-   [2,5,8], # right column
-   [0,4,8], # left diag
-   [2,4,6] # right diag
-]
-	#defines all the win combos to win the game.
+  WIN_COMBINATIONS = [
+    [0, 1, 2], # top row
+    [3, 4, 5], # middle row
+    [6, 7, 8], # bottom row
+    [0, 3, 6], # left column
+    [1, 4, 7], # middle column
+    [2, 5, 8], # right column
+    [0, 4, 8], # left diag
+    [2, 4, 6] # right diag
+  ]
 
   def initialize(player_1 = Human.new('X'), player_2 = Human.new('O'), board = Board.new)
     @player_1 = player_1
@@ -20,22 +18,21 @@ WIN_COMBINATIONS = [
     @board = board
   end
 
-	def current_player
-    @board.turn_count % 2 == 0 ? @player_1 : @player_2
-	end
+  def current_player
+    @board.turn_count.even? ? @player_1 : @player_2
+    end
 
-	def over?
-		(@board.full? || won? || draw?) ? true : false
-	end
+  def over?
+    (@board.full? || won? || draw?) ? true : false
+    end
 
   def won?
     WIN_COMBINATIONS.each do |combination|
-      if @board.get_cells(combination).uniq.length == 1
-        token = @board.cells[combination[0]]
-        if (token == "X") || (token == "O")
-          @winner = token
-          return true
-        end
+      next unless @board.get_cells(combination).uniq.length == 1
+      token = @board.cells[combination[0]]
+      if (token == 'X') || (token == 'O')
+        @winner = token
+        return true
       end
     end
     false
@@ -48,22 +45,25 @@ WIN_COMBINATIONS = [
 
   def winner
     self.won?
-    @winner == " " ? nil : @winner
+    @winner == ' ' ? nil : @winner
   end
 
-	def self.turn
-		#makes valid moves, asks for input for non-valid moves and changes to player 2 after first move
-	end
+  def turn
+    loop do
+      puts "It's player #{current_player.token}'s move"
+      move = current_player.move(board)
+      if @board.valid_move?(move)
+        @board.update(move, current_player)
+        break
+      end
+      puts 'Invalid move.  Please try again.'
+    end
+  end
 
-	def self.play
-		#asks for a players input on a turn
-		#checks if the game is over after each turn
-		#checks if a game is won after each turn
-		#checks if a game is a draw after each turn
-		#stops playing when someone has won the game
-		#prints "Cat's Game" in the event of a draw
-		#congradulates the winner, either X or O
-		#And, uh, plays through a whole game of course
-
-	end
+  def play
+    turn until self.over?
+    puts "Congratulations #{@winner}!" if @winner
+    puts "Cats Game!" if draw?
+    @board.display
+    end
 end
