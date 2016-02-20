87f8430bf0b5cf0cced40737e0c047584805459c
Matthew <mattpreiser@gmail.com>
2016-02-20 17:02:40 -0500

    added WIN_COMBINATIONS

--- lib/game.rb
+++ lib/game.rb
@@ -2,7 +2,16 @@ class Game
 
 attr_accessor :board, :player_1, :player_2, :cells
 
-	WIN_COMBINATIONS = []
+WIN_COMBINATIONS = [
+   [0,1,2], # top row
+   [3,4,5], # middle row
+   [6,7,8], # bottom row
+   [0,3,6], # left column
+   [1,4,7], # middle column
+   [2,5,8], # right column
+   [0,4,8], # left diag
+   [2,4,6] # right diag
+]
 	#defines all the win combos to win the game.
 
 	def initialize
