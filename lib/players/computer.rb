215125152d8272e3d1ea5911cea114e263a212d8
thelucasmoore <lucasedwardmoore@gmail.com>
2016-02-22 12:49:59 -0500

    Increased Computer Intelligence

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,6 +1,17 @@
 class Player::Computer < Player
-  def move(_board)
-    numbergen = (1..9).to_a
-    numbergen.sample.to_s
-  end
+  def move(board)
+  	corners = ["1", "3", "7", "9"]
+    random = (1..9).to_a.sample
+
+    if board.taken?(5) == false
+    	5
+    elsif corners.collect {|corner| board.taken?(corner)}.include?(false)
+		play = corners.sample
+		play = corners.sample while board.taken?(play)
+		play
+    else
+    	random
+    end
+	end
+
 end
