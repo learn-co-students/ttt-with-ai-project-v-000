105b1f215580d05c58e554d1bdbf51fc78d41276
Smulligan85 <sean.mulligan85@gmail.com>
2016-02-22 07:52:22 -0800

    Final CPU logic

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,42 +1,6 @@
 require "pry"
 class Player::Computer < Player
-  # attr_accessor :best_move
 
-  # def move(board)
-  #   binding.pry
-  # end
-
-  # def minimax(board, game)
-  #   minimax = {move: [], score: []}
-  #
-  #   (1..9).each do |i|
-  #     if board.valid_move?(i)
-  #       if game.won?
-  #        minimax[:move] << i
-  #        minimax[:score] << 1
-  #       elsif game.draw?
-  #        minimax[:move] << i
-  #        minimax[:score] << 0
-  #       else
-  #         minimax[:move] << i
-  #        minimax[:score] << -1
-  #       end
-  #     end
-  #   end
-  #   
-  #   #returns the index of the max value inside the :score array
-  #   max_value = minimax[:score].index(minimax[:score].max)
-  #   #assigns @best_move using max_value index and :move array.
-  #   @best_move = minimax[:move][max_value]
-  # end
-
-  # def move(board)
-  #   position = rand(1..9).to_s
-  #   until !board.taken?(position)
-  #     position = rand(1..9).to_s
-  #   end
-  #   position
-  # end
 WIN_COMBINATIONS = [
   [0, 1, 2],
   [3, 4, 5],
@@ -50,47 +14,56 @@ WIN_COMBINATIONS = [
 
   def move(board)
     minimax = []
-    (1..9).each do |i|
+    num = (1..9).to_a.shuffle
+    num.each do |i|
       if board.valid_move?(i)
         if self.token == "X"
-          if won?(board)
+          if won?(board) == "X"
             minimax << {move: i, score: 1}
           elsif draw?(board)
             minimax << {move: i, score: 0}
           else
             minimax << {move: i, score: -1}
           end
+        elsif self.token == "O"
+          if won?(board) == "O"
+            minimax << {move: i, score: -1}
+          elsif draw?(board)
+            minimax << {move: i, score: 0}
+          else
+            minimax << {move: i, score: 1}
+          end
         end
       end
     end
-    minimax.max_by {|k, v| v}[:move].to_s
+    minimax.max { |a, b| a[:score] <=> b[:score] }[:move].to_s
   end
 
-def won?(board)
-    WIN_COMBINATIONS.any? do |arr|
-      win_index_1 = arr[0]
-      win_index_2 = arr[1]
-      win_index_3 = arr[2]
-      position_1 = board.cells[win_index_1]
-      position_2 = board.cells[win_index_2]
-      position_3 = board.cells[win_index_3]
-      if position_1 == "X" && position_2 == "X" && position_3 == "X" || 
-         position_1 == "O" && position_2 == "O" && position_3 == "O"
-         return arr
-      else
-        false
+  def won?(board)
+      WIN_COMBINATIONS.any? do |arr|
+        win_index_1 = arr[0]
+        win_index_2 = arr[1]
+        win_index_3 = arr[2]
+        position_1 = board.cells[win_index_1]
+        position_2 = board.cells[win_index_2]
+        position_3 = board.cells[win_index_3]
+        if position_1 == "X" && position_2 == "X" && position_3 == "X" || 
+          position_1 == "O" && position_2 == "O" && position_3 == "O"
+          self.token
+        else
+          false
+        end
       end
-    end
-end
-
-def draw?(board)
-  if !(won?(board)) && board.full?
-    true
-  elsif !won?(board) && !board.full?
-    false
-  elsif won?(board)
-    false
   end
-end
+
+  def draw?(board)
+    if !(won?(board)) && board.full?
+      true
+    elsif !won?(board) && !board.full?
+      false
+    elsif won?(board)
+      false
+    end
+  end
 
 end
