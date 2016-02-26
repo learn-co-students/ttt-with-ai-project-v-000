28f5cc0a44c762b5e3643c9c652dd1957aeb18c3
Brian Holland <brian.holland@proatria.com>
2016-02-26 16:24:55 -0600

    pair programming push

--- lib/game.rb
+++ lib/game.rb
@@ -11,19 +11,19 @@ class Game
                         [6,4,2]]
   def initialize(player_1=nil, player_2=nil, board=nil)
     @board = board || Board.new()
-    @player_1 = player_1 || Player.new("X")
-    @player_2 = player_1 || Player.new("X")
+    @player_1 = player_1 || Player::Human.new("X")
+    @player_2 = player_2 || Player::Human.new("O")
     @winner = nil
   end
 
   def current_player
-    return @board.collect{|spot| (spot=="X") || (spot=="O")}.length%2 == 0 ? "X" : "O"
+    return @board.turn_count%2 == 0 ? @player_1 : @player_2
   end
 
   def over?
-    if self.won?
+    if won?
       return true
-    elsif self.draw?
+    elsif draw?
       return true
     else
       return false
@@ -32,25 +32,38 @@ class Game
 
   def won?
     WIN_COMBINATIONS.each do |triplet|
-      if triplet.all?{|s| @board[s]=="O"}
-        @winner = "X"
-        return true
-      elsif triplet.all?{|s| @board[s]=="O"}
+      if triplet.all?{|s| @board.cells[s]=="O"}
         @winner = "O"
+        return true
+      elsif triplet.all?{|s| @board.cells[s]=="X"}
+        @winner = "X"
         return true
       end
     end
     return false
   end
 
+  def winner
+    self.won?
+    return @winner
+  end
+
   def draw?
-    if won? || @board.all?{|s| s=="X" || s=="O"}
-      return false
+    if !self.won? && @board.full?
+      return true
     end
-    return true
+    return false
   end
 
   def turn
+    puts "Please enter 1-9:"
+    #answer = gets.strip
+    if @board.valid_move?(answer)
+      @board.update(answer, @board.current_player)
+      @board.display
+    else
+      turn
+    end
   end
 
   def play
