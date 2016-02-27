2664dc0a160d94ff850f68e1e057187b1c6097bd
Brian Holland <brian.holland@proatria.com>
2016-02-27 00:45:32 -0600

    add difficulty choice for Comps

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,9 +1,23 @@
 class Computer < Player
 
-  def initialize(token, level = 1)
+  def initialize(token, level = 3)
     @token = token
     @level = level
-    puts "level: #{@level}"
+    @wc = Game.new.class::WIN_COMBINATIONS
+    #level is set to 3, 'true' leads to a user's choice
+    if @level == true
+      puts "Choose difficulty level:"
+      puts "1. Normal"
+      puts "2. Hard"
+      puts "3. Impossible"
+      input = gets.strip.to_i
+      if input.is_a?(Integer) && input.between?(1,3)
+        @level = input
+      else
+        puts "Didn't quite catch that.  Impossible it is"
+        @level = 3
+      end
+    end
   end
 
   def move(token)
