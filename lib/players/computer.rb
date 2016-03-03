4cd990ffb8dacce7b535a00820c8943a2a3bdd17
reifnotreef <bridgerrhammond@gmail.com>
2016-03-03 11:09:05 -0600

    still working on AI of my own. reading through min/max

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,50 +1,76 @@
-class Player::Computer < Player
-  attr_reader :board, :cells
+class Computer < Player
 
-  def initialize(board)
-    @board = board
-    @cells = Array.new(9, " ")
-  end
-
-  def move(board)
-    valid_moves = possible_position
-  end
-
-  def win_or_block(sym)
-    finished = false
-    0.upto(8) do |i|
-      origin = @cells[i]
-      @cells[i] = sym if origin.nil?
-      finished = i + 1 if game.turn(sym)
-      @cells[i] = origin
+  def initialize(token, level = 3)
+    @token = token
+    #@opponents_token is to simplify the 'hard' strategy
+    if @token=="X"
+      @opponents_token="O"
+    else @token=="O"
+      @opponents_token="X"
     end
-    finished
+    @level = level
+    @wc = Game.new.class::WIN_COMBINATIONS
+    #level is set to 3, 'true' leads to a user's choice
+    if @level == true
+      puts "Choose difficulty level:"
+      puts "1. Normal"
+      puts "2. Hard"
+      #puts "3. Impossible"
+      input = gets.strip.to_i
+      if input.is_a?(Integer) && input.between?(1,3)
+        @level = input
+      else
+        puts "Didn't quite catch that."
+        Game.new.exit
+      end
+    end
   end
 
-  def check_defaults
-    if @cells[4]
-      rand < 0.51 ? possible_position(&:even?) : possible_position(&:odd?)
+  def move(token, board=nil)
+    #depending on the @level, call appropriate function
+    case @level
+    when 1
+      normal
+    when 2
+      hard(board)
+    when 3
+      impossible
     else
-      5
+      puts "ERROR! NEED INPUT STEPHANIE!"
+      return
     end
   end
 
-  def possible_position(&block)
-    result = (0..8).select(&block).each do |i|
-      return i + 1 if @cells[i].nil?
-    end
-    result.is_a?(Integer) ? result : @cells.rindex(nil)
+  def normal
+    return rand(1..9).to_s
   end
 
-  def other_symbol
-    symbol == 'X' ? 'O' : 'X'
+  def hard(board)
+    #go through each win combination looking for a win
+    @wc.each do |triplet|
+      #if there are 2 "good-guy" marks and an empty space: WIN, man, WIN!
+      if triplet.count{|s| board.cells[s]==@token }==2 && triplet.none?{|s| board.cells[s]==@opponents_token }
+        #puts "we're in it to win it!"
+        #this will return a 1-element array
+        hotspot = triplet.select{|s| board.cells[s]==" "}
+        return hotspot.first + 1
+      #if there are 2 "bad-guy" marks and an empty space: BLOCK, man, BLOCK!
+      end
+    end
+    #go through each win combination looking for a block
+    @wc.each do |triplet|
+      if triplet.count{|s| board.cells[s]==@opponents_token }==2 && triplet.none?{|s| board.cells[s]==@token }
+        #puts "we're in to block!"
+        #this will return a 1-element array
+        hotspot = triplet.select{|s| board.cells[s]==" "}
+        return hotspot.first + 1
+      end
+    end
+    return normal
   end
 
-  def loading_simulation
-    str = "Computer is scheming"
-    10.times do
-      print str += '.'
-      sleep(0.1)
-    end
+  def impossible
+    return rand(1..9).to_s
   end
+
 end
