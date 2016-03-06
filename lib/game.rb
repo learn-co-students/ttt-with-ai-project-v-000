e67a60730e200630ed6aa92566fd22131a0f82db
mary fossbender <fossbender@gmail.com>
2016-03-06 10:33:29 -0600

    cleaning up game.rb

--- lib/game.rb
+++ lib/game.rb
@@ -52,14 +52,13 @@ class Game
 
   
   def winner
-  # returns X when X won
-  # returns O when O won
+    # checks to see if game was won
     if won?
       winning_array = WIN_COMBINATIONS.detect do |win_array|
         board.cells[win_array[0]] == "X" && board.cells[win_array[1]] == "X" && board.cells[win_array[2]] == "X" || 
         board.cells[win_array[0]] == "O" && board.cells[win_array[1]] == "O" && board.cells[win_array[2]] == "O"
       end
-      # returns nil when no winner
+      # returns nil if no winner or returns token of winner
       winning_array.nil? ? nil : board.cells[winning_array[0]]
     end
   end
@@ -81,12 +80,10 @@ class Game
 
   # plays through an entire game
   def play
+    # checks if the game is over after every turn
     until over?
-      board.turn_count #=> asks for players input on a turn of the game
-      # checks if the game is over after every turn
       # plays the first few turns of the game
       turn
-      current_player
     end
 
     # checks if the game is won after every turn
@@ -106,7 +103,6 @@ class Game
 
   # Greet the user with a message
   def greeting
-
     puts "\n★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★"
     puts "  ● WELCOME TO TIC TAC TOE ●"
     puts "★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★"
@@ -123,7 +119,9 @@ class Game
   # Gives the player the option of selecting a game type
   def game_type
     greeting
-    type = ""
+
+    puts "\nPlease select a Game type"
+    type = gets.strip
 
     until type.match(/[0|1|2]/)
       puts "Please select a valid Game type"
@@ -131,31 +129,30 @@ class Game
     end
 
 
-  case type
-    when "2"
-      game = Game.new(player_1 = Human.new("X"), player_2 = Human.new("O"), board = Board.new)
-    when "1"
-      # ask who plays first and verify user input
-      first_player = "h"
-      puts "Who should go first?\nEnter c for computer or h for human"
-      first_player = gets.strip.downcase
-
-      until first_player.match(/[c|h]/)
-        puts "Please select a valid player (c for computer or h for human)"
+    case type
+      when "2"
+        game = Game.new(player_1 = Human.new("X"), player_2 = Human.new("O"), board = Board.new)
+      when "1"
+        # ask who plays first and verify user input
+        puts "Who should go first?\nEnter c for computer or h for human"
         first_player = gets.strip.downcase
-      end
 
-      if first_player == "c"
-        player1 = Computer.new("O")
-        player2 = Human.new("X")
-      elsif first_player == "h"
-        player1 = Human.new("X")
-        player2 = Computer.new("O")
-      end
-      game = Game.new(player_1 = player1, player_2 = player2, board = Board.new)
-    when "0"
-      game = Game.new(player_1 = Computer.new("X"), player_2 = Computer.new("O"), board = Board.new)
-  end
+        until first_player.match(/[c|h]/)
+          puts "Please select a valid player (c for computer or h for human)"
+          first_player = gets.strip.downcase
+        end
+
+        if first_player == "c"
+          player1 = Computer.new("O")
+          player2 = Human.new("X")
+        elsif first_player == "h"
+          player1 = Human.new("X")
+          player2 = Computer.new("O")
+        end
+        game = Game.new(player_1 = player1, player_2 = player2, board = Board.new)
+      when "0"
+        game = Game.new(player_1 = Computer.new("X"), player_2 = Computer.new("O"), board = Board.new)
+    end
   game
 end
 
