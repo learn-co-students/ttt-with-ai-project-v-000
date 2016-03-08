6a85fc0b5ba469c158b560dc3d6ae30ba39676d7
jjlippman <jjlippman@earthlink.net>
2016-03-07 18:58:36 -0700

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,10 +3,10 @@ class Computer < Player
   WIN_COMBINATIONS=[[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8],[0,4,8], [2,4,6]]
   def move(board)
     
-    move= winning_move?(board)
+    move = winning_move?(board)
     if move != " "
-      move +1
-    elsif board.turn_count < 3
+      move + 1
+    elsif board.turn_count < 4
       move_arr=["1", "3", "7", "9"]
         move_arr[rand(move_arr.length)]
     else
@@ -32,12 +32,24 @@ class Computer < Player
       end
     compare[cell] ="X"
         WIN_COMBINATIONS.each do |arr|
-          if  compare[arr[0]] == "X" &&  compare[arr[1]] == "X" &&  compare[arr[2]] == "X" ||
+          if  compare[arr[0]] == "X" &&  compare[arr[1]] == "X" &&  compare[arr[2]] == "X" 
+               
+               winning_array=arr # return the win_combination indexes that won.
+               # puts "this is the winning_array #{winning_array}"
+          end
+        end
+    
+    compare[cell] ="O"
+        WIN_COMBINATIONS.each do |arr|
+          if  
                compare[arr[0]] == "O" && compare[arr[1]] == "O" &&  compare[arr[2]] == "O"
                winning_array=arr # return the win_combination indexes that won.
                # puts "this is the winning_array #{winning_array}"
           end
         end
+
+
+
     end
        
     winning_array.each  do |cell|
