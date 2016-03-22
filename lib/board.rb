ef9e0a2826a29719aad8f81edebd77f4a4ede71c
LonghornRach <LonghornRach@users.noreply.github.com>
2016-03-22 17:35:36 -0500

    Done

--- lib/board.rb
+++ lib/board.rb
@@ -1,17 +1,6 @@
 class Board
   attr_accessor :cells
 
-  WIN_COMBINATIONS = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [6,4,2]
-    ]
-
   def initialize
     self.reset!
   end
@@ -56,28 +45,10 @@ class Board
     end
   end
 
-  def has_combo
-    WIN_COMBINATIONS.detect do |combo|
-      combo.all? { |position| cells[position] == "X" || cells[position] == "O" }
-    end
-  end
-
-  def winner
-    has_combo
-  end
-
   def available_spaces
     arr = []
     cells.each_with_index { |cell, index| arr << index if cell == " "}
     arr
   end
 
-  def place_token(space, token)
-    # binding.pry
-    cells[space] = token
-  end
-  
 end
-
-# board = Board.new
-# p board.available_spaces
