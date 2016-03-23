27fd8f21d3fa19669cb2d167b1baeb19d88d63b7
wsaxe <geosaxe@gmail.com>
2016-03-22 20:06:27 -0500

    working computer player with basic valid moves

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,2 +1,13 @@
 class Player::Computer < Player
+  @next_move = 1
+  def move(board)
+    if !board.taken?(5)
+      "5"
+    elsif !board.taken?(@next_move)
+      "#{@next_move}"
+    else
+      @next_move += 1
+      move
+    end
+  end
 end
