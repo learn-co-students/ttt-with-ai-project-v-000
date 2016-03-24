c018fe7b97fb850ff4bb90fe483858c0a048af2f
wsaxe <geosaxe@gmail.com>
2016-03-23 19:49:49 -0500

    half complete CLI

---
+++ lib/helpers.rb
@@ -0,0 +1,11 @@
+def greeting
+  puts "\r\n"
+  puts "Welcome to Tic-Tac-Toe!"
+  puts "\r\n"
+  puts "What type of game would you like to play?"
+  puts "\r\n"
+  puts "Enter 1 for a 1-Player game vs. the computer."
+  puts "Enter 2 for a 2-Player game with alternating turns."
+  puts "Enter 0 for a game played between 2 computer players."
+  puts "\r\n"
+end
