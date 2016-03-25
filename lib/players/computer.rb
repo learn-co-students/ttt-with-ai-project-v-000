358d0fe9db020f45b5e2e5c8718829c47c0f3319
wsaxe <geosaxe@gmail.com>
2016-03-24 21:35:14 -0500

    completed AI

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,9 +1,48 @@
 class Player::Computer < Player
-  def move(board)
-    next_move = 1
-    while board.taken?(next_move)
-      next_move += 1
+  attr_reader :token
+  def initialize(token)
+    @priority_moves = [5,1,3,7,9,2,4,6,8]
+    @next_move = nil
+    @token = token
+    if @token == "X"
+      @enemy = "O"
+    else
+      @enemy = "X"
     end
-    "#{next_move}"
   end
+
+  def move(board)
+
+    Game::WIN_COMBINATIONS.each do |combo|
+      if board.cells[combo[0]] == @enemy && board.cells[combo[0]] == board.cells[combo[1]] && board.cells[combo[2]] == " "
+        @priority_moves.unshift(combo[2]+1)
+      elsif board.cells[combo[0]] == @enemy && board.cells[combo[0]] == board.cells[combo[2]] && board.cells[combo[1]] == " "
+        @priority_moves.unshift(combo[1]+1)
+      elsif board.cells[combo[1]] == @enemy && board.cells[combo[1]] == board.cells[combo[2]] && board.cells[combo[0]] == " "
+        @priority_moves.unshift(combo[0]+1)
+      end
+    end
+
+    Game::WIN_COMBINATIONS.each do |combo|
+      if board.cells[combo[0]] == @token && board.cells[combo[0]] == board.cells[combo[1]] && board.cells[combo[2]] == " "
+        @priority_moves.unshift(combo[2]+1)
+      elsif board.cells[combo[0]] == @token && board.cells[combo[0]] == board.cells[combo[2]] && board.cells[combo[1]] == " "
+        @priority_moves.unshift(combo[1]+1)
+      elsif board.cells[combo[1]] == @token && board.cells[combo[1]] == board.cells[combo[2]] && board.cells[combo[0]] == " "
+        @priority_moves.unshift(combo[0]+1)
+      end
+    end
+    
+    if board.turn_count == 1
+      @priority_moves.unshift(@priority_moves.sample)
+    end
+
+    @next_move = @priority_moves.detect do |item|
+      !board.taken?(item)
+    end
+
+    "#{@next_move}"
+
+  end
+
 end
