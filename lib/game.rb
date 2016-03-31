bf948964397a97e0c97466d4e0bf9717bcb2a1eb
McN33ly <rmcneely23@gmail.com>
2016-03-31 09:22:56 -0400

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -2,35 +2,58 @@ require 'pry'
 class Game
 
   attr_accessor :board, :player_1, :player_2
-  def initialize(player_1 = nil, player_2 = nil, board = nil)
-    game = self
-    game.board = board
-    #@board = Board.new
-    #@player_1 = player_1
-    game.player_1 = player_1
-    #game.player_1 = Player.new("X")
+
+  def initialize(player_1 = Player::Human.new("X"), player_2 = Player::Human.new("O"), board = Board.new)
+    @board = board
+    @player_1 = player_1
     @player_2 = player_2
-    #@player_2 = Player.new("O")
   end
 
   def current_player
-    @board.turn_count.even?
-      player_1
+    @board.turn_count.even? ? player_1 : player_2
   end
 
   def won?
     WIN_COMBINATIONS.each do |combination|
-      if @board.position_taken?(combination[0])
-        if @board[combination[0]] == @board[combination[1]] && @board[combination[1]]== @board[combination[2]]
-          return combination
+      if @board.taken?(combination[0])
+        if @board.cells[combination[0]] == @board.cells[combination[1]] && @board.cells[combination[1]]== @board.cells[combination[2]]
+           return combination
         end
       end
     end
     false
   end
 
+  def over?
+    won? || draw?
+  end
+
   def draw?
-    board.full? && !Game.self.won?
+    @board.full? && !won?
+  end
+
+  def winner
+    win_combo = won?
+     if win_combo
+       if @board.cells[win_combo[0]] == " "
+           nil
+       else @board.cells[win_combo[0]]
+       end
+     end
+  end
+
+  def turn
+    #current_player.move(input)
+    input = gets
+    until @board.valid_move?(input)
+      puts "That's not a valid move! Try Again"
+      current_player.move(input)
+      input = gets
+    end
+    #player = current_player
+    @board.update(input, current_player)
+    @board.display
+
   end
 
 WIN_COMBINATIONS= [
@@ -44,5 +67,6 @@ WIN_COMBINATIONS= [
  [2,4,6]
 ]
 
-#binding.pry
+binding.pry
 end
+#binding.pry
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,12 +1,3 @@
-class Player
-
-  attr_reader :token
-
-  def initialize(token = "X")
-    @token = token
-  end
-
-end
 
 class Human < Player
   def move(input)
