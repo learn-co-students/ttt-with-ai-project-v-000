b7097b8f3e3ea2bbfe36d6b3e0b6938fe0020e8d
phaltoe <reverse9@live.com>
2016-04-06 08:28:48 -0300

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -4,8 +4,8 @@ class Game
   WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,4,8], [6,4,2], [0,3,6], [1,4,7], [2,5,8]]
 
   def initialize(player_1 = "X", player_2 = "O", board = [])
-    @player_1 = player_1
-    @player_2 = player_2
+    @player_1 = Human.new("X")
+    @player_2 = Human.new("O")
     @board = board
   end
 
