91e6a0e66dc3ac047196cb5332234474f2165cc6
kirangp <kirangp@gmail.com>
2016-04-17 01:30:50 +1000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,3 +1,11 @@
-class Player::Computer
+require 'pry'
+require_relative '../game.rb'
+require_relative '../board.rb'
 
+class Player::Computer < Player
+
+  def move(board)
+    valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    valid_moves.sample
+  end
 end
