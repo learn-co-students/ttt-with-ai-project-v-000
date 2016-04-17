8b873950041307d52898c4c414a94a0de52142c5
johnram528 <johnram528@gmail.com>
2016-04-16 21:29:21 -0400

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,8 +1,18 @@
 class Player::Computer < Player
 
   def move(board)
-    board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"].sample
-    board
+    choice = ["1", "2", "3", "4", "5", "6", "7", "8", "9"].sample
+    if board.valid_move?("5")
+      "5"
+    elsif board.valid_move?("1")
+      "1"
+    elsif board.valid_move?("3")
+      "3"
+    elsif board.valid_move?("6")
+      "6"
+    else
+      choice
+    end
   end
 end
 
