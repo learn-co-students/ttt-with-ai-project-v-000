7f70a2aef8dca3def9dcb7871c4857f7582f9a08
srutisriram <srutisriram87@gmail.com>
2016-04-18 03:20:23 -0400

    second attempt at computer AI

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,10 +3,20 @@ class Player
     def move(board)
       if board.turn_count == 0
         "5"
-      elsif board.valid_move?("5")
-        "5"
-      elsif board.valid_move?("1")
-        "1"
+      elsif board.turn_count == 1
+        if board.valid_move?("5")
+          "5"
+        elsif board.valid_move?("1")
+          "1"
+        end
+      else
+        WIN_COMBINATIONS.each do |combination|
+          combination.each do |position|
+            if board.cells[position] == self.token
+              position.index - 1
+            end
+          end
+        end
       end
     end
   end
