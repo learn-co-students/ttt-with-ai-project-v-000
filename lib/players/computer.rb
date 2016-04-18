6ce7cf89ab8c453616040d9435676deddc1c2b9c
srutisriram <srutisriram87@gmail.com>
2016-04-18 00:00:21 -0400

    updated computer AI

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,7 +1,13 @@
 class Player
   class Computer < Player
     def move(board)
-      
+      if board.turn_count == 0
+        "5"
+      elsif board.valid_move?("5")
+        "5"
+      elsif board.valid_move?("1")
+        "1"
+      end
     end
   end
 end
