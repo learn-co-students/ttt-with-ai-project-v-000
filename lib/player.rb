5b5c6d1e571eff1797f7d3af850e97fd65ebe7bc
maireadmurphy <mairead.aw.murphy@gmail.com>
2016-04-18 12:03:46 -0400

    Done.

---
+++ lib/player.rb
@@ -0,0 +1,13 @@
+require_relative 'game.rb'
+require_relative 'board.rb'
+
+class Player
+  attr_reader :token
+  
+  def initialize(token= "X")
+    @token = token
+  end
+  
+
+
+end
---
+++ lib/players/human.rb
@@ -0,0 +1,7 @@
+class Human < Player
+  def move(board)
+    puts "Please enter a position from 1-9"
+    input = ' '
+    input = gets.chomp
+  end
+end
