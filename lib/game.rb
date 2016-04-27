2abdd4c326497286b5de46606d80cb889833bd94
danielriley06 <driley3@gmail.com>
2016-04-27 17:37:24 -0400

    Completed Game class

---
+++ lib/game.rb
@@ -0,0 +1,67 @@
+class Game
+  attr_accessor :board, :player_1, :player_2
+
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2]
+  ]
+
+  def initialize(player_1 = Player::Human.new("X"), player_2 = Player::Human.new("O"), board = Board.new)
+    @player_1 = player_1
+    @player_2 = player_2
+    @board = board
+  end
+
+  def current_player
+    @board.turn_count.even? ? player_1 : player_2
+  end
+
+  def over?
+    won? || draw?
+  end
+
+  def won?
+    WIN_COMBINATIONS.detect do |combo|
+      (@board.cells[combo[0]] == "X" || @board.cells[combo[0]] == "O") &&
+       @board.cells[combo[0]] == @board.cells[combo[1]] &&
+       @board.cells[combo[1]] == @board.cells[combo[2]]
+     end
+  end
+
+  def draw?
+    @board.full? && !won?
+  end
+
+  def winner
+    !won? ? nil : board.cells[won?.first]
+  end
+
+  def turn
+    player = current_player
+    new_move = player.move(@board)
+    if !@board.valid_move?(new_move)
+      turn
+    else
+      @board.update(new_move, player)
+      @board.display
+    end
+  end
+
+  def play
+    until over?
+      turn
+    end
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cats Game!"
+    end
+  end
+
+end
