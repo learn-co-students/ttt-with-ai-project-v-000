65fd0ba2e243c1e18112ce71245b826a2b6bfb05
Michael Randell <michael.james.randell@gmail.com>
2016-05-10 12:47:35 -0400

    checking case statement

--- lib/player.rb
+++ lib/player.rb
@@ -7,8 +7,34 @@ class Player
 end
 
 class Human < Player
-  def move(board)
+  def move(board=[])
     input = gets.strip
     input
+  end
+end
+
+class Computer < Player
+  def move(board)
+
+    #board_chk = board.turn_count
+
+    case board_chk = board.turn_count
+    when board_chk === 0
+      if board.cells[4] == " "
+        "5"
+      end
+    when board_chk === 1
+      if board.cells[4] == " "
+        "5"
+      elsif board.cells[0] === " "
+        "1"
+      elsif board.cells[2] === " "
+        "3"
+      elsif board.cells[6] === " "
+        "7"
+      elsif board.cells[8] === " "
+        "9"
+      end
+    end
   end
 end
