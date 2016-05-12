8cadf263da87a8fbdb5bc7b9b7cb5f59c525dfa8
maireadmurphy <mairead.aw.murphy@gmail.com>
2016-05-12 11:52:25 -0400

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,18 +3,20 @@ class Player::Computer < Player
   #go to the center
 WIN_COMBINATIONS = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2] ]
 
- def move(board) 
-  if board.taken?(4) == false
+ def move(board)
+   @valid_move = board.cells.each_index.select { |index| board.cells[index] == " "}
+    if @valid_move.include?(4)
     "5"
-  else
+    else
     random || complete_combo(board)
-  end
+    end
     #complete_combo(board)
 #try to get 3 in a row or block opponent from getting 3 in a row
 #just needs to check if the win combo
 #has two of the same token. if 2 of self, go there. if 2 of opponent, block it
-  
-end
+
+  end
+
 
  def has_two_tokens(board)
     WIN_COMBINATIONS.detect do |combo|
