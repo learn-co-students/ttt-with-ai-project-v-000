42104632c24e64d8845bf0f9b75830a13a9e0f54
Dakota Martinez <dakotaleemusic@gmail.com>
2016-05-20 17:54:29 +0000

    adds blocking for opposite corners strategy

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,8 +1,17 @@
 class Player::Computer < Player
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2]
+  ]  
   
   def move(board)
     # binding.pry
-    best_move = nil
     board.available_moves.each do |move|
       return move if would_win?(board, move)
       return move if would_lose?(board, move)
@@ -10,16 +19,34 @@ class Player::Computer < Player
     if board.available_moves.include?("5")
       return "5"
     end
+    if opposite_corners_taken?(board)
+      counters = ["2", "4", "6", "8"]
+      return board.available_moves.detect { |move| counters.include?(move) }
+    end
     if board.available_moves.include?("1")
       return "1"
+    end
+    if board.available_moves.include?("7")
+      return "7"
     end
     if board.available_moves.include?("3")
       return "3"
     end 
-    if board.available_moves.include?("7")
-      return "7"
+    if board.available_moves.include?("9")
+      return "9"
     end
-    best_move ||= board.available_moves.sample
+    board.available_moves.sample
+  end
+  
+  def opposite_corners_taken?(board)
+    if board.position("1") == opponent_token && board.position("1") == board.position("9") && board.position("5") == self.token
+      true 
+    elsif
+      board.position("3") == opponent_token && board.position("3") == board.position("7") && board.position("5") == self.token
+      true
+    else 
+      false
+    end
   end
   
   def opponent_token
@@ -53,16 +80,7 @@ class Player::Computer < Player
     end
   end
   
-  WIN_COMBINATIONS = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [6,4,2]
-  ]  
+  
   
   def won?(board) 
     WIN_COMBINATIONS.detect do |win_combo|
