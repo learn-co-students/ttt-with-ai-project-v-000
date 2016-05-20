c58cc8d2e45cfb312791e1f1bdc3530fa0ee4ad6
jd2rogers2 <jd2rogers2@gmail.com>
2016-05-20 12:16:01 -0400

    nearly finished board except update method, researching stubbing and double-ing

---
+++ lib/board.rb
@@ -0,0 +1,58 @@
+require "pry"
+
+class Board
+
+  attr_accessor :cells
+
+  def initialize
+    @cells = [" ", " ", " ", 
+              " ", " ", " ", 
+              " ", " ", " "]
+  end
+
+  def reset!
+    @cells = [" ", " ", " ", 
+              " ", " ", " ", 
+              " ", " ", " "]
+  end
+
+  def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
+
+  def position(number)
+    @cells[number.to_i-1]
+  end
+
+  def full?
+    @cells.include?(" ") == true ? false : true
+  end
+
+  def turn_count
+    9 - @cells.count {|cell| cell == " "}
+  end
+
+  def taken?(cell)
+    @cells[cell.to_i-1] == " " ? false : true
+  end
+
+  def valid_move?(input)
+    taken?(input) == false && (1..9).include?(input.to_i) == true ? true : false
+  end
+
+  def update(arg1, arg2)
+    #@cells[space.to_i-1] = 
+binding.pry
+  end
+end
+
+=begin
+test = Board.new
+player = double("player", :token => "X")
+
+test.update("1", player)
+=end
