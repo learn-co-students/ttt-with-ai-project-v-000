dc8b4be0b3e6a385943718a2edf1c1ccd001dd04
jd2rogers2 <jd2rogers2@gmail.com>
2016-05-22 12:32:17 -0400

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -8,7 +8,7 @@ class Game
     [1,4,7], 
     [2,5,8], 
     [0,4,8], 
-    [6,4,2]
+    [2,4,6]
   ]
 
   def initialize(player_1 = Player::Human.new("X"), player_2 = Player::Human.new("O"), board = Board.new)
@@ -23,9 +23,44 @@ class Game
 
   def over?
     won? == true || draw? == true ? true : false
-    #board.cells.count {|space| space != " "} == 9 ? true : false
   end
 
   def won?
+    answer = false
+    WIN_COMBINATIONS.detect do |array|
+      @board.cells[array[0]] != " " && 
+      @board.cells[array[0]] == @board.cells[array[1]] && 
+      @board.cells[array[0]] == @board.cells[array[2]] ? 
+      answer = true : answer = false
+    end
+    answer
   end
+
+  def draw?
+    won? == false && board.full? == true ? true : false
+  end
+
+  def winner
+    if won? == true
+      if @board.cells.count {|space| space == "X"} > @board.cells.count {|space| space == "O"}
+        "X"
+      else
+        "O"
+      end 
+    else
+      nil
+    end
+  end
+
+  def turn
+    puts "Please enter 1-9:"
+    input = gets.chomp
+    until valid_move?(input) == true
+      puts "Please enter 1-9:"
+      input = gets.chomp
+    end
+    @board.update(input, current_player)
+  end
+
+
 end
