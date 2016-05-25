e2680ff3aeb52434e9bba0e15ea2c08442d9996e
Jason <jasonkwong11@gmail.com>
2016-05-24 22:43:28 -0400

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,13 +1,9 @@
 require 'pry'
 class Computer < Player
-#  attr_reader :token
-
- # def initialize(token)
-  #  @token = token
- # end
 
   def move(board)
-   input = (1 + rand(9)).to_s
+
+    input = (1 + rand(9)).to_s
 
    if board.valid_move?(input) == false
     self.move
