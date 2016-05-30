14f32c839153ba123658df35b270c7afaca090b5
kaileeagray <kaileeagray@gmail.com>
2016-05-30 12:08:30 -0500

    Done.

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -2,8 +2,9 @@ class Player::Human < Player
 
   def move(player)
     puts "Input an integer from 1 to 9 to select the location of your next move.\n\n"
-    gets.strip
+    input = gets.strip
     puts "\n\n"
+    input
   end
 
 
