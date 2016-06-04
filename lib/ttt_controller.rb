870776f8e8b21992732deb1b06d7c6605c5f1019
interestinall <justin.lefurjah@gmail.com>
2016-06-04 12:57:55 -0500

    improved cli

--- lib/ttt_controller.rb
+++ lib/ttt_controller.rb
@@ -3,7 +3,11 @@ class TTTController
   def start
     puts "Welcome to Tic-Tac-Toe. "
     print "How many players (0-2)? "
-    num = gets.chomp.to_i
+    num = gets.chomp
+    if num.length > 1
+      goofed
+    end
+    num = num.to_i
     if num == 0
       p1 = Player::Computer.new("X")
       p2 = Player::Computer.new("O")
@@ -17,8 +21,16 @@ class TTTController
     elsif num == 2
       g =Game.new
       g.play
+    else
+     goofed
     end
+  end
 
+  def goofed
+    puts "Ya dun goofed. Let's try this again."
+    puts ""
+    puts ""
+    start
   end
 
 end
