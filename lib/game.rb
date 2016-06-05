b57d9284378e9eb9048c01577fa9f9b923f430ba
Shoko Furushita <shktkt219@gmail.com>
2016-06-05 00:49:28 -0400

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -50,9 +50,12 @@ class Game
     if !@board.valid_move?(current_move)
       turn
     else
+      puts "Turn: #{@board.turn_count+1}\n"
       @board.display
       @board.update(current_move, player)
+      puts "#{player.token} moved #{current_move}"
       @board.display
+      puts "\n\n"
     end
   end
 
@@ -68,4 +71,44 @@ class Game
     end
   end
 
+  def start
+  end
+
+  def play_again
+    puts "Do you want to play this game again?"
+    puts "If you want, please input yes. If you don't, please input no."
+    play_again = ""
+    play_again = gets.strip
+    until play_again.downcase == "no"
+      play_again.downcase == "yes"
+      puts "What kind of game do you want to play? You can select a mode 0 player, 1 player and 2 players."
+      puts "Please input 0-2."
+      input = ""
+      input = gets.strip
+
+      if input.to_i == 2
+        puts "You will start #{input.to_i} player mode game. player_1 should go first and player_1's token will be X."
+        game = Game.new
+        until won?
+          play
+        end
+        play_again
+      elsif input.to_i == 1
+        puts "You will start #{input.to_i} player mode game. You should go first and your token will be X."
+        game = Game.new(player_1 = Player::Human.new("X"), player_2 = Player::Computer.new("O"))
+        until won?
+          play
+        end
+        play_again
+      elsif input.to_i == 0
+        game = Game.new(player_1 = Player::Computer.new("X"), player_2 = Player::Computer.new("O"))
+        until won?
+          play
+        end
+        play_again
+      else
+        puts "Please input 0-2."
+      end
+    end
+  end
 end
