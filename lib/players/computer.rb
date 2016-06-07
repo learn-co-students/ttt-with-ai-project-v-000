bb5dde865eb0020f6a9da4e71dae281f6ea8011e
Yechiel Kalmenson <chilikkal@gmail.com>
2016-06-07 05:00:05 +0000

    added wargames and some strategy

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -12,6 +12,7 @@ class Player::Computer < Player
   [0, 4, 8],
   [6, 4, 2]
   ]
+  CORNERS = [0, 2, 6, 8]
   def move(board)
     @board = board
 
@@ -19,6 +20,10 @@ class Player::Computer < Player
       input = winning_move + 1
     elsif blocking_move != nil
       input = blocking_move + 1
+    elsif center?
+      input = 5
+    elsif corner != nil
+      input = corner + 1
     else
       input = (1..9).to_a.sample
       until board.taken?(input) == false
@@ -33,7 +38,7 @@ class Player::Computer < Player
   end
   def winning_move
     winning_row = WIN_COMBINATIONS.find do |combo|
-      (board.cells[combo[0]] == token && board.cells[combo[1]] == token) || (board.cells[combo[2]] == token && board.cells[combo[1]] == token) || (board.cells[combo[0]] == token && board.cells[combo[2]] == token) && combo.any? {|cell| cell == " "}
+      (board.cells[combo[0]] == token && board.cells[combo[1]] == token && board.cells[combo[2]] == " ") || (board.cells[combo[2]] == token && board.cells[combo[1]] == token && board.cells[combo[0]] == " ") || (board.cells[combo[0]] == token && board.cells[combo[2]] == token && board.cells[combo[1]] == " ")
     end
     if winning_row != nil
       winning_cell = winning_row.find {|cell| board.cells[cell] == " "}
@@ -41,10 +46,16 @@ class Player::Computer < Player
   end
   def blocking_move
     winning_row = WIN_COMBINATIONS.find do |combo|
-      (board.cells[combo[0]] == other && board.cells[combo[1]] == other) || (board.cells[combo[1]] == other && board.cells[combo[2]] == other) || (board.cells[combo[2]] == other && board.cells[combo[0]] == other) && combo.any? {|cell| cell == " "}
+      (board.cells[combo[0]] == other && board.cells[combo[1]] == other && board.cells[combo[2]] == " ") || (board.cells[combo[1]] == other && board.cells[combo[2]] == other && board.cells[combo[0]] == " ") || (board.cells[combo[2]] == other && board.cells[combo[0]] == other && board.cells[combo[1]] == " ")
     end
     if winning_row != nil
       winning_cell = winning_row.find {|cell| board.cells[cell] == " "}
     end
+  end
+  def center?
+    board.cells[4] == " "
+  end
+  def corner
+    CORNERS.find {|corner| board.cells[corner] == " "}
   end
 end
