53b527daeebaf1461e7e7e0725e3434f7e6e9e10
tpstar <hanlee12@gmail.com>
2016-06-08 07:32:04 -0400

    start ttt with ai

---
+++ lib/board.rb
@@ -0,0 +1,31 @@
+require 'pry'
+
+class Board
+  attr_accessor :cells
+
+  def initialize
+    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+
+  def reset!
+    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+
+  def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
+
+  def position(position)
+    index = position.to_i - 1
+    @cells[index]
+  end
+
+end
+
+board = Board.new
+board.cells = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
+board.position("1")
