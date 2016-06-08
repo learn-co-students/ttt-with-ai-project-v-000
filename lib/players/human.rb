2171d54182f0b3fc38c1aa16fc7f8766536cf188
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-06-08 17:32:08 -0400

    Update solution to use proper namespacing

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,8 +1,9 @@
-class Player::Human < Player
+module Players
+  class Human < Player
 
-  def move(board)
-    puts "Please enter 1-9:"
-    gets.strip
+    def move(board)
+      puts "Please enter 1-9:"
+      gets.strip
+    end
   end
-
 end
