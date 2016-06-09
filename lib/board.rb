18ab40be0feb3956ba6df1b42170fe57c7a26640
CodeByLine <yleventhal@ymail.com>
2016-06-09 18:13:32 -0400

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -35,10 +35,11 @@ class Board
 
 
     def full?
-        @cells.each do |cell|
+      cells.all? {|pos| pos != " "}
+        # @cells.each do |cell|
     # binding.pry
 
-            return false if cell == " " || cell == "nil"
+            # return false if cell == " " || cell == "nil"
             # if cell == " " || cell == "nil"
             #     return true
             # elsif cell != " " and cell != "nil"
@@ -48,29 +49,31 @@ class Board
 
 # cell == " " || cell == "nil"
             # cell.exclude?(" ") || cells.exclude?("nil")
-        end
+        # end
     end
 
 
     def turn_count
-        turns = 0
-        @cells.each do |cell|
-            if cell != " " and cell != "nil"
-                turns += 1
-            end
-
-        end
-        turns
+      cells.count {|i| i !=" "}
+        # turns = 0
+        # @cells.each do |cell|
+        #     if cell != " " and cell != "nil"
+        #         turns += 1
+        #     end
+        #
+        # end
+        # turns
     end
 
     def taken?(pos)
-        if @cells[pos.to_i-1] == "X" or @cells[pos.to_i-1] == "O"
-          return true
-        elsif @cells[pos.to_i-1] == " " or @cells[pos.to_i-1] == "" or @cells[pos.to_i-1] == nil
-          return false
+      num = pos.to_i-1
+      cells[num] != " "
+        # if @cells[pos.to_i-1] == "X" or @cells[pos.to_i-1] == "O"
+        #   return true
+        # elsif @cells[pos.to_i-1] == " " or @cells[pos.to_i-1] == "" or @cells[pos.to_i-1] == nil
+        #   return false
         # return true if @cells[pos.to_i] == "X" || @cells[pos.to_i] == "O" || @cells[pos.to_i] != nil
-
-      end
+      # end
     end
 
     def valid_move?(pos)
@@ -78,13 +81,13 @@ class Board
     end
 
     def update(pos, player)
-          @cells[pos.to_i-1] = player.token        #   = current_player
+      @cells[pos.to_i-1] = player.token if valid_move?(pos)       #   = current_player
 
     end
 
 
     def reset!()
-        @cells = Array.new(9, " ")
-        #@cells = [" "," "," "," "," "," "," "," "," "]
+        # @cells = Array.new(9, " ")
+        @cells = [" "," "," "," "," "," "," "," "," "]
     end
 end
--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,12 +1,27 @@
 class Computer < Player
 
-    attr_accessor :move
+    # attr_accessor :move
+    #
+    #
+    # def initialize
+    #
+    # end
 
-
-    def initialize
-    end
-
-    def move
+    def move(board)
+         rand(board.cells.length).to_s
+        # player = current_player
+        # move = player.move(@board)
+#         pos = rand(1..9)
+# #         @board[pos.to_i-1] = rand(1..9)
+# # (board.cells.length).to_s
+# #    if
+#
+#         board.valid_move?(pos.to_i-1)
+#         #     turn
+#         # else
+#             @board.update(board) #, player)
+#             @board.display
+        # end
         # return valid_position
     end
 
