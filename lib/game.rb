df92f2bf743f893a00d6ba855be3bb01fc234a4c
tpstar <hanlee12@gmail.com>
2016-06-11 10:37:32 -0500

    finish ttt with ai

--- lib/game.rb
+++ lib/game.rb
@@ -14,13 +14,13 @@ end
 class Game
 
   WIN_COMBINATIONS = [[0,1,2],
-                      [3,4,5],
                       [6,7,8],
                       [0,3,6],
-                      [1,4,7],
                       [2,5,8],
                       [0,4,8],
-                      [6,4,2]  ]
+                      [6,4,2],
+                      [3,4,5],
+                      [1,4,7]]
 
   attr_accessor :board, :player_1, :player_2
 
--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,24 +1,13 @@
 class Computer < Player
 
-	  # WIN_COMBINATIONS = [[0,1,2],
-    #                   [3,4,5],
-    #                   [6,7,8],
-    #                   [0,3,6],
-    #                   [1,4,7],
-    #                   [2,5,8],
-    #                   [0,4,8],
-    #                   [6,4,2]  ]
-
-    #  WIN_POSITIONS = WIN_COMBINATIONS.collect do |combo|
-    #  	combo.collect {|n| (n + 1).to_s}
-    #  end
-  def tokens_in_wincombo
+  def tokens_in_wincombo    #wincombo to tokens
     Game::WIN_COMBINATIONS.collect do |combo|
   	   combo.collect { |c| c = @board_cells[c] }
   	end
   end
 
   def wincombo2token_count
+     #count each token and empty cells from tokens_in wincombo  and return hash
   	tokens_in_wincombo.collect do |combo|
   		{nX: combo.count {|c| c == "X" },
   		 nO: combo.count {|c| c == "O" },
@@ -28,21 +17,117 @@ class Computer < Player
 
 
 
-  def winning_array_index
+  def winning_array_index4_X
 	  wincombo2token_count.index do |combo|
 		  combo[:nX] == 2 && combo[:nE] == 1
 	  end
   end
 
-  def winning_cell_index
+  def winning_array_index4_O
+    wincombo2token_count.index do |combo|
+      combo[:nO] == 2 && combo[:nE] == 1
+    end
+  end
+
+  def winning_array_index
+    if @token == "X"
+      return winning_array_index4_X
+    elsif @token == "O"
+      return winning_array_index4_O
+    end
+  end
+
+
+
+  def empty_cell4w #find an empty cell in winning array
     Game::WIN_COMBINATIONS[winning_array_index].index do |c|
       @board_cells[c] == " "
     end
   end
 
+  def empty_cell4b #find an empty cell in blocking array
+    Game::WIN_COMBINATIONS[blocking_array_index].index do |c|
+      @board_cells[c] == " "
+    end
+  end
 
   def winning_position
-    (Game::WIN_COMBINATIONS[winning_array_index][winning_cell_index] + 1).to_s
+      (Game::WIN_COMBINATIONS[winning_array_index][empty_cell4w] + 1).to_s
+  end
+
+  def opponent_token
+    if self.token == "X"
+      return "O"
+    else
+      return "X"
+    end
+  end
+
+  def blocking_array_index
+    if @token == "O"
+      return winning_array_index4_X
+    elsif @token == "X"
+      return winning_array_index4_O
+    end
+  end
+
+  def blocking_position
+    (Game::WIN_COMBINATIONS[blocking_array_index][empty_cell4b] + 1).to_s
+  end
+
+  def center_position_empty?
+    empty_cells.include?("5")
+  end
+
+
+  def potential_array_index4_X
+    wincombo2token_count.index do |combo|
+      combo[:nX] == 1 && combo[:nE] == 2
+    end
+  end
+
+  def potential_array_index4_O
+    wincombo2token_count.index do |combo|
+      combo[:nO] == 1 && combo[:nE] == 2
+    end
+  end
+
+  def potential_array_index
+    if @token == "X"
+      return potential_array_index4_X
+    elsif @token == "O"
+      return potential_array_index4_O
+    end
+  end
+
+  def empty_cell4p #find an empty cell in potential winning array and return index of that array
+    #prefer a corner cell
+#    Game::WIN_COMBINATIONS[potential_array_index].index do |c| #collect with index
+#      @board_cells[c] == " "
+    potential_array = Game::WIN_COMBINATIONS[potential_array_index]
+    if @board_cells[potential_array[0]] == " " # corner cell
+      return 0
+    elsif @board_cells[potential_array[2]] == " " #another corner cell
+      return 2
+    else                  #middle cell
+      return 1
+    end
+  end
+
+
+  def potential_winning_move
+    (Game::WIN_COMBINATIONS[potential_array_index][empty_cell4p] + 1).to_s
+  end
+
+  def corner_position_empty?
+    collect_positions(" ").include?("1") ||
+    collect_positions(" ").include?("3") ||
+    collect_positions(" ").include?("7") ||
+    collect_positions(" ").include?("9")
+  end
+
+  def corner_position
+    collect_positions(" ").select {|c| c == "1" || c == "3" || c == "7" || c == "9"}.sample
   end
 
   def move(board_cells)
@@ -50,6 +135,14 @@ class Computer < Player
     sleep 1
     if winning_array_index
       return winning_position
+    elsif blocking_array_index
+      return blocking_position
+    elsif center_position_empty?
+      return "5"
+    elsif potential_array_index
+      return potential_winning_move
+    elsif corner_position_empty?
+      return corner_position
     else
       empty_cells.sample
     end
@@ -63,8 +156,6 @@ class Computer < Player
     collect_positions(" ")
   end
 
-  # def my_cells
-  # 	collect_positions("self.token")
-  # end
+
 
 end
