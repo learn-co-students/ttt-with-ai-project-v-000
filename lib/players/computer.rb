94f87f92d1608e2fb523ec441f311b2b0525e32d
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-06-13 21:55:36 +0000

    Created basic opening move AI logic

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,6 +1,14 @@
 class Computer < Player
 
     def move(board)
-        rand(9).to_s
+        if board.turn_count == 0 || board.turn_count == 1
+            if board.valid_move?("5")
+                return "5"
+            elsif [1, 3, 7, 9].select {|num| board.valid_move?("#{num}")}.length > 0
+                [1, 3, 7, 9].select {|num| board.valid_move?("#{num}")}.sample.to_s
+            end
+        else    
+            rand(9).to_s
+        end
     end
 end
