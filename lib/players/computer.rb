bb1f22d4743ce39f0a89f9b6c941621aa7052666
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-06-14 03:11:33 +0000

    incorporated blocking AI

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,16 +2,18 @@ require 'pry'
 
 class Player::Computer < Player
     
-    WIN_COMBINATIONS = [
-    [0, 1, 2], #top row, remember commas after each sub-array
-    [3, 4, 5], #middle row
-    [6, 7, 8], #bottom row
-    [0, 4, 8], #diagonal left
-    [6, 4, 2], #diagonal right
-    [0, 3, 6], #left column
-    [1, 4, 7], #middle column
-    [2, 5, 8] #right column
-    ]    
+    def win_combos
+        @win_combos = [
+        [0, 1, 2], #top row, remember commas after each sub-array
+        [3, 4, 5], #middle row
+        [6, 7, 8], #bottom row
+        [0, 4, 8], #diagonal left
+        [6, 4, 2], #diagonal right
+        [0, 3, 6], #left column
+        [1, 4, 7], #middle column
+        [2, 5, 8] #right column
+        ]
+    end
 
     def move(board)
         if board.turn_count == 0 || board.turn_count == 1
@@ -21,11 +23,9 @@ class Player::Computer < Player
                 [1, 3, 7, 9].select {|num| board.valid_move?("#{num}")}.sample.to_s
             end
         else
-            to_win = WIN_COMBINATIONS.dup.select {|combo| combo.reject! {|space| board.cells[space] == self.token && board.cells[space] != " "}}
-            to_block = WIN_COMBINATIONS.dup.select {|combo| combo.reject! {|space| board.cells[space] != self.token && board.cells[space] != " "}}
+            to_win = self.win_combos.dup.select {|combo| combo.reject! {|space| board.cells[space] == self.token && board.cells[space] != " "}}
+            to_block = self.win_combos.dup.select {|combo| combo.reject! {|space| board.cells[space] == self.opp_token && board.cells[space] != " "}}
              
-            
-            binding.pry
             if to_win.any? {|combo| combo.length == 1}
                 to_win = to_win.find {|combo| combo.length == 1}
                 return (to_win[0] + 1).to_s
