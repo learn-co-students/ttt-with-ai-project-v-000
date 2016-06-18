4fe278f1c2fc1f399e584a24ff51b32cc59ac473
Bob Habig <rfhab2@gmail.com>
2016-06-18 17:49:08 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -54,12 +54,15 @@ class Game
 
   def turn
     #self.current_player.move(board) #intention: find current player of game instance. get the player object. call #move method on that player for input
-    if !self.board.valid_move?(self.current_player.move(board)) #intention: call #valid move on current game's board to see if above input was a valid play. IF IT ISN'T (next line)
-      until self.board.valid_move?(self.current_player.move(board))
-        self.current_player.move(board) #ask the current player for input again because the first wasn't valid
+    after = self.board
+    current = self.current_player
+
+    if !after.valid_move?(current.move(board)) #intention: call #valid move on current game's board to see if above input was a valid play. IF IT ISN'T (next line)
+      until after.valid_move?(current.move(board))
+        current.move(board) #ask the current player for input again because the first wasn't valid
       end
-    else self.board.update(self.current_player.move(board), self.current_player) #if the current player's input was a valid move, call #update method on this game's board to place that player's token in the correct space
-      self.current_player.move(board) #now that the original current player went and the board has been updated, call current player on current game again (should give the other player instance) and call #move on them just like the previous player.
+    else after.update(current.move(board), current) #if the current player's input was a valid move, call #update method on this game's board to place that player's token in the correct space
+      current.move(board) #now that the original current player went and the board has been updated, call current player on current game again (should give the other player instance) and call #move on them just like the previous player.
     end
   end
 end
