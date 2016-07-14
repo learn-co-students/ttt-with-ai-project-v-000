e737a672264b761c3cce2d1f5a79924351d4f29a
Jesse Novotny <jessenovotny@gmail.com>
2016-07-14 09:04:28 -0700

    added code to board class

---
+++ lib/board.rb
@@ -0,0 +1,58 @@
+require "pry"
+
+class Board
+  attr_accessor :cells
+
+  def reset!
+    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+
+  def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
+
+  def initialize
+    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+
+  def position position
+    position = position.to_i - 1
+    @cells[position]
+  end
+
+  def full?
+    @cells.include?(" ") ? false : true
+  end
+
+  def turn_count
+    @cells.count("X") + @cells.count("O")
+  end
+
+  def taken? position
+    position = position.to_i
+    @cells[position] == " " ? false : true
+  end
+
+  def valid_move? position
+    position = position.to_i
+    position >= 0 && position < 9 && !self.taken?(position) ? true : false
+  end
+
+  def update position, player
+    position = position.to_i
+    @cells[position] = player.token
+  end
+
+
+end
+
+
+
+
+
+
+
