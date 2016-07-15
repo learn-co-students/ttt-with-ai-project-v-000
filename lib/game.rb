0278ba7de26f9531d6b594d2c8c7af2c75c18f1b
gabe <gj+github@mail.co.de>
2016-07-15 02:05:55 -0400

    Done

---
+++ lib/game.rb
@@ -0,0 +1,58 @@
+require 'pry'
+
+class Game
+  attr_accessor :board, :player_1, :player_2
+
+  def initialize(p1 = Players::Human.new("X"), p2 = Players::Human.new("O"), board = Board.new)
+    @player_1 = p1
+    @player_2 = p2
+    @board = board
+    board.display
+  end
+
+  WIN_COMBINATIONS = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
+    [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
+  ]
+
+  def current_player
+    board.turn_count % 2 == 0 ? player_1 : player_2
+  end
+
+  def over?
+    won? || draw?
+  end
+
+  def won?
+    WIN_COMBINATIONS.any? do |cmb|
+      if board.cells[cmb[0]] != " " && board.cells[cmb[0]] == board.cells[cmb[1]] && board.cells[cmb[1]] == board.cells[cmb[2]]
+        return cmb
+      end
+    end
+  end
+
+  def draw?
+    board.full? && !won?
+  end
+
+  def winner
+    board.cells[won?.first] if won?
+  end
+
+  def turn
+    move = current_player.move(board)
+    if board.valid_move?(move)
+      board.update(move, current_player)
+      puts "\n"
+      board.display
+    else
+      turn
+    end
+  end
+
+  def play
+    turn until over?
+    puts "Congratulations #{winner}!" if won?
+    puts "Cats Game!" if draw?
+  end
+end
