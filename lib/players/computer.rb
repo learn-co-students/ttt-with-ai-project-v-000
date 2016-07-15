e4de171d06cbc6f44aa72c7421cba455aa81f2ee
gabe <gj+github@mail.co.de>
2016-07-15 18:21:41 -0400

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -4,10 +4,9 @@ module Players
   class Computer < Player
     def move(board)
       move = nil
-
-      if !board.taken?(5) # if it's the first move, take 5; or, if human doesn't take 5 first, take it
+      if !board.taken?(5)
         move = "5"
-      elsif board.turn_count == 1 # if 
+      elsif board.turn_count == 1
         move = "1"
       elsif board.turn_count == 2
         move = [1, 3, 7, 9].detect{|i| !board.taken?(i)}.to_s
@@ -17,12 +16,8 @@ module Players
         Game::WIN_COMBINATIONS.detect do |cmb|
           if cmb.select{|i| board.position(i+1) == token}.size == 2 && cmb.any?{|i| board.position(i+1) == " "}
             move = cmb.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
-            true
           elsif cmb.select{|i| board.position(i+1) != " " && board.position(i+1) != token}.size == 2 && cmb.any?{|i| board.position(i+1) == " "}
             move = cmb.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
-            true
-          else
-            false
           end
         end
         move = [1, 3, 7, 9, 2, 4, 6, 8].detect{|i| !board.taken?(i)}.to_s if move == nil
