dcada3b9d4660f3683f8858220fab00189d78fef
Lenn Hypolite <lenntriniman@hotmail.com>
2016-07-24 00:04:45 +0000

    starting to build the update method

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,6 @@
 require "pry"
 class Board
+  # has an attribute to store the cells of the board'
   attr_accessor :cells
 
 
@@ -7,16 +8,70 @@ class Board
     reset!
    end
 
-  def cells
-    @board = []
-  end
-  #
+   # can reset the state of the cells in the board
+   # sets the cells of the board to a 9 element array of " "
   def reset!
-    @cells = Array.new(9," ")
+    @cells = Array.new(9, " ")
   end
 
+  # prints the board
   def display
-    print @board
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
   end
-  
+
+  # takes in user input and returns the value of the board cell
+  def position(position)
+    index = (position.to_i)-1
+    if @cells[index] !=" " ||  @cells[index] !=""
+       @cells[index]
+    end
+  end
+
+  # check to see if board is full, true returns full
+  def full?
+     @cells.none? do | position |
+       position == " " || position == ""
+     end
+  end
+
+  # returns the amount of turns based on cell value
+  def turn_count
+     counter = 0
+     @cells.each do | occupied_positions |
+       if  occupied_positions == "X" ||  occupied_positions =="O"
+         counter+=1
+       end
+     end
+       counter
+  end
+
+  # returns true if the position is X or O
+  # returns false if the position is empty or blank
+  def taken?(position)
+    index = (position.to_i)-1
+    if @cells[index] =="X" ||  @cells[index] =="O"
+      true
+    else
+      false
+    end
+  end
+
+  def valid_move?(position)
+    @index = position.to_i
+    if @index.between?(1,9) && !taken?(@index)
+      return true
+     else
+      return false
+     end
+  end
+
+  def update(:player, :token)
+
+  end
+
+
 end
