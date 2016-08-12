98c8ff9d33858ff01a39b401c217bc02f179fb83
KerryAlsace <kerrydougherty@gmail.com>
2016-08-12 13:24:16 -0400

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,12 +1,29 @@
 module Players
+  attr_accessor :board
 
   class Computer < Player
 
-    def move(position_number)
-      possible_positions = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-      position_number = possible_positions.sample
-    end
+def move(position_number)
+  possible_positions = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  position_number = possible_positions.sample
+end
 
   end
 
 end
+
+########## Works but not AI
+# def move(position_number)
+#   possible_positions = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+#   position_number = possible_positions.sample
+# end
+
+########### Doesn't work
+# def move(board)
+#   possible_positions = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+#   if !board.position(5).taken?
+#     board = 5
+#   else
+#     board = possible_positions.sample
+#   end
+# end
