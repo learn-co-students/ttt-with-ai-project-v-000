846c51aa5699037f73abbdc38ff3a847ab7632e2
zszalas2 <zszalas@gmail.com>
2016-08-12 18:02:43 -0700

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -20,10 +20,34 @@ class Board
     puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
   end
 
-  def self.position
-    cell_input = gets.strip
-    @cells[cell_input.to_i - 1]
+  def position(location)
+    @cells[location.to_i - 1]
   end
+
+  def turn_count
+    @cells.count{|cell| !(cell.strip.empty?)}
+  end
+
+  def full?
+    if turn_count == 9
+      true
+    end
+  end
+
+  def taken?(location)
+    @cells[location.to_i - 1] != " " && @cells[location.to_i - 1] != ""
+  end
+
+  def valid_move?(location)
+    location.to_i.between?(1,9) && !taken?(location)
+  end
+
+  def update(location, player)
+    @cells[location.to_i - 1] = player.token
+  end
+
+
+
 
 
 end
