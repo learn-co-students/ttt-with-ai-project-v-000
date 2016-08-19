5770bc6ccc97953a437003403951b0756faa894c
John Fewell <fewell@gmail.com>
2016-08-19 21:10:31 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,48 +2,52 @@ require 'pry'
 module Players
  class Computer < Player
 
-   attr_accessor :my_move
-
-   WIN_COMBINATIONS = [
-   [0,1,2],
-   [3,4,5],
-   [6,7,8],
-   [0,3,6],
-   [1,4,7],
-   [2,5,8],
-   [0,4,8],
-   [6,4,2]
-   ]
    def move(board)
-#     binding.pry
      case board.turn_count
+       #for any of the first three moves, take an empty corner
      when 0..2
+       #find the free cells
        free = board.cells.size.times.select { |i| board.cells[i] == " " }
+       #find the free corners in the free cells Array
        free_corners = free.select { |e| e == 0 || e == 2 || e == 6 || e == 8 }
+       #select a random free corner for the move
        free_corners.sample.+(1).to_s
      when 3..8
+       comp_move = ""
        #go through the win combinations
-       WIN_COMBINATIONS.each do |win|
+       Game::WIN_COMBINATIONS.find do |win|
          #and find the ones that have 2 of my tokens in them and that are on the board and a blank space and return the blank space as my move
+         #find the computer players moves on the board
         my_moves = board.cells.size.times.select { |i| board.cells[i] == token }
+        #find the opponent's moves on the board
         opp_moves = board.cells.size.times.select { |i| board.cells[i] != token && board.cells[i] != " " }
-        opp_wins = win - opp_moves - my_moves
-        leftover = win - my_moves - opp_moves
-
+        #From the win combination, subtract the opponent's moves and my moves (the computer's moves)
+        leftover = win - my_moves
+        opp_wins = win - opp_moves
+        
         if leftover.size == 1
-          self.my_move = leftover.first.+(1).to_s
+          leftover = leftover - opp_moves
+        end
+        if opp_wins.size == 1
+          opp_wins = opp_wins - my_moves
+        end
+        #There is an opportunity to win if the leftover Array contains 1 remaining item, the winning move.
+        if leftover.size == 1
+          comp_move = leftover.first.+(1).to_s
+          #There is an opportunity to block if the opp_wins Array contains 1 remaining item, the blocking move.
         elsif opp_wins.size == 1
-          self.my_move = opp_wins.first.+(1).to_s
+          comp_move = opp_wins.first.+(1).to_s
         else
-          self.my_move = [1, 3, 7, 9, 2, 4, 6, 8].detect { |i| !board.taken?(i) }.to_s
+          #if there is no opportinity to block or win, then comp_move is nil for the block
+          comp_move = nil
         end
       end
-        my_move
-
-    #   when 8
-    #     last_free = board.cells.size.times.select {|i| board.cells[i] == " "}
-    #     last_free.+(1).to_s
+      if comp_move == nil
+        [1, 3, 7, 9, 2, 4, 6, 8].detect { |i| !board.taken?(i) }.to_s
+      else
+        comp_move
       end
+     end
    end
  end
 end
