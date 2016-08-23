3987c81f3957c64fd75c2ce6d8f83fcd2ca3ef83
Baruch Steinmetz <boruchsteinmetz@outlook.com>
2016-08-23 17:19:40 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,5 +1,5 @@
-module Players
-  class Computer < Player
+#module Players
+  class Players::Computer < Player
     def move(board)
       board.cells
       move_array = ["1","5","2","4","7","3","8","9","6"]
