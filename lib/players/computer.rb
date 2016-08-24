a01cd235766d4c56c53855664b716596208decf6
Baruch Steinmetz <boruchsteinmetz@outlook.com>
2016-08-24 19:48:21 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -5,18 +5,19 @@ require "pry"
       #binding.pry
       puts "\r" , "\r"
       self.token == "O" ? enemy = "X" : enemy = "O"
-      if board.turn_count == 0
-        1
-      elsif board.turn_count == 1
-        5
-      else
+      #binding.pry
+      if board.turn_count > 2
       great = Game.win_comb.detect do |win_array|
         (board.cells[win_array[0]] == enemy || board.cells[win_array[0]] == " ") && (board.cells[win_array[1]] == enemy || board.cells[win_array[1]] == " ") && (board.cells[win_array[2]] == enemy || board.cells[win_array[2]] == " ")
       end
       better = great.detect do |array|
       board.valid_move?(array)
       end
-      better
+      better.to_s
+    elsif board.turn_count == 1
+      "5"
+    elsif board.turn_count == 0
+      "1"
     end
   end
 end
