e568d25160470da53930a6cf813367ee42c6b641
Baruch Steinmetz <boruchsteinmetz@outlook.com>
2016-08-25 15:35:50 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,23 +1,26 @@
 require "pry"
-  class Players::Computer < Player
-    def move(board)
-      #board.cells
-      #binding.pry
-      puts "\r" , "\r"
-      self.token == "O" ? enemy = "X" : enemy = "O"
-      #binding.pry
-      if board.turn_count >= 2
-        great = Game.win_comb.detect do |win_array|
-          (board.cells[win_array[0]] == enemy || board.cells[win_array[0]] == " ") && (board.cells[win_array[1]] == enemy || board.cells[win_array[1]] == " ") && (board.cells[win_array[2]] == enemy || board.cells[win_array[2]] == " ")
-        end
-      better = great.detect do |array|
+class Players::Computer < Player
+  def move(board)
+    #board.cells
+    #binding.pry
+    puts "\r" , "\r"
+    self.token == "O" ? enemy = "X" : enemy = "O"
+    #binding.pry
+    if board.turn_count >= 2
+      great = Game.win_comb.detect do |win_array|
+        (board.cells[win_array[0]] == enemy || board.cells[win_array[0]] == " ") && (board.cells[win_array[1]] == enemy || board.cells[win_array[1]] == " ") && (board.cells[win_array[2]] == enemy || board.cells[win_array[2]] == " ")
+      end
+      add1_and_str = great.map do |int|
+        int +=1
+        int.to_s
+      end
+      add1_and_str.detect do |array|
       board.valid_move?(array)
       end
-      better.to_s
-      elsif board.turn_count == 1
-        "5"
-      elsif board.turn_count == 0
-        "1"
-      end
+    elsif board.turn_count == 1
+      "5"
+    elsif board.turn_count == 0
+      "1"
+    end
   end
 end
