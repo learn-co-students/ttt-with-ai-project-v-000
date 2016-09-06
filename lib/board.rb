679e0d25a4aca8dcfc54dc1b4d5cf095372ecb01
eleanorgraham <eleanorcjkgraham@gmail.com>
2016-09-06 09:39:45 -0400

    refactoring board with Kenlyn

--- lib/board.rb
+++ lib/board.rb
@@ -3,10 +3,10 @@ class Board
 
   def reset!
     self.cells = [" "," "," "," "," "," "," "," "," "]
-    end
+  end
 
   def initialize
-    self.reset!
+    reset!
   end
 
   def display
@@ -17,40 +17,27 @@ class Board
     puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
   end
 
-  #it seemed clunky trying to use this position method in #update, #taken?, and #valid_move?, so I avoided it... Try again on refactor!
   def position(input_string)
-    self.cells[input_string.to_i-1]
-    #All other methods will take input in the user's perspective of 1-9 strings AND USE `#POSITION` TO LOOK UP THE VALUE ACCORDING TO THE CELLS' ARRAY INDEX.
+    cells[input_string.to_i-1]
   end
 
   def update(input_string, player)
-    self.cells[input_string.to_i-1] = player.token
+    cells[input_string.to_i-1] = player.token
   end
 
-  #Token is set in player.rb
-  #def token(object, index)
-    #set the appropriate index equal to the token of the player object
-  #end
-
   def full?
-    self.cells.none?{|cell| cell == " "}
+    cells.none?{|cell| cell == " "}
   end
 
   def turn_count
-    counter = 0
-    self.cells.each do |position|
-      if position != " " && position != ""
-        counter += 1
-      end
-    end
-    counter
+    cells.count {|cell| cell != " "}
   end
 
-  def taken?(position)
-    !(self.cells[position.to_i-1].nil? || self.cells[position.to_i-1] == " ")
+  def taken?(input_string)
+    position(input_string) != " "
   end
 
-  def valid_move?(position)
-    !self.taken?(position) && (position.to_i).between?(1,9)
+  def valid_move?(input_string)
+    !taken?(input_string) && (input_string.to_i).between?(1,9)
   end
 end
