553f675cb75bb5a9d9056505bed52c1d896e2994
Malind Nikulski <malind.nikulski@gmail.com>
2016-09-08 15:52:33 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -6,13 +6,12 @@ module Players
         move = "5"
       elsif !board.taken?(1)
         move = [1, 3, 7, 9].detect{|index| !board.taken?(index)}
-
       else
-        Game::WIN_COMBINATIONS.detect do |combo|
-          if combo.select{|i| board.position(i+1) == token}.size == 2 && combo.any?{|i| board.position(i+1) == " "}
-            move = combo.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
-          elsif combo.select{|i| board.position(i+1) != " " && board.position(i+1) != token}.size == 2 && combo.any?{|i| board.position(i+1) == " "}
-            move = combo.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
+        Game::WIN_COMBINATIONS.detect do |com|
+          if com.select{|i| board.position(i+1) == token}.size == 2 && com.any?{|i| board.position(i+1) == " "}
+            move = com.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
+          elsif com.select{|i| board.position(i+1) != " " && board.position(i+1) != token}.size == 2 && com.any?{|i| board.position(i+1) == " "}
+            move = com.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
           end
         end
         move = [1, 3, 7, 9, 2, 4, 6, 8].detect{|i| !board.taken?(i)}.to_s if move == nil
