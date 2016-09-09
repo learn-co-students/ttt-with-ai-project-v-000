9a789564f91cfa018218294901d30597d724a59c
Samuel Lair <lair001@gmail.com>
2016-09-09 21:01:55 +0000

    refactored Cli to include #integer_input_validation_loop

--- lib/cli.rb
+++ lib/cli.rb
@@ -42,50 +42,76 @@ class Cli
   #   end
   # end
 
-  def setup_game
-    puts "Will there be 0, 1 or 2 players?"
-    num_players = gets.strip.to_i
+  def integer_input_validation_loop(validation, prompt = "")
+    puts prompt
+    input = gets.strip.to_i
     valid = false
     while !valid do
+      valid = validation.(input)
+    end
+    return input
+  end
+
+  def setup_game
+    game_type = ->(num_players) do
       if num_players == 0 || num_players == 2
-        valid = true
+        return true
       elsif num_players == 1
         puts "In a single player game, Player 1 will be human while Player 2 will be computer."
-        valid = true
+        return true
       else
         puts "Invalid selection!"
         puts "Please enter 0, 1 or 2."
         num_players = gets.chomp.to_i
+        return false
       end
     end
-
-    puts "Should Player 1 or Player 2 play X and go first?"
-    first_player = gets.strip.to_i
-    valid = false
-    while !valid do
+    num_players = self.integer_input_validation_loop(game_type, "Will there be 0, 1 or 2 players?")
+    # puts "Will there be 0, 1 or 2 players?"
+    # num_players = gets.strip.to_i
+    # valid = false
+    # while !valid do
+    #   if num_players == 0 || num_players == 2
+    #     valid = true
+    #   elsif num_players == 1
+    #     puts "In a single player game, Player 1 will be human while Player 2 will be computer."
+    #     valid = true
+    #   else
+    #     puts "Invalid selection!"
+    #     puts "Please enter 0, 1 or 2."
+    #     num_players = gets.chomp.to_i
+    #   end
+    # end
+    go_first = ->(first_player) do
       if num_players == 0 && first_player == 1
         player_1 = Players::Computer.new("X")
         player_2 = Players::Computer.new("O")
+        self.game = Game.new(player_1, player_2)
         valid = true
       elsif num_players == 0 && first_player == 2
         player_2 = Players::Computer.new("X")
         player_1 = Players::Computer.new("O")
+        self.game = Game.new(player_1, player_2)
         valid = true
       elsif num_players == 1 && first_player == 1
         player_1 = Players::Human.new("X")
         player_2 = Players::Computer.new("O")
+        self.game = Game.new(player_1, player_2)
         valid = true
       elsif num_players == 1 && first_player == 2
         player_1 = Players::Human.new("O")
         player_2 = Players::Computer.new("X")
+        self.game = Game.new(player_1, player_2)
         valid = true
       elsif num_players == 2 && first_player == 1
         player_1 = Players::Human.new("X")
         player_2 = Players::Human.new("O")
+        self.game = Game.new(player_1, player_2)
         valid = true
       elsif num_players == 2 && first_player == 2
         player_1 = Players::Human.new("O")
         player_2 = Players::Human.new("X")
+        self.game = Game.new(player_1, player_2)
         valid = true
       else
         puts "Invalid selection!"
@@ -93,7 +119,43 @@ class Cli
         first_player = gets.chomp.to_i
       end
     end
-    self.game = Game.new(player_1, player_2)
+    first_player = self.integer_input_validation_loop(go_first, "Should Player 1 or Player 2 play X and go first?")
+
+    # puts "Should Player 1 or Player 2 play X and go first?"
+    # first_player = gets.strip.to_i
+    # valid = false
+    # while !valid do
+    #   if num_players == 0 && first_player == 1
+    #     player_1 = Players::Computer.new("X")
+    #     player_2 = Players::Computer.new("O")
+    #     valid = true
+    #   elsif num_players == 0 && first_player == 2
+    #     player_2 = Players::Computer.new("X")
+    #     player_1 = Players::Computer.new("O")
+    #     valid = true
+    #   elsif num_players == 1 && first_player == 1
+    #     player_1 = Players::Human.new("X")
+    #     player_2 = Players::Computer.new("O")
+    #     valid = true
+    #   elsif num_players == 1 && first_player == 2
+    #     player_1 = Players::Human.new("O")
+    #     player_2 = Players::Computer.new("X")
+    #     valid = true
+    #   elsif num_players == 2 && first_player == 1
+    #     player_1 = Players::Human.new("X")
+    #     player_2 = Players::Human.new("O")
+    #     valid = true
+    #   elsif num_players == 2 && first_player == 2
+    #     player_1 = Players::Human.new("O")
+    #     player_2 = Players::Human.new("X")
+    #     valid = true
+    #   else
+    #     puts "Invalid selection!"
+    #     puts "Please enter 1 or 2."
+    #     first_player = gets.chomp.to_i
+    #   end
+    # end
+    # self.game = Game.new(player_1, player_2)
     # self.game.cli = self
   end
 end
