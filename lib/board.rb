678dd2bfe5803bb160be58c924b06552de332e28
Mike Robey <robey.mc@gmail.com>
2016-09-12 14:25:59 +0000

    trying to fix last failure in board.rb

--- lib/board.rb
+++ lib/board.rb
@@ -1,57 +1,53 @@
 class Board
 attr_accessor :cells
 
-def initialize
-  reset!
-end
-
-def reset!
-  @cells = Array.new(9, " ")
-end
-
-def display
-  puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
-  puts "-----------"
-  puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
-  puts "-----------"
-  puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
-end
-
-def position(input)
-  @cells[input.to_i - 1]
-end
-
-def update(input, object)
-  object = @cells[input.to_i - 1]
-end
-
-def token
-end
-
-
-def full?
-  @cells.all? do |spaces|
-  if  spaces == "X" || spaces == "O"
-  true
-
-  else
-  false
-
+  def initialize
+    reset!
   end
-end
-end
+
+  def reset!
+    @cells = Array.new(9, " ")
+  end
+
+  def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
+
+  def position(input)
+    @cells[input.to_i - 1]
+  end
+
+  def update(input, player)
+    player.token = @cells[input.to_i - 1]
+  end
+
+  def full?
+    @cells.all? do |spaces|
+    if  spaces == "X" || spaces == "O"
+    true
+
+    else
+    false
+
+    end
+  end
+  end
 
 
-def taken?(input)
-  !(@cells[input.to_i - 1].nil? || @cells[input.to_i - 1] == " ")
-end
+  def taken?(input)
+    !(@cells[input.to_i - 1].nil? || @cells[input.to_i - 1] == " ")
+  end
 
-def valid_move?(input)
-  (input.to_i - 1).between?(0,8) && !taken?(input.to_i)
-end
+  def valid_move?(input)
+    (input.to_i - 1).between?(0,8) && !taken?(input.to_i)
+  end
 
-def turn_count
-   @cells.count{|token| token == "X" || token == "O"}
-end
+  def turn_count
+     @cells.count{|token| token == "X" || token == "O"}
+  end
 
 end
