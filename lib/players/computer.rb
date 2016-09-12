445bfdfd814aa982db75c4f028ae2a7a2b1a883e
eleanorgraham <eleanorcjkgraham@gmail.com>
2016-09-12 12:10:06 -0400

    #win, #block, #center, and #opposite_corner methods all working

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -15,139 +15,68 @@ module Players
       [2,4,6]
     ]
 
-    DIAGONALS = [
+    DIAGONAL_WINS = [
       [0,4,8],
       [2,4,6]
     ]
-    SIDES = [
+    SIDE_WINS = [
       [0,1,2],
-      [6,7,8],
       [0,3,6],
+      [6,7,8],
       [2,5,8]
     ]
-
-    VERTICALS = [
+    CENTER_WINS = [
       [3,4,5],
       [1,4,7]
     ]
 
-    MOVES = ["5", "1", "3", "7", "9", "2", "4", "6", "8"]
+    OPPOSITES = [[0,8],[2,6]]
+    CORNERS = ["1","3","7","9"]
+    SIDES = ["2","4","6","8"]
+    MOVES = ["1", "5", "3", "7", "9", "2", "4", "6", "8"]
 
     def move(board)
-      if win(board) == true
+      if win(board) !=nil
         win(board)
-      elsif block(board) == true
+      elsif block(board) !=nil
         block(board)
-      elsif diagonal_fork_move(board) == true
-        diagonal_fork_move(board)
-      elsif sides_fork_move(board) == true
-        sides_fork_move(board)
-      elsif vertical_block_fork(board) == true
-        vertical_block_fork(board)
-      elsif diagonal_block_fork(board) == true
-        diagonal_block_fork(board)
-      elsif sides_block_fork(board) == true
-        sides_block_fork(board)
-      elsif opposite_corner(board) == true
+      elsif center(board) !=nil
+        center(board)
+      elsif opposite_corner(board) !=nil
         opposite_corner(board)
       else
         moves(board)
       end
     end
 
-
     def win(board)
-      WIN_COMBINATIONS.each do |wc|
+      win = nil
+      WIN_COMBINATIONS.find do |wc|
          if board.cells[wc[0]] == self.token && board.cells[wc[1]] == self.token && board.cells[wc[2]] == " "
-           "#{wc[2]+1}"
+           win = "#{wc[2]+1}"
          elsif board.cells[wc[1]] == self.token && board.cells[wc[2]] == self.token && board.cells[wc[0]] == " "
-           "#{wc[0]+1}"
+           win = "#{wc[0]+1}"
          elsif board.cells[wc[0]] == self.token && board.cells[wc[2]] == self.token && board.cells[wc[1]] == " "
-           "#{wc[1]+1}"
+           win = "#{wc[1]+1}"
          end
        end
+       win
     end
+
 
     def block(board)
       self.token == "X" ? opponent_token = "O" : opponent_token = "X"
-      WIN_COMBINATIONS.each do |wc|
+      block = nil
+      WIN_COMBINATIONS.find do |wc|
          if board.cells[wc[0]] == opponent_token && board.cells[wc[1]] == opponent_token && board.cells[wc[2]] == " "
-           "#{wc[2]+1}"
+           block = "#{wc[2]+1}"
          elsif board.cells[wc[1]] == opponent_token && board.cells[wc[2]] == opponent_token && board.cells[wc[0]] == " "
-           "#{wc[0]+1}"
+           block = "#{wc[0]+1}"
          elsif board.cells[wc[0]] == opponent_token && board.cells[wc[2]] == opponent_token && board.cells[wc[1]] == " "
-           "#{wc[1]+1}"
+           block = "#{wc[1]+1}"
          end
        end
-    end
-
-    def diagonal_fork_move(board)
-      SIDES.each do |side|
-        if board.cells[side[0]] == self.token && board.cells[side[1]] == " " && board.cells[side[2]] == self.token
-          "8"
-        else
-          "6"
-        end
-      end
-    end
-
-    def sides_fork_move(board)
-      SIDES.each do |side|
-        if board.cells[side[0]] == self.token && board.cells[side[1]] == " " && board.cells[side[2]] == self.token
-          if board.cells[0] == " " && board.cells[1] == " " && board.cells[2] == self.token
-            "1"
-          elsif board.cells[6] == " " && board.cells[7] == " " && board.cells[8] == self.token
-            "7"
-          elsif board.cells[0] == " " && board.cells[3] == " " && board.cells[6] == self.token
-            "1"
-          else board.cells[2] == " " && board.cells[5] == " " && board.cells[8] == self.token
-            "3"
-          end
-        end
-      end
-    end
-
-    def vertical_block_fork(board)
-      self.token == "X" ? opponent_token = "O" : opponent_token = "X"
-      DIAGONALS.each do |diag|
-        if board.cells[diag[0]] == opponent_token && board.cells[diag[1]] == self.token && board.cells[diag[2]] == opponent_token
-          if board.cells[3] == " " && board.cells[4] == self.token && board.cells[5] == " "
-            "6"
-          elsif board.cells[1] == " " && board.cells[4] == self.token && board.cells[7] == " "
-            "8"
-          end
-        end
-      end
-    end
-
-    def diagonal_block_fork(board)
-      self.token == "X" ? opponent_token = "O" : opponent_token = "X"
-      SIDES.each do |side|
-        if board.cells[side[0]] == opponent_token && board.cells[side[1]] == " " && board.cells[side[2]] == opponent_token
-          if board.cells[0] == opponent_token && board.cells[4] == " " && board.cells[8] == " "
-            "9"
-          else board.cells[2] == opponent_token && board.cells[4] == " " && board.cells[6] == " "
-            "7"
-          end
-        end
-      end
-    end
-
-    def sides_block_fork(board)
-      self.token == "X" ? opponent_token = "O" : opponent_token = "X"
-      SIDES.each do |side|
-        if board.cells[side[0]] == opponent_token && board.cells[side[1]] == " " && board.cells[side[2]] == opponent_token
-          if board.cells[0] == opponent_token && board.cells[1] == " " && board.cells[2] == " "
-            "3"
-          elsif board.cells[6] == opponent_token && board.cells[7] == " " && board.cells[8] == " "
-            "9"
-          elsif board.cells[0] == opponent_token && board.cells[3] == " " && board.cells[6] == " "
-            "7"
-          else board.cells[2] == opponent_token && board.cells[5] == " " && board.cells[8] == " "
-            "9"
-          end
-        end
-      end
+       block
     end
 
     def center(board)
@@ -157,16 +86,15 @@ module Players
     end
 
     def opposite_corner(board)
-      self.token == "X" ? opponent_token = "O" : opponent_token = "X"
-      if board.cells[0] == opponent_token && board.cells[8] == " "
-        "9"
-      elsif board.cells[2] == opponent_token && board.cells[6] == " "
-        "7"
-      elsif board.cells[6] == opponent_token && board.cells[2] == " "
-        "3"
-      elsif board.cells[8] == opponent_token && board.cells[0] == " "
-        "1"
+      opposite = nil
+      OPPOSITES.find do |opp|
+        if board.taken?("#{opp[0]+1}") && board.cells[opp[1]] == " "
+          opposite = "#{opp[1]+1}"
+        elsif board.taken?("#{opp[1]+1}") && board.cells[opp[0]] == " "
+          opposite = "#{opp[0]+1}"
+        end
       end
+      opposite
     end
 
     def moves(board)
