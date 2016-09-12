cdf812c449d010c87077c49797b7ec3b821c0851
Clare <clare.e.horton@gmail.com>
2016-09-12 03:11:16 +0000

    fix bug in Players::Human

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,13 +1,19 @@
 module Players
     class Human < Player
         def move(board)
-            input = ""
-            while !board.valid_move?(input)
-                puts "Make your move by entering 1-9"
-                input = gets.strip
+            puts "Make your move by entering 1-9"
+            input = gets.strip
+            
+            if board.class != Board
+                return input
+            else
+                while board.class == Board && !board.valid_move?(input)
+                    puts "Make your move by entering 1-9"
+                    input = gets.strip
+                end
+                board.update(input, self)
+                input
             end
-            board.update(input, self)
-            input
         end
     end
 end
