49e2f16e0917113a053ce72f79c362fdd1385c3b
Clare <clare.e.horton@gmail.com>
2016-09-12 07:05:23 +0000

    start AI

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,8 +1,22 @@
 module Players
     class Computer < Player
+        
+        attr_accessor:moves
+        
+        def add_move(input)
+            @moves << input
+            input
+        end
+        
         def move(board)
-            input = 1 + rand(9)
-            input.to_s
+            turn_count = board.turn_count
+            case
+            when turn_count == 0
+                @moves = []
+                add_move("7")
+            when turn_count != 0 
+                add_move("3")
+            end
         end
     end
 end
