db876e21c5b18ea8a12a5466ec6dc3685637852c
eleanorgraham <eleanorcjkgraham@gmail.com>
2016-09-14 14:47:23 -0400

    clean up cli class by removing initialize and :board

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,63 +1,65 @@
 class Cli
-  attr_accessor :game
 
-  def initialize
+  def self.welcome
     puts "Hello, Welcome to Tic Tac Toe!"
     sleep(1)
-    game = self.start
-    game.play
-    sleep(1)
-    play_again?
+    self.start
   end
 
-  def start
+  def self.start
     puts "What kind of game would you like to play: 0, 1, or 2 players?"
     input = gets.strip
     if input == "0"
-      self.game = Game.new(Players::Computer.new("X"), Players::Computer.new("O"), Board.new)
+      game = Game.new(Players::Computer.new("X"), Players::Computer.new("O"), Board.new)
+      self.play_game(game)
     elsif input == "1"
       puts "Who should go first and be 'X'? (Human/Computer)"
       input = gets.strip.upcase
       case input
       when "HUMAN"
-          self.game = Game.new(Players::Human.new("X"), Players::Computer.new("O"), Board.new)
-          game
+          game = Game.new(Players::Human.new("X"), Players::Computer.new("O"), Board.new)
+          self.play_game(game)
       when "COMPUTER"
-          self.game = Game.new(Players::Computer.new("X"), Players::Human.new("O"), Board.new)
-          game
+          game = Game.new(Players::Computer.new("X"), Players::Human.new("O"), Board.new)
+          self.play_game(game)
       else
           puts "I didn't understand that."
           puts "Who should go first and be 'X'? (Human/Computer)"
           input = gets.strip.upcase
       end
     elsif input == "2"
-      self.game = Game.new
+      game = Game.new
+      self.play_game(game)
     elsif input == "wargames"
       puts "Executing wargames"
       sleep(0.5)
       Game.wargames
       puts "Game 100"
       sleep(0.1)
-      self.game = Game.new(Players::Computer.new("X"), Players::Computer.new("O"), Board.new)
+      game = Game.new(Players::Computer.new("X"), Players::Computer.new("O"), Board.new)
+      self.play_game(game)
     else
       puts "I didn't understand that."
-      start
+      self.start
     end
   end
 
-  def play_again?
-    self.game = nil
+  def self.play_game(game)
+    game.play
+    sleep(1)
+    self.play_again?
+  end
+
+  def self.play_again?
     puts "Would you like to play again? (Y/N)"
     input = gets.strip.upcase
     if input == "Y"
-      self.game = start
-      self.game.play
-      play_again?
+      self.start
     elsif input == "N"
       puts "Thanks for playing!"
     else
       puts "I didn't understand that."
-      play_again?
+      self.play_again?
     end
   end
 end
