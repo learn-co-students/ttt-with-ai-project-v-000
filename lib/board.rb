523c1894f9bac282c8fbeab3adc0f64046eb1083
Will Lynch <will.lynch91@gmail.com>
2016-09-19 17:48:06 -0400

    basic tests passing

---
+++ lib/board.rb
@@ -0,0 +1,60 @@
+class Board
+	attr_accessor :cells
+
+	def initialize
+		@cells = Array.new(9, " ")
+	end
+
+	def reset!
+		self.cells.clear
+		self.cells = Array.new(9, " ")
+	end
+
+	def display
+		puts " #{self.cells[0]} | #{self.cells[1]} | #{self.cells[2]} "
+		puts "-----------"
+		puts " #{self.cells[3]} | #{self.cells[4]} | #{self.cells[5]} "
+		puts "-----------"
+		puts " #{self.cells[6]} | #{self.cells[7]} | #{self.cells[8]} "
+	end
+
+	def position(user_input)
+		self.cells[user_input.to_i-1] #returns player from user input
+	end
+
+	def full?
+		self.cells.each do |cell| 
+			if cell == " " 
+				return false
+			end
+		end
+		true
+	end
+
+	def turn_count
+		self.cells.select { |cell| cell != " " }.count 
+	end
+
+	def taken?(user_input)
+		if position(user_input) == "X" || position(user_input) == "O"
+			true
+		else
+			false
+		end
+	end
+
+	def valid_move?(user_input)
+		if user_input.to_i > 9 || user_input.to_i <1
+			false
+		else
+			taken?(user_input) ? false : true
+		end
+	end
+
+	def update(user_input, player)
+		self.cells[user_input.to_i-1] = player.token
+		self.display
+	end
+
+
+end
---
+++ lib/game.rb
@@ -0,0 +1,83 @@
+require 'pry'
+
+class Game
+	attr_accessor :board, :player_1, :player_2
+
+	# Define your WIN_COMBINATIONS constant
+	WIN_COMBINATIONS = [
+	  [0,1,2], #top
+	  [3,4,5], #Middle
+	  [6,7,8], #last
+	  [0,3,6], #left
+	  [1,4,7], #center
+	  [2,5,8], #right
+	  [0,4,8], #backslash
+	  [6,4,2] #forward slash
+	]
+
+	def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+		@player_1 = player_1
+		@player_2 = player_2
+		@board = board
+	end
+
+	def current_player
+		self.board.turn_count.even? ? self.player_1 : self.player_2
+	end
+
+	def over?
+		if draw? || won?
+			true
+		else
+			false
+		end
+	end
+
+	def draw?
+		self.board.full? && !self.won?
+
+	end
+
+	def won?
+		WIN_COMBINATIONS.each do |combination|
+			a = combination.collect { |index| self.board.cells[index] }
+			if a.all?{ |a| a=="X"} 
+				return "X"
+			elsif a.all?{ |a| a=="O"}
+				return "O"
+			end
+		end
+		nil
+	end
+
+	def winner
+		self.won?
+	end
+
+	def turn
+		input = self.current_player.move([])
+		if self.board.valid_move?(input) 
+			self.board.update(input, self.current_player)
+		else
+		 self.turn
+		end
+	end
+
+	def play
+		while !self.over?
+			self.turn
+		end
+
+		self.won? ? (puts "Congratulations #{won?}!") : (puts "Cats Game!")
+	end
+
+	def start
+		input = ""
+		until input == "no" do
+			self.play
+			self.board.reset!
+			puts "Play again?"
+			input = gets.chomp
+		end
+	end
+end
