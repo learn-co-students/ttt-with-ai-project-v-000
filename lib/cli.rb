ff9b1989f11f474292884946096c8efb518f29f0
Clare <clare.e.horton@gmail.com>
2016-09-19 03:37:01 +0000

    refactor CLI class

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,47 +1,67 @@
 class CLI
-    def call
+    attr_accessor:game_mode, :token
+    
+    def greeting
         puts "Welcome to Tic Tac Toe!"
         puts "Enter 1 to play against another human."
         puts "Enter 2 to play against the computer."
         puts "Enter 3 or wargames to pit two computer players against each other."
-
-        game_mode = gets.strip
-        token = ""
-
-        while !game_mode.to_i.between?(1, 3) && game_mode != "wargames"
+    end
+    
+    def get_mode
+        @game_mode = gets.strip
+    end
+    
+    def get_token
+        @token = gets.strip.upcase
+    end
+    
+    def validate_mode
+        while !@game_mode.to_i.between?(1, 3) && @game_mode != "wargames"
             puts "I don't recognize that. Please enter 1, 2, 3, or wargames."
-            game_mode = gets.strip
+            get_mode
         end
+    end
+    
+    def human_vs_human
+        puts "X goes first! Are you ready?"
+        game = Game.new
+        game.play
+    end
+    
+    def call
+        greeting
+        get_mode
+        validate_mode
 
         case
-            when game_mode == "1" #human vs human
-                puts "X goes first! Are you ready?"
-                game = Game.new
-                game.play
+            when @game_mode == "1" #human vs human
+                human_vs_human
                 
-            when game_mode == "2" #human vs AI
+            when @game_mode == "2" #human vs AI
                 puts "X or O?"
-                token = gets.strip.upcase
+                get_token
                 
-                while token != "X" && token != "O"
-                    puts "I don't recognize that. Please enter X or O."
-                    token = gets.strip.upcase
+                while @token != "X" && @token != "O"
+                    puts "I don't recognize that. X or O?"
+                    get_token
                 end
                 
                 game = nil
-                if token == "X" 
+                if @token == "X" 
                     game = Game.new(Players::Human.new("X"), Players::Computer.new("O"), Board.new)
                 else
                     game = Game.new(Players::Computer.new("X"), Players::Human.new("O"), Board.new)
                 end
                 game.play
                 
-            when game_mode == "3" #AI vs AI
+            when @game_mode == "3" #AI vs AI
                 puts "Get ready to watch the battle unfold!"
+                sleep 2
                 game = Game.new(Players::Computer.new("X"), Players::Computer.new("O"), Board.new)
                 game.play
                 
-            when game_mode = "wargames"
+            when @game_mode == "wargames"
                 game_count = 0
                 won_count = 0
                 while game_count < 100
