9ad6be6faf7b44e024644942a05a835c2f61068e
David Kennell <davidkennell0@gmail.com>
2016-09-19 20:08:40 +0000

    Finished first turn AI, started second

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -8,23 +8,52 @@ module Players
         move = opening_position
         puts "The computer has chosen an opening move of #{move}."
       elsif board.turn_count == 1
+      
         # First move for second player.
         first_move = 1 + board.cells.find_index {|cell| cell != " "}
         puts "The previous first move was #{first_move}."
 
-        # if middle start in corner.
+        # if player_1 start in corner.
         if corner.include?(first_move)
           move = center.first
           puts "The second player has chosen an opening move of #{move}."
+        # if player_1 start in center
         elsif center.include?(first_move)
-          move = corner.sample(1).first
+          move = corner.sample
           puts "The second player picked a corner."
-        else
-          move = random_move(board)
-          puts "The second player played a random opening move of #{move}."
+        # if player_1 start on edge
+        elsif edge.include?(first_move)
+          case first_move
+            when 2
+              move = [1, 3].sample
+            when 6
+              move = [3, 9].sample
+            when 8
+              move = [7, 9].sample
+            when 4
+              move = [1, 7].sample
+            end
+          puts "The second player picked the corner in position #{move}."
         end
-        # if corner start in center.
-        # if edge pick corker next to opponent.
+        
+        elsif board.turn_count == 3
+        #First_player_second_move
+        
+        #First_player_is_in_center
+        #Second_player_is_on_edge
+        opponent_token = opponent_player_token
+        case 2nd_player_position
+            when 2
+              move = [1, 3].sample
+            when 6
+              move = [3, 9].sample
+            when 8
+              move = [7, 9].sample
+            when 4
+              move = [1, 7].sample
+            end
+        
+        
 
       else
         # if board.turn_count.even? then first player.
@@ -38,6 +67,14 @@ module Players
     end
 
     private
+    def opponent_player_token
+      if @token == "X"
+        "O"
+      else
+        @token
+      end
+    end
+    
     def opening_position
       (corner << center).sample(1).first
     end
