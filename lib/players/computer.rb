40d8a681da2593a642904acead59d81e109c0fd7
Scott Savage <snsavage@gmail.com>
2016-09-20 16:02:15 -0400

    Fix Computer indentation and return value
    Fixed Computer indentation using vim's auto format command.  Also
    changed Computer#move's return value from int to string in order to keep
    passing tests.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -7,13 +7,13 @@ module Players
         # First move for first player.
         move = opening_position
         puts "The computer has chosen an opening move of #{move}."
-        
+
       elsif board.turn_count == 1
         move = first_move_second_player(board)
-       
+
       elsif board.turn_count == 3
         move = second_move_first_player(board)
-        
+
       else
         # if board.turn_count.even? then first player.
         # if board.turn_count.odd? then second player.
@@ -22,7 +22,8 @@ module Players
       end
 
       sleep 2
-      return move
+      binding.pry
+      return move.to_s
     end
 
     private
@@ -33,7 +34,7 @@ module Players
         @token
       end
     end
-    
+
     def opening_position
       (corner << center).sample(1).first
     end
@@ -49,7 +50,7 @@ module Players
     def center
       [5]
     end
-    
+
     def random_move(board)
       (1..9).each_with_object("") do |x, obj|
         if board.valid_move?(x)
@@ -58,55 +59,56 @@ module Players
         end
       end
     end
-    
+
     def first_move_second_player(board)
-        first_move = 1 + board.cells.find_index {|cell| cell != " "}
-        puts "The previous first move was #{first_move}."
+      first_move = 1 + board.cells.find_index {|cell| cell != " "}
+      puts "The previous first move was #{first_move}."
 
-        # if player_1 start in corner.
-        if corner.include?(first_move)
-          move = center.first
-          puts "The second player has chosen an opening move of #{move}."
+      # if player_1 start in corner.
+      if corner.include?(first_move)
+        move = center.first
+        puts "The second player has chosen an opening move of #{move}."
         # if player_1 start in center
-        elsif center.include?(first_move)
-          move = corner.sample
-          puts "The second player picked a corner."
+      elsif center.include?(first_move)
+        move = corner.sample
+        puts "The second player picked a corner."
         # if player_1 start on edge
-        elsif edge.include?(first_move)
-          case first_move
-            when 2
-              move = [1, 3].sample
-            when 6
-              move = [3, 9].sample
-            when 8
-              move = [7, 9].sample
-            when 4
-              move = [1, 7].sample
-          end
-          puts "The second player picked the corner in position #{move}."
+      elsif edge.include?(first_move)
+        case first_move
+        when 2
+          move = [1, 3].sample
+        when 6
+          move = [3, 9].sample
+        when 8
+          move = [7, 9].sample
+        when 4
+          move = [1, 7].sample
         end
-        move
-    end
-      
-      def second_move_first_player(board)
-        #First_player_is_in_center
-        #Second_player_is_on_edge
-        opponent_token = opponent_player_token
-        second_player_position = 1 + board.cells.find_index {|cell| cell == opponent_token}
-
-        case 2nd_player_position
-          when 2
-            move = [1, 3].sample
-          when 6
-            move = [3, 9].sample
-          when 8
-            move = [7, 9].sample
-          when 4
-            move = [1, 7].sample
-        end
+        puts "The second player picked the corner in position #{move}."
       end
-  
+      move
+    end
 
+    def second_move_first_player(board)
+      #First_player_is_in_center
+      #Second_player_is_on_edge
+      opponent_token = opponent_player_token
+      second_player_position = 1 + board.cells.find_index {|cell| cell == opponent_token}
+
+      case second_player_position
+      when 2
+        move = [1, 3].sample
+      when 6
+        move = [3, 9].sample
+      when 8
+        move = [7, 9].sample
+      when 4
+        move = [1, 7].sample
+      end
+
+      move = random_move(board)
+      move
+    end
   end
 end
 
