8bc6ca34a167e99f5174076c6112002bb892d0d9
David Kennell <davidkennell0@gmail.com>
2016-09-20 19:25:17 +0000

    refactored computer class

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -7,54 +7,13 @@ module Players
         # First move for first player.
         move = opening_position
         puts "The computer has chosen an opening move of #{move}."
+        
       elsif board.turn_count == 1
-      
-        # First move for second player.
-        first_move = 1 + board.cells.find_index {|cell| cell != " "}
-        puts "The previous first move was #{first_move}."
-
-        # if player_1 start in corner.
-        if corner.include?(first_move)
-          move = center.first
-          puts "The second player has chosen an opening move of #{move}."
-        # if player_1 start in center
-        elsif center.include?(first_move)
-          move = corner.sample
-          puts "The second player picked a corner."
-        # if player_1 start on edge
-        elsif edge.include?(first_move)
-          case first_move
-            when 2
-              move = [1, 3].sample
-            when 6
-              move = [3, 9].sample
-            when 8
-              move = [7, 9].sample
-            when 4
-              move = [1, 7].sample
-            end
-          puts "The second player picked the corner in position #{move}."
-        end
+        move = first_move_second_player(board)
+       
+      elsif board.turn_count == 3
+        move = second_move_first_player(board)
         
-        elsif board.turn_count == 3
-        #First_player_second_move
-        
-        #First_player_is_in_center
-        #Second_player_is_on_edge
-        opponent_token = opponent_player_token
-        case 2nd_player_position
-            when 2
-              move = [1, 3].sample
-            when 6
-              move = [3, 9].sample
-            when 8
-              move = [7, 9].sample
-            when 4
-              move = [1, 7].sample
-            end
-        
-        
-
       else
         # if board.turn_count.even? then first player.
         # if board.turn_count.odd? then second player.
@@ -99,6 +58,57 @@ module Players
         end
       end
     end
+    
+    def first_move_second_player(board)
+        first_move = 1 + board.cells.find_index {|cell| cell != " "}
+        puts "The previous first move was #{first_move}."
+
+        # if player_1 start in corner.
+        if corner.include?(first_move)
+          move = center.first
+          puts "The second player has chosen an opening move of #{move}."
+        # if player_1 start in center
+        elsif center.include?(first_move)
+          move = corner.sample
+          puts "The second player picked a corner."
+        # if player_1 start on edge
+        elsif edge.include?(first_move)
+          case first_move
+            when 2
+              move = [1, 3].sample
+            when 6
+              move = [3, 9].sample
+            when 8
+              move = [7, 9].sample
+            when 4
+              move = [1, 7].sample
+          end
+          puts "The second player picked the corner in position #{move}."
+        end
+        move
+    end
+      
+      def second_move_first_player(board)
+        #First_player_is_in_center
+        #Second_player_is_on_edge
+        opponent_token = opponent_player_token
+        second_player_position = 1 + board.cells.find_index {|cell| cell == opponent_token}
+
+        case 2nd_player_position
+          when 2
+            move = [1, 3].sample
+          when 6
+            move = [3, 9].sample
+          when 8
+            move = [7, 9].sample
+          when 4
+            move = [1, 7].sample
+        end
+      end
+  
+
   end
 end
+
+
 
