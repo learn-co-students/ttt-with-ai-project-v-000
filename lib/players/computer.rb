fcd5416bed046efea87ce17ac1992ece4e77499a
Christina Cole <christinal.cole@gmail.com>
2016-09-28 01:15:53 +0000

    Initial AI strategy based on TTT wiki

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,7 +3,7 @@ module Players
 
   class Computer < Player
 
-    def move(board)
+    def old_move(board)  #old move method; change name back to move to use
       if board.valid_move?(1)
         "1"
       elsif board.valid_move?(5)
@@ -23,6 +23,19 @@ module Players
       else
         "7"
       end
+    end
+
+    def move(board)
+      move = nil  #placeholder that will return the computer's move as a string
+
+      if board.turn_count.even? && board.turn_count < 4
+        move = ["1", "3", "5"].detect {|pick| board.valid_move?(pick)}
+      elsif board.turn_count.odd? && board.turn_count < 5
+        move = ["5", "1", "3", "7", "9"].detect {|pick| board.valid_move?(pick)}
+      else
+        move = ["2", "4", "6", "7", "8", "9"].sample
+      end
+      #binding.pry
     end
 
   end #class end
