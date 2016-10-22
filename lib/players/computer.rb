419a6622feed7f5d7f471673cfddacb45f394a8c
Adam Cooper <amcooper@gmail.com>
2016-10-22 16:10:50 -0400

    Abortive minimax effort chimerically mixed with a less elegant a.i. We'll merge after this commit.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,44 +1,22 @@
 module Players
 	class Players::Computer < Player
 
-		def score(game)
-		    if game.win?(@player)
-		        return 10
-		    elsif game.win?(@opponent)
-		        return -10
-		    else
-		        return 0
-		    end
-		end
-
-		def minimax(game)
-		    return score(game) if game.over?
-		    scores = [] # an array of scores
-		    moves = []  # an array of moves
-
-		    # Populate the scores array, recursing as needed
-		    game.get_available_moves.each do |move|
-		        possible_game = game.get_new_state(move)
-		        scores.push minimax(possible_game)
-		        moves.push move
-		    end
-
-		    # Do the min or the max calculation
-		    if game.active_turn == @player
-		        # This is the max calculation
-		        max_score_index = scores.each_with_index.max[1]
-		        @choice = moves[max_score_index]
-		        return scores[max_score_index]
-		    else
-		        # This is the min calculation
-		        min_score_index = scores.each_with_index.min[1]
-		        @choice = moves[min_score_index]
-		        return scores[min_score_index]
-		    end
-		end
+    def winnable(board,token)
+    	cells = board.cells
+      win_combo = Game::WIN_COMBINATIONS.find {|combo| combo.count(token) == 2 && combo.count(" ") == 1}
+      win_combo ? { truth: true, move: win_combo.find(" ") }
+    end
 
 		def move(board)
-
+      win_hash = winnable(board,self.token)
+      if win_hash[:truth]
+      	win_hash[:move]
+      else
+      	block_hash = winnable(board, self.token == "X" ? "O" : "X")
+      	if block_hash[:truth]
+      		block_hash[:move]
+      	end
+      end
 		end
 
 	end
