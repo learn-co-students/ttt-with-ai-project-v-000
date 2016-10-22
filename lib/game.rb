1a5bb0ab82028ab839294111f753581da94ef2f0
Adam Cooper <amcooper@gmail.com>
2016-10-22 19:04:40 -0400

    A.I. includes closing the victory and blocking imminent opponent victory.

--- lib/game.rb
+++ lib/game.rb
@@ -51,13 +51,15 @@ class Game
 		current_turn = ''
 		player = current_player
 		while !@board.valid_move?(current_turn)
-			print "\nInvalid move. "
+			print "\nInvalid move. " if current_turn != ''
 		  current_turn = player.move(@board)
 		end
 		@board.update(current_turn,player)
 	end
 
 	def play
+		@board.display
+
 		while !over?
 			turn
 			@board.display
@@ -70,8 +72,47 @@ class Game
 		end
 	end
 
+  def self.input_validation(input,valid_array)
+    while !valid_array.include?(input)
+    	print "\nInvalid input. Try again. "
+    	input = gets.chomp
+    end
+    input
+  end
+
 	def self.start
-		
+		exit = false
+		while !exit
+			puts 'Welcome to our Tic Tac Toe.'
+			print 'How many human players? (0,1,2): '
+			humans = input_validation(gets.chomp, ['0','1','2'])
+			puts "\n\nO.K. Playing with #{humans} human player."
+			if humans == '1'
+				print 'Who goes first, computer or human? '
+				first = input_validation(gets.chomp, ['computer','human'])
+				if first == "computer"
+					player_1 = Players::Computer.new("X")
+					player_2 = Players::Human.new("O")
+				else
+					player_1 = Players::Human.new("X")
+					player_2 = Players::Computer.new("O")
+				end
+				puts "\nO.K. The #{first} opens play."
+			elsif humans == '0'
+				player_1 = Players::Computer.new("X")
+				player_2 = Players::Computer.new("O")
+			elsif humans == '2'
+				player_1 = Players::Human.new("X")
+				player_2 = Players::Human.new("O")
+			end
+			game = self.new(player_1,player_2)
+			game.play
+			print "\nWould you like to play a new game? (y,n) "
+			repeat = input_validation(gets.chomp, ['y','n'])
+			repeat.downcase == "y" ? exit = false : exit = true
+		end
+
+		puts "\nThank you for playing!"		
 	end
 
 end
--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,13 +1,22 @@
 module Players
 	class Players::Computer < Player
 
+    # winnable returns a hash. If the game is winnable on this move, return { truth: true, move: N }, where N is the winning move. Otherwise return { truth: false, move: nil }
     def winnable(board,token)
     	cells = board.cells
-      win_combo = Game::WIN_COMBINATIONS.find {|combo| combo.count(token) == 2 && combo.count(" ") == 1}
-      win_combo ? { truth: true, move: win_combo.find(" ") }
+      win_combo = Game::WIN_COMBINATIONS.find do |combo|
+      	[ cells[combo[0]], cells[combo[1]], cells[combo[2]] ].sort.join == " #{token}#{token}"
+      end
+      if win_combo
+      	{ truth: true, move: (win_combo.find {|cell| cells[cell] == " "} + 1).to_s }
+      else
+      	{ truth: false, move: nil }
+      end
     end
 
+    # The computer tries to (1) win, (2) block the opponent's imminent win, (3) move into an empty space.
 		def move(board)
+			puts "\nPlayer #{self.token} moves."
       win_hash = winnable(board,self.token)
       if win_hash[:truth]
       	win_hash[:move]
@@ -15,6 +24,8 @@ module Players
       	block_hash = winnable(board, self.token == "X" ? "O" : "X")
       	if block_hash[:truth]
       		block_hash[:move]
+      	else
+      		(board.cells.find_index {|cell| cell == " "} + 1).to_s
       	end
       end
 		end
