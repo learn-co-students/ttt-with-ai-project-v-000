73fc872e0c38c03608d5a2b693016d0259398dbb
mo6709 <mo6709@gmail.com>
2016-10-28 11:43:40 -0400

    Change the access to token to be attr_reader instead of attr_accessor

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,12 @@
 class Player
-  attr_accessor :token
+  attr_reader :token
 
   def initialize(token)
   	@token = token
   end
-  
+
+  #Represents how the player makes a move on the board 
+  # def move(index)
+  	
+  # end
 end
