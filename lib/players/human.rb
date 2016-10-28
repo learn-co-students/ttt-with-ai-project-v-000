bd7bfd1f15100ee907ea787eba96b0831d1b2270
mo6709 <mo6709@gmail.com>
2016-10-28 13:40:37 -0400

    human.rb update

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,8 +1,5 @@
-class Human < Player
-
-	def move
-		puts "Please select the position that you would like to move to"
-		user_input = gets.chomp
+module Players
+	class Human < Player
+		
 	end
-
 end
