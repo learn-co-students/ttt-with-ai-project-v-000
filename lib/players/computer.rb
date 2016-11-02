64901ea5364fbe8e28322c69a4e21d9f37fe5b40
Jon Fougner <jon.fougner@gmail.com>
2016-11-02 23:38:11 +0000

    Block check-mate by opponent

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -4,26 +4,18 @@ module Players
   class Computer < Player
     def move(board)
       if win(board, self) != nil # If you can win ...
-        win(board, self) # win.
+        win(board, self) # ... win.
       elsif win(board, opponent) != nil # Otherwise, if your opponent might win next turn ...
-        win(board, opponent) # ...block him.
-      elsif check_mate(board, self) != nil # Otherwise, if you can check mate ...
-        check_mate(board, self) # ... check mate.
-      elsif turn_count == 1
-        go_for_the_middle(board)
-      elsif turn_count == 0
-        move_first(board)
-      else
-        move_randomly(board)
+        win(board, opponent) # ... block him.
+      elsif check_mate(board, self) != nil # Otherwise, if you can check-mate him ...
+        check_mate(board, self) # ... check-mate him.
+      elsif check_mate(board, opponent) != nil # Otherwise, if your opponent might check-mate you next turn ...
+        check_mate(board, opponent) # ... block his check-mate.
+      elsif move_strongly(board) != nil # Otherwise, if a generally strong position is available ...
+        move_strongly(board) # ... take it.
+      else # Otherwise ...
+        move_randomly(board) # ... make any available move.
       end
-    end
-
-    def go_for_the_middle(board)
-      board.cells[4] == " " ? 5 : 1 # Play in the upper-left WLOG.
-    end
-
-    def move_first(board)
-      "5" # This is a string to satisfy a test that is looking for a string. It's unclear why the test is looking for strings.
     end
 
     def win(board, player)
@@ -32,9 +24,10 @@ module Players
         free = 0
         candidate = nil
         wc.each_with_index do |cell, index|
-          if board.cells[cell] == player.token
+          case board.cells[cell]
+          when player.token
             already += 1
-          elsif board.cells[cell] == " "
+          when " "
             free += 1
             candidate = wc[index] + 1
           end
@@ -57,9 +50,10 @@ module Players
           free = 0
           candidate = nil
           wc.each_with_index do |cell, index|
-            if pb[cell] == player.token
+            case pb[cell]
+            when player.token
               already += 1
-            elsif pb[cell] == " "
+            when " "
               free += 1
               candidate = wc[index] + 1 # A candidate is a possible winning move on the NEXT turn, 1 through 9, subject to playing m on THIS turn.
             end
@@ -69,10 +63,20 @@ module Players
           end
         end # wc
         if winners.uniq.size > 1
-          return m
+          return m unless win(board, @opponent) != nil && win(board, @opponent) != m # i.e., if the other player can win on the next turn, and your would-be check-mate doesn't block him, then it's not a check-mate after all. While the flow of move(board) makes this check unnecessary when check_mate is invoked on self (since we've already confirmed the opponent can't win next turn), it is important when projecting the *opponent's* possible check-mate.
         end
       end
       nil
+    end
+
+    def move_strongly(board)
+      valid_moves = valid_moves(board)
+      if valid_moves.include? "5"
+        return "5"
+      else
+        free_corners = ["1", "3", "7", "9"] & valid_moves
+        free_corners[rand(free_corners.length)]
+      end
     end
 
     def move_randomly(board)
