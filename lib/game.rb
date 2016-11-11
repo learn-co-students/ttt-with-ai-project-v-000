20f7a35fda13d1dbf005dcf7adca9a09be6f59f7
Justin  Gore <justin.gore@outlook.com>
2016-11-11 19:37:54 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,28 +1,24 @@
+require 'pry'
 class Game
 
-  def initialize
+  attr_accessor :board, :player_1, :player_2
+
+  def initialize(player_1 = "X", player_2 = "O", board = Array.new(9, " "))
+    @player_1 = player_1
+    @player_2 = player_2
+    @board = board
   end
 
   WIN_COMBINATIONS = [
-                        [0,1,2],
-                        [3,4,5],
-                        [6,7,8],
-                        [0,3,6],
-                        [1,4,7],
-                        [2,5,8],
-                        [0,4,8],
-                        [2,4,6]
-                        ]
-
-  def board
-
-  end
-
-  def player_1
-  end
-
-  def player_2
-  end
+                      [0,1,2],
+                      [3,4,5],
+                      [6,7,8],
+                      [0,3,6],
+                      [1,4,7],
+                      [2,5,8],
+                      [0,4,8],
+                      [2,4,6]
+                              ]
 
   def current_player
   end
