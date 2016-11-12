8bb0858f4ed65361b1d1b3f2cd0bf3952d7b2167
Juan <gongora.animations@gmail.com>
2016-11-12 17:00:51 -0500

    Update human.rb

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -2,15 +2,8 @@ module Players
   class Human < Player
 
     def move(value)
-      loop do
-        puts "Please choose a cell between 1-9"
-        choice = gets.to_i
-        case choice
-          when 1..9
-            puts "You chose #{choice}."
-            break
-        end
-      end
+      puts "Please choose a cell between 1-9"
+      choice = gets.chomp
     end
   end
 end
