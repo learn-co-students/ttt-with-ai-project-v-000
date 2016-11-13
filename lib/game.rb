b3d0c422db2ccd3cbfe95809ef7112bc40de505e
Juan <gongora.animations@gmail.com>
2016-11-13 17:12:40 -0500

    Update game.rb

--- lib/game.rb
+++ lib/game.rb
@@ -12,7 +12,7 @@ class Game
   ]
 
   def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
-    @board = board; @player_1 = player_1; @player_2 = player_2
+    @player_1 = player_1; @player_2 = player_2; @board = board
   end
 
   def current_player
