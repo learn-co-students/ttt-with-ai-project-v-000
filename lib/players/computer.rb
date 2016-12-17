a7d17473c373291577d5645ca0cf43e888c2363f
Pamela Bortnick <pbortnick@gmail.com>
2016-12-17 01:08:02 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -6,20 +6,20 @@ module Players
         "5"
       elsif !board.taken?(1)
         "1"
+      elsif !board.taken?(9)
+        "9"
       elsif !board.taken?(3)
         "3"
       elsif !board.taken?(7)
         "7"
-      elsif !board.taken?(9)
-        "9"
       elsif !board.taken?(2)
         "2"
+      elsif !board.taken?(8)
+        "8"
       elsif !board.taken?(4)
         "4"
       elsif !board.taken?(6)
         "6"
-      elsif !board.taken?(8)
-        "8"
       end
     end
 
