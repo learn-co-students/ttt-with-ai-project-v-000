d2ce767a2b01dbded3bf23f06799143ebde271a3
M. Simon Borg <msimonborg@gmail.com>
2016-12-29 01:33:02 -0500

    extract some methods

--- lib/war_games.rb
+++ lib/war_games.rb
@@ -1,5 +1,5 @@
 class WarGames < Game
-  attr_accessor :games, :winners, :start_time, :end_time
+  attr_accessor :games, :winners, :start_time, :end_time, :x_wins, :o_wins
 
   def initialize(player1, player2, board = Board.new)
     super
@@ -9,6 +9,7 @@ class WarGames < Game
 
   def play
     self.start_time = Time.now
+
     eval("#{games}").times do
       board.reset!
       board.display
@@ -17,7 +18,12 @@ class WarGames < Game
       puts '', "Cat's Game!" if draw?
       winners << winner unless winner.nil?
     end
+
     self.end_time = Time.now
+    wrap_up
+  end
+
+  def wrap_up
     winners_report
     sleep(1)
     play_again?
@@ -25,16 +31,19 @@ class WarGames < Game
 
   def num_of_games
     puts '', 'How many games do you want to run through?'
-    input = gets.chomp.to_i
+    gets.chomp.to_i
   end
 
   def winners_report
-    x_wins = winners.select { |winner| winner == 'X' }.size
-    o_wins = winners.select { |winner| winner == 'O' }.size
-    puts
+    count_winners
     puts '', "#{games} war games completed in " + (end_time - start_time).to_s.yellow + ' seconds.'
     puts "X won #{x_wins.to_f / games * 100}%".red + " of the time (#{x_wins} games)."
     puts "O won #{o_wins.to_f / games * 100}%".blue + " of the time (#{o_wins} games)."
     puts "Cat's game #{100 - (winners.size.to_f / games * 100)}%".green + " of the time (#{games - winners.size} games)."
+  end
+
+  def count_winners
+    self.x_wins = winners.select { |winner| winner == 'X' }.size
+    self.o_wins = winners.select { |winner| winner == 'O' }.size
   end
 end
