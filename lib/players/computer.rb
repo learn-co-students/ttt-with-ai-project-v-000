3247b37fe105ff594ba63f468a921a88ca09b0d4
tracypholmes <tracyholmes@gmail.com>
2017-01-04 15:38:21 -0600

    Updated with beginning greetings and structure

---
+++ lib/players/computer.rb
@@ -0,0 +1,3 @@
+class Players::Computer
+  # represents a computer player of Tic Tac Toe. Implement a #move method that accepts a board and returns the move the computer wants to make in the form of a 1-9 string.
+end
