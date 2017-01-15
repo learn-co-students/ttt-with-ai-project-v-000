a9a1e2105d52c55a1b4a7bbba832cd27e5251027
David Lewitan <dovidlwtn@gmail.com>
2017-01-15 22:16:52 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -8,4 +8,41 @@ class Board
   def reset!
     @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   end
+
+  def display
+    i = -1
+    3.times do
+      puts " #{@cells[i+=1]} | #{@cells[i+=1]} | #{@cells[i+=1]} "
+      puts "-----------" if i < 6
+    end
+  end
+
+  def position(pos)
+    pos = pos.to_i - 1
+    @cells[pos]
+  end
+
+  def full?
+    !@cells.any? { |cell| cell == " " }
+  end
+
+  def turn_count
+    @cells.keep_if { |cell| cell != " "}.count
+  end
+
+  def taken?(pos)
+    pos = pos.to_i - 1
+    @cells[pos] != " "
+  end
+
+  def valid_move?(pos)
+    return false if !/[123456789]/.match(pos)
+    pos = pos.to_i - 1
+    @cells[pos] == " "
+  end
+
+  def update(pos, player)
+    pos = pos.to_i - 1
+    @cells[pos] = player.token
+  end
 end
---
+++ lib/game.rb
@@ -0,0 +1,46 @@
+class Game
+  attr_accessor :board, :player_1, :player_2
+
+  WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
+
+  def initialize(player_1 = Players::Human.new('X'), player_2 = Players::Human.new('O'), board = Board.new)
+    @player_1 = player_1
+    @player_2 = player_2
+    @board = board
+  end
+
+  def current_player
+    @board.turn_count % 2 == 0 ? @player_1 : @player_2
+  end
+
+  def over?
+    board.full?
+  end
+
+  def won?
+    WIN_COMBINATIONS.any? do |combo|
+      #non are empty
+      (!combo.any?{|e| @board.cells[e] == " "}) &&
+      #they are all the same
+      combo.collect{|e| @board.cells[e]}.uniq.count == 1
+    end
+  end
+
+  def draw?
+    over? && !won?
+  end
+
+  def winner
+    WIN_COMBINATIONS.collect do |combo|
+      (!combo.any?{|e| @board.cells[e] == " "}) && combo.collect{|e| @board.cells[e]}.uniq.count == 1 ? @board.cells[combo[0]] : nil
+    end.compact[0]
+  end
+
+  def turn
+    pos = gets
+    while !@board.valid_move?(pos)
+      pos = gets
+    end
+    board.update(pos, current_player)
+  end
+end
