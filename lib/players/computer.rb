6eed589ce33e7ba397403aa710a83beb5ef61dbd
Jon foster <redcabinny@gmail.com>
2017-01-15 23:42:08 -0500

    Fixed AI, Added 3rd move

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,8 +3,12 @@ require_relative '../player.rb'
 module Players
 	class Computer < Player
 		def move(board, game)
-
-			if board.needs_block?
+			corners1 = [board.cells[0], board.cells[8]]
+			corners2 = [board.cells[2], board.cells[6]]
+			opponent_token = game.current_player.token == "X" ? "O" : "X"
+			if board.turn_count == 3 && (corners1 == [opponent_token, opponent_token] || corners1 == [opponent_token, opponent_token]) && board.cells[4] != " "
+				["2", "4", "6", "8"].sample
+			elsif board.needs_block?
 				board.block_positions
 				if board.answer_token.include?(game.current_player.token)
 					index = board.answer_token.index(game.current_player.token)
