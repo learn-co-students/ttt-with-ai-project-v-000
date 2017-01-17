c8bb622456485619cf09ee1bbbea7a38b53f7755
tracypholmes <tracyholmes@gmail.com>
2017-01-16 22:52:22 -0600

    updated with wargames structure

--- lib/board.rb
+++ lib/board.rb
@@ -15,6 +15,7 @@ class Board
     puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
     puts '-----------'
     puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
+    puts "\n"
   end
 
   def input_index(input) # this one line is all over the place!
