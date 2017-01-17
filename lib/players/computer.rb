e4c3e797bc466b08845907ff14bafd6e63c4d334
tracypholmes <tracyholmes@gmail.com>
2017-01-16 18:38:13 -0600

    removed Sleep from code - this introduced a weird bug with index[0]

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,9 +1,9 @@
-class Players::Computer < Player
-  # represents a computer player of Tic Tac Toe. Implement a #move method that accepts a board and returns the move the computer wants to make in the form of a 1-9 string.
+module Players
+  class Players::Computer < Player   # represents a computer player of Tic Tac Toe. Implement a #move method that accepts a board and returns the move the computer wants to make in the form of a 1-9 string.
 
-  def move(board)
-    move = nil
-    input = (1..9).to_a.sample
-    input.to_s
-  end
+    def move(board)
+      move = nil
+      input = (1..9).to_a.sample
+      input.to_s
+    end
 end
