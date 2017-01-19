9a9eb31d6c6b31f4402d6ed21623d8102623832e
github username <github email address>
2017-01-19 00:27:53 -0500

    fixed issue in #move and built game engine

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Game
 
   attr_accessor :board, :player_1, :player_2
@@ -13,9 +15,67 @@ class Game
     [2,4,6]
   ]
 
-  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+  # spec requires arguemnts to be passed in in this order
+  def initialize(player_1=Players::Human.new("X"), player_2=Players::Human.new("O"), board=Board.new)
     @board = board
     @player_1 = player_1
     @player_2 = player_2
+  end
+
+  def current_player
+    # test calls on player.token, changed true/false statements to player_1/player_2
+      @board.turn_count % 2 == 0 ? @player_1 : @player_2
+  end
+
+  def won?
+    WIN_COMBINATIONS.find do |win|
+      #binding.pry
+      @board.cells[win[0]] == @board.cells[win[1]] &&
+      @board.cells[win[1]] == @board.cells[win[2]] &&
+      @board.taken?(win[0]+1) # fatal w/o +1, breaks #winner
+    end
+  end
+
+  def draw?
+    !won? && @board.full?
+  end
+
+  def over?
+    draw? || won?
+  end
+
+  def winner
+    #binding.pry
+    #thanks Avi and Brad!
+    # first get the combo that won
+    if winning_combo = won?
+    # take first position from winning combo and thats the winner
+    @board.cells[winning_combo[0]]
+    end
+  end
+
+  def turn
+    # binding.pry
+    # check for valid move
+    # repeat if failed validation
+    current_move = current_player.move(board)
+    if !@board.valid_move?(current_move)
+      turn
+    else
+      # make move and switch current_player
+      @board.update(current_move, current_player)
+      @board.display
+    end
+  end
+
+  def play
+    until over?
+     turn
+    end
+    if won?
+     puts "Congratulations #{winner}!"
+    elsif draw?
+     puts "Cat's Game!"
+   end
   end
 end
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -3,10 +3,9 @@ require 'pry'
 module Players
   class Human < Player
 
-    def move(input)
-      input = gets.strip
-      input
+    def move(board)
+      puts "Please enter 1-9:"
+      gets.strip
     end
-
   end
 end
