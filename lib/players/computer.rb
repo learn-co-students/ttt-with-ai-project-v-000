7f58cd856166e44db0f2b26fc5147d6922a65ece
github username <github email address>
2017-01-21 12:11:40 -0500

    typo in lib/computer.rb

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -6,7 +6,7 @@ module Players
     ### simple AI ###
     def move(board)
       # unable to use (1..9).to_a.sample.to_s #=> SyntaxError
-      # ["1", "2", "3", "4", "5", "6", "7", "8", "9"].sample.to_s
+      # ["1", "2", "3", "4", "5", "6", "7", "8", "9"].sample.to_s # Error Prone
       move = (1..9).to_a
       move.sample.to_s
     end
