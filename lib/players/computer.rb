23dfcb475d7832ac50783ce47026756dff0e695e
David Lewitan <dovidlwtn@gmail.com>
2017-01-24 04:00:39 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -10,49 +10,73 @@ module Players
     @@corners = [0,2,6,8]
     @@middles = [1,3,5,7]
 
-    @@current_board = []
+    # @@current_board = []
 
-    def opponent
-      self.token == "X" ? "O" : "X"
+    def opponent(current)
+      return "O" if current == "X"
+      return "X" if current == "O"
+      current.token == "X" ? "O" : "X"
     end
 
-    def available(spots)
-      empties = []
-      spots.each do |x|
-        empties << x if @@current_board[x] == " "
-      end
-      empties
+    def meta_iteration(cells, search)
+
     end
 
-    def win(cells)
+    def win(cells, token = self.token)
       wins = @@all.collect do |e|
-        if e.collect{|t| cells[t]}.join.gsub(/\s+/, "")  == self.token + self.token
+        if e.collect{|t| cells[t]}.join.gsub(/\s+/, "")  == token + token
           e[e.index{|s| cells[s] == " "}]
         end
       end.compact!
       wins.count > 0 ? wins : false
     end
 
-    def block(cells)
+    def block(cells, token = opponent(self))
       wins = @@all.collect do |e|
-        if e.collect{|t| cells[t]}.join.gsub(/\s+/, "")  == opponent + opponent
+        if e.collect{|t| cells[t]}.join.gsub(/\s+/, "")  == token + token
           e[e.index{|s| cells[s] == " "}]
         end
       end.compact!
       wins.count > 0 ? wins : false
     end
 
-    def double(cells)
+    def double(cells, token = self.token)
       doubles = cells.each_with_index.collect do |x, i|
         if x != " "
           nil
         else
           temp = cells.dup
-          temp[i] = self.token
-          win(temp) && win(temp).count > 1 ? i : nil
+          temp[i] = token
+          win(temp, token) && win(temp, token).count > 1 ? i : nil
         end
       end.compact!
       doubles.count > 0 ? doubles : false
+    end
+
+    def force(cells)
+      def ng(cells)
+        brd = Board.new
+        brd.cells = cells.dup
+        gm = Game.new(Players::Computer.new('X'), Players::Computer.new('O'), brd)
+      end
+
+      gm = ng(cells)
+      current = gm.current_player
+      opponent(current)
+
+      gm.board.cells.each_with_index do |e, i|
+        if e == " "
+          temp_gm = ng(cells)
+          temp_gm.board.cells[i] = current.token
+          if temp_gm.board.full?
+            return false
+          else
+            temp_gm.turn
+            return i if double(temp_gm.board.cells, current.token)
+          end
+        end
+      end
+      false
     end
 
     def best_move(cells, turn = cells.count { |cell| cell != " "})
@@ -72,9 +96,14 @@ module Players
 
       elsif double(cells)
         options.concat(double(cells))
-      # elsif force
-      #   options << force
 
+      elsif force(cells)
+        options << force(cells)
+
+      elsif block_force(cells)
+        cells[block_force(cells)] = "T"
+        options << cells.index(" ")
+        
       else
         options << cells.index(" ")
       end
@@ -83,8 +112,8 @@ module Players
     end
 
     def move(cells)
-      @@current_board = cells
-      best_move(cells)
+      # @@current_board = cells
+      best_move(cells.dup)
     end
   end
 end
