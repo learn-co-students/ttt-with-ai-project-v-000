1296727efb09852abe071a31dc2227bd9fbfec26
tkanen <tina.kane@gmail.com>
2017-01-31 20:36:52 -0800

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,25 @@
+class Board
+attr_accessor :cells
+
+  def initialize
+    @cells = Array.new(9, " ")
+  end
+
+  def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
+
+  def reset!
+    cells.clear
+    initialize
+  end
+
+  def position(user_input)
+
+  end
+
+end
