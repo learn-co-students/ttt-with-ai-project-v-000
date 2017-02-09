f95f37e8d3fb2ec6e419eaff5932436c25fa6e63
tkanen <tina.kane@gmail.com>
2017-02-08 21:18:25 -0800

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,13 +1,16 @@
 class Game
-WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8],
-[0,4,8], [2,4,6]]
-attr_accessor :board, :player_1, :player_2
-include Players
+  WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8],
+  [0,4,8], [2,4,6]]
+  attr_accessor :board, :player_1, :player_2
 
-  def initialize(player_1 = Player.new(token = "X"), player_2 = Player.new(token = "O"), board = [])
+  def initialize(player_1 = Players::Human.new(token = "X"), player_2 = Players::Human.new(token = "O"), board = Board.new)
     @board = board
     @player_1 = player_1
     @player_2 = player_2
+  end
+
+  def current_player
+    board.turn_count % 2 == 0 ? "X" : "O"
   end
 
 end
