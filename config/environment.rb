c1587f9e1fb2f03b76e017c5c4b00972acf2c438
Damien de Lartigue <damien@delartigue.co.uk>
2017-02-10 15:31:09 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,5 @@
 require 'bundler'
 Bundler.require
 
+require 'rspec'
 require_all 'lib'
---
+++ lib/players/computer.rb
@@ -0,0 +1,11 @@
+module Players
+  class Computer < Player
+    # def move(board)
+    #   # binding.pry
+    #   puts "It's your move #{self.token}"
+    #   board.display
+    #   gets.strip
+    # end
+
+  end# class Computer
+end# module Players
