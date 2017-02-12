b183f0406f78c36d9adb5032d1c0c405c3b8042d
tkanen <tina.kane@gmail.com>
2017-02-12 15:58:14 -0800

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,8 +1,16 @@
 module Players
   class Computer < Player
     def move(board)
-
+      move = nil
+      if board.cells.all? { |cell| cell == " " }
+        move = "1"
+      elsif board.cells[2]
+        move = "3"
+      elsif board.cells[4]
+        move = "5"
+      elsif board.cells[8]
+        move = "9"
+      end
     end
-
   end
 end
