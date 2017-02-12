c5048663606fa17144e9a6cd199c83b8d69d92b2
tkanen <tina.kane@gmail.com>
2017-02-12 12:27:19 -0800

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,7 @@
 class Game
   WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8],
   [0,4,8], [2,4,6]]
-  attr_accessor :board, :player_1, :player_2
+  attr_accessor :board, :player_1, :player_2, :index, :user_input
 
   def initialize(player_1 = Players::Human.new(token = "X"), player_2 = Players::Human.new(token = "O"), board = Board.new)
     @board = board
@@ -13,9 +13,20 @@ class Game
     board.turn_count % 2 == 0 ? player_1 : player_2
   end
 
+  def turn
+    player = current_player
+    move(user_input)
+    if board.valid_move?
+      board.update(user_input, player)
+      board.display
+    else
+      turn
+    end
+  end
+
   def won?
     WIN_COMBINATIONS.detect do |combination|
-      board.cells[combination[0]] == board.cells[combination[1]] && board.cells[combination[1]] == board.cells[combination[2]] && !(board.cells.nil? || board.cells == " ")
+      !(board.cells[combination[0]].include? " ") && board.cells[combination[0]] == board.cells[combination[1]] && board.cells[combination[1]] == board.cells[combination[2]]
     end
   end
 
@@ -29,7 +40,7 @@ class Game
 
   def winner
     if won?
-      binding.pry
+      #binding.pry
       board.cells[won?[0]]
     else
       nil
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,5 +1,9 @@
 module Players
   class Human < Player
-    
+
+    def move(user_input)
+      puts "Please enter 1-9:"
+      user_input = gets.strip
+    end
   end
 end
