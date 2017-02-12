b13bbe2f717e471e82cfce9d2dc436c524dd0ef8
Terry-Thompson <terry.lyn.thompson@gmail.com>
2017-02-12 12:54:10 -0600

    Done.

--- lib/player.rb
+++ lib/player.rb
@@ -1,15 +1,10 @@
 class Player
-  
-attr_reader :token
+
+  attr_reader :token 
 
   def initialize(token)
     @token = token
   end# of initialize
 
-
-  def move
-
-  end# of move
-
-
+  
 end# of class
--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -4,7 +4,6 @@ class Players::Computer < Player
   def move(board)
     viable = board.cells.select {|cell| cell == " " }
     (viable.index(viable.sample) + 1).to_s
-    
   end# of move 
 
 end# of class
