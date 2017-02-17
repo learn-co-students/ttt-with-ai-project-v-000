1d1b2c0fe510959df80654e16c54b7e477aa827f
Terry-Thompson <terry.lyn.thompson@gmail.com>
2017-02-17 10:41:59 -0600

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,17 +1,4 @@
-require 'pry'
 class Players::Computer < Player 
-
-WIN_COMBINATIONS = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,4,8],
-  [2,4,6],
-  [0,3,6],
-  [2,5,8],
-  [1,4,7]
-]
-
 
   def move(board)
     viable = []
