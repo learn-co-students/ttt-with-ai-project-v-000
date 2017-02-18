9015f09d206675fbf83d1213bf629fd04454367c
github username <github email address>
2017-02-18 13:33:37 -0500

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 module Players
     class Computer < Player
         def move(board)
@@ -8,6 +10,10 @@ module Players
 #            valid_moves.sample.to_s #pick a random valid move
             
             # let's make this thing smarter
+            possible_win_combinations = WIN_COMBINATIONS.collect { |wc|
+                wc.any? { |space| space == @token    }
+                }
+                binding.pry
         end
     end
 end
