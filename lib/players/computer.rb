4e2051c78c74906a7e27f34fe0a23a69c858a144
github username <github email address>
2017-02-19 13:39:43 -0500

    notes for computer

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -16,11 +16,19 @@ module Players
    # end
 
   #   1) if computer went first
-		# place X on right upper corner (1st turn)
-		# then use board.valid_move? to check for blank spot (2nd turn)
-		# place X on right bottom corner
-		# then use board.valid_move? to check for blank spot (3rd turn)
-		# then use WIN_COMBINATIONS to place X
+		X = [] #keep track of where computer placed X
+		board.turn_count == 1 #computer went first in turn
+		place X on upper right corner #placed X on spot #3
+		push 3 into X #place 3 in [] 
+		corners = [1,3,7,9] 
+		board.turn_count == 3 #computer went third in turn 
+		loop through corners checking aganist board.valid_move? #loop through corners, compare 1,3,7,9 aganist board.valid_move? to find empty space
+		place X on an empty corner #place X on the blank spot
+		push 1,7, or 9 into X #place another number where the 2nd X is placed into [3, ]
+		board.turn_count == 5 #computer went fifth in turn
+		loop through WIN_COMBINATIONS and compare X. 
+		place the third X
+		
 
 
 
