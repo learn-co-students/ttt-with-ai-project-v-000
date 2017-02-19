3688f4c1de3f08484a441c5f1b133844cac6b3d2
github username <github email address>
2017-02-18 20:39:35 -0500

    updated computer

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,6 +1,17 @@
 module Players
   class Computer < Player
-  
-    
-  end
-end
+    def move(board)
+      spot = nil
+      if board.turn_count == 0
+      	spot = "5"
+      else board.turn_count == 2
+    	if board.valid_move?(5)
+    		spot = "1"
+    	else 
+    		spot ="5"
+    	end	
+      end
+      spot
+  	end
+   end
+ end
