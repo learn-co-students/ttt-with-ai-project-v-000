da71c86d27d5d5a29af0ead6dc66ea53f2a60f0d
Jenna Mandel <jenna.b.mandel@gmail.com>
2017-02-23 04:12:23 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,57 @@
+class Board
+attr_accessor :cells
+
+def initialize
+reset!
+end
+
+def reset!
+  @cells = Array.new(9, " ")
+end
+
+def display
+  puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
+  puts "-----------"
+  puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
+  puts "-----------"
+  puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
+end
+
+def position(input)
+  @cells[input.to_i - 1]
+end
+
+
+def full?
+  @cells.all? do |position|
+ position == "X" || position == "O"
+ end
+end
+
+def turn_count
+counter = 0
+@cells.each do |turns|
+   if turns == "X" || turns == "O"
+     counter += 1
+   end
+  end
+  counter
+ end
+
+
+
+ def taken?(input)
+position(input) == "X" || position(input) == "O"
+end
+
+
+
+ def valid_move?(input)
+ input.to_i.between?(1,9) && !taken?(input)
+
+  end
+
+ def update(input, player)
+ cells[input.to_i - 1] = player.token
+end
+ end
