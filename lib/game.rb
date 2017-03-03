54539056ee1c752459bcc6eecf3784cccca3053b
ryan mcmahon <ryan@mcmahon.work>
2017-03-03 17:04:42 -0500

    beginning Game::curent_player method

---
+++ lib/game.rb
@@ -0,0 +1,22 @@
+class Game
+  include Players
+
+  attr_accessor :board, :player_1, :player_2
+
+  WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],
+                      [0,3,6],[1,4,7],[2,5,8],
+                      [0,4,8],[2,4,6]]
+
+  def initialize(player_1 = Human.new("X"), player_2 = Human.new("O"), board = Board.new)
+    @player_1 = player_1
+    @player_2 = player_2
+    @board = board
+  end
+
+  def current_player
+    if board.cells.count(player_1.token) > board.cells.count(player_2.token)
+      
+
+  end
+
+end
