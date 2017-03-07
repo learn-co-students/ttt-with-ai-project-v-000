c5d53a05cdc00fe9a10cfb76d7e269915c9799e4
Christian Whitesides <whitesidescr@gmail.com>
2017-03-07 15:10:08 -0700

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -18,6 +18,29 @@ class Game
     [6,4,2]
   ]
 
+  def call
+    puts "Would you like to play with 0, 1, or 2 players?"
+    input = gets.strip
+    if input.to_i == 0
+      @player_1 = Player::Computer.new("X")
+      @player_2 = Player::Computer.new("O")
+      play
+    elsif input.to_i == 1
+        puts "Should the first player, X, be a human or computer?"
+        species = gets.strip
+        if species.to_s.downcase == "human"
+          @player_1 = Player::Human.new("X")
+          @player_2 = Player::Computer.new("O")
+          play
+        elsif species.to_s.downcase == "computer"
+          @player_1 = Player::Computer.new("X")
+          play
+        end
+    elsif input.to_i == 2
+      play
+    end
+  end
+
   def current_player
     @board.turn_count % 2 == 0 ? player_1 : player_2
   end
@@ -51,6 +74,34 @@ class Game
   end
 
   def winner
-    
+    if over? && won? != nil
+      winning_combination = won?
+      @board.cells[winning_combination[0]]
+    else
+      nil
+    end
+  end
+
+  def turn
+    input = current_player.move(@board)
+    if @board.valid_move?(input)
+      @board.update(input, current_player)
+    else
+    "invalid"
+     input = current_player.move(@board)
+    end
+    @board.display
+  end
+
+  def play
+    until over?
+      turn
+    end
+
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
+    end
   end
 end
---
+++ lib/players/computer.rb
@@ -0,0 +1,23 @@
+class Player::Computer < Player
+  def move(board)
+    if !board.taken?("5")
+      "5"
+    elsif !board.taken?("1")
+      "1"
+    elsif !board.taken?("9")
+      "9"
+    elsif !board.taken?("3")
+      "3"
+    elsif !board.taken?("2")
+      "2"
+    elsif !board.taken?("8")
+      "8"
+    elsif !board.taken?("7")
+      "7"
+    elsif !board.taken?("4")
+      "4"
+    elsif !board.taken?("6")
+      "6"
+    end      
+  end
+end
