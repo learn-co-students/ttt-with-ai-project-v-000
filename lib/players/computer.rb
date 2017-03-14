43eea76e6998760923fcaf299bc0615154329261
johnfelixespinosa <johnfelix.espinosa@icloud.com>
2017-03-14 19:26:36 -0400

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,18 +1,20 @@
 module Players
-class Computer < Player
+  class Computer < Player
 
-  #  WIN_COMBINATIONS = [
-  #  [0,1,2],
-  #  [3,4,5],
-  #  [6,7,8],
-  #  [0,3,6],
-  #  [1,4,7],
-  #  [2,5,8],
-  #  [0,4,8],
-  #  [6,4,2]
-  #  ]
-  
+    #  WIN_COMBINATIONS = [
+    #  [0,1,2],
+    #  [3,4,5],
+    #  [6,7,8],
+    #  [0,3,6],
+    #  [1,4,7],
+    #  [2,5,8],
+    #  [0,4,8],
+    #  [6,4,2]
+    #  ]
+    def move(board)
+      "1"
+    end
 
 
-end
+  end
 end
