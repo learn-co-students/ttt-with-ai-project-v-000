3d5532a82245e6f95876ac6a14fc9bcafdc2d675
johnfelixespinosa <johnfelix.espinosa@icloud.com>
2017-03-15 18:11:50 -0400

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,57 +1,74 @@
 module Players
   class Computer < Player
 
-    #  WIN_COMBINATIONS = [
-    #  [0,1,2],
-    #  [3,4,5],
-    #  [6,7,8],
-    #  [0,3,6],
-    #  [1,4,7],
-    #  [2,5,8],
-    #  [0,4,8],
-    #  [6,4,2]
-    #  ]
+      WIN_COMBINATIONS = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [6,4,2]
+      ]
+
     def move(board)
-      self_combo_tracker = []
-      opponent_combo_tracker = []
-      block_move = 0
-      win_move = 0
-      if board.valid_move?("5")
-      "5"
-       # elsif board.valid_move?("1")
-       #   "1"
-       # elsif board.valid_move?("3")
-       #   "3"
-       # elsif board.valid_move?("7")
-       #   "7"
-       # elsif board.valid_move?("9")
-       #   "9"    
-       # elsif board.valid_move?("2")
-       #   "2"
-       # elsif board.valid_move?("4")
-       #   "4"    
-       # elsif board.valid_move?("6")
-       #   "6"
-       # elsif board.valid_move?("8")
-       #   "8"
-        else WIN_COMBINATIONS.each do |combo|
-          combo.each do |x|
-            if board.cells[x] != self.token && board.cells[x] != " "
-              opponent_combo_tracker << x 
-            elsif board.cells[x] == " "
-                
-            end
-              if opponent_combo_tracker.size == 2
+      #Iterate through combos to see if there are any win combos with two identical tokens 
+      #and an empty space
+      WIN_COMBINATIONS.each do |combo|
+        two_token_combo = []
+        if (combo[0] == combo[1] && combo[2] == " ") || (combo[1] == combo[2] && combo[0] == " ") || (combo[0] == combo[2] && combo[1] == " ")
+          two_token_combo << combo
+          win_move
+        else
+          no_combos(board)
+        end
+      end
+      #If there are no possible wins for either player, goto the no_combos method
+        
+    end
 
-
-          #  else board.cells[x] == self.token && board.cells[x] == 
-          #   self_combo_tracker += 1
-
-
+    def no_combos(board)
+      #will try to take middle always if available, if not will try to take a corner next
+      if !board.taken?(5)
+        move = "5"
+      elsif board.turn_count == 2
+        move = [1, 3, 7, 9].detect do |i| 
+          !board.taken?(i).to_s
         end
       end
     end
 
+    def win_move
+      two_token_combo.each do |combo| 
+        if (combo[0] || combo[1] || combo[2]) == self.token
+          combo.each do |cell|
+            position = nil
+            if cell == " "
+              position << cell
+              move = (position+1).to_s
+            end
+          end
+        else 
+          block_move
+        end
+      end
+    end
+
+    def block_move
+      two_token_combo.each do |combo| 
+        if (combo[0] || combo[1] || combo[2]) == !self.token
+          combo.each do |cell|
+            position = nil
+            if cell == " "
+              position << cell
+              move = (position+1).to_s
+            end
+          end
+        end
+      end
+    end
+          
 
   end
 end
