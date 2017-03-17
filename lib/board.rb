0c7e052c2101fa343a59724c53ff313f78674204
walwoodr <walwoodr@gmail.com>
2017-03-17 10:12:45 -0500

    Add board and basic board functionality

---
+++ lib/board.rb
@@ -0,0 +1,52 @@
+class Board
+  attr_accessor :cells
+
+  def initialize
+    self.cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+
+  def reset!
+    self.cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+
+  def display
+    puts "| #{self.cells[0]} | #{self.cells[1]} | #{self.cells[2]} |"
+    puts "-----------"
+    puts "| #{self.cells[3]} | #{self.cells[4]} | #{self.cells[5]} |"
+    puts "-----------"
+    puts "| #{self.cells[6]} | #{self.cells[7]} | #{self.cells[8]} |"
+  end
+
+  def input_to_index(input)
+    input.to_i - 1
+  end
+
+  def position(input)
+    self.cells[input_to_index(input)]
+  end
+
+  def full?
+    self.cells.none?{|cell| cell == " "}
+  end
+
+  def turn_count
+    self.cells.select{|cell| cell != " "}.size
+  end
+
+  def taken?(input)
+    self.position(input) != " "
+  end
+
+  def valid_move?(input)
+    if input.to_i > 0 && input.to_i < 10
+      taken?(input) == false ? true : false
+    else
+      false
+    end
+  end
+
+  def update(input, player)
+    self.cells[input_to_index(input)] = player.token if self.valid_move?(input)
+  end
+
+end
