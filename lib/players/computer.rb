ae3b7ae75e683612c585dddafab12b31f3bb4b9a
walwoodr <walwoodr@gmail.com>
2017-03-17 22:07:52 -0500

    Committing some logic to checkout master

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,20 +3,37 @@ require 'pry'
 
 module Players
   class Computer < Player
+
+    WIN_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+
     def move(board)
-      choice = (rand*9).ceil
-      puts "The computer (#{self.token}) has moved in spot #{choice}"
-      "#{(rand*9).ceil}"
+      if self.winning_move(board) == nil
+        choice = (rand*9).ceil
+      else
+        choice = self.winning_move(board)
+      end
+      puts "The computer (#{self.token}) would like to take spot #{choice}"
+      "#{choice}"
     end
 
-    def winning_move(board)
-      
-  end
+    def winning_move(board) # this method should return the index to be played
+      winning_line = WIN_COMBOS.detect do |combo|
+        binding.pry
+        line = combo.collect! do |index|
+          board.cells[index.to_i]
+        end
+        line.sort == [" ", self.token, self.token]
+      end #at this point winning_line should be something like [2,5,8] or nil
+      # i want to find the first item in the winning_line array for which the board doesn't have an "X"
+      winning_line.find {|index| board.cells[index] != "X"} if winning_line != nil
+    end # winning_move end # this should return an index to play.
+
+  end # class end
 end
 
-win_combos = {a => [0,1,2], b => [3,4,5], c => [6,7,8], d => [0,3,6], e => [1,4,7], f => [2,5,8], g => [0,4,8], h => [2,4,6]}
-
-forks = {0 => [[a,d], [d,g], [a,g]], 1 => [a,e], 2 => [[a,f], [a,h], [f,h]], 3 => [d,b], 4 => [b,e], 5 => [f,b], 6 => [[d,c], [c,h], [d,h]], 7 => [c,e], 8 => [[c,f], [f,g], [c,g]]}
+# win_combos = {a => [0,1,2], b => [3,4,5], c => [6,7,8], d => [0,3,6], e => [1,4,7], f => [2,5,8], g => [0,4,8], h => [2,4,6]}
+#
+# forks = {0 => [[a,d], [d,g], [a,g]], 1 => [a,e], 2 => [[a,f], [a,h], [f,h]], 3 => [d,b], 4 => [b,e], 5 => [f,b], 6 => [[d,c], [c,h], [d,h]], 7 => [c,e], 8 => [[c,f], [f,g], [c,g]]}
 
 #                 D   E   F            G             H
 #             A | 0 | 1 | 2 | A         | 0 | 1 | 2 |
