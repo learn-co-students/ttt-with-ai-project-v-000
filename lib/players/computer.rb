bfff1eea29faed3685deddb0a13775bc0a16863e
mxdavis <md01@me.com>
2017-03-19 20:05:04 +0200

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,14 +2,14 @@ module Players
   class Computer < Player
 
     WIN_COMBINATIONS = [
-      [0,1,2], # Top row
-      [3,4,5],  # Middle row
-      [6,7,8],  # Bottom row
-      [0,3,6],  # Left Column
-      [1,4,7],  # Middle Column
-      [2,5,8],  # Right Column
-      [0,4,8],  # Diagnoal L to R
-      [2,4,6]  # Diagnoal R to L
+      [1,2,3], # Top row
+      [4,5,6],  # Middle row
+      [7,8,9],  # Bottom row
+      [1,4,7],  # Left Column
+      [2,5,8],  # Middle Column
+      [3,6,9],  # Right Column
+      [1,5,9],  # Diagnoal L to R
+      [3,5,7]  # Diagnoal R to L
     ]
 
     def move(board)
