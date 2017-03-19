fe978386b0cecc7b1952eb67bbfb881923e4f5ab
mxdavis <md01@me.com>
2017-03-19 22:58:19 +0200

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -12,50 +12,50 @@ module Players
       [2,4,6]  # Diagnoal R to L
     ]
 
-    @i = 0
 
     def move(board)
+      @i = 0
 
-      for_the_win
-      block_a_combo unless @i > 0
+      # for_the_win
+      # block_a_combo unless @i > 0
       # best_spots unless @i > 0
 
-      board.cells.detect do |c|
+      board.cells.find do |c|
         if c == " "
-          board.cells[@i] == self.token
+          # board.cells[@i] = self.token
           @i += 1
         end
       end
     "#{@i}"
     end
 
-    def for_the_win
-      WIN_COMBINATIONS.each do |combo|
-        if combo[0] == combo[1] && combo[2] == " " && combo[0] == self.token
-          @i = combo[2]
-        elsif combo[1] == combo[2] && combo[0] == " " && combo[0] == self.token
-          @i = combo[0]
-        elsif combo[0] == combo[2] && combo[1] == " " && combo[0] == self.token
-          @i = combo[1]
-        else
-          @i = 0
-        end
-      end
-    end
-
-    def block_a_combo
-      WIN_COMBINATIONS.each do |combo|
-        if combo[0] == combo[1] && combo[2] == " " && combo[0] != " "
-          @i = combo[2]
-        elsif combo[1] == combo[2] && combo[0] == " " && combo[0] != " "
-          @i = combo[0]
-        elsif combo[0] == combo[2] && combo[1] == " " && combo[0] != " "
-          @i = combo[1]
-        else
-          @i = 0
-        end
-      end
-    end
+    # def for_the_win
+    #   WIN_COMBINATIONS.each do |combo|
+    #     if combo[0] == combo[1] && combo[2] == " " && combo[0] == self.token
+    #       @i = combo[2]
+    #     elsif combo[1] == combo[2] && combo[0] == " " && combo[0] == self.token
+    #       @i = combo[0]
+    #     elsif combo[0] == combo[2] && combo[1] == " " && combo[0] == self.token
+    #       @i = combo[1]
+    #     else
+    #       @i = 0
+    #     end
+    #   end
+    # end
+    #
+    # def block_a_combo
+    #   WIN_COMBINATIONS.each do |combo|
+    #     if combo[0] == combo[1] && combo[2] == " " && combo[0] != " "
+    #       @i = combo[2]
+    #     elsif combo[1] == combo[2] && combo[0] == " " && combo[0] != " "
+    #       @i = combo[0]
+    #     elsif combo[0] == combo[2] && combo[1] == " " && combo[0] != " "
+    #       @i = combo[1]
+    #     else
+    #       @i = 0
+    #     end
+    #   end
+    # end
 
   # def best_spots
   #   [0, 2, 4, 6, 8].any? |i|
