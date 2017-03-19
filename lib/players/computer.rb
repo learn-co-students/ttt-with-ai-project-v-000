9c112faea7c324d88940cc011aafb1ec49acde11
Camille Jackson <cj@mail.co.de>
2017-03-19 18:45:18 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -19,61 +19,10 @@ module Players
           board.cells[i] == self.token
           i += 1
         end
-    
+
       end
     "#{i + 1}"
-      # WIN_COMBINATIONS.each do |combo|
-      #   two_token_combo = []
-      #   if (combo[0] == combo[1] && combo[2] == " ") || (combo[1] == combo[2] && combo[0] == " ") || (combo[0] == combo[2] && combo[1] == " ")
-      #     binding.pry
-      #     two_token_combo << combo
-      #     win_move
-      #   else
-      #     binding.pry
-      #     no_combos(board)
-      #   end
-      # end
     end
-
-    def no_combos(board)
-      if !board.taken?(5)
-        move = "5"
-      elsif board.turn_count == 2
-        move = [1, 3, 7, 9].detect do |i|
-          !board.taken?(i).to_s
-        end
-      end
-    end
-
-    def win_move
-      two_token_combo.each do |combo|
-        if (combo[0] || combo[1] || combo[2]) == self.token
-          combo.each do |cell|
-            position = nil
-            if cell == " "
-              position << cell
-              move = (position+1).to_s
-            end
-          end
-        else
-          block_move
-        end
-      end
-    end
-
-   def block_move
-     two_token_combo.each do |combo|
-      if (combo[0] || combo[1] || combo[2]) == !self.token
-        combo.each do |cell|
-          position = nil
-          if cell == " "
-            position << cell
-            move = (position+1).to_s
-          end
-        end
-      end
-    end
-  end
 
   end
 end
