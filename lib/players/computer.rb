ff1b58cf7797032f414ce180a9694978025e5314
mxdavis <md01@me.com>
2017-03-19 21:31:08 +0200

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -12,68 +12,88 @@ module Players
       [2,4,6]  # Diagnoal R to L
     ]
 
+    @i = 0
+
     def move(board)
-      i = 0
+      for_the_win
+      block_a_combo unless @i > 0
+
       board.cells.find do |c|
         if c == " "
-          board.cells[i] == self.token
-          i += 1
-        end
-    
-      end
-    "#{i + 1}"
-      # WIN_COMBINATIONS.each do |combo|
-      #   two_token_combo = []
-      #   if (combo[0] == combo[1] && combo[2] == " ") || (combo[1] == combo[2] && combo[0] == " ") || (combo[0] == combo[2] && combo[1] == " ")
-      #     binding.pry
-      #     two_token_combo << combo
-      #     win_move
-      #   else
-      #     binding.pry
-      #     no_combos(board)
-      #   end
-      # end
-    end
-
-    def no_combos(board)
-      if !board.taken?(5)
-        move = "5"
-      elsif board.turn_count == 2
-        move = [1, 3, 7, 9].detect do |i|
-          !board.taken?(i).to_s
+          board.cells[@i] == self.token
+          @i += 1
         end
       end
+    "#{@i}"
     end
 
-    def win_move
-      two_token_combo.each do |combo|
-        if (combo[0] || combo[1] || combo[2]) == self.token
-          combo.each do |cell|
-            position = nil
-            if cell == " "
-              position << cell
-              move = (position+1).to_s
-            end
-          end
+    def for_the_win
+      WIN_COMBINATIONS.each do |combo|
+        if combo[0] == combo[1] && combo[2] == " " && combo[0] == self.token
+          @i = combo[2]
+        elsif combo[1] == combo[2] && combo[0] == " " && combo[0] == self.token
+          @i = combo[0]
+        elsif combo[0] == combo[2] && combo[1] == " " && combo[0] == self.token
+          @i = combo[1]
         else
-          block_move
+          @i = 0
         end
       end
     end
 
-   def block_move
-     two_token_combo.each do |combo|
-      if (combo[0] || combo[1] || combo[2]) == !self.token
-        combo.each do |cell|
-          position = nil
-          if cell == " "
-            position << cell
-            move = (position+1).to_s
-          end
+    def block_a_combo
+      WIN_COMBINATIONS.each do |combo|
+        if combo[0] == combo[1] && combo[2] == " " && combo[0] != " "
+          @i = combo[2]
+        elsif combo[1] == combo[2] && combo[0] == " " && combo[0] != " "
+          @i = combo[0]
+        elsif combo[0] == combo[2] && combo[1] == " " && combo[0] != " "
+          @i = combo[1]
+        else
+          @i = 0
         end
       end
     end
-  end
+
+    # def no_combos(board)
+    #   if !board.taken?(5)
+    #     move = "5"
+    #   elsif board.turn_count == 2
+    #     move = [1, 3, 7, 9].detect do |i|
+    #       !board.taken?(i).to_s
+    #     end
+    #   end
+    # end
+
+    # def win_move
+    #   @two_token_combo.each do |combo|
+    #     if (combo[0] || combo[1] || combo[2]) == self.token
+    #       combo.each do |cell|
+    #         position = nil
+    #         if cell == " "
+    #           position << cell
+    #           move = (position+1).to_s
+    #         end
+    #       end
+    #     else
+    #       block_move
+    #     end
+    #   end
+    # end
+
+  #  def block_move
+  #    @two_token_combo.each do |combo|
+  #     if (combo[0] || combo[1] || combo[2]) == !self.token
+  #       combo.each do |cell|
+  #         position = nil
+  #         if cell == " "
+  #           position << cell
+  #           move = (position+1).to_s
+  #         end
+  #       end
+  #     end
+  #   end
+  # end
 
   end
 end
