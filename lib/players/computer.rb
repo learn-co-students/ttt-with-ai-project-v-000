107bedf328702414d9842453afc3d85396faab7b
Seth <itz.saga@gmail.com>
2017-03-20 21:17:04 -0500

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,12 +1,16 @@
 module Players
   class Computer < Player
     def move(board)
-      possible_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-      valid_moves = []
-      possible_moves.each do |move|
-        valid_moves << move if board.cells[move.to_i-1] == " "
+      if self.turn_count == 0
+        "1"
+      else
+        possible_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        valid_moves = []
+        possible_moves.each do |move|
+          valid_moves << move if board.cells[move.to_i-1] == " "
+        end
+        valid_moves.sample
       end
-      valid_moves.sample
     end
   end
 end
