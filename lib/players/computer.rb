1040c8fb89b14223b2433605b5e26f2c696022a7
Seth <itz.saga@gmail.com>
2017-03-20 21:07:20 -0500

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -4,7 +4,7 @@ module Players
       possible_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
       valid_moves = []
       possible_moves.each do |move|
-        valid_moves << move if board.cells[move.to_i-1] = " "
+        valid_moves << move if board.cells[move.to_i-1] == " "
       end
       valid_moves.sample
     end
