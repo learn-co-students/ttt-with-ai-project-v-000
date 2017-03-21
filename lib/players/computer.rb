f3a48672dd241b69c6882db103e4d284fd7663d6
Seth <itz.saga@gmail.com>
2017-03-20 21:42:27 -0500

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,8 +1,10 @@
 module Players
   class Computer < Player
     def move(board)
-      if self.turn_count == 0
+      if board.cells.count{|square| square != " " } == 0
         "1"
+      elsif board.cells.count{|square| square != " " } == 1
+        board.cells.find_index("X") == 4 ? "1" : "5"
       else
         possible_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
         valid_moves = []
@@ -14,3 +16,4 @@ module Players
     end
   end
 end
+# To continue my AI I'll take the WIN_COMBINATIONS and iterate over them to see if any 2 of the positions are == to the current_player's :token. If so play in the 3rd space from that array. If not, check to see if the other player's token has 2. then play the 3rd. If neither, play a random square.
