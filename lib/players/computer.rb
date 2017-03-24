3e2aaf065840a7e0ab912da3f55d5babf864b159
Kristi Thompson <kristiforpresident@gmail.com>
2017-03-24 19:57:14 +0000

    rudimentary ai, not smart but works

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,16 +1,14 @@
 module Players
-
   class Computer < Player
 
-    def move(board)
-      if board[4] == " "
+    def move(board = [])
+      if board.cells[4] == " "
         "5"
       else
-        move_cell = board.detect {|cell| cell == " "}
-        "#{board.find_index(move_cell).to_i + 1}"
+        move_cell = board.cells.detect {|cell| cell == " "}
+        "#{board.cells.find_index(move_cell) + 1}"
       end
     end
 
   end
-
 end
