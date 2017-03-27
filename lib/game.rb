8792bfa0339d16c30d28a05455857c2f320edcf5
Holger Paas <holger.paas@gmail.com>
2017-03-26 17:22:20 -0700

    First commit

---
+++ lib/game.rb
@@ -0,0 +1,70 @@
+require 'pry'
+class Game
+
+	attr_accessor :board, :player_1, :player_2
+	extend Players
+
+	WIN_COMBINATIONS = [
+    [0,1,2],[3,4,5],[6,7,8],
+    [0,3,6],[1,4,7],[2,5,8],
+    [0,4,8],[2,4,6]]
+
+    def initialize(player_1 = Players::Human.new('X'), player_2 = Players::Human.new('O'), board=Board.new)
+    	@player_1 = player_1
+    	@player_2 = player_2
+    	@board = board
+    end
+
+    def current_player
+    	count = self.board.turn_count
+    	return self.player_1 if count.even?
+    	return self.player_2 if count.odd?
+    end
+
+    def over?
+    	self.board.full? || self.won?
+    end
+
+    def won?
+    	WIN_COMBINATIONS.detect do |array|
+      		self.board.cells[array[0]] == self.board.cells[array[1]] && self.board.cells[array[1]] == self.board.cells[array[2]] && (self.board.cells[array[0]]=='X' ||self.board.cells[array[0]]=='O')
+    	end
+    end
+
+    def draw?
+    	self.over? && !self.won?
+    end
+
+    def winner
+    	if self.won?
+      		array_won = self.won?
+      		return self.board.cells[array_won[0]]
+    	end
+    end
+
+    def turn 
+    	puts "Please enter 1-9 for your next move:"
+    	binding.pry
+    	user_input = gets.chomp
+    	#binding.pry
+
+    	if self.board.valid_move?(user_input)
+     		self.board.update(user_input, self.current_player)
+    	# else
+    	# 	self.turn
+    	end
+    end
+
+    def play
+    	until self.over?
+    		puts "Please enter 1-9 for your next move:"
+      		self.turn
+    	end
+    	if self.won?
+      		puts "Congratulations #{self.winner}!"
+    	else
+      		puts "Cat's Game!"
+    	end
+    end
+
+end
---
+++ lib/players/human.rb
@@ -0,0 +1,13 @@
+#require_relative '../player.rb'
+
+module Players
+	class Human < Player
+
+		def move(board)
+			user_input = gets.chomp
+			puts user_input
+			user_input
+		end
+
+	end
+end
