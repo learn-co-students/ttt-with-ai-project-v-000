71e7e6b8f9344c9a2ea9ef79658a050374f13c8d
Michael Ries <michael.ries@gmail.com>
2017-04-08 20:12:32 +0000

    fix multiple type errors

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,22 +1,28 @@
+require('pry')
+
 module Players
   class Computer < Player
     def move(board)
       valid_moves = ["1","2","3","4","5","6","7","8","9"].select {|m| board.valid_move?(m)} # only check valid moves
-      scores = valid_moves.collect {|i|  [i,score_move(i)]}
-      sorted = scores.sort {|i,j| i[1] <=> j[1]} # sort the scores by score
+      scores = valid_moves.collect {|i|  [i,score_move(i,board)]}
+      sorted = scores.sort {|i,j| j[1] <=> j=i[1]} # sort the scores by score
       sorted[0][0] # return the move with the highest score
     end
 
-    def score_move(position)
+    def score_move(position,board)
       score = 0
-      relevant_combos = Game::WIN_COMBINATIONS.select{|c| c.include?(position)}
-      relevant_combos.each {|combo| score+=score_combo(combo)}
+      relevant_combos = Game::WIN_COMBINATIONS.select{|c| c.include?(position.to_i-1)}
+      relevant_combos.each do |combo| 
+        tokens=["#{board.cells[combo[0]]}", "#{board.cells[combo[1]]}", "#{board.cells[combo[2]]}"]
+        score+=score_combo(tokens)
+      end
+      score
     end
 
-    def score_combo(combo)
-      mine = combo.count(self.token)
-      theirs = combo.count(self.opponent_token)
-      blanks = combo.count(" ")
+    def score_combo(tokens)
+      mine = tokens.count(self.token)
+      theirs = tokens.count(self.opponent_token)
+      blanks = tokens.count(" ")
       if blanks==0  then return 0
       elsif mine==2 then return 5
       elsif theirs==2 then return 4
