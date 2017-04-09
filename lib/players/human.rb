f9dcb9db857d923c399463483f72517f38db2540
charlie lee <ctlee9096@gmail.com>
2017-04-09 18:16:08 +0000

    Done.

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,7 +1,9 @@
-class Human < Player
-  #https://www.ruby-forum.com/topic/113558
-  def move(user_input)
-  user_input = gets
-  "#{user_input}"
+module Players
+  class Human < Player
+    #https://www.ruby-forum.com/topic/113558
+    def move(user_input)
+    user_input = gets
+    "#{user_input}"
+    end
   end
 end
