cbe9be93b95523aff992dbd4a1b3f82b74eaa4b7
Kaisa Piipari <kaisa.piipari@gmail.com>
2017-04-10 01:19:19 +0100

    Update on AI logic. Work in progress.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,90 +1,81 @@
 module Players
+
+  class Integer
+    N_BYTES = [42].pack('i').size
+    N_BITS = N_BYTES * 8
+    MAX = 2 ** (N_BITS - 2) - 1
+    MIN = -MAX - 1
+  end
+
   class Computer < Player
 
     attr_reader :board, :game
-    attr_accessor :x_player_index, :o_player_index, :free_cells
+    attr_accessor :x_player_index, :o_player_index
 
     def initialize(token)
       super(token)
-      @x_player_index = []
-      @o_player_index = []
-      @free_cells = []
     end
 
-    def free_cell_indices
-      #Returns an array of board indices (0-8) that are free (no X or O)
-      (0..board.cells.length).flat_map do |i|
-        board.taken?(i + 1) ? [] : [i]
-      end
+    def opponent_token
+      self.player.token == "X" ? "O" : "X"
     end
 
-    def board_index_for_token(token)
-      if token == "X"
-        @x_player_index
-      elsif token == "O"
-         @o_player_index
-      end
-    end
+    def best_move
+      # Returns the number of winning opportunites for each board cell.
+      # Depends on 'min_distance' i.e. how many steps each cell is from a win, and
+      # How many different routes, 'opportunity_count' can be taken a given cell that can provide a win
+      # Smaller distance, less routes is better than many routes with equal distances.
+  
+      board.cells.map do |i|
+        #Integer::Max == Infinity
+        return Integer::MAX if board.cells[i] != ' '
 
-    def combination_with_free_cells
-      #iterates over WIN_COMBINATIONS to access each winning combination
-      game.WIN_COMBINATIONS.each do |combinations|
-        #iterates over each winning combinations, selects and returns those combinations
-        #that include free cell indices from free_cell_indices method
-        combinations.select do |combination|
-          free_cell_indices.each { |i| combination.include?(i)}
-        end
-      end
-    end
-
-    def one_from_winning(token)
-      one_from_win = []
-      game.WIN_COMBINATIONS.each do |combination|
-        one_from_win << (combination - board_index_for_token(token)).count == 1
-      end 
-    end
-
-        
-
-    def free_cells
-      game.WIN_COMBINATIONS.detect do |combination|
-        combo_chars = combination.map { |i| board.cells[i] }
-        combo_chars.all? { |c| c == " " } 
-      end
-    end
-
-    def winning_route(token)
-      combination_with_free_cells
-
-    def cell_indices
-      #Returns an array of board indices (0-8) that are free (no X or O)
-      board.cells.each_with_index do |c,i|
-        if board.taken?(i + 1) && c == "X"
-           @x_player_index << i
-        elsif board.taken?(i + 1) && c == "O"
-          @o_player_index << i
-        elsif !board.taken?
-          @free_cells << i
-        else
-          nil
-        end
-      end
-    end
-
+        scores_for_i = Game::WIN_COMBINATIONS.map do |win_combo| 
+          return Integer::MAX if not win_combo.include?(i)
    
+          score = 0
+          combo_cells = board.cells.values_at(*win_combo)
+          for cell in combo_cells
+            return Integer::MAX if cell == opponent_token
+            score += cell == ' ' ? 1 : 0
+          end
+          score
+        end
+
+        min_distance = scores_for_i.min
+        opportunity_count = scores_for_i.filter { |s| s == min_distance }.count
+        cell_index = i
+       
+        return Move.new(min_distance, opportunity_count, cell_index)
+      end
+    end
+
+    def game_tactic
+      # Determines whether the next move is to attack or block the opponent
+      if best_move
+    end
+
+    def move(board)
+      game_tactic
+    end
 
 
-    
+      
+    class Move 
 
-    def one_step_from_winning(token)
-      combination_with_free_cells
+      attr_accessor :distance, :opportunity_count
 
-    
+      def initialize(distance, opportunity_count, cell_index)
+        @distance = distance
+        @opportunity_count = opportunity_count
+        @cell_index = cell_index
+      end
+    end
 
-
-  end
 end
 
+    
+    
 
 
   
