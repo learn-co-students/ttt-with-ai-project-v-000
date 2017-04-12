d5bdc9297829b408420a51f1128b6777adf43f92
antley chu <antley.chu@gmail.com>
2017-04-12 15:20:35 +0000

    semi smart

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,10 +1,29 @@
 require_relative "../player.rb"
 module Players
   class Computer < Player
-
+    attr_accessor :board
     def move(board)
       @board = board
-      move = rand(1..9).to_s
+      move = self.smart_choices.to_s
+    end
+
+    def smart_choices
+      turn = self.board.turn_count
+      case turn
+      when 0
+          5
+      when 1
+        corners = [1,3,7,9]
+        index = rand(0..3)
+        corners[index]
+      when 2
+        corners = [1,3,7,9]
+        index = rand(0..3)
+        corners[index]
+      else
+        rand(1..9)
+      end
+
 
     end
   end
