77f2238f063641b15fe6dba79aac7a61bd9a48af
William Barela <william.barela.dev@gmail.com>
2017-04-14 18:39:55 +0000

    notes to self about improvement of AI

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,6 +1,11 @@
 module Players
 
   class Computer < Player
+    # This is a very dumb implementation of the AI.
+    # Improve by using Minimax Algorithm with Alpha-Beta Pruning
+    # This implementation of minimax should be recursive.
+    # Remember for your scoring, on the first move, corners > center > edge moves
+    # Then, rank according to Win, Block, Fork, Block Fork, etc.
 
     def move(board=[])
       computer_move = rand(1..9).to_s
