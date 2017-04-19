5fa8811d0a7f9aca40df27f50b2dbe2a618da654
github username <github email address>
2017-04-20 03:53:21 +0900

    Modified some parts of Players.rb

--- lib/Players.rb
+++ lib/Players.rb
@@ -1,36 +1,41 @@
+require 'pry'
 class Players
   class Human < Player
-     def move(board)
-       gets.chomp
-     end
+    def move(board)
+      gets.chomp
+    end
   end
 
   class Computer < Player
-    def strategy
-      available_moves = []
-      pos_taken = []
-      board.cells.each.with_index do |val,index|
-              if val != "O" || val != "X"
-                available_moves << index
-              end #checks which spots are available
-              if val == self.token
-                  pos_taken << index
-              end #indexes where the computer's token occupies
-           end
-        board.WIN_COMBINATIONS.detect.with_index do |x,i|
-             x[0] == self.token && self.token == x[1] && available_moves.include?(i)
-             #i want to use the WIN_COMBINATIONS array to detect our next best move to win.
-             #checking if the token is occupying two out of three winning spots, and hopefully returning the move
-        end
-      end
-     def move(board)
-        if board.turn_count <= 5
-          Rand.rand(1..10)
-          #make the first few moves random then strategize the remaining moves
-        else
-           strategy #this method is in progress
+    attr_accessor :available_moves, :pos_taken, :board
+
+    def move(board)
+      @board = board #we can access to this board in any function with self.board_Niki
+      board.turn_count <=5 ? Random.rand(1..10) : self.strategy
+    end
+ # maybe it would be better to divide this function (strategy -> search_moves + strategy)_Niki
+    def search_moves
+      self.available_moves = []
+      self.pos_taken = []
+      self.board.cells.each.with_index do |val,index|
+        if val == " "
+          self.available_moves << index
+        elsif val == self.token
+          self.pos_taken << index
         end
       end
     end
 
-end#end of players
+    def strategy
+      self.board.WIN_COMBINATIONS.each do |win_combination|
+        win_combination.detect.with_index do |x,i|
+          x[0] == self.token && self.token == x[1] && self.available_moves.include?(i)
+        end
+      end
+    end
+        #i want to use the WIN_COMBINATIONS array to detect our next best move to win.
+        #checking if the token is occupying two out of three winning spots, and hopefully returning the move
+
+        #I think we should use yield twice because the WIN_COMBINATIONS is nested array. However, the IDEA is GREAT! _Niki
+  end
+end
