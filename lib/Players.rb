89ccbab7901a03ee0f066f306f66c4dcf5a46889
Aditi Green <asaki444@gmail.com>
2017-04-20 03:35:04 +0000

    adjusted the move method, now it works in the cli :-)

--- lib/Players.rb
+++ lib/Players.rb
@@ -10,8 +10,10 @@ class Players
     attr_accessor :available_moves, :pos_taken, :board
 
     def move(board)
-      @board = board   #we can access to this board in any function with self.board_Niki
-      board.turn_count <=5 ? Random.rand(0..9) : self.strategy
+      #we can access to this board in any function with self.board_Niki
+
+      board.turn_count <= 5 ? Random.rand(0..9).to_s : self.strategy
+
     end
  # maybe it would be better to divide this function (strategy -> search_moves + strategy)_Niki
     def search_moves
@@ -29,7 +31,8 @@ class Players
     def strategy
       self.board.WIN_COMBINATIONS.each do |win_combination|
         win_combination.detect.with_index do |x,i|
-          x[0] == self.token && self.token == x[1] && self.available_moves.include?(i)
+          x == self.token && self.available_moves.include?(x)
+          #wouldnt a nested detect would give us a single index at a time. so I just compared x to the token, and asked if the spot is available.
         end
       end
     end
@@ -37,5 +40,8 @@ class Players
         #checking if the token is occupying two out of three winning spots, and hopefully returning the move
 
         #I think we should use yield twice because the WIN_COMBINATIONS is nested array. However, the IDEA is GREAT! _Niki
+
+  #binding.pry
   end
+
 end
