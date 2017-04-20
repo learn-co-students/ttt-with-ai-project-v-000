ce2dcdbd18299982b5a4054319307e4cdbf2dd0d
Alexander Friedland <friedlandaj@gmail.com>
2017-04-20 02:00:47 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,4 +1,38 @@
+module Players
+  class Computer < Player
+    def move(board)
+      move = nil
 
+      if board.cells[4] == " "
+        move = "5"
+
+      elsif #going second and they took middle, or they went first and didn't take middle, take a corner
+        board.cells.count{|i| i != " "} == 1
+        move = [1, 3, 7, 9].detect{|i| !board.taken?(i)}.to_s
+
+      else
+        Game::WIN_COMBINATIONS.detect do |cmb|
+
+          # First, check whether you have any chances to win, since it doesn't matter whether the opponent has a chance to win if you can win first.
+          if cmb.select{|i| board.position(i+1) == token}.size == 2 && cmb.any?{|i| board.position(i+1) == " "}
+            move = cmb.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
+
+          # If you can't play any winning moves, play a move to block the opponent from winning.
+          elsif cmb.select{|i| board.position(i+1) != " " && board.position(i+1) != token}.size == 2 && cmb.any?{|i| board.position(i+1) == " "}
+            move = cmb.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
+          end
+        end
+
+        # If none of the WIN_COMBINATIONS patterns have two squares taken by the same token and a third empty square, play into the first open square you find, first checking corners and then checking sides.
+        move = [1, 3, 7, 9, 2, 4, 6, 8].detect{|i| !board.taken?(i)}.to_s if move == nil
+      end
+      move
+    end
+  end
+end
+
+
+=begin
 module Players
   class Computer < Player
 
@@ -50,3 +84,4 @@ module Players
     end
   end
 end
+=end
