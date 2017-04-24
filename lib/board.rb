c1abcd37adf20dd6ebbfd4cf284f7c1ae310dd00
Liliana Alonzo <lillyneveu@gmail.com>
2017-04-24 21:42:47 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -39,14 +39,4 @@ class Board
     return counter
   end
 
-  #def turn_count(board)
-    # counter = 0
-    #     board.each do |turn|
-    #     if turn == "X" || turn == "O"
-    #     counter += 1
-    #       end
-    #   end
-    #   return counter
-  # end
-
 end
