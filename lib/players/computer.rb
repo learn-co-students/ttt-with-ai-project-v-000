6a319824deedd4ff2c02146bd35f5d5fe3a6f840
Liliana Alonzo <lillyneveu@gmail.com>
2017-04-26 23:04:34 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,19 +1,14 @@
 module Players
   class Computer < Player
-    attr_accessor :board, :cells
+    attr_reader :board, :cells
 
     def move(board)
-      first_move
-    end
-
-    def first_move
       if board.cells[0] == " " && board.cells[4] == " "
         "1"
       else board.cells[4] == "0"
         "5"
       end
     end
-
 
   end
 end
