fa73cab6b0288097d2fc3929ac2f36c08e7f0146
Liliana Alonzo <lillyneveu@gmail.com>
2017-04-26 22:09:17 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,7 +2,14 @@ module Players
   class Computer < Player
 
     def move(board)
-      
+
+    end
+
+    def first_move
+      if board.cells[0] == " " && board.cells[4] == " "
+        "1"
+      else
+        
     end
 
 
