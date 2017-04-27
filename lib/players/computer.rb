b8622fb91b9bf232bca97ecf0593dc4b872e6d9e
Liliana Alonzo <lillyneveu@gmail.com>
2017-04-27 00:10:12 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,20 +1,21 @@
 module Players
   class Computer < Player
-    attr_reader :board, :cells, :won?
+    attr_reader :board, :cells
 
     def move(board)
-      if board.cells[0] == " " && board.cells[4] == " "
-        "1"
-      elsif board.cells[4] == " "
-        "5"
-      elsif board.cells[2] == " "
-        "3"
-      elsif board.cells[6] == " "
-        "7"
-      elsif board.cells[8] == " "
-        "9"
+      until over? do
+        if board.cells[0] == " " && board.cells[4] == " "
+          "1"
+        elsif board.cells[4] == " "
+          "5"
+        elsif board.cells[2] == " " && !won?
+          "3"
+        elsif board.cells[6] == " "
+          "7"
+        elsif board.cells[8] == " "
+          "9"
+        end
       end
-      # game.won?
     end
 
   end
