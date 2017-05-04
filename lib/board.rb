419d285740f2cda089b8bba1ce14a51024942033
Nick Bair <nickbair@golfingbuddha.com>
2017-05-04 15:46:26 -0400

    Board spec passing minus #update.

---
+++ lib/board.rb
@@ -0,0 +1,52 @@
+class Board
+  attr_accessor :cells
+
+  def initialize
+    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+
+  def reset!
+    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+
+  def display
+    row1 = " #{cells[0]} " + "|" + " #{cells[1]} " + "|" + " #{cells[2]} "
+    row2 = " #{cells[3]} " + "|" + " #{cells[4]} " + "|" + " #{cells[5]} "
+    row3 = " #{cells[6]} " + "|" + " #{cells[7]} " + "|" + " #{cells[8]} "
+    line = "-----------"
+
+    puts row1
+    puts line
+    puts row2
+    puts line
+    puts row3
+  end
+
+  def input_to_index(user_input)
+    user_input.to_i - 1
+  end
+
+  def position(i)
+    cells[input_to_index(i)]
+  end
+
+  def taken?(i)
+    cells[input_to_index(i)] == " " ? false : true
+  end
+
+  def full?
+    !cells.include?(" ")
+  end
+
+  def turn_count
+    9 - cells.count { |x| x == " " }
+  end
+
+  def valid_move?(m)
+    !taken?(m) && input_to_index(m).between?(0, 8)
+  end
+
+  def update(position, player)
+    valid_move?(position) ? cells[input_to_index(position)] == player : nil
+  end
+end
