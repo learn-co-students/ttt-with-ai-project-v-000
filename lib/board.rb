1c75426568f57e2d5ddc9e0b4c565ac0b6315726
Zain <zain@halai.us>
2017-05-04 20:24:39 -0500

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,47 @@
+class Board
+	attr_accessor :cells
+
+	def initialize
+		reset!
+	end
+
+	def display
+		puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
+		puts "-----------"
+		puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
+		puts "-----------"
+		puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "		
+	end
+
+	def input_to_index(input)
+		input.to_i - 1
+	end
+
+	def position(input)
+		cells[input_to_index(input)]
+	end
+
+	def full?
+		cells.all? { |cell| cell == 'X' || cell == 'O' }
+	end
+
+	def turn_count
+		cells.count { |element| element == 'X' || element == 'O' }
+	end
+
+	def taken?(input)
+		position(input) != ' '
+	end
+
+	def valid_move?(input)
+		input.to_i.between?(1, 9) && !taken?(input)
+	end
+
+	def update(input, player)
+		cells[input_to_index(input)] = player.token
+	end
+
+	def reset!
+		self.cells = Array.new(9, " ")
+	end
+end
---
+++ lib/game.rb
@@ -0,0 +1,57 @@
+class Game
+
+	attr_accessor :board, :player_1, :player_2
+
+	WIN_COMBINATIONS = [
+    [0, 1, 2],  # Top row
+    [3, 4, 5],  # Middle row
+    [6, 7, 8],  # Bottom row
+    [0, 3, 6],  # Left column
+    [1, 4, 7],  # Middle column
+    [2, 5, 8],  # Right column
+    [0, 4, 8],  # Left diagonal
+    [6, 4, 2]   # Right diagonal
+	]
+
+	def initialize(player_1 = nil, player_2 = nil, board = nil)
+		@player_1 = player_1 || Players::Human.new('X')
+		@player_2 = player_2 || Players::Human.new('O')
+		@board = board || Board.new 
+	end
+
+	def current_player
+		board.turn_count.even? ? player_1 : player_2
+	end
+
+	def over?
+		board.full?
+	end
+
+	def won?
+		WIN_COMBINATIONS.detect do |combination|
+			board.taken?(combination[0]) &&
+			board.cells[combination[0]] == board.cells[combination[1]] &&
+			board.cells[combination[1]] == board.cells[combination[2]]
+		end
+	end
+
+	def draw?
+		!won? && over?
+	end
+
+	def winner
+		board.cells[won?[0]] if won?
+	end
+
+	def turn
+		puts 'test'
+		input = gets.chomp
+
+		if board.valid_move?(input)
+			board.update(input, current_player)
+		else
+			turn
+		end
+	end
+
+end
