f96007a47059c6a72f7a5bd4cddc3cb0acbb879b
Saad Ahmed <dropheaven@live.com>
2017-05-08 21:52:18 -0400

    Update game.rb

--- lib/game.rb
+++ lib/game.rb
@@ -10,9 +10,7 @@ class Game
 
   def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
     @player_1 = player_1
-    player_1.current_board = board
     @player_2 = player_2
-    player_2.current_board = board
     @board = board
   end
 
