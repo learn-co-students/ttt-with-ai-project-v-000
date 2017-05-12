18d00374369f3cd3d1d585dce66c852127f2c0e6
Amanda George <github email address>
2017-05-12 07:25:34 -0500

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -6,13 +6,15 @@ module Players
     attr_reader :playermove
 
     def move(board)
-      board.cells.each do |space|
-        if space.valid_move?
-          valid_moves << space.index.to_str
-        end
-      end
-      binding.pry
-      player_move = valid_moves
+      board[(board.size*rand).floor]
+
+      # board.cells.each do |space|
+      #   if space.valid_move?
+      #     valid_moves << space.index.to_str
+      #   end
+      # end
+      # binding.pry
+      # player_move = valid_moves
       #   if space = " "
       #     avail << space.index.to_str
       #   end
