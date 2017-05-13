ed2ccffcfb2b01ebc0d656e1790a0ae96bd8c0fa
Sean Quillen <sean.r.quillen@gmail.com>
2017-05-13 15:15:54 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,47 @@
+# ttt-with-ai-project-v-000
+
+class Board
+
+  attr_accessor :cells
+
+  def initialize
+    @cells = Array.new(9, " ")
+  end
+
+  def reset!
+    @cells = Array.new(9, " ")
+  end
+
+  def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
+
+  def position(input)
+    @cells[input.to_i - 1]
+  end
+
+  def full?
+    @cells.include?(" ") || @cells.include?("") ? false : true
+  end
+
+  def turn_count
+    @cells.count("X") + @cells.count("O")
+  end
+
+  def taken?(input)
+    position(input) == "X" || position(input) == "O"
+  end
+
+  def valid_move?(input)
+    input.to_i.between?(1,9) && !taken?(input)
+  end
+
+  def update(input, player)
+     cells[input.to_i-1] = player.token
+  end
+  
+end
