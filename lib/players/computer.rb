56a8044e0c99e2b4bf972859024d20b2139c0e5f
Michael Mirzakandov <mikemirzak@aol.com>
2017-05-15 15:57:59 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -5,20 +5,23 @@ module Players
        [0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,5,8],[2,4,6],[3,4,5],[6,7,8]
      ]
 
-
-
-    def move(board)
-      combo2 = WIN_COMBINATIONS.detect do |combo|
-        board.cells[combo[0]] == board.cells[combo[1]] && board.taken?(combo[0]+1) ||
-        board.cells[combo[1]] == board.cells[combo[2]] && board.taken?(combo[1]+1) ||
-        board.cells[combo[0]] == board.cells[combo[2]] && board.taken?(combo[0]+1)
-      end
-      if combo2 != nil
-        input = combo2.detect {|space| space == " "}
-      else
-        input = @@valid_moves.sample
+      def move(board)
+        combo2 = WIN_COMBINATIONS.detect do |combo|
+          board.cells[combo[0]] == board.cells[combo[1]] && board.taken?(combo[0]) ||
+          board.cells[combo[1]] == board.cells[combo[2]] && board.taken?(combo[1]) ||
+          board.cells[combo[0]] == board.cells[combo[2]] && board.taken?(combo[0])
+        end
+        if combo2 != nil
+          blocker = combo2.map {|index| board.cells[index]}
+          index = blocker.find_index(" ")
+           if index != nil
+            index2 = combo2[blocker.find_index(" ")]
+            index2 +=1
+           end
+        else
+          @@valid_moves.sample
+        end
       end
     end
-  end
 
 end
