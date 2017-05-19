f500da5719355738804226ef9eef0d218b83a220
Travis Cole <iamtraviscole@gmail.com>
2017-05-19 21:12:48 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -6,9 +6,9 @@ module Players
       if !board.taken?(5)
         move = "5"
       elsif board.turn_count == 1 || board.turn_count == 2 || board.turn_count == 3 || board.turn_count == 4
-        move = detect[1, 3, 7, 9] {|i| !board.taken?(i)}.to_s
+        move =[1, 3, 7, 9].detect {|i| !board.taken?(i)}.to_s
       elsif board.turn_count == 5 || board.turn_count == 6 || board.turn_count == 7 || board.turn_count == 8
-        move = detect[2, 4, 6, 8] {|i| !board.taken?(i)}.to_s
+        move = [2, 4, 6, 8].detect {|i| !board.taken?(i)}.to_s
       end
       move
     end
