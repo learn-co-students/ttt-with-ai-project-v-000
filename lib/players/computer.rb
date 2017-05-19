e9c7b55321e5753df7b986c073564f01bab51915
CLackrone <charisaanne@gmail.com>
2017-05-19 10:33:35 -0400

    working AI in Computer class

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -4,33 +4,27 @@ module Players
 
 	class Computer < Player
 
-		attr_accessor :board, :winning_moves
+		attr_accessor :board 
 
 		VALID_MOVES = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-
-#by exposing winning_moves, once all methods can see it, I can check from anywhere. Write move logic in move. 
-
 
 		def move(board)
 			if board.cells.all? {|i| i == " "}
 				"5"
-			elsif win_options?(board)
-				ai_move = self.winning_moves[0]+1
+				#if #win_options returns an array with at least one possible move, player moves to that space
+			elsif win_options(board).count >=1
+				ai_move = win_options(board)[0]+1
 				ai_move.to_s
-			elsif winning_moves.count == 0
+				#if #win_options returns an empty array, make a random move based on valid moves
+			elsif win_options(board).count == 0
 				VALID_MOVES.sample
 			end
 		end
 
-		def winning_moves
-			@winning_moves 
-		end
-
-
-		def win_options?(board)
-			#winning_moves = []
+		def win_options(board)
+			winning_moves = []
+			#If 2/3 cells in a given combo are occupied by player and 3rd cell is empty, add cell to array of winning moves
 			Game::WIN_COMBINATIONS.each do |combo|
-				#puts "*** SELF IS -> " + self.token.to_s
 				if board.cells[combo[0]] == self.token.to_s && board.cells[combo[1]] == self.token.to_s && board.cells[combo[2]] == " "
 					winning_moves << combo[2]
 				elsif board.cells[combo[1]] == self.token.to_s && board.cells[combo[2]] == self.token.to_s && board.cells[combo[0]] == " "
@@ -39,18 +33,10 @@ module Players
 					winning_moves << combo[1]
 				end
 			end
-			if self.winning_moves.count >=1
-				true
-			end
+			#returns array 
+			winning_moves
 		end
 
-
-#board_empty method?
-		
-
-	
 	end
-
-
 
 end
