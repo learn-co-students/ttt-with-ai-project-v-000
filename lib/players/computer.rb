61f5547384622cfde7650cc1949d5f1720decd80
Travis Cole <iamtraviscole@gmail.com>
2017-05-19 20:32:11 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,9 +2,16 @@ module Players
   class Computer < Player
 
     def move(board)
-      rand(1..9).to_s
+      move = nil
+      if !board.taken?(5)
+        move = "5"
+      elsif board.turn_count == 1 || board.turn_count == 2 || board.turn_count == 3 || board.turn_count == 4
+        move = detect[1, 3, 7, 9] {|i| !board.taken?(i)}.to_s
+      elsif board.turn_count == 5 || board.turn_count == 6 || board.turn_count == 7 || board.turn_count == 8
+        move = detect[2, 4, 6, 8] {|i| !board.taken?(i)}.to_s
+      end
+      move
     end
-
 
   end
 end
