b63dff5b0830d80fe7ab780ed3c24e3c65b75420
hyoyou <heatherhyou@gmail.com>
2017-05-20 11:59:41 -0400

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -9,10 +9,6 @@ class Game
     [0, 4, 8], [2, 4, 6]
   ]
 
-  #player_1 = Players::Human.new("X")
-  #player_2 = Players::Human.new("O")
-  #board = Board.new
-
   def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
     @player_1 = player_1
     @player_2 = player_2
@@ -20,7 +16,7 @@ class Game
   end
 
   def current_player
-    board.turn_count.even? ? player_1 : player_2
+    board.turn_count % 2 == 0 ? player_1 : player_2
   end
 
   def won?
@@ -41,7 +37,6 @@ class Game
   end
 
   def winner
-    #current_player if won?
     if won?
       win_combo = WIN_COMBINATIONS.detect do |combo|
         @board.cells[combo[0]] == @board.cells[combo[1]] &&
