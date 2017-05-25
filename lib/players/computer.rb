59627c57827b4caab345fce27f3adba74137b8b3
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2017-05-25 16:42:59 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -4,7 +4,7 @@ class Players::Computer < Player
 
     valid_move = ""
       number = Random.new
-      input = number.rand(9)
+      input = number.rand(10)
     #  empty_board =  board.cells.all?{|c| c == " "}
       if board.valid_move?(input) && board.valid_input?(input)
         valid_move.clear
@@ -14,29 +14,55 @@ class Players::Computer < Player
         valid_move.clear
         valid_move << 5.to_s
       end
-      if board.turn_count == 1 || board.turn_count == 2 && !board.cells[5].include?("X" || "O")
+      if board.turn_count < 1 && !board.cells[5].include?("X" || "O") && board.valid_move?(input) && board.valid_input?(input)
         valid_move.clear
         valid_move << 5.to_s
       end
 
-      # combinations = Game::WIN_COMBINATIONS
-      # def find_target
-      #   if board.turn_count > 2
-      #     combinations.find do |combo|
-      #       combo.values_at(0, 1, 2) != [" ", " ", " "]
-      #     end #find
-      #   end #if
-      # end #find_target
-      #
-
-        if board.turn_count > 2 && board.cells[0] != " " && board.cells[1] != " "
-          valid_move.clear
-          valid_move << 3.to_s
-        end
-        if board.turn_count > 2 && board.cells[0] != " " && board.cells[3] != " "
-          valid_move.clear
-          valid_move << 7.to_s
-        end
+      if board.turn_count >= 3 && board.cells[0] != " " && board.cells[1] != " " &&  board.cells[2] == " " && board.valid_input?(input)
+        valid_move.clear
+        valid_move << 3.to_s
+      end
+      if board.turn_count >= 3 && board.cells[0] != " " && board.cells[3] != " " &&  board.cells[6] == " " && board.valid_input?(input)
+        valid_move.clear
+        valid_move << 7.to_s
+      end
+      if board.turn_count >= 3 && board.cells[8] != " " && board.cells[5] != " " &&  board.cells[2] == " " && board.valid_input?(input)
+        valid_move.clear
+        valid_move << 3.to_s
+      end
+      if board.turn_count >= 3 && board.cells[8] != " " && board.cells[7] != " " &&  board.cells[6] == " " && board.valid_input?(input)
+        valid_move.clear
+        valid_move << 7.to_s
+      end
+      if board.turn_count >= 3 && board.cells[8] != " " && board.cells[3] != " " &&  board.cells[5] == " " && board.valid_input?(input)
+        valid_move.clear
+        valid_move << 6.to_s
+      end
+      if board.turn_count >= 3 && board.cells[8] != " " && board.cells[6] != " " &&  board.cells[7] == " " && board.valid_input?(input)
+        valid_move.clear
+        valid_move << 8.to_s
+      end
+      if board.turn_count >= 3 && board.cells[6] != " " && board.cells[7] != " " &&  board.cells[8] == " " && board.valid_input?(input)
+        valid_move.clear
+        valid_move << 9.to_s
+      end
+      if board.turn_count >= 3 && board.cells[6] != " " && board.cells[5] != " " &&  board.cells[8] == " " && board.valid_input?(input)
+        valid_move.clear
+        valid_move << 9.to_s
+      end
+      if board.turn_count >= 3 && board.cells[0] != " " && board.cells[4] != " " &&  board.cells[8] == " " && board.valid_input?(input)
+        valid_move.clear
+        valid_move << 9.to_s
+      end
+      if board.turn_count >= 3 && board.cells[0] != " " && board.cells[8] != " " &&  board.cells[4] == " " && board.valid_input?(input)
+        valid_move.clear
+        valid_move << 5.to_s
+      end
+      if board.turn_count >= 3 && board.cells[8] != " " && board.cells[4] != " " &&  board.cells[0] == " " && board.valid_input?(input)
+        valid_move.clear
+        valid_move << 1.to_s
+      end
 
   #     binding.pry
 
