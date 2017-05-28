d9db93b66b0893b8a2d3d81930fc72f1acc2c099
Joe Kim <josephkim0224@gmail.com>
2017-05-28 16:26:46 -0400

    Implemented unbeatable AI

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -15,10 +15,10 @@ module Players
     end
 
     def choose_move(board)
-      chance = 65 if difficulty == "1"
-      chance = 85 if difficulty == "2"
+      chance = 80 if difficulty == "1"
+      chance = 90 if difficulty == "2"
       chance = 100 if difficulty == "3"
-      chance <= Random.rand(100) + 1 ? poor_move(board) : better_move(board)
+      chance < Random.rand(100) + 1 ? poor_move(board) : better_move(board)
     end
 
     def poor_move(board)
@@ -50,6 +50,10 @@ module Players
       #pick middle if possible
       cell = 4 if !cell && board.cells[4] == " "
 
+      #look for common patterns
+      cell = look_for_common_patterns(board) if !cell && board.turn_count == 3
+
+
       #pick corners next if possible
       cell = 0 if !cell && board.cells[0] == " "
       cell = 2 if !cell && board.cells[2] == " "
@@ -64,6 +68,31 @@ module Players
     end
 
     private
+
+    def look_for_common_patterns(board)
+      cells = []
+      board.cells.each_with_index { |e, i| cells << i if e != " " }
+      cell = nil
+      cell = 1 if cells == [0,4,8] && board.cells[4] == 'O'
+      cell = 2 if cells == [0,4,8] && board.cells[4] == 'X'
+      cell = 5 if cells == [2,4,6] && board.cells[4] == 'O'
+      cell = 6 if cells == [0,4,7]
+      cell = 8 if cells == [4,5,6] || cells == [4,5,7] || cells == [2,4,7]
+      cell
+    end
+
+    def retrieve_next_available_non_corner_cell(board)
+      cell = nil
+      board.cells.each_with_index do |e, index|
+        if index != 0 || index != 2 || index != 6 || index != 8
+          if e == " "
+            cell = index
+            break
+          end
+        end
+      end
+      cell
+    end
 
     def find_at_risk_cell(combination, board, token)
       array = combination.map { |e| board.cells[e] }
