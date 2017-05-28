3183d58de7a3f955390868a3ff36fee493d6cd52
Joe Kim <josephkim0224@gmail.com>
2017-05-28 09:18:37 -0400

    ability to set difficulty level added

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,11 +1,25 @@
 module Players
   class Computer < Player
 
+    attr_reader :difficulty
+
+    def initialize(token, difficulty = 1)
+      super(token)
+      @difficulty = difficulty
+    end
+
     def move(board)
+      puts "difficulty: #{difficulty}"
       # temporarily brute-forces a choice
       selected = ["1", "2", "3", "4", "5", "6", "7", "8", "9"].sample
       move(board) if !board.valid_move?(selected)
       selected
+    end
+
+    def poor_move(board)
+    end
+
+    def best_move(board)
     end
   end
 end
