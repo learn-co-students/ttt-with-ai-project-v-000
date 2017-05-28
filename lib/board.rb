0d04f136aba5d559a626767b770d8548fb3afe37
Joe Kim <josephkim0224@gmail.com>
2017-05-28 00:45:44 -0400

    added board position, full?, and turn_count methods

--- lib/board.rb
+++ lib/board.rb
@@ -18,4 +18,16 @@ class Board
     puts "-----------"
     puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
   end
+
+  def position(input)
+    cells[input.to_i - 1]
+  end
+
+  def full?
+    !cells.any? { |cell| cell == " " }
+  end
+
+  def turn_count
+    cells.select { |cell| cell != " " }.size
+  end
 end
