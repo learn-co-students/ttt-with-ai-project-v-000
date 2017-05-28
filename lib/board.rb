e1f992ce29772842bf6f036034e30ac6ddf1ec7d
Joe Kim <josephkim0224@gmail.com>
2017-05-28 00:39:58 -0400

    added board initialize and display methods

--- lib/board.rb
+++ lib/board.rb
@@ -6,4 +6,16 @@ class Board
     cells.clear
     9.times { cells << " " }
   end
+
+  def initialize
+    @cells = Array.new(9, " ")
+  end
+
+  def display
+    puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
+    puts "-----------"
+    puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
+    puts "-----------"
+    puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
+  end
 end
