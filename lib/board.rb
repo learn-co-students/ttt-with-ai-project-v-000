0d5a408a79a87decb983d230953c288b271abc06
Lara Matyushenko <larabrightside@gmail.com>
2017-06-01 10:29:25 +0000

    almost done Board class

---
+++ lib/board.rb
@@ -0,0 +1,43 @@
+class Board
+  attr_accessor :cells
+
+  @cells = []
+
+  def reset!
+    self.cells = Array.new(9, " ")
+  end
+
+  def initialize
+    self.cells = Array.new(9, " ")
+  end
+
+  def display
+    puts " #{self.cells[0]} | #{self.cells[1]} | #{self.cells[2]} "
+    puts "-----------"
+    puts " #{self.cells[3]} | #{self.cells[4]} | #{self.cells[5]} "
+    puts "-----------"
+    puts " #{self.cells[6]} | #{self.cells[7]} | #{self.cells[8]} "
+  end
+
+  def position(user_input)
+    self.cells[user_input.to_i-1]
+  end
+
+  def full?
+    self.cells.all? {|cell| cell == "X" || cell == "O"}
+  end
+
+  def turn_count
+    self.cells.count("X") + self.cells.count("O")
+  end
+
+  def taken?(position)
+    self.cells[position.to_i-1] == "X" || self.cells[position.to_i-1] == "O"
+  end
+
+  def valid_move?(position)
+    !taken?(position) && position >0 && position <=9
+  end
+
+
+end
