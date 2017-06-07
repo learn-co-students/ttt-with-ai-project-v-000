31137f794486a61d9696aa9116f82cd4ee64657e
Luisa Scavo <luisa@Pauls-iMac.local>
2017-06-06 20:46:41 -0400

    add methods to Game, add functionaly to Human Player class

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,7 +1,19 @@
 class Players
   class Human < Player
-    def move
+    attr_reader :token
+    def initialize(token)
+      @token = token
+    end
 
+    def move(board)
+       move = gets.strip
+       move = move.to_i
+       if board.valid_move?(move)
+         board.cells[move - 1] = @token
+         move
+       else
+         nil
+       end
     end
   end
 end
