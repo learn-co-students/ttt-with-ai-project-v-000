6102f58cace36939fc587b1d98ff39fb4d6f030c
Samson Yuwono <samson.yuwono@gmail.com>
2017-06-07 17:36:38 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -4,30 +4,32 @@ class Players::Computer < Player
 #if not "try to go in a corner"
 #otherwise try a random move
 
-def move(board)
-if board.valid_move?("5")
-  "5"
-elsif corner_mover(board) 
-    corner_move
-else
-  random_mover(board) 
- end
-end
+  def move(board)
+    if board.valid_move?("5")
+      "5"
+    elsif corner_move(board) #goes for an available corner
+          corner_move(board)
+    else
+        random_num(board)
+      end
+    end
 
-def corner_mover(board)
-  corner_move = [1, 3, 7, 9].shuffle.first.to_s
-  if board.valid_move?(corner_move)
-    corner_move
+ def corner_move(board)
+    corner = [1, 3, 7, 9].sample.to_s
+    if board.valid_move?(corner)
+      corner
+    end
   end
-end
 
-def random_mover(board)
-   move = rand(1..9).to_s
-   if board.valid_move?(move)
-       random_move
-   else
-     move(board)
-   end
- end
 
-end
+
+def random_num(board)
+      random_move = rand(1..9).to_s
+      if board.valid_move?(random_move)
+        computer_move = random_move
+      else
+        move(board)
+      end
+    end
+
+  end
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -3,7 +3,7 @@ module Players
 
   def move(board)
     puts "Please input your move"
-    gets.strip
+    move = gets.chomp
   end
 
   end
