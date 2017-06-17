ebb181c32f914c11993f9916cebc43d3f747fac5
Mel Chang <melanie.a.chang@gmail.com>
2017-06-17 19:28:44 +0000

    update #won?

--- lib/game.rb
+++ lib/game.rb
@@ -37,7 +37,7 @@ class Game
     WIN_COMBINATIONS.detect do |combination|
       @board.cells[combination[0]] == @board.cells[combination[1]] &&
       @board.cells[combination[1]] == @board.cells[combination[2]] &&
-      @board.cells[combination[0]] != " "
+      @board.cells[combination[0]] != ""
     end
   end
 
