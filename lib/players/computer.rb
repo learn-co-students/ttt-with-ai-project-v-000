0b8a3ce8bacd3c59ce173bef6d722bb20457eeb3
Mel Chang <melanie.a.chang@gmail.com>
2017-06-19 20:19:20 +0000

    Cleaned up final condition in Computer class

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -13,9 +13,9 @@ module Players
           move = "1"
         end
       elsif board.turn_count == 3 #if 3rd move, play first open corner: iterate through all corners, test against taken?
-        move = ["1","3","7","9"].detect {|position| !board.taken?(position)}
+        move = ["1", "3", "7", "9"].detect {|position| !board.taken?(position)}
       else #play first open position
-        move = ["1", "2", "3", "4", "5", "6", "7", "8", "9"].sample
+        move = ["1", "2", "3", "4", "5", "6", "7", "8", "9"].detect {|position| !board.taken(position)}
       end
       move #return move var
     end
