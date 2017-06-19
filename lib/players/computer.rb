e7d5e3479ff0f33e6147feaa926cc0f4150146da
Mel Chang <melanie.a.chang@gmail.com>
2017-06-19 23:48:11 +0000

    Added game invitation to bin

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,3 +1,4 @@
+
 module Players
 
   class Computer < Player
@@ -7,7 +8,7 @@ module Players
       if board.turn_count == 0   #if 1st move, play corner 1
         move = "1"  #rspec is testing against strings
       elsif board.turn_count == 2 #if 2nd move, play center, if center taken, play corner 1
-        if !board.taken(5)
+        if !board.taken?(5)
           move = "5"
         else
           move = "1"
@@ -15,7 +16,7 @@ module Players
       elsif board.turn_count == 3 #if 3rd move, play first open corner: iterate through all corners, test against taken?
         move = ["1", "3", "7", "9"].detect {|position| !board.taken?(position)}
       else #play first open position
-        move = ["1", "2", "3", "4", "5", "6", "7", "8", "9"].detect {|position| !board.taken(position)}
+        move = ["1", "2", "3", "4", "5", "6", "7", "8", "9"].detect {|position| !board.taken?(position)}
       end
       move #return move var
     end
