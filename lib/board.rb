d6a67f1a40f7c11a94ba5daf0b7bc8436594b147
psnider30 <pbsnider@gmail.com>
2017-06-25 17:46:31 -0700

    Finished Board class and finished Player class

--- lib/board.rb
+++ lib/board.rb
@@ -18,8 +18,34 @@ class Board
     puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
   end
 
-  def position(user_input)
-    self.cells[user_input.to_i - 1]
+  def position(pos)
+    self.cells[pos.to_i - 1]
+  end
+
+  def full?
+    !self.cells.include?(" ")
+  end
+
+  def turn_count
+    counter = 0
+    self.cells.each do |cell|
+      if cell == 'X' || cell == "O"
+        counter += 1
+      end
+    end
+    counter
+  end
+
+  def taken?(pos)
+    self.cells[pos.to_i - 1] != " "
+  end
+
+  def valid_move?(pos)
+    pos.to_i.between?(1, 9) && !self.taken?(pos)
+  end
+
+  def update(pos, player)
+    self.cells[pos.to_i - 1] = player.token
   end
 
 end
