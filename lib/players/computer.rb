65a3bad1c469ebca9ef86f03a6abb2313cd4e90a
psnider30 <pbsnider@gmail.com>
2017-06-26 13:29:11 -0700

    finished Players::Computer including ai for move method

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,4 +1,148 @@
 module Players
   class Computer < Player
+
+    def move(board)
+      puts "Your move hot shot!"
+      if board.taken?('1') && board.taken?('2') && board.position('2') == board.position('1')
+        pos = '3'
+      elsif board.taken?('1') && board.taken?('3') && board.position('3') == board.position('1')
+        pos = '2'
+      elsif board.taken?('2') && board.taken?('3') && board.position('3') == board.position('2')
+        pos = '1'
+      elsif board.taken?('1') && board.taken?('5') && board.position('5') == board.position('1')
+        pos = '9'
+      elsif board.taken?('1') && board.taken?('9') && board.position('9') == board.position('1')
+        pos = '5'
+      elsif board.taken?('5') && board.taken?('9') && board.position('9') == board.position('5')
+        pos = '1'
+      elsif board.taken?('1') && board.taken?('4') && board.position('4') == board.position('1')
+        pos = '6'
+      elsif board.taken?('1') && board.taken?('6') && board.position('6') == board.position('1')
+        pos = '4'
+      elsif board.taken?('4') && board.taken?('6') && board.position('6') == board.position('4')
+        pos = '1'
+      elsif board.taken?('3') && board.taken?('7') && board.position('7') == board.position('3')
+        pos = '5'
+      elsif board.taken?('3') && board.taken?('5') && board.position('5') == board.position('3')
+        pos = '7'
+      elsif board.taken?('7') && board.taken?('5') && board.position('5') == board.position('7')
+        pos = '3'
+      elsif board.taken?('3') && board.taken?('6') && board.position('6') == board.position('3')
+        pos = '9'
+      elsif board.taken?('3') && board.taken?('9') && board.position('9') == board.position('3')
+        pos = '6'
+      elsif board.taken?('6') && board.taken?('9') && board.position('9') == board.position('6')
+        pos = '3'
+      elsif board.taken?('2') && board.taken?('8') && board.position('8') == board.position('2')
+        pos = '5'
+      elsif board.taken?('2') && bboard.taken?('5') && board.position('5') == board.position('2')
+        pos = '8'
+      elsif board.taken?('5') && board.taken?('8') && board.position('8') == board.position('5')
+        pos = '2'
+      elsif board.taken?('4') && board.taken?('5') && board.position('5') == board.position('4')
+        pos = '6'
+      elsif board.taken?('4') && board.taken?('6') && board.position('6') == board.position('4')
+        pos = '5'
+      elsif board.taken?('5') && board.taken?('6') && board.position('6') == board.position('5')
+        pos = '4'
+      elsif board.taken?('7') && board.taken?('9') && board.position('9') == board.position('7')
+        pos = '8'
+      elsif board.taken?('7') && board.taken?('8') && board.position('8') == board.position('7')
+        pos = '9'
+      elsif board.taken?('8') && board.taken?('9') && board.position('9') == board.position('8')
+        pos = '7'
+      elsif !board.taken?('5')
+        pos = '5'
+      elsif !board.taken?('1') && ((!board.taken?('9') || board.position('9') == self.token) && (!board.taken?('5') || board.position('5') == self.token))
+        pos = '1'
+      elsif !board.taken?('3') && ((!board.taken?('7') || board.position('7') == self.token) && (!board.taken?('5') || board.position('5') == self.token))
+        pos = '3'
+      elsif !board.taken?('9') && ((!board.taken?('5') || board.position('5') == self.token) && (!board.taken?('1') || board.position('1') == self.token))
+        pos = '9'
+      elsif !board.taken?('7') && ((!board.taken?('5') || board.position('5') == self.token) && (!board.taken?('3') || board.position('3') == self.token))
+        pos = '7'
+      elsif !board.taken?('1') && ((!board.taken?('7') || board.position('7') == self.token) && (!board.taken?('4') || board.position('4') == self.token))
+        pos = '1'
+      elsif !board.taken?('3') && ((!board.taken?('9') || board.position('9') == self.token) && (!board.taken?('6') || board.position('6') == self.token))
+        pos = '1'
+      elsif !board.taken?('2') && ((!board.taken?('8') || board.position('8') == self.token) && (!board.taken?('5') || board.position('5') == self.token))
+        pos = '2'
+      elsif !board.taken?('3') && ((!board.taken?('1') || board.position('1') == self.token) && (!board.taken?('2') || board.position('2') == self.token))
+        pos = '3'
+      elsif !board.taken?('9') && ((!board.taken?('7') || board.position('7') == self.token) && (!board.taken?('8') || board.position('8') == self.token))
+        pos = '9'
+      elsif !board.taken?('6') && ((!board.taken?('4') || board.position('4') == self.token) && (!board.taken?('5') || board.position('5') == self.token))
+        pos = '6'
+      elsif !board.taken?('1')
+        pos = '1'
+      elsif !board.taken?('3')
+        pos = '3'
+      elsif !board.taken?('7')
+        pos = '7'
+      elsif !board.taken?('9')
+        pos = '9'
+      elsif !board.taken?('2')
+        pos = '2'
+      elsif !board.taken?('8')
+        pos = '8'
+      elsif !board.taken?('4')
+        pos = '4'
+      elsif !board.taken?('6')
+        pos = '6'
+      end
+    end
   end
 end
+
+=begin
+if board.taken?('1') && board.taken?('2') && board.position('2') != self.token
+  pos = '3'
+elsif board.taken?('1') && board.position('1') != self.token &&  board.taken?('3') && board.position('3') != self.token
+  pos = '2'
+elsif board.taken?('2') && board.position('2') != self.token &&  board.taken?('3') && board.position('3') != self.token
+  pos = '1'
+elsif board.taken?('1') && board.position('1') != self.token &&  board.taken?('5') && board.position('5') != self.token
+  pos = '9'
+elsif board.taken?('1') && board.position('1') != self.token &&  board.taken?('9') && board.position('9') != self.token
+  pos = '5'
+elsif board.taken?('5') && board.position('5') != self.token &&  board.taken?('9') && board.position('9') != self.token
+  pos = '1'
+elsif board.taken?('1') && board.position('1') != self.token &&  board.taken?('4') && board.position('4') != self.token
+  pos = '6'
+elsif board.taken?('1') && board.position('1') != self.token &&  board.taken?('6') && board.position('6') != self.token
+  pos = '4'
+elsif board.taken?('1') && board.position('1') != self.token &&  board.taken?('6') && board.position('6') != self.token
+  pos = '4'
+elsif board.taken?('4') && board.position('4') != self.token &&  board.taken?('6') && board.position('6') != self.token
+  pos = '1'
+elsif board.taken?('3') && board.position('3') != self.token &&  board.taken?('7') && board.position('7') != self.token
+  pos = '5'
+elsif board.taken?('3') && board.position('3') != self.token &&  board.taken?('5') && board.position('5') != self.token
+  pos = '7'
+elsif board.taken?('7') && board.position('7') != self.token &&  board.taken?('5') && board.position('5') != self.token
+  pos = '3'
+elsif board.taken?('3') && board.position('3') != self.token &&  board.taken?('6') && board.position('6') != self.token
+  pos = '9'
+elsif board.taken?('3') && board.position('3') != self.token &&  board.taken?('9') && board.position('9') != self.token
+  pos = '6'
+elsif board.taken?('6') && board.position('6') != self.token &&  board.taken?('9') && board.position('9') != self.token
+  pos = '3'
+elsif board.taken?('2') && board.position('2') != self.token &&  board.taken?('8') && board.position('8') != self.token
+  pos = '5'
+elsif board.taken?('2') && board.position('2') != self.token &&  board.taken?('5') && board.position('5') != self.token
+  pos = '8'
+elsif board.taken?('5') && board.position('5') != self.token &&  board.taken?('8') && board.position('8') != self.token
+  pos = '2'
+elsif board.taken?('4') && board.position('4') != self.token &&  board.taken?('5') && board.position('5') != self.token
+  pos = '6'
+elsif board.taken?('4') && board.position('4') != self.token &&  board.taken?('6') && board.position('6') != self.token
+  pos = '5'
+elsif board.taken?('5') && board.position('5') != self.token &&  board.taken?('6') && board.position('6') != self.token
+  pos = '4'
+elsif board.taken?('7') && board.position('7') != self.token &&  board.taken?('9') && board.position('9') != self.token
+  pos = '8'
+elsif board.taken?('7') && board.position('7') != self.token &&  board.taken?('8') && board.position('8') != self.token
+  pos = '9'
+elsif board.taken?('8') && board.position('8') != self.token &&  board.taken?('9') && board.position('9') != self.token
+  pos = '7'
+=end
