600d9805975d27122eef48868843033d9ab0d4a1
Nick Alan DAmico <nickalan82@icloud.com>
2017-06-29 20:11:06 +0000

    Added Computer playing Logic

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,8 +1,42 @@
 module Players
   class Computer < Player
 
-    def move
-      
+    def move(board)
+        move = nil
+        msg = "Computer moves to "
+        if !board.taken?(5)
+             move = "5"
+             puts "Computer moves to #{move}."
+             move
+        elsif !board.taken?(3)
+            move = "3"
+            puts msg + "#{move}."
+            move
+        elsif !board.taken?(1)
+            move = "1"
+            puts msg + "#{move}."
+            move
+        elsif !board.taken?(9) 
+            move = "9"
+            puts msg + "#{move}."
+            move
+         elsif !board.taken?(7) 
+            move = "7"
+            puts msg + "#{move}."
+            move
+         elsif !board.taken?(6)
+            move = "6"
+            puts msg + "#{move}."
+            move
+        elsif !board.taken?(4)
+            move = "4"
+            puts msg + "#{move}."
+            move
+        elsif !board.taken?(2)
+            move = "2"
+            puts msg + "#{move}."
+            move
+        end
     end
 
   end
