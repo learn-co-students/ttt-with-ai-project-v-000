168450334c629df69901f22084d14a566612783b
Ninian Bond <gentile.bond@gmail.com>
2017-06-30 18:27:51 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -9,30 +9,30 @@ module Players
        if board.cells[4] == " "
           "5"
        end
-       if board.cells.select {|cell| cell == "X" || cell == "O"}.size == 1
-         "1"
-       end
-       if board.cells.select {|cell| cell == "X" || cell == "O"}.size == 2  && board.cells[1] == " "
-         "2"
-       end
-       if board.cells.select {|cell| cell == "X"}.size == 2 && board.cells[1] == "X"
-         "8"
-       end
-       if board.cells.select {|cell| cell == " "}.size == 5
-         "4"
-       end
-       if board.cells[3] != " " && board.cells[4] != " "
-         "6"
-       end
-       if board.cells.select {|cell| cell == " "}.size == 3
-         "9"
-       end
-       if board.cells.select {|cell| cell == " "}.size == 2
-         "3"
-       end
-       if board.cells.select {|cell| cell == " "}.size == 1
-         "7"
-       end
+      #  if board.cells.select {|cell| cell == "X" || cell == "O"}.size == 1
+      #    "1"
+      #  end
+      #  if board.cells.select {|cell| cell == "X" || cell == "O"}.size == 2  && board.cells[1] == " "
+      #    "2"
+      #  end
+      #  if board.cells.select {|cell| cell == "X"}.size == 2 && board.cells[1] == "X"
+      #    "8"
+      #  end
+      #  if board.cells.select {|cell| cell == " "}.size == 5
+      #    "4"
+      #  end
+      #  if board.cells[3] != " " && board.cells[4] != " "
+      #    "6"
+      #  end
+      #  if board.cells.select {|cell| cell == " "}.size == 3
+      #    "9"
+      #  end
+      #  if board.cells.select {|cell| cell == " "}.size == 2
+      #    "3"
+      #  end
+      #  if board.cells.select {|cell| cell == " "}.size == 1
+      #    "7"
+      #  end
 
      end
 
