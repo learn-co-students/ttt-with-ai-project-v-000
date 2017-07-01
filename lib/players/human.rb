2be4d6f91a9f108cf15f16e2ac1976f9eefe494c
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-07-01 13:21:57 +0000

    Done.

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -4,4 +4,11 @@ module Players
       input = gets.strip
     end
   end
+
+  class Computer < Player
+    def move(board)
+    valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    valid_moves.sample
+    end
+  end
 end
