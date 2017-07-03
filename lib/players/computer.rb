8dd338f0bb8f25d4664359aab9886aa2084beb0d
Ninian Bond <gentile.bond@gmail.com>
2017-07-03 19:59:24 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -7,32 +7,33 @@ module Players
 
      def move(board)
        if board.cells[4] == " "
-          "5"
+         return  "5"
        end
-      #  if board.cells.select {|cell| cell == "X" || cell == "O"}.size == 1
-      #    "1"
-      #  end
-      #  if board.cells.select {|cell| cell == "X" || cell == "O"}.size == 2  && board.cells[1] == " "
-      #    "2"
-      #  end
-      #  if board.cells.select {|cell| cell == "X"}.size == 2 && board.cells[1] == "X"
-      #    "8"
-      #  end
-      #  if board.cells.select {|cell| cell == " "}.size == 5
-      #    "4"
-      #  end
-      #  if board.cells[3] != " " && board.cells[4] != " "
-      #    "6"
-      #  end
-      #  if board.cells.select {|cell| cell == " "}.size == 3
-      #    "9"
-      #  end
-      #  if board.cells.select {|cell| cell == " "}.size == 2
-      #    "3"
-      #  end
-      #  if board.cells.select {|cell| cell == " "}.size == 1
-      #    "7"
-      #  end
+       if board.cells.select {|cell| cell == "X" || cell == "O"}.size == 1
+         return "1"
+       end
+       if board.cells.select {|cell| cell == "X" || cell == "O"}.size == 2  && board.cells[1] == " "
+         return "2"
+       end
+       if board.cells.select {|cell| cell == " "}.size == 6
+         return "8"
+       end
+       if board.cells.select {|cell| cell == " "}.size == 5
+         return "4"
+       end
+
+       if board.cells.select {|cell| cell == " "}.size == 4
+         return "6"
+       end
+       if board.cells.select {|cell| cell == " "}.size == 3
+         return "9"
+       end
+       if board.cells.select {|cell| cell == " "}.size == 2
+         return "3"
+       end
+       if board.cells.select {|cell| cell == " "}.size == 1
+         return "7"
+       end
 
      end
 
