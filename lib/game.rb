7d849c4597409c32c0372198a0a2ff3ae2bc1b16
Sydney Chun <schun08@gmail.com>
2017-07-11 02:35:56 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -19,7 +19,45 @@ class Game
   end
 
   def current_player
-
+    board.turn_count.odd? && player_1.token == "X" ? player_2 : player_1
   end
-  
+
+  def over?
+    self.draw? || self.won? ? true : false
+    # true = draw or won
+    # false = in progress (not full or no winner)
+  end
+
+  def draw?
+    # if board.full? && !won? => true
+    board.full? && !self.won? ? true : false
+  end
+
+  def won?
+    # if the cells in any of the combos are all the same symbol
+    # there should be a better way to do this
+    WIN_COMBINATIONS.each do |combo|
+      if board.cells[combo[0]] == "X" && board.cells[combo[1]] == "X" && board.cells[combo[2]] == "X"
+        return combo
+      elsif board.cells[combo[0]] == "O" && board.cells[combo[1]] == "O" && board.cells[combo[2]] == "O"
+        return combo
+      end
+    end
+    return false
+  end
+
+  def winner
+    if !self.won?
+      nil
+    else
+      self.won?.all?{|item| board.cells[item] == "X"} ? winner = "X" : winner = "O"
+    end
+  end
+
+  def turn
+    self.current_player.move
+  end
+
+  def play
+  end
 end
