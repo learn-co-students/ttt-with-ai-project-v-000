45a0b1d73d53e5b9505f17f8358b930e897c8a50
hansenjl <jenn.leigh.hansen@gmail.com>
2017-07-17 13:46:48 -0700

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -78,6 +78,12 @@ class Game
   end
 
   def start
+    puts " 1 | 2 | 3 "
+    puts "-----------"
+    puts " 4 | 5 | 6 "
+    puts "-----------"
+    puts " 7 | 8 | 9 "
+    play
   end
 
   def play
