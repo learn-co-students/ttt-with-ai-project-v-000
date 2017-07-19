c118e6b262ec1c35c8bb8f717797105e06c4ee9a
Rebecca Bratu <beckybratu@gmail.com>
2017-07-19 03:09:45 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -11,20 +11,96 @@ module Players
       elsif board.turn_count == 1
         move = "5"
 
-      elsif board.turn_count == 2
-        move = [1, 7].detect{|i| !board.taken?(i)}.to_s
+      end
 
-      elsif board.turn_count == 3
-        move = [4, 8].detect{|i| !board.taken?(i)}.to_s
+      # elsif board.turn_count == 2
+      #   move = [1, 7].detect{|i| !board.taken?(i)}.to_s
 
-      elsif board.position(1) == "X" && board.position(2) == " " && board.position(3) == " "
+      if board.position(1) == board.position(2)
         move = "3"
 
-      elsif board.position(4) == "X" && board.position(5) == "X" && board.position(6) == " "
+      elsif board.position(1) == board.position(3)
+        move = "2"
+
+      elsif board.position(2) == board.position(3)
+        move = "1"
+
+      end
+
+      if board.position(4) == board.position(5)
         move = "6"
 
-      elsif board.position(7) == "X" && board.position(8) == "X" && board.position(9) == " "
+      elsif board.position(4) == board.position(6)
+        move = "5"
+
+      elsif board.position(5) == board.position(6)
+        move = "4"
+
+      end
+
+      if board.position(7) == board.position(8)
         move = "9"
+
+      elsif board.position(7) == board.position(9)
+        move = "8"
+
+      elsif board.position(8) == board.position(9)
+        move = "7"
+
+      end
+
+      if board.position(1) == board.position(5)
+        move = "9"
+
+      elsif board.position(9) == board.position(5)
+        move = "1"
+
+      elsif board.position(1) == board.position(9)
+        move = "5"
+
+      end
+
+      if board.position(3) == board.position(5)
+        move = "7"
+
+      elsif board.position(3) == board.position(7)
+        move = "5"
+
+      elsif board.position(7) == board.position(5)
+        move = "3"
+
+      end
+
+      if board.position(7) == board.position(4)
+        move = "1"
+
+      elsif board.position(7) == board.position(1)
+        move = "4"
+
+      elsif board.position(1) == board.position(4)
+        move = "7"
+
+      end
+
+      if board.position(2) == board.position(8)
+        move = "5"
+
+      elsif board.position(2) == board.position(5)
+        move = "8"
+
+      elsif board.position(5) == board.position(8)
+        move = "2"
+
+      end
+
+      if board.position(3) == board.position(6)
+        move = "9"
+
+      elsif board.position(6) == board.position(9)
+        move = "3"
+
+      elsif board.position(3) == board.position(9)
+        move = "6"
 
       end
 
