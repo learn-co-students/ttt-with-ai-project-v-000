9c1641e769298fee65eedb4aa0f909b08379dd91
Alex <github email address>
2017-07-20 10:21:00 -0400

    attempt to commit only the computer.rb file

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,9 +2,11 @@ module Players
 
   class Computer < Player
     def move(board)
-        valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-        comp_move = valid_moves.sample
+        valid_moves = ["1", "2", "3", "4", "5", "6", "7", " 8", "9"]
+        comp_move = valid_moves.sampleto
     end
   end
 
 end
+
+#test
