ec1a38c2399736c29ceeefed509c83fb2938b523
Alex <github email address>
2017-07-20 16:43:52 -0400

    working notes on computer.rb with ai. Still needs improvement

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,11 +2,38 @@ module Players
 
   class Computer < Player
     def move(board)
-        valid_moves = ["1", "2", "3", "4", "5", "6", "7", " 8", "9"]
-        comp_move = valid_moves.sample
+
+        #first move for computer should be center [4], if not go to a corner
+          if !board.taken?(5)git
+            comp_move = "5"
+          elsif board.turn_count == 1
+            move = "1"
+          elsif board.turn_count ==2git
+            move = [1,3,7,9].detect{|i| !board.taken?(i)}.to_s
+      #    elsif board.turn_count ==3 && (board.position(1) == board.position(9) || board.position(3) == board.position(7))
+      #      move = "2"
+          end
+
     end
   end
 
 end
 
-#test
+
+
+# human first
+# =>      load "config/environment.rb"
+# =>      computer = Players::Computer.new("X")
+# =>      human = Players::Human.new("0")
+# =>      board = Board.new
+# =>      computer_move = computer.move(board)
+#          game = Game.new(human, computer)
+
+
+#computer first
+# =>      load "config/environment.rb"
+# =>      computer = Players::Computer.new("X")
+# =>      human = Players::Human.new("0")
+# =>      board = Board.new
+# =>      computer_move = computer.move(board)
+#          game = Game.new(computer)
