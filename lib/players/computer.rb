463b4cf645198d30a9f9ff417196d775607752dc
Rebecca Bratu <beckybratu@gmail.com>
2017-07-24 00:05:02 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -30,14 +30,14 @@ module Players
       #
       #   end
 
-      if !board.taken?(5)
-            move = "5"
+      if !board.taken?(7)
+            move = "7"
 
       elsif board.turn_count == 1
-            move = "1"
+            move = "5"
 
       elsif board.turn_count == 2
-            move = [1, 3, 7, 9].detect{|i| !board.taken?(i)}.to_s
+            move = [1, 3, 9].detect{|i| !board.taken?(i)}.to_s
 
       elsif board.turn_count == 3 && (board.position(1) == board.position(9) || board.position(3) == board.position(7))
             move = "2"
