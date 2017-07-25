f18cd258e5b469ba24f76fcf4a4fd53511c34da4
Donnadieu <donnadieu.86@gmail.com>
2017-07-25 13:51:00 -0400

    Save

---
+++ lib/game.rb
@@ -0,0 +1,28 @@
+class Game
+  attr_accessor :board, :player_1, :player_2
+
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+    @board = board
+    @player_1 = player_1
+    @player_2 = player_2
+  end
+
+  WIN_COMBINATIONS = [
+    [0,1,2], #Top row
+    [3,4,5], #Middle row
+    [6,7,8], #Bottom row
+    [0,3,6], #First column
+    [1,4,7], #Second column
+    [2,5,8], #Third column
+    [0,4,8], #Diagonal left-to-right
+    [6,4,2]  #Diagonal right-to-left
+  ]
+
+  def current_player
+    if self.board.turn_count.even?
+      player_1
+    else
+      player_2
+    end
+  end
+end
