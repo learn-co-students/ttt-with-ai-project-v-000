7c7e371e297a855b84915aa20b8a56d1941c2699
jonathan <jonathanyi81@gmail.com>
2017-07-25 00:05:55 -0400

    Up to game class. working on play method

--- lib/game.rb
+++ lib/game.rb
@@ -11,17 +11,62 @@ class Game
     [2,4,6] #diagonal_2_win
   ]
 
-  def initialize(player1, player2, board)
-    @player1 = player1
-    @player2 = player2
+  def initialize(player1 = Players::Human.new("X"), player2 = Players::Human.new("O"), board=Board.new)
+    @player_1 = player1
+    @player_2 = player2
     @board = board
-    #@board = [" "," "," "," "," "," "," "," "," "]
   end
 
-  attr_accessor :player1, :player2, :board
+  attr_accessor :player_1, :player_2, :board
+  attr_writer :winner
 
-  def board
-    self.board
+  def current_player
+    self.board.turn_count%2 == 0 ? player_1 : player_2
   end
 
+  def over?
+    self.won? || self.draw?
+  end
+
+  def won?
+    WIN_COMBINATIONS.any? {|each| won_winner_helper(each)}
+  end
+
+  def won_winner_helper(arr)
+    #for each win combination
+    #check each index for board.cellls and see if they all match
+    winner_x = arr.all?{|each| self.board.cells[each] == "X" }
+    winner_o = arr.all?{|each| self.board.cells[each] == "O"}
+    if winner_x
+      self.winner = "X"
+    elsif winner_o
+      self.winner = "O"
+    end
+    winner_x || winner_o
+  end
+
+  def draw?
+    self.board.full? && !self.won?
+  end
+
+  def winner
+    self.won?
+    @winner
+  end
+
+  def turn
+    input = self.current_player.move(self.board)
+    if self.board.valid_move?(input)
+      self.board.cells[input.to_i-1] = self.current_player.token
+      self.board.display
+    else
+      turn
+    end
+  end
+
+  def play
+    until won? || draw?
+      self.turn
+    end
+  end
 end
---
+++ lib/players/computer.rb
@@ -0,0 +1,6 @@
+module Players
+  class Computer < Player
+    def move
+    end
+  end
+end
