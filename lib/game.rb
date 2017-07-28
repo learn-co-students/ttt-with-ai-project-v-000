de2bd56ea4637816a1a3c017fbb8d3619bc35b72
Robert Peckham <peckham.rob@gmail.com>
2017-07-28 09:56:19 +0000

    all game types passing

--- lib/game.rb
+++ lib/game.rb
@@ -1,16 +1,6 @@
 class Game
   attr_accessor :board, :player_1, :player_2
 
-  WIN_COMBINATIONS = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-  ]
 
   def initialize(player_1 = Players::Human.new('X'), player_2 = Players::Human.new('O'), board = Board.new)
     @player_1 = player_1
@@ -62,7 +52,18 @@ class Game
     end
   end
 
-  def won? ### get explanation for how this works
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+
+  def won?
     WIN_COMBINATIONS.detect do |combo|
       @board.cells[combo[0]] == @board.cells[combo[1]] &&
       @board.cells[combo[1]] == @board.cells[combo[2]] &&
--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -15,7 +15,7 @@ module Players
           if combo.select{|i| board.position(i+1) == token}.size == 2 && combo.any?{|i| board.position(i+1) == " "}
             move = combo.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
 
-          elsif combo.select{|i| board.postition(i+1) != " " && board.postition(i+1) != token}.size == 2 && combo.any?{|i| board.postition(i+1) == " "}
+          elsif combo.select{|i| board.position(i+1) != " " && board.position(i+1) != token}.size == 2 && combo.any?{|i| board.position(i+1) == " "}
             move = combo.select { |i| !board.taken?(i+1)}.first.to_i.+(1).to_s
           end
         end
