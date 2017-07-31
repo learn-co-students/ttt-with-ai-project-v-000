d00bce588a937a70a78377e5f96d019c04f2e683
Robert Peckham <peckham.rob@gmail.com>
2017-07-31 16:22:45 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -8,8 +8,13 @@ module Players
         move ="1"
       elsif board.turn_count == 2
         move = [1, 3, 7, 9].detect{|i| !board.taken?(i)}.to_s
-      elsif board.turn_count == 3 && (board.position(1) == board.position(9) || board.position(3) == board.position(7))
-        move = "2"
+      ####This causes infinite loop on 1 player mode when humans first move is 2, the second move causes crash
+      # elsif board.turn_count == 3 && (board.position(1) == board.position(9) || board.position(3) == board.position(7))
+      #   binding.pry
+      #   move = "2"
+      # elsif board.turn_count == 3
+      #   binding.pry
+
       else
         Game::WIN_COMBINATIONS.detect do |combo|
           if combo.select{|i| board.position(i+1) == token}.size == 2 && combo.any?{|i| board.position(i+1) == " "}
