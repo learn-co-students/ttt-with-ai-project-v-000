6a73f9b63d0e953fb88efc6d525ea92eb43da03e
Sarah Schaller <sarcsch@aol.com>
2017-08-04 15:01:49 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,26 +2,13 @@ module Players
   class Computer < Player
 
     def move(board)
-      if board.taken?("1") == false
-        move = "1"
-      elsif board.taken?("2") == false
-        move = "2"
-      elsif board.taken?("3") == false
-        move = "3"
-      elsif board.taken?("4") == false
-        move = "4"
-      elsif board.taken?("5") == false
-        move = "5"
-      elsif board.taken?("6") == false
-        move = "6"
-      elsif board.taken?("7") == false
-        move = "7"
-      elsif board.taken?("8") == false
-        move = "8"
-      elsif board.taken?("9") == false
-        move = "9"
-      end
-    end
+      valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+      random_move = valid_moves.sample
 
+      if board.taken?(random_move) == false
+        move = random_move
+      end
+
+    end
   end
 end
