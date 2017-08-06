dedaf4983d14c1f87d1f68425c826ff0721a203e
Aly Alidina <aly_alidina@hotmail.com>
2017-08-06 15:40:13 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,52 @@
+require 'pry'
+class Board
+  attr_accessor :cells
+
+  def initialize
+    self.reset!
+  end
+
+  def reset!
+    @cells = Array.new(9, " ")
+  end
+
+  def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts     "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts  "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
+
+  def position(input)
+
+    @cells[input.to_i - 1]
+
+  end
+
+  def full?
+
+    @cells.all?{|cell| cell == "O" || cell == "X"}
+
+  end
+
+  def turn_count
+
+    counts = 0
+    @cells.each do |position|
+
+    counts += 1 if position == "X" || position == "O"
+
+    end
+
+    counts
+
+  end
+
+  def taken?(input)
+
+    position(input) == "X" || position(input) == "O"
+
+  end
+
+end
