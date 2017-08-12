11effaf493f0fa6c15fcb6507745a8d09e32f114
Caitlin Lamborne <malfene@netscape.net>
2017-08-12 22:46:38 +0000

    Done.

---
+++ lib/players/computer.rb
@@ -0,0 +1,18 @@
+module Players
+  class Computer < Player
+
+def move(board)
+  move = nil
+  if !board.taken?("5")
+    move = "5"
+  elsif board.turn_count == 1
+    move = "1"
+  elsif board.turn_count == 2
+    move = [1,3,7,9].detect {|i| !board.taken?(i)}.to_s
+  else
+    move =  [1,3,7,9,2,4,6,8].detect {|i| !board.taken?(i)}.to_s
+  end
+  end
+
+end
+end
