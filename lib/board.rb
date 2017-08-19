23999278c33ada72b31d362b468eaea1deb3123e
Yvonne Dreptate <yvonnedreptate@gmail.com>
2017-08-19 22:13:42 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,18 @@
+class Board
+  attr_accessor :cells
+  
+  def reset!
+    @cells= [" "," "," "," "," "," "," "," "," "]
+  end
+  
+  def initialize
+    @cells = [" "," "," "," "," "," "," "," "," "]
+  end
+  
+  
+  
+  
+  
+  
+  # class end
+end
