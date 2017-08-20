db5d96fdba9f91f262cc69686dad0afdb8daa76c
Yvonne Dreptate <yvonnedreptate@gmail.com>
2017-08-20 21:05:32 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,12 +1,11 @@
+require 'pry'
 module Players
-
-  attr_accessor :board
+  # attr_accessor :board
   class Computer < Player
     def move(board)
-      @board = board
-      move = nil
-      self.board = board
-      valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+      # @board = board
+      move = ["1", "2", "3", "4", "5", "6", "7", "8", "9"].select {|position| board.valid_move?(position)}.sample
+
     end
   end
 end
