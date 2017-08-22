50ac7854cc1e9111aec95c13d019e659a57b49dd
Donna <github email address>
2017-08-22 10:08:21 -0400

    started attempting the minmax method

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,19 +1,62 @@
 module Players
 	class Computer < Player
-    	def move(board)
-	    	# if board.cells[5] == board.valid_move?(position)
-	    	# 	5
-	    	# elsif board.cells[1] == board.valid_move?(position)
-	    	# 	1
-	    	# elsif board.cells[3] == board.valid_move?(position)
-	    	# 	3
-	    	# elsif board.cells[7] == board.valid_move?(position)
-	    	# 	7
-	    	# elsif board.cells[9] == board.valid_move?(position)
-	    	# 	9
-	    	# else
-	    		["1", "2", "3", "4", "5", "6", "7", "8", "9"].select {|position| board.valid_move?(position)}.sample
-    		# end #end if block
-    	end #end method
+		
+		attr_reader :best_choice
+
+
+		def move(board)
+			minmax(board, token)
+		end
+
+		def minmax(board, token)
+			#return score(board) if game.over?
+
+			scores = {}
+
+			#define an available space as an array
+			board.available_spaces = []
+			board.cells each do |cell|
+				if valid_move?(cell)
+					board.available_spaces << cell
+				end
+				board.available_spaces
+			end
+
+			board.available_spaces.each do |space|
+				# Copy board so we don't mess up original
+				potential_board = board.dup
+				potential_board.update(space, token)
+
+				scores[space] = minmax(potential_board, switch(token))
+			end
+			@best_choice, best_score = best_move(token, scores)
+			best_choice
+		end
+
+		def best_move(token, scores)
+			if token == @token
+			  scores.max_by { |_k, v| v }
+			else
+			  scores.min_by { |_k, v| v }
+			end
+		end
+
+		def score(game)
+			if game.winner == token
+			  return 10
+			else
+			  return -10
+			end
+		0
+		end
+
+		def switch(token)
+			token == 'X' ? 'O' : 'X'
+		end 
+
+		# def move(board) #EASY MODE
+	 	#   ["1", "2", "3", "4", "5", "6", "7", "8", "9"].select {|position| board.valid_move?(position)}.sample
+		# end #end method
+
 	end #end class
 end #end module
