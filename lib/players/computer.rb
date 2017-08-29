649b0e51f1f0130183a8b08336ceefa2eb12aa59
churchwerks <steven.lennis@icloud.com>
2017-08-29 14:09:05 -0500

    All 53 tests passing

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,7 +2,9 @@ module Players
   class Computer < Player
 
     def move(board)
-
+      prng = Random.new
+      user_input = prng.rand(9) + 1
+      board.valid_move?(user_input) ? user_input.to_s : nil
     end
   end
 end
