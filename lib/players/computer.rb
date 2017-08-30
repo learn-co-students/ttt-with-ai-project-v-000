ddd9dbd84c442255825f6637ceff2e40cb008dd9
churchwerks <steven.lennis@icloud.com>
2017-08-30 11:38:23 -0500

    X smart O dumb

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -6,5 +6,28 @@ module Players
       user_input = prng.rand(9) + 1
       board.valid_move?(user_input) ? user_input.to_s : nil
     end
+
+    def logical_move(board)
+      #binding.pry
+      case board.turn_count.even?
+      when true
+        if board.valid_move?("5")
+          #binding.pry
+          user_input = "5"
+        elsif board.valid_move?("1")
+          user_input = "1"
+        elsif board.valid_move?("3")
+          user_input = "3"
+        elsif board.valid_move?("7")
+          user_input = "7"
+        elsif board.valid_move?("9")
+          user_input = "9"
+        else
+          move(board)
+        end
+      when false
+        move(board)
+      end
+    end
   end
 end
