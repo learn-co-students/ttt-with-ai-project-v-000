dcf370043cc7eb6edb36ac93849e1743a9c7c9d2
Annette Maza <nettymaza@gmail.com>
2017-09-03 18:24:53 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,7 +1,15 @@
 module Players
   class Computer < Player
     def move(board)
+      #First move takes center if availabe if not take any corner.
+      #Blocking: iterate through win combination array. 
+      #If any two tokens of the same kind take two/three comb spots, and is not my token, take third spot. 
+      #Winning, iterate through win comb array
+      #If any two of my tokens in win combination, place my token on third spot. 
       
+      
+
+
     end
 
   end
