f60082380607cb065702c5f2dcabff63394235a7
Annette Maza <nettymaza@gmail.com>
2017-09-04 15:27:11 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -13,14 +13,19 @@ module Players
         current_move = "1"
       #computer 1st player, 2nd move
       elsif @board.turn_count.even?
-        # binding.pry
-        current_move = [1, 3, 7, 9].find |corner|
-          !@board.taken?(corner)
-      #computer 2nd player, 2nd move
-    elsif @board.turn_count == 3 && @board.position("5") == @board.position("1") \
-        || @board.position("5") == @board.position("3") \
-        || @board.position("5") == @board.position("7") \
-        || @board.position("5") == @board.position("9")
+      current_move = [1, 3, 7, 9].find |corner|
+      !@board.taken?(corner)
+      #computer 2nd player, 2nd move, check if any two corners match, then block it.
+      elsif @board.turn_count == 3 && @board.position("1") == @board.position("3")\
+        || @board.position("7") == @board.position("9")\
+        || @board.position("1") == @board.position("9")
+        current_move = [2, 4, 6, 8].find |corner|
+        !@board.taken?(corner)
+
+
+
+
+
 
 
 
@@ -36,7 +41,7 @@ end
 # 3. If computer is first player place token in middle a.k.a positon 5, no need to check for turn_count
 # 4. If computer is second player aka turn_count is not even, place token in corner position if position 5 has been taken else position 5
 # 5. If computer went first and took the middle on first move, take an open corner. Corners = [1,3,7,9]
-
+# 5.1 Its the third move if you went second, you wanna check if any of the other two moves match, put in path
 # 6. Check if any of combos within WIN_COMBINATIONS has two spots filled through iteration
 # 7. Winning! If two spots filled are my token, fill third spot with my token
 # 8. Blocking! If two spots filled are NOT my token, fill thrid spot with my token
