12c79242b4076595c2b7bc78cbd1a84cbbe2d8f8
Annette Maza <nettymaza@gmail.com>
2017-09-04 16:12:35 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -9,24 +9,25 @@ module Players
       if !@board.taken?("5")
         current_move = "5"
       #computer 2nd player, 1st move
-      elsif @board.turn_count.odd? && @board.taken?("5")
+    elsif @board.turn_count == 1 && !@board.taken?("1") && @board.taken?("5")
         current_move = "1"
       #computer 1st player, 2nd move
-      elsif @board.turn_count.even?
+    elsif @board.turn_count < 3
       current_move = [1, 3, 7, 9].find |corner|
-      !@board.taken?(corner)
-      #computer 2nd player, 2nd move, one spot is yours, two spots aren't check if any two corners match, then block it.
+        !@board.taken?(corner)
+      end
+      #computer 2nd player, 2nd move, 4th spot on board, one spot is yours, two spots aren't check if any two corners match, then block it.
       elsif @board.turn_count == 3 && @board.position("1") == @board.position("3")\
         || @board.position("7") == @board.position("9")\
         || @board.position("1") == @board.position("9")
         current_move = [2, 4, 6, 8].find |corner|
         !@board.taken?(corner)
       else
-        GAME::WIN_COMBINATIONS
-
-
-
-
+        GAME::WIN_COMBINATIONS go through each  |combo|
+          board at combo[0] == board at combo[1] && board at combo[2] is " "  && board at combo[0] != ' '
+          board at combo[0] == board at combo[2] && board at combo[1] is " "
+          board at combo[1] == board at combo[2] && board at combo[0] is " "
+        end
 
 
 
