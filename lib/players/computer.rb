eddd3490544d366de71cf895109eeae45743f9e6
Annette Maza <nettymaza@gmail.com>
2017-09-04 18:26:37 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -6,33 +6,31 @@ module Players
     def move(board)
       current_move = nil
       #computer 1st player, 1st move
-      if !@board.taken?("5")
+      if !board.taken?("5")
         current_move = "5"
       #computer 2nd player, 1st move
-      elsif @board.turn_count == 1 && !@board.taken?("1") && @board.taken?("5")
+      elsif board.turn_count == 1 && !board.taken?("1") && board.taken?("5")
         current_move = "1"
       #computer 1st player, 2nd move
-      elsif @board.turn_count <= 3
+      elsif board.turn_count <= 3
         current_move = [1, 3, 7, 9].find do |corner|
-        !@board.taken?(corner).to_s
+        !board.taken?(corner).to_s
         end
       else
         combo = GAME::WIN_COMBINATIONS.find do |combo|
-          @board[combo[0]] == @board[combo[1]] && @board[combo[2]] == " " && @board[combo[0]] != ' '
-          @board[combo[0]] == @board[combo[2]] && @board[combo[1]] == " " && @board[combo[2]] != ' '
-          @board[combo[1]] == @board[combo[2]] && @board[combo[0]] == " " && @board[combo[1]] != ' '
+          board[combo[0]] == board[combo[1]] && board[combo[2]] == " " && board[combo[0]] != ' '
+          board[combo[0]] == board[combo[2]] && board[combo[1]] == " " && board[combo[2]] != ' '
+          board[combo[1]] == board[combo[2]] && board[combo[0]] == " " && board[combo[1]] != ' '
         end
-          if @board[combo[0]] == ' '
-            current_move = @board[combo[0 + 1]]
-          elsif @board[combo[1]] == ' '
-            current_move = @board[combo[1 + 1]]
-          elsif @board[board[2]] == ' '
-            current_move = @board[combo[2 + 1]]
+          if board[combo[0]] == ' '
+            current_move = board[combo[0 + 1]]
+          elsif board[combo[1]] == ' '
+            current_move = board[combo[1 + 1]]
+          elsif board[board[2]] == ' '
+            current_move = board[combo[2 + 1]]
           end
-
-
-
       end #closes if st
+      current_move
     end # closes method
   end #closes class
 end #closes module
@@ -47,11 +45,3 @@ end #closes module
 # 7. Winning! If two spots filled are my token, fill third spot with my token
 # 8. Blocking! If two spots filled are NOT my token, fill thrid spot with my token
 # 9. return current_move
-
-
-# #computer 2nd player, 2nd move, 4th spot on board, one spot is yours, two spots aren't check if any two corners match, then block it.
-# elsif @board.turn_count == 3 && @board.position("1") == @board.position("3")\
-#   || @board.position("7") == @board.position("9")\
-#   || @board.position("1") == @board.position("9")
-#   current_move = [2, 4, 6, 8].find |corner|
-#   !@board.taken?(corner)
