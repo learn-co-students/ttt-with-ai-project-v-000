a2f9f3bad812e9c1a9f7e01d7bb9be0e2b44ed38
Annette Maza <nettymaza@gmail.com>
2017-09-04 05:48:10 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -5,9 +5,6 @@ module Players
     def move(board)
       if !@board.taken?("5")
         "5"
-      elsif @board.turn_count == 1 && !@board.taken?("1")
-        "1"
-      elsif
       end
     end
 
@@ -15,20 +12,13 @@ module Players
   end
 end
 
-      #First move takes center
-      #Second move takes center if availabe if not takes any corner
-      #Blocking: iterate through win combination array.
-      #If any two tokens of the same kind take two/three comb spots, and is not my token, take third spot.
-      #Winning, iterate through win comb array
-      #If any two of my tokens in win combination, place my token on third spot.
-
-      # WIN_COMBINATIONS = [
-      #   [0,1,2],
-      #   [3,4,5],
-      #   [6,7,8],
-      #   [0,3,6],
-      #   [1,4,7],
-      #   [2,5,8],
-      #   [2,4,6],
-      #   [0,4,8]
-      # ]
+1. Checks board positions to make sure board has open spots using the !taken? method
+2. Takes in a string as input and puts it on board
+3. If computer is first player place token in middle a.k.a positon 5, no need to check for turn_count
+4. If computer is second player aka turn_count is not even, place token in corner position if position 5 has been taken else position 5
+5. If computer went first and took the middle on first move, take an open corner. Corners = [1,3,7,9]
+5.1 
+6. Check if any of combos within WIN_COMBINATIONS has two spots filled through iteration
+7. Winning! If two spots filled are my token, fill third spot with my token
+8. Blocking! If two spots filled are NOT my token, fill thrid spot with my token
+9. move
