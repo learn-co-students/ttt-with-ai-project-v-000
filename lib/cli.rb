45bf6b01e6569238ac81021a70018faf3de08e0a
Annette Maza <nettymaza@gmail.com>
2017-09-04 22:43:54 +0000

    Done.

---
+++ lib/cli.rb
@@ -0,0 +1,15 @@
+class Cli
+  def call
+    puts "Welcome to Tic Tac Toe!"
+    choose_game
+  end
+
+  def choose_game
+    puts "Please choose your game:"
+    puts "Enter 0 for Computer vs Computer, 1 for Computer vs Human, 2 for Human vs Human, exit"
+    # get user input
+    # check user input
+    # if 0 is entered, maybe call a method that starts a 0 player game
+    
+  end
+end
