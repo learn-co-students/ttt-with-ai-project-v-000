20cc83f875af341139c004b244736c8025c86f01
Annette Maza <nettymaza@gmail.com>
2017-09-04 23:29:26 +0000

    Done.

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,15 +1,57 @@
 class Cli
+
   def call
     puts "Welcome to Tic Tac Toe!"
     choose_game
   end
 
   def choose_game
-    puts "Please choose your game:"
-    puts "Enter 0 for Computer vs Computer, 1 for Computer vs Human, 2 for Human vs Human, exit"
-    # get user input
-    # check user input
-    # if 0 is entered, maybe call a method that starts a 0 player game
+    puts "What kind of game would you like to play?"
+    puts "Enter 0 for Computer vs Computer"
+    puts "Enter 1 for Computer vs Human"
+    puts "Enter 2 for Human vs Human"
+    puts "To quit, type 'exit'."
     
+
+
+    input = gets.strip
+
+
+
+    end
+
   end
 end
+
+def call
+    input = ""
+
+    while input != "exit"
+      puts "Welcome to your music library!"
+      puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs of a particular genre, enter 'list genre'."
+      puts "To play a song, enter 'play song'."
+      puts "To quit, type 'exit'."
+      puts "What would you like to do?"
+
+      input = gets.strip
+
+      case input
+      when "list songs"
+        list_songs
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+      when "list artist"
+        list_songs_by_artist
+      when "list genre"
+        list_songs_by_genre
+      when "play song"
+        play_song
+      end
+    end
+  end
