11433e059b737ac0c8fb74b85bcdbe0d6747b9fd
Veronica Miller <vmcmiller@gmail.com>
2017-09-06 21:38:10 +0000

    added content to player, human, and game classes

---
+++ lib/game.rb
@@ -0,0 +1,23 @@
+class Game
+
+  attr_accessor :board, :player_1, :player_2
+
+  def start
+  end
+
+  def play
+  end
+
+  def turn
+  end
+
+  def current_player
+  end
+
+  def won?
+  end
+
+  def winner
+  end
+
+end
