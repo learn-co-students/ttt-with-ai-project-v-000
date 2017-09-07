0f6df70fa6e0e5790c5d1d0e65dc74b944d51d25
Veronica Miller <vmcmiller@gmail.com>
2017-09-07 12:24:08 +0000

    added methods to Game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,13 @@
 class Game
 
   attr_accessor :board, :player_1, :player_2
+  attr_reader :WIN_COMBINATIONS
+
+  def initialize(board = Board.new, player_1 = Human.new("X"), player_2 = Human.new("O"))
+    @board = board
+    @player_1 = player_1
+    @player_2 = player_2
+  end
 
   def start
   end
@@ -12,12 +19,22 @@ class Game
   end
 
   def current_player
+    taken = @board.select {|cell| cell != " "}
+    taken.length %2 == 0 ? "X" : "O"
   end
 
   def won?
   end
 
   def winner
+  end
+
+  def over?
+    @board.draw? || @board.won?
+  end
+
+  def draw?
+    @board.full? && !@board.won?
   end
 
 end
