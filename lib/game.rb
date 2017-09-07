42511e86827808d82b99d79e09bd56b99bc8c8c3
Veronica Miller <vmcmiller@gmail.com>
2017-09-07 21:37:32 +0000

    corrected & added to Game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,11 @@
+require 'pry'
 class Game
 
   attr_accessor :board, :player_1, :player_2
-  attr_reader :WIN_COMBINATIONS
 
-  def initialize(board = Board.new, player_1 = Human.new("X"), player_2 = Human.new("O"))
+  WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
     @board = board
     @player_1 = player_1
     @player_2 = player_2
@@ -13,28 +15,36 @@ class Game
   end
 
   def play
+    #until over?
+    #end
   end
 
   def turn
   end
 
   def current_player
-    taken = @board.select {|cell| cell != " "}
-    taken.length %2 == 0 ? "X" : "O"
+    board.turn_count % 2 == 0 ? @player_1 : @player_2
   end
 
   def won?
+    won = false
+    WIN_COMBINATIONS.each do |win|
+      if !won && win.all? {|cell| board.cells[cell] == "X"} || win.all? {|cell| board.cells[cell] == "O"}
+        won = win
+      end
+    end
+    won
   end
 
   def winner
   end
 
   def over?
-    @board.draw? || @board.won?
+    self.draw? || self.won?
   end
 
   def draw?
-    @board.full? && !@board.won?
+    board.full? && !self.won?
   end
 
 end
