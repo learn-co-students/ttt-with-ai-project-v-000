bd82bc64d5d45cd70e11d065c5b369a7f2f2893f
Chris Rudolph <chrisjames.r@gmail.com>
2017-09-08 19:58:47 +0000

    revert to dumb ai

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -6,43 +6,43 @@ module Players
       super
     end
 
-    # def move(board)
-    #   input = (rand * 10).floor
-    #   board.valid_move?(input) ? input.to_s : move(board)
-    # end
-
-
     def move(board)
-      if board.turn_count > 4
-        Game::WIN_COMBINATIONS.detect do |win_combination|  # returns array
-          if win_combination.one?{|index|board.cells[index] != "X"}
-            # binding.pry
-            next_move(win_combination, board)
-          elsif win_combination.one?{|index|board.cells[index] != "O"}
-            # binding.pry
-            next_move(win_combination, board)
-          end
-        end
-      else
-        input = (rand * 10).floor
-        board.valid_move?(input) ? input.to_s : move(board)
-      end
+      input = (rand * 10).floor
+      board.valid_move?(input) ? input.to_s : move(board)
     end
 
-    def next_move(win_combo, board)
-      # binding.pry
-      win_combo.detect do |m|
-        # binding.pry
-        m += 1
-        board.valid_move?(m)
-        # binding.pry
 
-      end
-    end
-
-    def index_to_input(index)
-      index + 1
-    end
+    # def move(board)
+    #   if board.turn_count > 4
+    #     Game::WIN_COMBINATIONS.detect do |win_combination|  # returns array
+    #       if win_combination.one?{|index|board.cells[index] != "X"}
+    #         # binding.pry
+    #         next_move(win_combination, board)
+    #       elsif win_combination.one?{|index|board.cells[index] != "O"}
+    #         # binding.pry
+    #         next_move(win_combination, board)
+    #       end
+    #     end
+    #   else
+    #     input = (rand * 10).floor
+    #     board.valid_move?(input) ? input.to_s : move(board)
+    #   end
+    # end
+    #
+    # def next_move(win_combo, board)
+    #   # binding.pry
+    #   win_combo.detect do |m|
+    #     # binding.pry
+    #     m += 1
+    #     board.valid_move?(m)
+    #     # binding.pry
+    #
+    #   end
+    # end
+    #
+    # def index_to_input(index)
+    #   index + 1
+    # end
 
 
   end  # End of Class
