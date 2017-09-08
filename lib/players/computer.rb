c833fecd241f51524fcaeba48eaf2a4157cb47c1
Chris Rudolph <chrisjames.r@gmail.com>
2017-09-08 18:25:51 +0000

    save progress AI logic

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -12,5 +12,21 @@ module Players
     end
 
 
+    def defense(board)
+      WIN_COMBINATIONS.sort.detect do |win_combination|
+        binding.pry
+        if win_combination.one?{|index|board[index] != "X"}
+          binding.pry
+          win_combination
+        elsif win_combination.one?{|index|board[index] == "O"}
+          binding.pry
+          win_combination
+        else
+         move(board)
+        end
+      end
+    end
+
+
   end  # End of Class
 end  # End of Module
