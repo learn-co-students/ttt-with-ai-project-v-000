904b8757abe358982eaa8a209c5aab3468f98cb5
Chris Rudolph <chrisjames.r@gmail.com>
2017-09-12 22:00:44 +0000

    save progress

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,87 +1,55 @@
 
 module Players
   class Computer < Player
-  attr_accessor :moves
 
     def initialize(token)
       super
-      @moves = []
     end
-
-    NEXT_MOVES = [1, -1, 3, -3, 4, -4, 6, -6]
 
     def move(board)
-      if board.turn_count < 4
-        random_move(board)
-      elsif board.turn_count > 8
-        index = next_move(board) + self.moves.last
-      else
-        board.open_spaces.sample
-      end
+      input = (rand * 10).floor
+      board.valid_move?(input) ? input.to_s : move(board)
     end
 
+    if board.turn_count == 0
+      move = "5"
+    elsif turn_count < 5
+      move = ["1", "3", "7", "9"].find {|m| board.position(m) == " "}
+    elsif turn_count > 5
 
 
-    def next_move(board)
-      NEXT_MOVES.find do |index|
-        (0..8).include?(board.position(self.moves.last + index)) &&
-          board.position(self.moves.last + index) == " "
-      end
-    end
-
-
-
-    def to_win?(board)
-      Game::WIN_COMBINATIONS.detect do |combo|
-        if combo.select {|a| board.cells[a] == self.token}.count = 2
-          combo
-        elsif combo.select {|a| board.cells[a] == other}.count == 2
-          combo
-        else
-          false
-        end
-      end
-    end
-
-    # def find_index(board)
-    #   to_win? ? to_win?(board).detect{|a| board.cells[a] == " "} : false
-    # end
-
-    #   if !self.win_combo.nil?
-    #     self.win_combo.find{|e| board.valid_move?(e+1)}
-    #   elsif !self.block_combo.nil?
-    #     self.block_combo.find{|e| board.valid_move?(e+1)}
+    # def move(board)
+    #   if board.turn_count > 4
+    #     Game::WIN_COMBINATIONS.detect do |win_combination|  # returns array
+    #       if win_combination.one?{|index|board.cells[index] != "X"}
+    #         # binding.pry
+    #         next_move(win_combination, board)
+    #       elsif win_combination.one?{|index|board.cells[index] != "O"}
+    #         # binding.pry
+    #         next_move(win_combination, board)
+    #       end
+    #     end
+    #   else
+    #     input = (rand * 10).floor
+    #     board.valid_move?(input) ? input.to_s : move(board)
     #   end
     # end
-
-
-
-    def other
-      self.token.eql?("X") ? "O" : "X"
-    end
-
-    def random_move(board)
-      input = (rand * 10).floor
-      if board.valid_move?(input)
-        self.moves << input
-        input.to_s
-      end
-    end
+    #
+    # def next_move(win_combo, board)
+    #   # binding.pry
+    #   win_combo.detect do |m|
+    #     # binding.pry
+    #     m += 1
+    #     board.valid_move?(m)
+    #     # binding.pry
+    #
+    #   end
+    # end
+    #
+    # def index_to_input(index)
+    #   index + 1
+    # end
 
 
   end  # End of Class
 end  # End of Module
-
-  # def offense(board)
-  #   if combo.count{|e| board.position(e) == "X"}.eql?(2) && combo.count{|e| board.position(e) == " "}.eql?(1)
-  #     combo
-
-  # def move(board)
-  #   if board.turn_count < 4
-  #     random_move(board)
-  #   else f find_index(board)
-  #     find_index(board)
-  #   else
-  #     midway(board)
-  #   end
-  # end
