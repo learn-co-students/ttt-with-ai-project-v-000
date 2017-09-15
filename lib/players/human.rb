2a2ceab2f3a0d75ae0b1dc0028656420e52e0ec0
github username <github email address>
2017-09-14 21:26:46 -0500

    tic-tac-toe w/ AI

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,8 +1,7 @@
 module Players
   class Human < Player
-    # a move method that takes in a board argument and allows a human player to enter a move via the CLI
     def move(board)
-      return gets
+      return gets.strip
     end
   end
 end
