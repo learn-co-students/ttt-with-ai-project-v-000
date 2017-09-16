956835e68f04e6ecab097a9171740429e4d2c985
Chris Nady <nady.christopher@gmail.com>
2017-09-16 19:43:31 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -8,4 +8,37 @@ class Board
   def reset!
     @cells = Array.new(9, " ")
   end
+
+
+  def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
+
+  def position(location)
+    @cells[location.to_i-1]
+  end
+
+  def position_taken?(location)
+    @cells[location] != " " && @cells[location] != ""
+  end
+
+  def full?
+    count = 0
+    while count < 9
+      if position_taken?(count) == false
+        return false
+      end
+      count += 1
+    end
+    return true
+  end
+
+  def turn_count
+    @cells.count{|token| token == "X" || token == "O"}
+  end
+
 end
--- lib/game.rb
+++ lib/game.rb
@@ -15,14 +15,6 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 
-def display_board
-  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-  puts "-----------"
-  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-  puts "-----------"
-  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-end
-
 def input_to_index(user_input)
   user_input.to_i - 1
 end
