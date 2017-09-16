2340d8956df63a054998ae4676c8dde8ad731230
Johan Esteves <jsesteves91@gmail.com>
2017-09-16 22:35:04 +0000

    Done.

---
+++ lib/game.rb
@@ -0,0 +1,30 @@
+class Game
+attr_accessor :board, :player_1, :player_2
+extend Players
+
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+]
+
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board=Board.new)
+    @board = board
+    @player_1 = player_1
+    @player_2 = player_2
+  end
+
+  def current_player
+    (self.board.turn_count.odd?) ? self.player_2 : self.player_1
+  end
+
+  def over?
+
+  end
+
+end
