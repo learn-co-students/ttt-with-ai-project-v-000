41cd0600d395926bc6cba933bf168dd2b56e950f
Robert Laws <roblaws@me.com>
2017-09-17 20:19:11 +0300

    basic AI for computer

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,7 +1,15 @@
 module Players
   class Computer < Player
     def move(board)
-      pos = [1, 2, 3, 4, 5, 6, 7, 8, 9].sample
+      pos = ''
+      if !board.taken?(5)
+        pos = "5"
+      elsif board.turn_count == 1
+        pos = "1"
+      else
+        pos = [1, 2, 3, 4, 5, 6, 7, 8, 9].sample
+      end
+
       if board.valid_move?(pos)
         pos.to_s
       else
