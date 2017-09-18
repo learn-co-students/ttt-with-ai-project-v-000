37f0d6eb9ead2cf7ae474d2ec047866a56cc71be
Janusz Szubert <theinvalidnonce@gmail.com>
2017-09-18 16:06:52 +0000

    Done.

---
+++ lib/game.rb
@@ -0,0 +1,39 @@
+class Game
+  attr_accessor :board, :player_1, :player_2
+
+  # Initialize #
+  def initialize(board)
+    @board = board
+    @player_1 = player_1
+    @player_2 = player_2
+  end
+
+    def current_player
+
+    end
+
+    def won?
+
+    end
+
+    def winner
+
+    end
+
+    def start
+
+    end
+
+    def play
+
+    end
+
+    def turn
+
+    end
+
+
+
+
+
+end
