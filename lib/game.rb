010c45e0e42a504c9410ee6a57141e3c680a4948
Yehuda Makarov <yehudamakarov@gmail.com>
2017-09-18 19:12:58 -0400

    .

--- lib/game.rb
+++ lib/game.rb
@@ -5,8 +5,21 @@
 # defaults to two human players, X and O, with an empty board
 
 class Game
-  WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [2,4,6], [0,4,8]]
+  # improve readability of array like that
   attr_accessor :board, :player_1, :player_2
+
+  WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [2,4,6],
+  [0,4,8]
+
+]
+
 
   def self.win_combinations
     WIN_COMBINATIONS
