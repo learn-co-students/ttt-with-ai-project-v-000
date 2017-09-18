48e29099cb202ea8cee5b54f0475789366cf6b1d
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-18 04:20:28 +0000

    Done.

---
+++ lib/game.rb
@@ -0,0 +1,12 @@
+class Game
+
+  attr_accessor :player_1, :player_2, :board
+
+  def initialize(player_1 = "X", player_2 = "O", board = [" "," "," "," "," "," "," "," "," "])
+    @player_1 = player_1
+    @player_2 = player_2
+    @board = Board.new
+  end
+
+
+end
