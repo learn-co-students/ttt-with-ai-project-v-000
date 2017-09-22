5a626612f4683337cd3aec94a2fc4507498c355e
GusVilla303 <gusvilla303@gmail.com>
2017-09-22 11:40:36 -0600

    Add #won? and working on #winner

---
+++ lib/game.rb
@@ -0,0 +1,52 @@
+require 'pry'
+
+class Game
+
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2]
+  ]
+
+  attr_accessor :player_1, :player_2, :board
+
+  def initialize(player_1=nil, player_2=nil, board=nil)
+    @board    = board || Board.new
+    @player_1 = player_1 || Players::Human.new("X")
+    @player_2 = player_2 || Players::Human.new("O")
+  end
+
+  def current_player
+    board.turn_count.even? ? player_1 : player_2
+  end
+
+  def over?
+    board.full? || won?
+  end
+
+  def won?
+    WIN_COMBINATIONS.detect do |combo|
+      if (board.taken?(combo[0]) && board.cells[combo[0]] &&
+        board.cells[combo[0]] == board.cells[combo[1]] &&
+        board.cells[combo[1]] == board.cells[combo[2]])
+        return combo
+      end
+    end
+  end
+
+  def draw?
+    board.full? && !won?
+  end
+
+  def winner
+    if won?
+      board.turn_count.even? ? "X" : "O"
+    end
+  end
+
+end
