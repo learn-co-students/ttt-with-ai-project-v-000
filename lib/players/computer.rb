4632f8d63ee0d3831bc4c7f9b2a8524db661ab1c
GusVilla303 <gusvilla303@gmail.com>
2017-09-25 11:38:37 -0600

    Remove extra whitespaces in computer.rb

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -4,11 +4,6 @@ module Players
   class Computer < Player
 
     def move(board)
-      # iterate over board.cells to check which are blank
-      # if blank, add the index to my collection
-      # using my collection, get a sample and use its index number to convert to a string number referencing the board numbers #1-9
-
-
       valid_cells = []
       board.cells.each.with_index do |cell, index|
         if cell == " "
@@ -17,6 +12,5 @@ module Players
       end
       input = valid_cells.sample
     end
-
   end
 end
