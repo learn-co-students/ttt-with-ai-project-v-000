3df75c0ad00e197f3e6f22a409237429a7e1d452
GusVilla303 <gusvilla303@gmail.com>
2017-09-25 11:37:23 -0600

    Add Computer #move and all specs are passing!

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,6 +1,22 @@
+require 'pry'
+
 module Players
   class Computer < Player
 
+    def move(board)
+      # iterate over board.cells to check which are blank
+      # if blank, add the index to my collection
+      # using my collection, get a sample and use its index number to convert to a string number referencing the board numbers #1-9
+
+
+      valid_cells = []
+      board.cells.each.with_index do |cell, index|
+        if cell == " "
+          valid_cells << "#{index + 1}"
+        end
+      end
+      input = valid_cells.sample
+    end
 
   end
 end
