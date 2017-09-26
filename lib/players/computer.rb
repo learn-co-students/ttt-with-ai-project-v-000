e6536534ebfa12993871a423d7bce0d78b7a12b1
Ricardo <github email address>
2017-09-26 15:26:41 -0400

    add simple strategy to Players::Computer

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -8,7 +8,16 @@ module Players
     end
 
     def move(board)
-      valid_moves.sample
+      result = case
+      when !board.taken?(5) then "5"
+      when !board.taken?(3) then "3"
+      when !board.taken?(6) then "6"
+      when !board.taken?(9) then "9"
+      when board.turn_count == 1 then "1"
+      else valid_moves.sample
+      end
+
+      board.valid_move?(result) ? result : move(board)
     end
   end
 end
