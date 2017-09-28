fe44392c1d2ab9c06d3bcfc8b4bcb26deb0daf18
Dana ONeil <doneil1580@gmail.com>
2017-09-28 16:41:32 +0000

    Done.

---
+++ lib/game.rb
@@ -0,0 +1,69 @@
+class Game
+
+  attr_accessor :player_1, :player_2, :board
+
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+    @player_1 = player_1
+    @player_2 = player_2
+    @board = board
+  end
+
+  def current_player
+    board.turn_count % 2 == 0 ? player_1 : player_2
+  end
+
+  def over?
+    draw? || won?
+  end
+
+  def won?
+    WIN_COMBINATIONS.detect do |win_array|
+      board.cells[win_array[0]] == board.cells[win_array[1]] &&
+      board.cells[win_array[1]] == board.cells[win_array[2]] &&
+      board.cells[win_array[0]] != " "
+    end
+  end
+
+  def draw?
+    board.full? && !won?
+  end
+
+  def winner
+    won? ? board.cells[won?[0]] : nil
+  end
+
+  def turn
+    puts "Please enter 1-9:"
+    user_input = current_player.move(board)
+    if board.valid_move?(user_input)
+      board.update(user_input,current_player)
+    elsif !board.valid_move?(user_input)
+      puts "Invalid, please try again."
+      turn
+    end
+      board.display
+  end
+
+  def play
+    while !over?
+      turn
+    end
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
+    end  
+  end
+
+end
