60ae5d8759537130efce96c199c8e93ce4ef92ce
Sophie Chang <soapycee@icloud.com>
2017-09-29 20:32:51 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -4,6 +4,12 @@ module Players
     # Players::Computer
 
     def move(board)
+
+
+      puts "Let's Play Tic Tac Toe!"
+      display
+
+      play
       puts "Please enter a number from 1-9"
       if !board.taken?(3)
         move = "3"
