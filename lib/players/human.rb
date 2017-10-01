f2e1e9385a16217e2019c2802ea9a132fe2e472f
Melissa Amaya <amaya.melissa@gmail.com>
2017-10-01 22:29:20 +0000

    human and computer players complete

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -7,8 +7,17 @@ module Players
       choice = gets.chomp
       choice
     end
-
-
   end
 
+  class Computer < Player
+    def move(board)
+      options = []
+      board.cells.each_with_index do |spot, index|
+        if spot == " "
+          options << index.to_i + 1
+        end
+      end
+      options.sample.to_s
+    end
+  end
 end
