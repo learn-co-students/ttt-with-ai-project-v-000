e2402598a88198330f0c9275bdbed4f51a5142c1
Justin Daniel <justinddaniel@gmail.com>
2017-10-04 00:20:01 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -7,13 +7,23 @@ module Players
       @token = token
     end
 
-    def move(x)
-       if x.valid_move?(index=rand(1..9))
+    def move(board)
+      index = self.strategy(board)
+       if board.valid_move?(index)
          index.to_s
        else
          nil
        end
      end
 
-  end
+     def strategy(board)
+       if board == [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+         5
+       elsif board == [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+         1
+       else rand(1..9)
+     end
+   end
+
+ end
 end
