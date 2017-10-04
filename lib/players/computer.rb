3e86e154a0c78d46acdef056aaf34f1ec5b7ac40
Justin Daniel <justinddaniel@gmail.com>
2017-10-04 00:56:48 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -17,10 +17,31 @@ module Players
      end
 
      def strategy(board)
-       if board == [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+       if board.cells == [" ", " ", " ", " ", " ", " ", " ", " ", " "]
          5
-       elsif board == [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+       elsif board.cells == [" ", " ", " ", " ", "X", " ", " ", " ", " "]
          1
+       elsif board.cells == ["X", " ", " ", " ", " ", " ", " ", " ", " "]
+         5
+       elsif (board.cells == [" ", "X", " ", " ", " ", " ", " ", " ", " "] ||
+             board.cells == [" ", " ", " ", "X", " ", " ", " ", " ", " "] ||
+             board.cells == [" ", " ", " ", " ", " ", "X", " ", " ", " "] ||
+             board.cells == [" ", " ", " ", " ", " ", " ", " ", "X", " "])
+             5
+        elsif (board.cells == ["O", " ", " ", " ", "X", " ", " ", " ", " "] ||
+              board.cells == [" ", " ", "O", " ", "X", " ", " ", " ", " "])
+          2
+        elsif (board.cells == [" ", " ", " ", " ", "X", " ", "O", " ", " "] ||
+              board.cells == [" ", " ", " ", " ", "X", " ", " ", " ", "O"])
+          8
+        elsif board.cells == [" ", "O", " ", " ", "X", " ", " ", " ", " "]
+          3
+        elsif board.cells == [" ", " ", " ", "O", "X", " ", " ", " ", " "]
+          7
+        elsif board.cells == [" ", " ", " ", " ", "X", "O", " ", " ", " "]
+          3
+        elsif board.cells == [" ", " ", " ", " ", "X", " ", " ", "O", " "]
+          7
        else rand(1..9)
      end
    end
