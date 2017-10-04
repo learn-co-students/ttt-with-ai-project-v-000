eec0fdb84f6557ae936a486faf5630745e42ca42
Lauren Feld <lauren.feld@aol.com>
2017-10-04 20:23:12 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -19,4 +19,60 @@ class Game
     @board = board
   end
 
-end 
+  def current_player
+    if board.turn_count % 2 == 0
+      player_1
+    else
+      player_2
+    end
+  end
+
+  def over?
+    won? || draw?
+  end
+
+  def won?
+    WIN_COMBINATIONS.detect do |win_combinations|
+     board.cells[win_combinations[0]] == board.cells[win_combinations[1]] && board.cells[win_combinations[1]] == board.cells[win_combinations[2]] && board.taken?(win_combinations[0]+1)
+  end
+  end
+
+  def draw?
+    if !won? && board.full?
+      true
+    else
+      false
+    end
+  end
+
+  def winner
+    if won?
+      winning_array = won?
+      board.cells[winning_array[0]]
+    end
+  end
+
+  def turn
+    player = current_player
+    current_move = player.move(@board)
+    if @board.valid_move?(current_move)
+      puts "#{@board.turn_count+1}"
+      @board.display
+      @board.update(current_move, player)
+    else
+      turn
+    end
+  end
+
+  def play
+    while !over?
+      turn
+    end
+    if won?
+      puts "Congratulations #{winner}!"
+    else
+      puts "Cat's Game!"
+    end
+  end
+
+end
