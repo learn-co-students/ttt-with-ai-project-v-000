959ba9e274731d055c12ae3997b8f58f4bca623a
Susan Fowler <infopro94@gmail.com>
2017-10-05 17:17:39 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -29,9 +29,9 @@ class Game
 
     def won? #returns false for a draw, correct winning combination for a win, isn't hard-coded
       WIN_COMBINATIONS.detect do |combo|
-        board[combo[0]] == board[combo[1]] &&
-        board[combo[1]] == board[combo[2]] &&
-        position_taken?(board, combo[0])
+        @board.cells[combo[0]] == @board.cells[combo[1]] &&
+        @board.cells[combo[1]] == @board.cells[combo[2]] &&
+        @board.taken?(combo[0]+1)
       end
     end
 
@@ -40,15 +40,27 @@ class Game
     end
 
     def winner #returns X when X won, O when O won, nil when no winner
-      if winning_combo = won?(board)
-        board[winning_combo.first]
+      if winning_combo = won?
+          @winner = @board.cells[winning_combo.first]
       end
     end
 
-    def turn #makes valid moves, asks for input again after a failed validation, changes to player 2 after the first turn
+    def turn #makes valid moves, changes to player 2 after the first turn
+      player = current_player
+      current_move = player.move(@board)
+        if !@board.valid_move?(current_move)
+          turn
+        else
+         puts "Turn number: #{@board.turn_count+1}"
+         @board.display
+         @board.update(current_move, player)
+         puts "#{player.token} made #{current_move}"
+         @board.display #shows status
+        end
     end
 
     def play #asks for players input on a turn of the game, checks if the game is over after every turn, plays the first turn of the game, plays the first few turns of the game, checks if the game is a draw after every turn, stops playing if someone has won, congratulations the Winner X, Congratulates the winner O, stops playing in a draw, prints "Cat's Game!" on adraw, plays through an entire game
+
     end
 
 end
