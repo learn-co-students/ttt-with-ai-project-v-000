5abe250a58d5bafedf5d6bed5f5254a3658a5263
Susan Fowler <infopro94@gmail.com>
2017-10-05 22:02:15 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,8 +2,9 @@ module Players
 
   class Computer<Player
 
-    def move
-      #returns the move the computer wants to make in the form of a 1-9 string
+    def move(board)
+      puts "Please enter a number from 1 to 9"#allows a human player to enter a move via the CLI
+      gets.strip#returns the move the computer wants to make in the form of a 1-9 string
     end
 
   end
