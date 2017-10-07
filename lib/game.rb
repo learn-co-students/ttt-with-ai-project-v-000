89348c4e9ae295853980d66cbdd6193a13943072
Nicholas Halloran <nmhalloran@hotmail.com>
2017-10-07 16:26:19 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,5 @@
 class Game
-  attr_accessor :board, :player_1, :player_2
+  attr_accessor :board, :player_1, :player_2, :token
 
   WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],[1, 4, 7],[2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
@@ -7,6 +7,38 @@ class Game
     @board = board
     @player_1 = player_1
     @player_2 = player_2
+  end
+
+  def current_player
+  board.turn_count.even? ? player_1 : player_2
+  end
+
+  def over?
+    won? && draw? && !board.full?
+  end
+
+  def won?
+    winning_array = []
+    WIN_COMBINATIONS.each do |combination|
+      board.cells[combination[0]] == board.cells[combination[1]] && board.cells[combination[0]] == board.cells[combination[2]]
+      winning_array = combination
+    end
+    winning_array
+  end
+
+  def draw?
+  end
+
+  def winner
+  end
+
+  def turn
+  end
+
+  def play
+  end
+
+  def start
   end
 
 
