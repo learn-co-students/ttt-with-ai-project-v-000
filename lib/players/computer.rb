8bf7ff374c616b2c7a98043849a777fe8b24ae54
Susan Fowler <infopro94@gmail.com>
2017-10-07 17:14:52 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,10 +2,22 @@ module Players
 
   class Computer<Player
 
-    def move(board)
-      puts "Please enter a number from 1 to 9"#allows a human player to enter a move via the CLI
-      gets.strip#returns the move the computer wants to make in the form of a 1-9 string
-    end
+    def move(board) #returns a valid position for the computer to move
+      #on first move go to center
+      if !board.taken?(5)
+        move = "5"
+
+      #if 5 is taken? go to 1, 3, 7, or 9
+        elsif board.turn_count == 1
+          move = ["1", "3", "7", "9"]
+
+      #block diagonal:
+        elsif board.turn_count == 2 && board.position(1) == board.position(9) || board.position(3) == board.position(7)
+          move = "2"
+
+        end
+      end
+
 
   end
 end
