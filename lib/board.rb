830fbbca67357d8a833cafb04a052e10df7f8018
Nicholas Halloran <nmhalloran@hotmail.com>
2017-10-07 15:34:50 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,48 @@
+class Board
+  attr_accessor :cells
+
+  def initialize
+    @cells = Array.new(9, " ")
+  end
+
+  def reset!
+    @cells = Array.new(9, " ")
+    #turn_count = 0
+  end
+
+  def display
+    puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
+    puts "-----------"
+    puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
+    puts "-----------"
+    puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
+  end
+
+  def input_to_index(input)
+    input.to_i-1
+  end
+
+  def position(input)
+    cells[input_to_index(input)]
+  end
+
+  def full?
+    cells.none? {|cell| cell == " "}
+  end
+
+  def turn_count
+    cells.select {|cell| cell == "X" || cell == "O"}.size
+  end
+
+  def taken?(input)
+    position(input) == "X" || position(input) == "O"
+  end
+
+  def valid_move?(input)
+    input_to_index(input).between?(0,8) && !taken?(input)
+  end
+
+  def update(input, player)
+    cells[input_to_index(input)] = player.token
+  end
+end
---
+++ lib/players/human.rb
@@ -0,0 +1,10 @@
+class Players
+  class Human < Player 
+
+    def move(input)
+      input = gets.chomp
+    end
+
+  end
+end
+
