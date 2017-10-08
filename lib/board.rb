88a26878dd3bd1757a01bcff8df1e19b00564ba7
Denis Bondin <dsbondin@gmail.com>
2017-10-08 04:14:55 +0000

    finished board.rb

--- lib/board.rb
+++ lib/board.rb
@@ -2,7 +2,7 @@ class Board
   attr_accessor :cells
 
   def initialize
-    @cells = Array.new(9, " ")
+    reset!
   end
 
   def reset!
@@ -15,6 +15,32 @@ class Board
     puts " #{self.cells[3]} | #{self.cells[4]} | #{self.cells[5]} "
     puts "-----------"
     puts " #{self.cells[6]} | #{self.cells[7]} | #{self.cells[8]} "
+  end
+
+  def position(input)
+    cells[input.to_i - 1]
+  end
+
+  def full?
+    !cells.include?(" ")
+  end
+
+  def turn_count
+    i = 9
+    cells.each {|cell| i -= 1 if cell == " "}
+    i
+  end
+
+  def taken?(input)
+    position(input) == "X" or position(input) == "O"
+  end
+
+  def valid_move?(input)
+    !taken?(input) and (1..9).include?(input.to_i)
+  end
+
+  def update(input, player)
+    cells[input.to_i - 1] = player.token
   end
 
 end
