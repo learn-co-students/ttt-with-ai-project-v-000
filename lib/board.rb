2558da64aa0022aefe04acc9526ff2a91e8efa17
Miriam Peskowitz <miriam5000@gmail.com>
2017-10-11 22:36:11 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,44 @@
+class Board
+  attr_accessor :cells
+
+  def initialize
+    #set board, clear array/ reset!
+  end
+
+  def position
+
+  end
+
+  def reset!
+
+  end
+
+  def display
+
+  end
+
+  def position
+
+  end
+
+  def update
+
+  end
+
+  def full
+
+  end
+
+  def turn_count
+
+  end
+
+  def taken
+
+  end
+
+  def valid_move
+
+  end
+
+end
