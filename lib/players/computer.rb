4c21368edb3ae5d3fecbfe05c9cd131930959842
Lisa Lee <snickers495@gmail.com>
2017-10-11 03:55:51 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,6 +1,13 @@
+require 'pry'
 module Players
-  class Computer < Player 
-
+  class Computer < Player
+    def move(board)
+      number = rand(1...9)
+      until board.valid_move?(number) == true
+        move(board)
+      end
+      return number
+    end
   end
 
 end
