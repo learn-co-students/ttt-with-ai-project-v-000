766a4ca16200ba7e6eb000057ecc7f7426b0e342
Miriam Peskowitz <miriam5000@gmail.com>
2017-10-12 14:19:23 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,9 @@
+require 'pry'
 class Board
   attr_accessor :cells
 
   def initialize
-    #set board, clear array/ reset!
+    reset!
   end
 
   def position
@@ -10,35 +11,42 @@ class Board
   end
 
   def reset!
+    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
 
   end
 
   def display
-
+   puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+   puts "-----------"
+   puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+   puts "-----------"
+   puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
   end
 
-  def position
-
+  def position(user_input)
+    @cells[ user_input.to_i - 1 ]#takes in user input -- puts/gets returns value of the board cell
   end
 
   def update
 
   end
 
-  def full
-
+  def full?
+    # @cells.any?{|c| c != " "} # ask why doesn't this work?
+    @cells.all?{|token| token == "X" || token == "O"}
   end
 
   def turn_count
-
+    @cells.count{ |token| token == "X" || token == "0"}
   end
 
-  def taken
-
+  def taken(index)
+    @cells[index]=="X" || @cells[index]=="O"
   end
 
-  def valid_move
-
+  def valid_move(index)
+    index.between[0,8] &&  !taken[index]
   end
 
 end
--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,22 +1,22 @@
-module Players
-	class Computer < Players # c inherits from player, is this right?
-	#will be Players::Computer
-		def move(board)
-			#returns the move the computer wants to make, in
-			#the form of a 1-9 string
-			#valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-			# up to me how the computer wants to decide this.
-			#random/validator/keep picking?
-			# game should be unwinnable -- you can hardcode logic
-			# condition-based algorithm:
-			# "On turn 1 always try to go in the middle if you can"
-			# and if not "try to go in a corner
-
-		end
-
-		def computer_move
-			computer.move
-		end
-	end
-
-end
+# module Players
+# 	class Computer < Players # c inherits from player, is this right?
+# 	#will be Players::Computer
+# 		def move(board)
+# 			#returns the move the computer wants to make, in
+# 			#the form of a 1-9 string
+# 			#valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+# 			# up to me how the computer wants to decide this.
+# 			#random/validator/keep picking?
+# 			# game should be unwinnable -- you can hardcode logic
+# 			# condition-based algorithm:
+# 			# "On turn 1 always try to go in the middle if you can"
+# 			# and if not "try to go in a corner
+#
+# 		end
+#
+# 		def computer_move
+# 			computer.move
+# 		end
+# 	end
+#
+# end
