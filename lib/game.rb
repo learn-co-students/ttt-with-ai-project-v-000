5071a228bc1003c1d3683754e6a882e29d2beb91
Miriam Peskowitz <miriam5000@gmail.com>
2017-10-12 13:12:54 +0000

    Done.

---
+++ lib/game.rb
@@ -0,0 +1,31 @@
+require 'pry'
+
+class Game
+	attr_accessor :player_1, :player_2
+
+end
+# main model, instance is a ttt session
+# a game has one board  (like Pet has one owner)
+# game has two players, stored in properties player_1 and player_2
+# board and player do not relate directly, but they collaborate through arguments
+# game procides basic game runtime and logic
+ def current_player
+
+ end
+
+ def won
+ end
+
+ def winner
+ end
+
+ def start
+ end
+
+ def play
+ end
+
+ def turn
+
+ end
+ 
---
+++ lib/players/human.rb
@@ -0,0 +1,16 @@
+module Players
+	class Human < Player # h inherits from player, is this right?
+
+		def move(board)
+			puts "Where would you like to move?"
+			input = gets.chomp
+
+			#allows human player to enter a move via the CLI
+			# returns: value entered by the hyuman dplayer
+			# doesn't have to use the board argument.
+
+
+		end
+	end
+
+end 
