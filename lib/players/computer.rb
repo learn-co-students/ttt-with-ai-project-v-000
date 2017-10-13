e1e643db617f02fa3b27e943c4feb76d0aea5514
ronald vilorio <soccer23415@gmail.com>
2017-10-13 21:42:51 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,8 +3,9 @@ module Players
   class Computer < Player
     def move(board)
       user_input = gets.chomp
-      board.cells
       valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+      valid_moves.detect{|move| move}
+
     end
 
   end
