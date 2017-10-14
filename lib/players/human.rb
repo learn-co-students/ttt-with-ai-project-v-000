520940290ec76f7db782996c08cba0b6f7455015
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-10-14 23:32:23 +0000

    Done.

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,7 +1,7 @@
 module Players
   class Human < Player
 
-    def move(board)
+    def move(board) #argument is unecessary
       puts ""
       puts "Player #{@token}, make your move! [1-9]"
       input = gets.chomp
