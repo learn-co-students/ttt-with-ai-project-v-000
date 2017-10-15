48171cb948b3248fab5a7b97846d9f81df402fa3
Adam Tropp <adt6261@gmail.com>
2017-10-15 06:22:54 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -19,18 +19,21 @@ module Players
       board.cells.each_with_index do |space, index|
         valid_moves << (index + 1).to_s if board.valid_move?(index + 1)
       end
-      if WIN_COMBINATIONS.any? do |combination|
-        board.cells[combination[0]] == board.cells[combination[1]] && board.cells[combination[0]] != self.token && board.cells[combination[0]] != " "
+      if
+        WIN_COMBINATIONS.any? do |combination|
+          board.cells[combination[0]] == board.cells[combination[1]] && board.cells[combination[0]] != self.token && board.cells[combination[0]] != " "
+        end
         move = board.cells[combination[2]]
-      end
-      elsif WIN_COMBINATIONS.any? do |combination|
-        board.cells[combination[0]] == board.cells[combination[2]] && board.cells[combination[0]] != self.token && board.cells[combination[0]] != " "
-        move = board.cells[combination[1]]
-      end
-      elsif WIN_COMBINATIONS.any? do |combination|
-        board.cells[combination[1]] == board.cells[combination[2]] && board.cells[combination[1]] != self.token && board.cells[combination[1]] != " "
+      elsif
+         WIN_COMBINATIONS.any? do |combination|
+           board.cells[combination[0]] == board.cells[combination[2]] && board.cells[combination[0]] != self.token && board.cells[combination[0]] != " "
+         end
+         move = board.cells[combination[1]]
+      elsif
+        WIN_COMBINATIONS.any? do |combination|
+          board.cells[combination[1]] == board.cells[combination[2]] && board.cells[combination[1]] != self.token && board.cells[combination[1]] != " "
+        end
         move = board.cells[combination[0]]
-      end
       else
         move = valid_moves.sample
       end
