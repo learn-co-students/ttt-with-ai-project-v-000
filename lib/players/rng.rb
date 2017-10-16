5607a4fdb7f9dec818f6b59d35e3c41804649b75
Nikolai Pawlowski <nikolaip@gmail.com>
2017-10-16 08:44:18 +0000

    Done.

--- lib/players/rng.rb
+++ lib/players/rng.rb
@@ -1,3 +1,8 @@
+# This was the 3 minute attempt at AI. It was useful for fine-tuning the
+# AdvancedAI because it would randomly perform real attack patterns. It was
+# crucial in refining the #edge_attack package, as well as figuring out some
+# board states to avoid. It now gets beaten 95+% of the time by the other 2 AIs.
+
 module Players
   class RNG < Player
 
