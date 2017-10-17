0ff2182d8f3ca0328a49c1cdbb9cf1eacabb3004
miriam <miriam5000@gmail.com>
2017-10-16 21:43:02 -0400

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -20,40 +20,38 @@ class Game
 	   @board = board
 	 end
 
-# # a game has one board  (like Pet has one owner)
-# # board and player do not relate directly, but they collaborate through arguments
-# # game provide s basic game runtime and logic
-#
-# # NEED HELP ON THIS
 	 def current_player
-		 #why is this not working?
+	
 		 @board.turn_count.even? ? @player_1 : @player_2
 	 end
 
 	 def over?
-		  won || draw
+		won? || draw?
  	 end
 
-	 #
-	 #
-	#  def won?
-	# 	 # cells = any of the winning combos
-	# 	# game.board.cells --
-	# 	#WIN_COMBINATIONS.detect
-	# 		#detect what -- board cells with WIN_COMBINATIONS
-	# 		#how?
-	#   end
+	 
+	 def won?
+		 # I used this from the earlier version/trying to figure out the logic. Ask coaches
+	 	WIN_COMBINATIONS.detect do |combo|
+      		@board.cells[combo[0]] == @board.cells[combo[1]] &&
+      		@board.cells[combo[1]] == @board.cells[combo[2]] &&
+      		@board.taken?(combo[0]+1)
+    	end
+	 end
 
 
 	 def draw?
-		!won && @board.full
+		 @board.full? && !won?
 	#		if cells are full, but no winning combo, use board.full
 	 end
 
 	 def winner
-		 	#if winning combo and ..last player, or first to get to Win_combos?
+		 if winning_combo = won? 
+		 	@winner = @board.cells[winning_combo.first]
+		 	#and ..last player, or first to get to Win_combos?
 			# if WIN_COMBINATIONS = won
 			# 	@board[WIN_COMBINATIONS.first]??
+	 	end
 	 end
 
 	 def start
@@ -61,31 +59,31 @@ class Game
 		 #what else
 	 end
 
-	#  def play
-	# 	 #puts"Where would like to move?"
-	# 	#input = gets.strip
-	# 	 #uses board methods to place it
-	# 	 while !over?
-	# 	 		turn
-	#    end
-	#  	if won?
-	# 	 	puts "Congratulations #{winner}!"
-	#  	else
-	# 	 	puts "Cat's Game"
-	# 	end
-	#  end
+	 def play
+	 	while !over?
+	 		turn
+	 	end
+	 	if won?
+	 		puts "Congratulations #{winner}!"
+	 	elsif draw?
+	 		puts "Cat's Game!"
+	 	end
+	 end
 
-	#  def turn
-	# 	 if !over?
-	# 		 #current_player switches
-	# 	# @cells.any have an X, switch to O
-	# 	#last play was o, switch to X
-	# 		# how do i figure out whose play was last?
-	# 		# use current_player?
-	# 		#puts"Player #{ }'s turn"
-	# 	 #
-	#  end
-	# end
+	  def turn
+    	player = current_player
+	    current_move = player.move(@board)
+	    if !@board.valid_move?(current_move)
+	      turn
+	    else
+	      puts "Turn: #{@board.turn_count+1}\n"
+	      @board.display
+	      @board.update(current_move, player)
+	      puts "#{player.token} moved #{current_move}"
+	      @board.display
+	      puts "\n\n"
+	    end
+	  end
 
 
 end
