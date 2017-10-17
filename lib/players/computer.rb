717e712f7b2f199f40387439d89b2544842395bf
Adam Tropp <adt6261@gmail.com>
2017-10-17 06:58:05 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -28,9 +28,12 @@ module Players
       end
       move = valid_moves.sample
       valid_moves.detect do |space|
-        if space == "1" || space == "3" || space == "5" || space == "7" || space == "9"
+        if space == "1" || space == "3" || space == "7" || space == "9"
           move = space
         end
+      end
+      if valid_moves.include?("5")
+        move = "5"
       end
       WIN_COMBINATIONS.any? do |combination|
         if board.cells[combination[0]] == opponent_token && board.cells[combination[1]] == opponent_token && board.cells[combination[2]] == " "
