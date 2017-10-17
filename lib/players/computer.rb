7b8475482a0626da4d9cee7dbce9c654449bb29f
Sally Zasloff <szasloff@gmail.com>
2017-10-17 15:37:59 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,10 +1,26 @@
 module Players
-
+#in process under construction
   class Computer < Player
 
     def move (board)
-      valid_moves = ("1".."9").to_a
-      computer_move = valid_moves[4]
+      move = nil
+      # start with a middle position if not taken
+      if !board.taken?(5)
+        move = "5"
+
+      elsif board.turn_count == 1 && !board.taken?(1)
+        move = "1"
+
+      elsif board.turn_count == 2
+        move = [2,4,6,8].detect {|m| !board.taken?(m)}.to_s
+
+
+
+    
+
+
+      end
+
 
     end
   end
