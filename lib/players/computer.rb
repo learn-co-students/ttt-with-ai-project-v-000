42594e80a016907c0144fe545ee919dafea452a0
ronald vilorio <soccer23415@gmail.com>
2017-10-17 17:04:04 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,9 +2,10 @@ require 'pry'
 module Players
   class Computer < Player
     def move(board)
-      user_input = gets.chomp
       valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-      valid_moves.detect{|move| move}
+
+      move = valid_moves.detect{|move| move }
+      move if !board.full? && !board.taken?(move) && board.valid_move?(move)
 
     end
 
