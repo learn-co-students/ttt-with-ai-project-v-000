769bac08f112bd85f8f293c546fbddcf388a6e54
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2017-10-18 15:06:45 -0400

    Reverting to d0621b4 - to try to undo problems I created with fast-forwarding master. "This is it. Going to merge with master and submit."
    This reverts commit d0621b4b530945d87e08dbc158aa560965a83681.

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -4,14 +4,11 @@ module Players
 
 
 	 def move(board)
-	 	puts "#{token}, please select move (1-9): "
+	 	puts "Please select move (1-9): "
 	 	input = gets.strip
 	 	tester = input.to_i - 1
-	 			# The condition "board == []"" is here b/c of the rspec test for humans, which for some reason written with ([])
-				# as an argument #for move(arg).  I wanted to test for valid move baked into #move...here seemed the best place
-				# to put it... expecially since (board) is requied argument, but again, I had to find a way to work within the
-				# confines of the rspec test.
-				# Also, can't use board.valid_test? b/c it won't allow me to access the board and test for "board == [ ]"
+	 			# The condition "board == []"" is here b/c of the rspec test for humans, which for some reason written with ([]) as an argument #for move(arg).  I wanted to test for valid move baked into #move...that seemed the best place to put it... expecially since (board) is requied argument, but again, I had to find a way to work within the confines of the rspec test
+				#Can't use board.valid_test? b/c it won't allow me to access the board and test for "board == [ ]""
 	 	if tester.between?(0,8) && (board == [ ] || board.cells[tester] == " ")
 	 		input
 	 	else
