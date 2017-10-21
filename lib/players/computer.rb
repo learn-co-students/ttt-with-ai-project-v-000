2c06939e03d30731767e8e654421eeff8713ab28
Melissa Rudick <mel.madrugada@gmail.com>
2017-10-21 02:09:50 +0000

    Fiished computer.rb. All tests passing

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,8 +1,17 @@
 module Players
   class Computer < Player
-    #
-    # def move(board)
-    #   self.
-    # end
+    
+    def move(board)
+      move = nil
+      corner = [1, 3, 7, 9]
+      if board.valid_move?("5")
+        move = "5"
+      elsif board.valid_move?(corner)
+        move = corner.detect {|i| board.valid_move?(i).to_s}
+      else
+        move = Rand(1..9).to_s
+      end
+        binding.pry
+    end
   end
 end
