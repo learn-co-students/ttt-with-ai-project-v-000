313a77fa42844d421fc2feceb08cb2edc8ca1b7d
Michele Conway <mconway890@gmail.com>
2017-10-27 13:22:53 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,63 @@
+class Board
+  attr_accessor :cells
+
+  def initialize
+    reset!
+  end
+
+  def reset!
+    @cells = Array.new(9, " ")
+  end
+
+  def display
+    puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
+    puts "-----------"
+    puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
+    puts "-----------"
+    puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
+  end
+
+  def input_to_index(input)
+    input.to_i - 1
+  end
+
+  def position(input)
+    input = input_to_index(input)
+    cells[input]
+  end
+
+  def full?
+    cells.all? do |cell|
+      cell == "X" || cell == "O"
+    end
+  end
+
+  def turn_count
+    turns = 0
+    cells.each do |cell|
+      if cell == "X" || cell == "O"
+        turns += 1
+      end
+    end
+    turns
+  end
+
+  def taken?(input)
+    if position(input) == "X" || position(input) == "O"
+      true
+    else
+      false
+    end
+  end
+
+  def valid_move?(input)
+    if !taken?(input) && input.to_i.between?(1,9)
+      true
+    end
+  end
+
+  def update(input, player)
+    cells[input.to_i - 1] = player.token
+  end
+
+end
---
+++ lib/game.rb
@@ -0,0 +1,72 @@
+require './lib/player.rb'
+
+class Game
+  Players::Human
+  attr_accessor :board, :player_1, :player_2
+
+  WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]]
+
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+    @player_1 = player_1
+    @player_2 = player_2
+    @board = board
+  end
+
+  def current_player
+    @board.turn_count.even? ? player_1 : player_2
+  end
+
+  def full?(cells)
+    cells.all? do |cell|
+    cell == "X" || cell == "O"
+    end
+  end
+
+  def over?
+    won? || draw? || @board.full?
+  end
+
+  def won?
+    WIN_COMBINATIONS.detect do |combo|
+      mapped_cells = combo.map {|i| board.cells[i]}
+       if mapped_cells.count(mapped_cells.first) == 3 && mapped_cells.first != ' '
+         combo
+       else
+         false
+       end
+     end
+  end
+
+  def draw?
+    !won? && @board.full?
+  end
+
+  def winner
+    if won?
+      return @board.cells[won?[1]]
+    end
+  end
+
+  def turn
+    move = current_player.move(@board)
+    if @board.valid_move?(move)
+      @board.update(move, current_player)
+      @board.display
+    else
+      puts "invalid"
+      turn
+    end
+  end
+
+  def play
+    until over?
+      turn
+    end
+    if won?
+      puts "Congratulations #{winner}!"
+    else
+      puts "Cat's Game!"
+    end
+  end
+
+end
