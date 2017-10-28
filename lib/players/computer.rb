7e9a468270026a674aad6e7639f33b5e6501a0c5
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-28 15:59:37 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,47 +2,40 @@ module Players
   class Computer < Player
 
     def move(board)
-      #binding.pry
       move = nil
 
       if board.turn_count == 0
-
         move = 4
 
       elsif board.turn_count == 1
-
          if !board.taken?(5)
            move = 4
          else
            move = 0
          end
+
        elsif board.turn_count == 2
          if !board.taken?(2)
            move = 1
          else
            move = 3
          end
+
        else Game::WIN_COMBINATIONS.find do |combo|
-          #binding.pry
+
            if combo.select{|cell| board.cells[cell] == token}.length==2
-            #binding.pry
              move = combo.detect{|cell| board.cells[cell] == " "}
            end
 
            if combo.select{|cell| board.cells[cell] != " " }.length==2 || combo.select{|cell| board.cells[cell] != token}.length==2
-            #binding.pry
-             move =combo.detect{|cell| board.cells[cell] == " "}
+             move = combo.detect{|cell| board.cells[cell] == " "}
            end
-
 
           end
         end
-        #binding.pry
         move + 1
+        move.to_s
       end
-
-
-
 
   end
 end
