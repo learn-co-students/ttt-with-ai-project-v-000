fd99daa7c482b192385d61744c79d7f28c1d5b93
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-29 14:44:15 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -20,7 +20,9 @@ module Players
          else
            move = 0
          end
-
+         #binding.pry
+       elsif board.turn_count == 3 && (board.position(1) == board.position(9) || board.position(3) == board.position(7))
+         move = 1
          else Game::WIN_COMBINATIONS.find do |combo|
            if combo.select{|cell| board.cells[cell] == token}.length==2 && combo.select{|cell| board.position(cell) == " "}.length==1
              move = combo.detect{|cell| board.cells[cell] == " "}
