592ffeecbb558c5c959d2015d25d88c615931c15
Jessica Bensch <github email address>
2017-10-31 19:16:35 -0400

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,6 @@
 class Game
 
-  attr_accessor :board, :player_1, :player_2
+  attr_accessor :board, :player_1, :player_2, :human, :player, :board
 
   WIN_COMBINATIONS = [
     [0,1,2],
@@ -13,11 +13,30 @@ class Game
     [6,4,2]
   ]
 
-  def board
-    @board = []
+  def initialize(player_1=Players::Human.new("X"), player_2=Players::Human.new("O"), board=Board.new)
+    @player_1 = player_1
+    @player_2 = player_2
+    @board = board
   end
 
-  def initialize(player_1, player_2, board)
+  def current_player
+    counter = 0
+    board.cells.each do |space|
+      if space == "X" || space == "O"
+        counter += 1
+      end
+    end
+    if counter % 2 == 0
+      return player_1
+    else
+      return player_2
+    end
+  end
+
+  def over?
+     board.cells.all? do |space|
+      space == "X" || space == "O"
+    end
   end
 
 end
