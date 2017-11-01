e8e97597f4053badbb5628f712bb72a63a38bd75
Jessica Bensch <github email address>
2017-10-31 21:43:28 -0400

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -39,4 +39,46 @@ class Game
     end
   end
 
+  def won?
+    WIN_COMBINATIONS.each do |win_combination|
+       win_index_1 = win_combination[0]
+       win_index_2 = win_combination[1]
+       win_index_3 = win_combination[2]
+
+       position_1 = @board.cells[win_index_1]
+       position_2 = @board.cells[win_index_2]
+       position_3 = @board.cells[win_index_3]
+
+       if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O"  && position_3 == "O"
+       return win_combination
+      end
+    end
+    false
+  end
+
+  def draw?
+    if over? && !won?
+      return true
+    else
+      return false
+    end
+  end
+
+  def winner
+    if !won?
+      return nil
+    elsif @board.cells[won?[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
+  end
+
+  def turn
+    input = @player_1.move(board)
+    until @board.valid_move?(input)
+      input = @player_1.move(board)
+    end
+  end
+
 end
