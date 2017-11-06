72925c998a68e84d9f3d4ac4108254f28eb8d6aa
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-11-06 02:53:45 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,20 +1,6 @@
 module Players
   class Computer < Player
 
-
-    # def move(board)
-    #  move = ["1", "2", "3", "4", "5", "6", "7", "8", "9"].detect {|i| !board.taken?(i)}.to_s
-    # end
-
-
-    # def move(board)
-    #   moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    #   moves.detect do |move|
-    #     board.valid_move?(move)
-    #   end
-    #   move
-    # end
-
     def move(board)
       move = nil
       if !board.taken?(5)
@@ -23,6 +9,10 @@ module Players
         move = "3"
       elsif !board.taken?(7)
         move = "7"
+      elsif !board.taken?(9)
+        move = "9"
+      elsif !board.taken?(1)
+        move = "1"
       else
         move = ["1", "2", "3", "4", "5", "6", "7", "8", "9"].detect{ |i| !board.taken?(i)}.to_s
       end
