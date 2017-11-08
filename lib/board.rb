3c4a67f41c3f56957a0d42704e78c9641ae1bc86
Richard Chang <rchang9999@yahoo.com>
2017-11-08 01:56:59 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,57 @@
+class Board
+
+attr_accessor :cells
+
+def initialize
+ @cells = Array.new(9," ")
+
+end
+
+def reset!
+ @cells = Array.new(9," ")
+end
+
+def display
+  puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+  puts "-----------"
+  puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+  puts "-----------"
+  puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+end
+
+def position(input)
+  @cells[input.to_i-1]
+end
+
+def full?
+ @cells.all?{|cell| cell == "X" || cell == "O"}
+
+end
+
+def turn_count
+@cells.select{|cell| cell == "X" || cell == "O"}.length
+end
+
+def taken?(input)
+ if position(input) == "X" || position(input) == "O"
+   true
+ else
+   false
+ end
+ end
+
+ def valid_move?(input)
+   if !taken?(input) && input.to_i.between?(1,9)
+   true
+ else
+   false
+ end
+end
+
+def update(input, player)
+if valid_move?(input)
+  @cells[input.to_i-1] = player.token
+end
+end
+
+end
---
+++ lib/players/human.rb
@@ -0,0 +1,13 @@
+module Players
+  class Human < Player
+
+    def move(board)
+      input = ""
+      input = gets.strip
+      input
+    end
+
+end
+
+
+end
