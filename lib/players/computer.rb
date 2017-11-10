803444214748af2593b764a66c2be1cf74897d9c
Richard Chang <rchang9999@yahoo.com>
2017-11-10 17:12:31 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,13 +1,16 @@
 module Players
   class Computer < Player
     def move(board)
+      #AI's moves
       move = nil
 
-      # When going first, take the middle square. When going second, take the middle square if it isn't yet taken.
+
       if !board.taken?(5)
         move = "5"
+          # go in the middle if empty
       else
         move = [1, 3, 7, 9, 2, 4, 6, 8].detect{|i| !board.taken?(i)}.to_s if move == nil
+        # go corners, then sides
       end
       move
     end
