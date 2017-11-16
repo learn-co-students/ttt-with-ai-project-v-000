ecf2468da2c9c5d97dc99ebc3a9b1fc6a6752c9a
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-16 22:12:33 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -57,6 +57,9 @@ class Game
     end
     board.display
     won? ? (puts "Congratulations #{winner}!") : (puts "Cat's Game!")
+    return 0 if draw?
+    return 1 if winner == "X"
+    return 2 if winner == "O"
   end
 
   def start
@@ -65,27 +68,52 @@ class Game
     input = gets.strip
     if input == "0"
       new_game = Game.new(Players::Computer.new("X"), Players::Computer.new("O"))
+
     elsif input == "1"
-      puts "Who goes first? (p(layer)/c(omp))"
+      puts "Who goes first? p(layer)/c(omp)"
       first = gets.strip
       puts "X or O?"
       piece = gets.strip.upcase
       piece == "X" ? other_piece = "O" : other_piece = "X"
-
-
         if first == "p"
           new_game = Game.new(Players::Human.new(piece), Players::Computer.new(other_piece))
         else
           new_game = Game.new(Players::Computer.new(other_piece), Players::Human.new(piece))
         end
+
     elsif input == "2"
       puts "Player 1 is X or O?"
       piece = gets.strip.upcase
       piece == "X" ? other_piece = "O" : other_piece = "X"
       new_game = Game.new(Players::Human.new(piece), Players::Human.new(other_piece))
+
+    elsif input == "wargames"
+      draw = 0
+      player_1 = 0
+      player_2 = 0
+      10.times do
+        new_game = Game.new(Players::Computer.new("X"), Players::Computer.new("O"))
+        case new_game.play
+        when 0
+          draw += 1
+        when 1
+          player_1 +=1
+        when 2
+          player_2 += 1
+        end
+        puts "Player 1: #{player_1}"
+        puts "Player 2: #{player_2}"
+        puts "Draw: #{draw}"
+        puts ""
+      end
+      puts "Play again? (y/n)"
+      input = gets.strip.downcase
+      start if input == "y"
+      exit if input == "n"
     else
       start
     end
+
     new_game.play
     puts "Play again? (y/n)"
     input = gets.strip
