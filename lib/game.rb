cdf37399f9df4e91a2f1edfbdca44d515a64ba45
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-17 22:41:16 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,16 +1,8 @@
 require 'pry'
 
 class Game
+  include Rules
   attr_accessor :board, :player_1, :player_2
-  WIN_COMBINATIONS = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [6,4,2]]
 
   def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
     @player_1 = player_1
@@ -24,17 +16,6 @@ class Game
 
   def over?
     draw? || won?
-  end
-
-  def won?
-    WIN_COMBINATIONS.detect do |comb|
-      array = [board.cells[comb[0]], board.cells[comb[1]], board.cells[comb[2]]]
-      array == ["X", "X", "X"] || array == ["O", "O", "O"]
-    end
-  end
-
-  def draw?
-    true if board.full? && !won?
   end
 
   def winner
--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,17 +2,7 @@ require 'pry'
 
 module Players
   class Computer < Player
-    attr_accessor :move
-
-    WIN_COMBINATIONS = [
-      [0,1,2],
-      [3,4,5],
-      [6,7,8],
-      [0,3,6],
-      [1,4,7],
-      [2,5,8],
-      [0,4,8],
-      [6,4,2]]
+    include Rules
 
     def move(board)
       puts ""
@@ -23,7 +13,7 @@ module Players
 
     def minimax(board, current_p, depth = 0)
       return 0 if draw?(board)
-      return -1 if game_won?(board)
+      return -1 if won?(board)
       scores = {}
 
       possible_moves(board).each do |move|
@@ -37,21 +27,6 @@ module Players
       else
         highest_score = scores.max_by{|k,v|v}[1]
       end
-    end
-
-    def game_won?(board)
-      WIN_COMBINATIONS.detect do |comb|
-        array = [board.cells[comb[0]], board.cells[comb[1]], board.cells[comb[2]]]
-        array == ["X", "X", "X"] || array == ["O", "O", "O"]
-      end
-    end
-
-    def draw?(board)
-      board_full?(board) && !game_won?(board)
-    end
-
-    def board_full?(board)
-      board.cells.detect {|i| i == " "} ? false : true
     end
 
     def possible_moves(board)
