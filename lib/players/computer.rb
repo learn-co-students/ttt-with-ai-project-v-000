15efa6fd8b099da33ee3dca68178c0797819d02e
Travis Petersen <travis.e.petersen@gmail.com>
2017-11-17 01:26:35 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,7 +1,20 @@
 module Players
   class Computer < Player
     def move(board)
-      computer_move = '5' if board.valid_move?('5')
+      if board.valid_move?('5')
+        computer_move = '5'
+      elsif board.valid_move?('1')
+        computer_move = '1'
+      elsif board.valid_move?('3')
+        computer_move = '3'
+      elsif board.valid_move?('7')
+        computer_move = '7'
+      elsif board.valid_move?('9')
+        computer_move = '9'
+      else
+        ['2','4','6','8'].each { |n| computer_move = n if board.valid_move?(n) }
+        computer_move
+      end
     end
   end
 end
