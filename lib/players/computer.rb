244f7c09d7a7db02b254e1bea85ce93978b17ad0
Brittany Hartmire <bkhartmire@gmail.com>
2017-11-30 07:47:21 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,8 +1,8 @@
 module Players
   class Computer < Player
     def move(board = nil)
-      puts "Please enter your move:"
-      input = gets.chomp
+      valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+      corners = ["1", "3", "7", "9"]
     end
   end
 end
