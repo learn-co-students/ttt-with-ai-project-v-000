7de3b76fdfdd9b1de9d47ccf68f38217fe4bb558
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-11-30 15:56:43 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,6 @@
 class Game
 
-  attr_accessor :board, :player_1, :player_2
+  attr_accessor :board, :player_1, :player_2, :current_player
 
   WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
 
@@ -12,9 +12,9 @@ class Game
 
   def current_player
     if board.turn_count % 2 == 0
-      player_1
+      @current_player = player_1
     else
-      player_2
+      @current_player = player_2
     end
   end
 
