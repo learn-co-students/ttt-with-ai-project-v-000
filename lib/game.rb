25624de939407f14a137f3f4cb2daa76e82a9a9c
Brittany Hartmire <bkhartmire@gmail.com>
2017-11-30 05:40:36 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -11,13 +11,38 @@ class Game
     [6,4,2]
   ]
 
-  def board
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+    @player_1 = player_1
+    @player_2 = player_2
+    @board = board
   end
 
   def current_player
+    if self.board.turn_count % 2 == 0
+      @player_1
+    else
+      @player_2
+    end
+  end
+
+  def over?
+    !self.board.cells.any?{|cell| cell == " "}
   end
 
   def won?
+    WIN_COMBINATIONS.each do |combo|
+      win_index_1 = combo[0]
+      win_index_2 = combo[1]
+      win_index_3 = combo[2]
+      position_1 = self.board.cells[win_index_1]
+      position_2 = self.board.cells[win_index_2]
+      position_3 = self.board.cells[win_index_3]
+      if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+        return combo
+      end
+    end
+  else
+    false
   end
 
   def winner
