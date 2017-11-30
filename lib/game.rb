925189f7dc200c5486909b79c2c5e68c22c532de
Brittany Hartmire <bkhartmire@gmail.com>
2017-11-30 04:00:53 +0000

    Done.

---
+++ lib/game.rb
@@ -0,0 +1,35 @@
+class Game
+  attr_accessor :board, :player_1, :player_2
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2]
+  ]
+
+  def board
+  end
+
+  def current_player
+  end
+
+  def won?
+  end
+
+  def winner
+  end
+
+  def start
+  end
+
+  def play
+  end
+
+  def turn
+  end
+
+end
