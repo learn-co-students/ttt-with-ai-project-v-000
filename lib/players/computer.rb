aa58375d6d777063d18403d2fd857ece5d5198d5
Sherry El Garhy <sherryptk@gmail.com>
2017-12-03 14:01:38 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,9 +3,17 @@ module Players
   class Computer < Player
 
     def move(board)
-      rand(0...8).to_s
+      # rand(0...8).to_s
+      if !board.taken?(5)
+        move = "5"
+      elsif !board.taken?(1)
+        move = "1"
+      else
+        move = [1, 3, 7, 9].detect{|i| !board.taken?(i)}.to_s
+      else
+        move = [2, 4, 6, 8].detect{|i| !board.taken?(i)}.to_s
+      end
     end
-
   end
 end
 
