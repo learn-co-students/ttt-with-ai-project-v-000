0620a3acc832b939b6263f4d93e7ea4ff517184f
Sherry El Garhy <sherryptk@gmail.com>
2017-12-03 01:40:29 +0000

    Done.

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,6 +1,14 @@
 
-class Human < Player
+module Players
+  class Human < Player
 
+    def move(board)
+      puts "Please enter your move"
+      gets.strip
+    end
 
-
+  end
 end
+
+
+# rspec spec/03_human_player_spec.rb
