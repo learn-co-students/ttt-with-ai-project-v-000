f3ce30788d31e1d49f3c6f32ceeb052e5f020880
Blakeley Bond <burrick2003@yahoo.com>
2017-12-03 17:54:19 +0000

    Done.

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -2,7 +2,7 @@ module Players
 
   class Human < Player
 
-    def move(board) #why does the test have an argument?  Because computer needs it.
+    def move(board) #why does the test require an argument?  Because computer needs it.
       @board.display
       puts "Greetings, Human.  Please enter your flawed move, 1-9."
       input = gets.strip
