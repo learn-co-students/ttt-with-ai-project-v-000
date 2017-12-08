e617602dbe64e2b7bac28904e3823588a88f5b29
github username <github email address>
2017-12-08 14:52:17 -0600

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -4,7 +4,7 @@ module Players
         attr_accessor :player_1, :player_2, :board
 
         def move(board)
-            [1, 2, 3, 4, 5, 6, 7, 8, 9].find {|i| !board.taken?(i)}.to_
+            [1, 2, 3, 4, 5, 6, 7, 8, 9].find {|i| !board.taken?(i)}.to_s
         end 
     end
 end 
