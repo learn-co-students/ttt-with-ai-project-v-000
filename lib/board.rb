7def3087ae021f29d100750727db6ed1cb55e4cb
Liz Burton <liz.burton147@gmail.com>
2017-12-11 13:41:36 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,18 +1,38 @@
 class Board
-attr_accessor :cells
+  attr_accessor :cells
 
-cells = []
+  @cells = []
+  turn_count = 0
 
-def initialize
-  @cells = Array.new(9," ")
-end
+  def initialize
+    @cells = Array.new(9," ")
+  end
 
-def reset!
-  self.cells = Array.new(9," ")
-end
+  def reset!
+    self.cells = Array.new(9," ")
+  end
 
-def display
+  def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
 
-end
+  def position(user_input)
+    cells[user_input.to_i-1]
+  end
 
+  def full?
+    self.cells.all? { | entry | entry == "X" || position == "O" }
+  end
+
+  def turn_count
+    self.cells.count{ | x | x == "X" || x == "O" }
+  end
+
+  # def taken?(position)
+  #   self.cells[position.to_i-1] == "X" || self.cells[position.to_i-1] == "O"
+  # end
 end
