a08f23a078233c9693585cc67c6343a6a2ade5cd
Chad Montoya <chadmontoya21@yahoo.com>
2017-12-12 02:11:34 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,6 @@
+require 'pry'
 class Game
+  attr_accessor :board, :player_1, :player_2
 
   WIN_COMBINATIONS = [
     [0,1,2], [3,4,5], [6,7,8],
@@ -6,5 +8,18 @@ class Game
     [0,4,8], [2,4,6]
   ]
 
+  def initialize(player_1 = Players::Human.new('X'), player_2 = Players::Human.new('O'), board = Board.new)
+    self.player_1 = player_1
+    self.player_2 = player_2
+    self.board = board
+  end
+
+  def current_player
+    self.board.turn_count.even? ? self.player_1 : self.player_2
+  end
+
+  def won?
+    WIN_COMBINATIONS
+  end
 
 end
