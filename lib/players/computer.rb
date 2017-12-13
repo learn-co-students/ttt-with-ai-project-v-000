e129696268fb3d13ab521236096e914ee1df6b33
Jacqueline Lee <anginblu@gmail.com>
2017-12-13 06:54:13 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -6,7 +6,7 @@ class Players
       possible_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
       possible_moves.delete_if {|number| !board.valid_move?(number)}
 
-      if !board.valid_move?("5")
+      if board.valid_move?("5")
         "5"
       else
         possible_moves[0]
