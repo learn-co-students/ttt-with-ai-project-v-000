1552308c0c184c62341e41fceae93d99280e36bf
Chad Montoya <chadmontoya21@yahoo.com>
2017-12-14 05:13:06 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -5,24 +5,33 @@ module Players
     def move(board)
       # if !board.taken?("5")
       #   "5"
-        binding.pry
       board.cells.collect.with_index do |v,i|
          (i+1).to_s if v == " "
        end.compact.sample
     end
 
+    def combos(board)
+      Game::WIN_COMBINATIONS.select do |x|
+        x.any?{|i| board.cells[i] == token}
+       end
+    end
+
+    def best_choice(board)
+      unless combos(board).empty?
+      combos(board).select do |i|
+         if board.cells[i[0]] && board.cells[i[1]] == token || board.cells[i[0]] && board.cells[i[2]] == token || board.cells[i[1]] && board.cells[i[2]] == token
+            i
+         elsif board.cells[i[0]] == token
+         end
+       end
+     end
+    end
+
   end
 end
 
-m = Game::WIN_COMBINATIONS.select do |x|
-  x.any?{|i| board.cells[i] == token}
- end
+#  (f.select{|x| board.cells[x] != token}[0] + 1).to_s
 
-f = m.select do |i|
-   if board.cells[i[0]] && board.cells[i[1]] == token || board.cells[i[0]] && board.cells[i[2]] == token || board.cells[i[1]] && board.cells[i[2]] == token
-     i
-   elsif board.cells[i[0]] == token
-   end
- end.flatten
-
- f.select{|x| board.cells[x] != token}[0] + 1
+ # l = Game::WIN_COMBINATIONS.collect do |x|
+ #    x.count(8).sum
+ #  end
