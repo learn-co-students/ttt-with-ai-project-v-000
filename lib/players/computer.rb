7d8f7cbce99d8dbe241a935d44a414fd3a07c599
Liz Burton <liz.burton147@gmail.com>
2017-12-14 14:56:48 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -4,35 +4,48 @@ module Players
 
     def move(board)
       @available = []
-      board.cells.each.with_index do | position, index |
-        if board.valid_move?( index + 1 )
-          @available << ( index + 1 ).to_s
-        end
-      end
-      computer_move = @available.sample
-    end
+      if check_for_tictac(board)
+        computer_move = check_for_tictac(board)
+        binding.pry
+      else
+        board.cells.each.with_index do | position, index |
+          if board.valid_move?( index + 1 )
+            @available << ( index + 1 ).to_s
+          end
+        end #board.cells.each
+      end #if/else
+      computer_move = @available.sample.to_s
+    end # #move(board)
 
-    def check_tictac(board)
-  #    for each win combo, check board.cells[positions associated with that combo] to see if 2 of them are the same (X or O)
+    def check_for_tictac(board)
       Game::WIN_COMBINATIONS.each do | combo |
         check_board = []
         combo.each do | slot |
           check_board << board.cells[slot]
-          binding.pry
         end
         if (check_board.count("X") == 2 || check_board.count("O") == 2) && check_board.count(" ") == 1
           puts "tictac in combo #{combo}"
+          combo.each do | board_index |
+            if game.board.valid_move?(board_index+1)
+              board_index+1.to_s
+            end
+          end #combo.each
+        else
+          nil
           binding.pry
-          play_toe
-        end
-      end
-    end
+        end #if/else
+      end #WIN_COMBINATIONS.each
 
-    def play_toe
-    end
+    end #check_for_tictac
+
+    # def play_toe(combo)
+    #   combo.each do |position|
+    #     if game.board.valid_move?
+    #       computer_move = position + 1
+    #     end
+    #   end
+    # end
 
   end # Class Computer
 
 end # Module Players
-# game = Game.new
-#
