57e3da70631a9c8000ddaf232a0e71dfaefa5dc5
Liz Burton <liz.burton147@gmail.com>
2017-12-14 12:39:20 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,6 +1,6 @@
 module Players
   class Computer < Player
-    attr_accessor :available
+    attr_accessor :available, :combo
 
     def move(board)
       @available = []
@@ -11,5 +11,21 @@ module Players
       end
       computer_move = @available.sample
     end
-  end
-end
+
+    def check_tictac(board)
+      check_board = []
+  #    for each win combo, check board.cells[positions associated with that combo] to see if 2 of them are the same (X or O)
+      Game::WIN_COMBINATIONS.each do | combo |
+        combo.each do | slot |
+          check_board << game.cells[slot]
+        end
+        if check_board.count("X") == 2 || check_board.count("O") == 2
+          play.toe
+        end
+      end
+    end
+
+  end # Class Computer
+
+end # Module Players
+board = ["X","X"," ","O"," "," ","O"," "," "]
