4b484cf1a5db01ebe7df79999ffcb5250b468986
Hannah Carnes <hecarnes@gmail.com>
2017-12-14 21:41:15 -0500

    23 left

--- lib/game.rb
+++ lib/game.rb
@@ -1,12 +1,12 @@
 class Game
-  attr_accessor :board, :player_1, :player_2
+  attr_accessor :player_1, :player_2, :board
 
-  def initialize
-    @player_1 = self.player_1
-    @player_2 = self.player_2
-    @player_3 = self.player_3
+  def initialize(player_1=Players::Human.new("X"), player_2=Players::Human.new("O"), board=Board.new)
+    @player_1 = player_1
+    @player_2 = player_2
+    @board = board
   end
-  
+
   WIN_COMBINATIONS = [
     [0, 1, 2],
     [3, 4, 5],
@@ -17,10 +17,28 @@ class Game
     [0, 4, 8],
     [6, 4, 2],
   ]
-
-  def board
-    @board = []
+  
+  def current_player
+    @board.turn_count.even? ? @player_1 : @player_2
   end
 
-  
+  def full?
+    @board.cells.all? {|board_position| %w[X O].include?(board_position)}
+  end
+
+  def won?
+    WIN_COMBINATIONS.detect do |win_combination|
+      win_combination.all? {|win_index| @board.cells[win_index] == @player_1 } ||
+      win_combination.all? {|win_index| @board.cells[win_index] == @player_2 }
+    end
+  end
+
+  def draw?
+    !won? && full?
+  end
+
+  def over?
+    won? || draw?
+  end
+
 end
