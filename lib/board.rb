df46817dd1bdb00091b836561d7c7f91467ecf89
Hannah Carnes <hecarnes@gmail.com>
2017-12-17 13:13:21 -0500

    add color

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,4 @@
+require 'colorize'
 class Board
   attr_accessor :cells
 
@@ -10,11 +11,22 @@ class Board
   end
 
   def display
-    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts " #{colorize_cell(@cells[0])} | #{colorize_cell(@cells[1])} | #{colorize_cell(@cells[2])} "
     puts "-----------"
-    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts " #{colorize_cell(@cells[3])} | #{colorize_cell(@cells[4])} | #{colorize_cell(@cells[5])} "
     puts "-----------"                                                                            
-    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+    puts " #{colorize_cell(@cells[6])} | #{colorize_cell(@cells[7])} | #{colorize_cell(@cells[8])} "
+  end
+
+  def colorize_cell(cell)
+    case cell
+    when "X"
+      cell.light_cyan
+    when "O"
+      cell.red
+    else
+      cell.green
+    end
   end
 
   def user_input_to_index(user_input)
