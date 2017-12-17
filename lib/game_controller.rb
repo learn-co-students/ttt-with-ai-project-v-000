ed2589beee6428aa3450f64360eeea6790c093aa
Hannah Carnes <hecarnes@gmail.com>
2017-12-17 13:32:43 -0500

    allow play again and use case statement

--- lib/game_controller.rb
+++ lib/game_controller.rb
@@ -1,9 +1,9 @@
 require 'io/console'
 class GameController
 
-  def initialize()
+  def initialize(clear_screen: true)
     # clear screen
-    puts "\e[H\e[2J"
+    puts "\e[H\e[2J" if clear_screen
 
     puts "Welcome to Tic-Tac-Toe!"
     setup_players
@@ -13,6 +13,8 @@ class GameController
 
   def play_game
     @game.play
+    puts "*"*18
+    GameController.new(clear_screen: false)
   end
 
   private
@@ -20,22 +22,28 @@ class GameController
   def setup_players
     input = nil
     # Until user enters yes or no
-    until input =~ /1|2|3/ do
+    until input =~ /1|2|3|x/ do
       puts "1. Play against the computer"
       puts "2. Play against a friend"
       puts "3. Computer versus computer"
+      puts "x. Exit"
 
       input = STDIN.getch
     end
-    if input == "1"
+
+    case input
+    when "1"
       @player_1 = Players::Human.new("X")
       @player_2 = Players::Computer.new("O")
-    elsif input == "2"
+    when "2"
       @player_1 = Players::Human.new("X")
       @player_2 = Players::Human.new("O")
-    elsif input == "3"
+    when "3"
       @player_1 = Players::Computer.new("X")
       @player_2 = Players::Computer.new("O")
+    when "x"
+      puts "Let's play again later!"
+      Kernel.exit
     end
   end
 end
