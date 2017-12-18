fbbf34fe4321ec53eee2314e4e651b3a5765ce1e
Alyssa Clair <clairaly13@gmail.com>
2017-12-18 17:33:34 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,7 @@
 class Game
+  
   attr_accessor :board, :player_1, :player_2
-  #Initialize with three optional arguments, new board, instances of humans
+
   def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
     @player_1 = player_1
     @player_2 = player_2
