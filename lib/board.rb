8ec4a42ed47cae6b2fdc29115b0fe82d0458c512
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-22 17:13:37 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -21,28 +21,35 @@ attr_accessor :cells
         end
 
 
-        def position
+        def position(input)
         #takes in user input and returns the value of the board cell
+         cells[input.to_i-1]
         end
 
         def full? #returns true for a full board
           #returns false for an in-progress game
+            cells.all?{|token| token == "X" || token == "O"}
+
         end
 
         def turn_count #returns the amount of turns based on cell value
+            cells.count{|token| token == "X" || token == "O"}
         end
 
-        def taken?
+        def taken?(input)
           # returns true if the position is X or O
           # returns false if the position is empty or blank
+          !(position(input) == " " || position(input) == "")
         end
 
-        def valid_move?
+        def valid_move?(input)
         # returns true for user input between 1-9 that is not taken
+            input.to_i.between?(1,9) && !taken?(input)
         end
 
-        def update
+        def update(input, player)
         # updates the cells in the board with the player token according to the input
+          cells[input.to_i-1] = player.token
         end
 
 
