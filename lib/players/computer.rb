117b28b4e77f45dca7346454a5ea2c5cf0323cf5
Annette <micha232@umn.edu>
2017-12-23 21:22:32 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -11,29 +11,31 @@ module Players
         def move(board)
             # board.cells = ["X","X", " ", " ", " ", " ", " ", " ", " "]
             x = almost_won?(board)
+            
+            ## if the next move will be a winning move do that else move to block current player's two in a row
           if x != nil
               n = x.detect {|i| board.cells[i] == " "}
               n += 1
                
               return n.to_s
+              
+              ##if no almost_won move exists randomely pick a spot to play
+              ##this can be improved to choose a spot with strategy instead of randomness
           else
-            n = (1..9).to_a.sample.to_s
-            n
+            (1..9).to_a.sample.to_s
             end
         end 
         
         ##AI Methods  
-        ##refactor to look pretty later
+        ##refactor to look pretty  and use higher order methods later
+        
+        ##check to see if there is a move for either player that will cause a win.
         def almost_won?(board)
             maybe_move = nil
             Game::WIN_COMBINATIONS.each do |combo| 
               c = match_two(combo, board)
-              if c == self.token
-                  return combo
-              end
-              if c != nil
-                  maybe_move = combo
-              end
+              return combo if c == self.token
+              maybe_move = combo if c != nil
           end
           maybe_move
         end
