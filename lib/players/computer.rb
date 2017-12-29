5a74c9870149712782b49a772444cac635225afd
github username <github email address>
2017-12-29 14:04:45 -0600

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,48 +1,128 @@
 module Players
     class Computer < Player
 
+def move(board)
+  move = nil
+  if array[5].empty?
+    return move = "5"
+  elsif board.turn_count == 1
+    return move = ["1", "3", "7", "9"].sample
+  elsif board.turn_count == 2
+    return move = ["1", "3", "7", "9"].detect {|i| !board.taken?(i)}
+#turn_count 3, then start checking win_combinations and tracking and check if there
+#are two squares
+end
+              def move(board)
+                move = nil
+          
+                # When going first, take the middle square. When going second, take the middle square if it isn't yet taken.
+                if !board.taken?(5)
+                  move = "5"
+          
+                # If going second and the middle square is taken, take the upper-left corner square.
+                elsif board.turn_count == 1
+                  move = "1"
+          
+                # If you went first (and took the middle), take a corner square with your second move.
+                elsif board.turn_count == 2
+                  move = [1, 3, 7, 9].detect{|i| !board.taken?(i)}.to_s
+          
+                # If you went second (and took the middle) and the other player has occupied opposing corner squares, blow up the attempted trap by taking a side square.
+                elsif board.turn_count == 3 && (board.position(1) == board.position(9) || board.position(3) == board.position(7))
+                  move = "2"
+          
+                # From here on, run through the WIN_COMBINATIONS array, checking whether any of the combinations have two squares filled with the same token and a third, empty square.
+                else
+                  Game::WIN_COMBINATIONS.detect do |cmb|
+          
+                    # First, check whether you have any chances to win, since it doesn't matter whether the opponent has a chance to win if you can win first.
+                    if cmb.select{|i| board.position(i+1) == token}.size == 2 && cmb.any?{|i| board.position(i+1) == " "}
+                      move = cmb.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
+          
+                    # If you can't play any winning moves, play a move to block the opponent from winning.
+                    elsif cmb.select{|i| board.position(i+1) != " " && board.position(i+1) != token}.size == 2 && cmb.any?{|i| board.position(i+1) == " "}
+                      move = cmb.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
+                    end
+                  end
+          
+                  # If none of the WIN_COMBINATIONS patterns have two squares taken by the same token and a third empty square, play into the first open square you find, first checking corners and then checking sides.
+                  move = [1, 3, 7, 9, 2, 4, 6, 8].detect{|i| !board.taken?(i)}.to_s if move == nil
+                end
+                move
+              end
+            end
+          end
+          
+
+
+#         def move(array)
+#             valid_moves = []
+#             i = 0
+#             until i > 8
+#                if array.valid_move?(i) == true
+#                  valid_moves << i
+#                end
+#                 i += 1
+#              end
+#                 array_high_values = [0, 2, 4, 6, 8]
+#                 lesser_moves = [1, 3, 5, 7]
+#                 high_value = array_high_values.detect do |value|
+#                 valid_moves.include?(value)
+#             end
+
+#                 if high_value == nil
+#                     low_value = lesser_moves.detect do |value2| valid_moves.include?(value2)
+#                   low_value
+#                 else
+#                     high_value
+#                 end
+#             end
+#         end
+#     end
+# end
+
+#         def move(array)
+#             valid_moves = []
+#             i = 0
+#             until i > 8
+#                 if array.valid_move?(i) == true
+#                     valid_moves << i
+#                 end
+#                 i += 1
+#                 end
+#         valid_moves.sample.to_s
+#     end
+#  end
+        
+#             # class Computer::Easy < Player
+
+        
+#     class Computer::Hard < Player
+
         def move(array)
             valid_moves = []
             i = 0
             until i > 8
-                if array.valid_move?(i) == true
-                    valid_moves << i
-                end
+               if array.valid_move?(i) == true
+                 valid_moves << i
+               end
                 i += 1
+             end
+                array_high_values = [0, 2, 4, 6, 8]
+                # lesser_moves = [1, 3, 5, 7]
+                high_value = array_high_values.detect do |value|
+                valid_moves.include?(value)
+            end
+
+                if high_value == nil
+                    # low_value = lesser_moves.detect do |value2| valid_moves.include?(value2)
+                #   low_value
+                    else 
+                    high_value
                 end
-            valid_moves.sample.to_s
             end
         end
-end
-        
-            # class Computer::Easy < Player
-
-        
-    # class Computer::Hard < Player
-
-    #     def move(array)
-    #         valid_moves = []
-    #         i = 0
-    #         until i > 8
-    #            if array.valid_move?(i) == true
-    #              valid_moves << i
-    #            end
-    #             i += 1
-    #          end
-    #             array_high_values = [0, 2, 4, 6, 8]
-    #             lesser_moves = [1, 3, 5, 7]
-    #             high_value = array_high_values.detect do |value|
-    #             valid_moves.include?(value)
-    #         end
-
-    #             if high_value == nil
-    #                 lesser_moves = [1, 3, 5, 7]
-    #             else
-    #                 high_value
-    #             end
-            # end
-        # end
-    # end
+    end
 end
 
               
@@ -165,3 +245,17 @@ end
         #     [2,4,6]  
         #   ]
 
+# #first move always middle (5), check if position is taken
+# def move(board)
+#   move = nil
+# #if middle square is empty, we want to take middle square
+#   if array[5].empty?
+#     return move = "5"
+#   elsif board.turn_count == 1
+#     return move = ["1", "3", "7", "9"].sample
+#   elsif board.turn_count == 2
+# #we want to find the first index that is not taken
+#     return move = ["1", "3", "7", "9"].detect {|i| !board.taken?(i)}
+# #turn_count 3, then start checking win_combinations and tracking and check if there
+# #are two squares
+# end
