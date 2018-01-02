8acfb8d75a322ab808fa6c4e199cfcdf2158057c
James Ferguson <ncjamesferguson@gmail.com>
2018-01-02 20:25:56 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -21,12 +21,44 @@ class Computer < Player
     # TURN ONE: takes the top left corner
     if board.turn_count == 0
       "1"
+
+    # TURN TWO (GOIN SECOND)
+  elsif board.turn_count == 1
+      if (CORNERS.any? {|corner|  board.taken?(corner)})
+        filled_corner = CORNERS.find {|corner| board.taken?(corner)}
+
+        case filled_corner
+        when 1
+          return "2"
+        when 3
+          return "2"
+        else
+          return "8"
+        end
+      else
+        corner_check(board)
+      end
+
     # TURN THREE: takes the bottom right corner, or the top right corner.
     elsif board.turn_count == 2
-      if board.taken?(9)
+      if !board.taken?(9)
+        "9"
+      elsif !board.taken?(3)
         "3"
       else
-        "9"
+        any_move(board)
+      end
+    #Turn Four: Checks for combos, otherwise takes the central square.
+    elsif board.turn_count == 3
+
+      if combo_check(board) != nil
+        combo_check(board)
+      elsif !board.taken?(5)
+          "5"
+      elsif (corner_check(board) != nil)
+        corner_check(board)
+      else
+        any_move(board)
       end
     #TURN FIVE: Checks to see if it can win, otherwise makes another move.
 
@@ -45,15 +77,12 @@ class Computer < Player
 
   #Checks to see if computer can win game, or if opponent is in position to win the game, and acts accordingly.
   def combo_check(board)
-    WIN_COMBINATIONS_2.find do |combo|
-      if board.taken?(combo[0]) && board.position(combo[0]) == board.position(combo[1])
-        puts combo[2]
+    WIN_COMBINATIONS_2.each do |combo|
+      if board.taken?(combo[0]) && board.position(combo[0]) == board.position(combo[1]) && !board.taken?(combo[2])
         return combo[2].to_s
-      elsif board.taken?(combo[1]) && board.position(combo[1]) == board.position(combo[2])
-        puts combo[0]
+      elsif board.taken?(combo[1]) && board.position(combo[1]) == board.position(combo[2]) && !board.taken?(combo[0])
         return combo[0].to_s
-      elsif board.taken?(combo[2]) && board.position(combo[0]) == board.position(combo[2])
-        puts combo[1]
+      elsif board.taken?(combo[2]) && board.position(combo[0]) == board.position(combo[2]) && !board.taken?(combo[1])
         return combo[1].to_s
       end
     end
