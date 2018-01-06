a0f3f6f038c098623b5f0e7dfc47c0e03bb305b9
ElleeB <laura.trager@gmail.com>
2018-01-05 21:08:50 -0500

    First Update Since Fix

--- lib/game.rb
+++ lib/game.rb
@@ -26,75 +26,46 @@ class Game
   end
 
   def draw?
-    #board.full? == true && board.cells.count("X") == board.cells.count("O") ? true : false
+    if won? == nil && self.board.full? == true
+      true
+    else
+      false
+    end
   end
-
-  #    returns the correct winning combination in the case of a win
-  #    Failure/Error: expect(game.won?).to contain_exactly(2, 5, 8)
-  #      expected a collection that can be converted to an array with `#to_ary` or `#to_a`, but got false
-
-  #    isn't hard-coded
-  #    Failure/Error: expect(game.won?).to contain_exactly(0, 1, 2)
-  #      expected a collection that can be converted to an array with `#to_ary` or `#to_a`, but got false
 
   def won?
+    @cells = self.board.cells
+    result = WIN_COMBINATIONS.find_all do |win_index|
 
-    cells = self.board.cells
+      position_1 = @cells[win_index[0]]
+      position_2 = @cells[win_index[1]]
+      position_3 = @cells[win_index[2]]
 
-    if self.board.full? != true
-      false
-
-    else
-    WIN_COMBINATIONS.detect do |combo|
-
-      position_1 = cells[combo[0]]
-      position_2 = cells[combo[1]]
-      position_3 = cells[combo[2]]
-
-      if
-        position_1 == "X" && position_2 == "X" && position_3 == "X" ||
-        position_1 ==
-
-      end
-
+      position_1 == "X" && position_2 == "X" && position_3 == "X" ||
+      position_1 == "O" && position_2 == "O" && position_3 == "O"
     end
-
+    result.last
   end
 
-#  def over?
-#    if won? == true
-#      true
-#    elsif
-#      won? == false && draw == true
-#      true
-#    elsif
-
-#  end
+  def over?
+    if won? !=nil
+      true
+    elsif
+      draw? == true
+      true
+    end
+  end
 
   def winner
   end
 
   def start
-    ###Testing!!!
   end
 
   def play
-    #######################ADFGVAERF AEGAERGAERG
   end
 
   def turn
-    #I AM A COMMENT!!!!!
   end
 
 end
-
-#WIN_COMBINATIONS = [
-#  [self.board.cells[0], self.board.cells[1], self.board.cells[2]],
-#  [self.board.cells[3], self.board.cells[4], self.board.cells[5]],
-#  [self.board.cells[6], self.board.cells[7], self.board.cells[8]],
-#  [self.board.cells[0], self.board.cells[4], self.board.cells[8]],
-#  [self.board.cells[2], self.board.cells[4], self.board.cells[6]],
-#  [self.board.cells[0], self.board.cells[3], self.board.cells[6]],
-#  [self.board.cells[1], self.board.cells[4], self.board.cells[7]],
-#  [self.board.cells[2], self.board.cells[5], self.board.cells[8]]
-#]
