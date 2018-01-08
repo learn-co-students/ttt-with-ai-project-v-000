bab6fc222b88314579e75849c6d2f0c590840dae
ElleeB <laura.trager@gmail.com>
2018-01-08 13:45:28 -0500

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -39,15 +39,7 @@ class Board
   end
 
   def update(input, player)
-
-    #if valid_move?(input)
-      @cells[input.to_i - 1] = player.token
-    #elsif
-      #puts "Please enter a valid move, 1-9"
-    #else
-      #nil
-    #end
-
+    @cells[input.to_i - 1] = player.token
   end
 
 end
