cc64ad59c099b0e2a1654496452f3ac438a55ec4
ElleeB <laura.trager@gmail.com>
2018-01-08 13:59:13 -0500

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -15,14 +15,28 @@ module Players
       @valid_moves[player_move.to_i - 1] = player_move
       player_move
 
-      #strategy coming:
-      #if two next to each other, block/put next to
-      #if sandwich, place between
-      #if corner, put in center
-      #if center, put in corner
-
     end
 
   end
 
 end
+
+
+# strategy coming:
+
+  # if two next to each other, block/put adjecent to
+    # H [[1, 2], [2, 3], [4, 5], [5, 6], [7, 8], [8, 9]]
+    # V [[1, 4], [2, 5], [3, 6], [4, 7], [5, 8], [6, 9]]
+
+  # if sandwich, place between
+    # H [[1, 3], [4, 6], [7, 9]]
+    # V [[1, 7], [2, 8], [3, 9]]
+    # D [[1, 9], [3, 7]]
+
+  # if corner, put in center
+    # 1 || 3 || 7 || 9
+
+  # if center, put in corner
+    # 5
+
+  # else random or adjacent...
