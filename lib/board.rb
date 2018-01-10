f457e17acb7ea069e4528db2f41087f205d1ba8e
Remmy Clay <remmy.lauren@gmail.com>
2018-01-10 02:08:17 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -3,13 +3,45 @@ class Board
 
   attr_accessor :cells
 
-  def cells
-    @cells = []
-  end
-
   def reset!
-    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-    binding.pry
+    self.cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   end
 
+  def initialize
+    self.reset!
+  end
+
+  def display
+    puts " #{self.cells[0]} | #{self.cells[1]} | #{self.cells[2]} "
+    puts "-----------"
+    puts " #{self.cells[3]} | #{self.cells[4]} | #{self.cells[5]} "
+    puts "-----------"
+    puts " #{self.cells[6]} | #{self.cells[7]} | #{self.cells[8]} "
+  end
+
+  def position(input)
+    i = input.to_i - 1
+    self.cells[i]
+  end
+
+  def full?
+    @cells.all? {|token| token == "X" || token == "O"}
+  end
+
+  def turn_count
+    @cells.count{|token| token == "X" || token == "O"}
+  end
+
+  def taken?(input)
+    position(input) == "X" || position(input) == "O"
+  end
+
+  def valid_move?(input)
+    input.to_i.between?(1, 9) && position(input) == " "
+  end
+
+  def update(token, player)
+
+  end
+  
 end
