ce7121046813c97f0eef5667bc53b10d82207f39
Rebecca Tong <rtong@buffalo.edu>
2018-01-10 19:17:46 +0000

    board class

---
+++ lib/board.rb
@@ -0,0 +1,37 @@
+class Board
+
+  attr_accessor :cells
+
+  def initialize
+    @cells = []
+  end
+
+  def reset!
+  end
+
+
+  def display
+  end
+
+  def position
+  end
+
+  def update
+  end
+
+  def token
+  end
+
+  def full?
+  end
+
+  def turn_count
+  end
+
+  def taken?
+  end
+
+  def valid_move?
+  end
+
+end
