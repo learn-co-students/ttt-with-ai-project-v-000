49265d1e2ae95adbae84d9ed22b14b5a506c0c5b
Remmy Clay <remmy.lauren@gmail.com>
2018-01-11 01:04:39 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -30,7 +30,6 @@ class Game
   def won?
     win_combo = WIN_COMBINATIONS.find{|combo| board.cells[combo[0]] == board.cells[combo[1]] && board.cells[combo[1]] == board.cells[combo[2]] && board.cells[combo[0]] != " "}
     win_combo == nil ? false : win_combo
-    #WIN_COMBINATIONS.find{|c| board.cells[c[0]] == board.cells[c[1]] && board.cells[c[0]] == board.cells[c[2]]}
   end
 
   def draw?
@@ -45,9 +44,12 @@ class Game
 
   def turn
     player = current_player
-    player_input = player.move(self.board.cells)
-    if board.valid_move?(player_input)
-      board.update(player_input, player)
+    puts "It's #{player.token}'s move."
+    board.display
+    player_move = player.move(self.board) #self.board.cells
+    #computer.move(board) not working
+    if board.valid_move?(player_move)
+      board.update(player_move, player)
     else
       puts "Sorry, that move is not valid. Please enter another position."
       turn
@@ -60,6 +62,49 @@ class Game
       puts "Congratulations #{winner}!"
     elsif draw?
       puts "Cat's Game!"
+    end
+  end
+
+  def start
+    puts "Hi, Welcome to Remmy's Tic Tac Toe!"
+    puts "How would you like to play? 0, 1 or 2 players."
+    puts "0 - Computer vs Computer"
+    puts "1 - Person vs Computer"
+    puts "2 - Person vs Person"
+    mode_number = gets
+
+    if mode_number == "0" # computer vs computer
+      player_1 = Players::Computer.new("X")
+      player_2 = Players::Computer.new("Y")
+
+    elsif mode_number == "1" #person vs computer
+      puts "Who should go first? This player will be represented by \"X\"."
+      puts "1 - Person"
+      puts "2 - Computer"
+      player_i = gets
+      if player_i == "1"
+        player_1 = Players::Human.new("X")
+        player_2 = Players::Computer.new("Y")
+      elsif player_i == "2"
+        player_1 = Players::Computer.new("X")
+        player_2 = Players::Human.new("Y")
+      end
+
+    elsif mode_number == "2" #person vs person
+      player_1 = Players::Human.new("X")
+      player_2 = Players::Human.new("Y")
+    end
+
+    puts "Let's begin!"
+    Game.new(player_1 = player_1, player_2 = player_2, board = Board.new)
+    play
+
+    puts "Would you like to play again? (Y/N)"
+    play_again_input = gets
+    if play_again_input == "Y"
+      start
+    elsif play_again_input == "N"
+      exit
     end
   end
 
--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,5 +1,13 @@
+require 'pry'
+
 module Players
+
   class Computer < Player
+
+    def move(board)
+      i = board.cells.find_index {|cell| cell == " "}
+      (i + 1).to_s
+    end
 
   end
 end
