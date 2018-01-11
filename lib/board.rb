5630209f87532fd815a98d6de0bfd398069acd88
Neil Ryan <neilmarkoliverryan@gmail.com>
2018-01-10 20:41:10 -0800

    Update board.rb

--- lib/board.rb
+++ lib/board.rb
@@ -12,10 +12,10 @@ class Board
 
 	def display
 		puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
-        puts "-----------"
-        puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
-        puts "-----------"
-        puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+		puts "-----------"
+		puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+		puts "-----------"
+		puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
 	end
 
 	def position(input)
