c83e9bae1951daca00bd347bc0c07f296b5db149
Adjoa <adjoa.darien@gmail.com>
2018-01-11 22:28:16 -0500

    added #won? to Game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Game
     WIN_COMBINATIONS = [
         [0,1,2],
@@ -12,16 +13,28 @@ class Game
 
     attr_accessor :player_1, :player_2, :board
     
-    def initialize(player_1 = Players::Human('X'), player_2 = Players::Human('O'), board = Board.new)
+    def initialize(player_1 = Players::Human.new('X'), player_2 = Players::Human.new('O'), board = Board.new)
+        @player_1 = player_1
+        @player_2 = player_2
+        @board = board
     end
 
     def current_player
+        board.turn_count.even? ? player_1 : player_2
     end
 
     def over?
     end
 
     def won?
+        WIN_COMBINATIONS.detect do |combo|
+            first_position = combo[0] + 1
+            token1_matches_token2 = board.cells[combo[0]] == board.cells[combo[1]]
+            token2_matches_token3 = board.cells[combo[1]] == board.cells[combo[2]]
+            if board.taken?(first_position) && token1_matches_token2 && token2_matches_token3 
+                combo
+            end
+        end
     end
 
     def draw?
