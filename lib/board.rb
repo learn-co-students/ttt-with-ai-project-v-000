1e7ab02e860ea8e1b3a5a804a268affb83bfa333
Andy Maskiell <maskiella@gmail.com>
2018-01-13 19:04:46 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -17,27 +17,36 @@ class Board
     puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
   end
 
-  def position(user_input)
-    (user_input.to_i - 1).to_str
-    # board_input
+  def position(input)
+    @cells[input.to_i - 1]
   end
 
   def full?
-    @cells.all? do |position|
-      position == "X" || position == "O"
+    @cells.all? do |space|
+      space == "X" || space == "O"
     end
   end
 
   def turn_count
+    counter = 0
+    @cells.each do |space|
+      if (space == "X" || space == "O")
+        counter += 1
+      end
+    end
+    counter
   end
 
-  def taken?
+  def taken?(input)
+    self.position(input) != " " ? true : false
   end
 
-  def valid_move?
+  def valid_move?(input)
+    (self.taken?(input) ? false : true) unless input.length > 1
   end
 
-  def update
+  def update(input, player)
+    @cells[input.to_i - 1] = player.token
   end
 
 end
