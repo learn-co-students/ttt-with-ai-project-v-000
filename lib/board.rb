b188228f903947cbe06c9c84dba9c3e02d4afbcb
ElleeB <laura.trager@gmail.com>
2018-01-12 19:28:18 -0500

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -3,7 +3,7 @@ class Board
   attr_accessor :cells
 
   def initialize
-    @cells = [" ", " ", "O", " ", "O", " ", " ", " ", " "]
+    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   end
 
   def reset!
@@ -40,6 +40,95 @@ class Board
 
   def update(input, player)
     @cells[input.to_i - 1] = player.token
+  end
+
+#end
+
+####
+
+  def first_moves
+    ideal_moves = ["1", "3", "5", "7", "9"]
+    ideal_moves.collect! do |value|
+      self.valid_move?(value)
+      @play_move = ideal_moves.sample
+    end
+    @play_move.to_i
+  end
+
+  def check_block_combos
+
+    @block_combos = {
+
+      :block_rt => [[0, 1], [3, 4], [6, 7]],
+      :block_lft => [[1, 2], [4, 5], [7, 8]],
+      :block_blw => [[0, 3], [1, 4], [2, 5]],
+      :block_abv => [[3, 6], [4, 7], [5, 8]],
+      :hor_block_cntr => [[0, 2], [3, 5], [6, 8]],
+      :ver_block_cntr => [[0, 6], [1, 7], [2, 8]],
+      :diag_block_cntr => [[0, 8], [2, 6]]
+
+    }
+
+    @block_combos.each do |key, value|
+      value.each do |combo|
+        if @cells[combo[0]] == "X" && @cells[combo[1]] == "X" || @cells[combo[0]] == "O" && @cells[combo[1]] == "O"
+          if key == :block_rt
+            @play_move = (combo[1] + 1) + 1
+          elsif key == :block_lft
+            @play_move = (combo[0] + 1) - 1
+          elsif key == :block_blw
+            @play_move = (combo[1] + 1) + 3
+          elsif key == :block_abv
+            @play_move = (combo[0] + 1) - 3
+          elsif key == :hor_block_cntr
+            @play_move = (combo[0] + 1) + 1
+          elsif key == :ver_block_cntr
+            @play_move = (combo[1] + 1) - 3
+          elsif key == :diag_block_cntr
+            @play_move = 5
+          else
+            nil
+          end
+        else
+          nil
+        end
+      end
+    end
+    @play_move
+  end
+
+
+  def check_diag_combos
+
+    @diag_block_combos = {
+
+      :block_up_rt => [4, 6],
+      :block_up_lft => [4, 8],
+      :block_low_rt => [0, 4],
+      :block_low_lft => [2, 4]
+
+    }
+
+    @diag_block_combos.each do |key, value| # ex. value == [4, 6]
+
+      if @cells[value[0]] == "X" && @cells[value[1]] == "X" || @cells[value[0]] == "O" && @cells[value[1]] == "O"
+
+        if key == :block_up_rt
+          @play_move = 3
+        elsif key == :block_up_lft
+          @play_move = 1
+        elsif key == :block_low_rt
+          @play_move = 9
+        elsif key == :block_low_lft
+          @play_move = 7
+        else
+          nil
+        end
+      else
+        nil
+      end
+    end
+    @play_move
   end
 
 end
--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,87 +3,49 @@ module Players
   class Computer < Player
 
     def move(board)
+
       @board = board
+
       @valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-      cells = @board.cells
-
-      block_combos = {
-
-        :block_rt => [[0, 1], [3, 4], [6, 7]],
-        :block_lft => [[1, 2], [4, 5], [7, 8]],
-        :block_blw => [[0, 3], [1, 4], [2, 5]],
-        :block_abv => [[3, 6], [4, 7], [5, 8]],
-        :hor_block_cntr => [[0, 2], [3, 5], [6, 8]],
-        :ver_block_cntr => [[0, 6], [1, 7], [2, 8]],
-        :diag_block_cntr => [[0, 8], [2, 6]]
-
-      }
-
-      diag_block_combos = {
-
-        :block_up_rt => [6, 4],
-        :block_up_lft => [8, 4],
-        :block_low_rt => [0, 4],
-        :block_low_lft => [2, 4]
-
-      }
-
-      block_combos.each do |key, value|
-        value.each do |combo|
-          if cells[combo[0]] == "X" && cells[combo[1]] == "X" || cells[combo[0]] == "O" && cells[combo[1]] == "O"
-            if key == :block_rt
-              @play_move = (combo[1] + 1) + 1
-            elsif key == :block_lft
-              @play_move = (combo[0] + 1) - 1
-            elsif key == :block_blw
-              @play_move = (combo[1] + 1) + 3
-            elsif key == :block_abv
-              @play_move = (combo[0] + 1) - 3
-            elsif key == :hor_block_cntr
-              @play_move = (combo[0] + 1) + 1
-            elsif key == :ver_block_cntr
-              @play_move = (combo[1] + 1) - 3
-            elsif key == :diag_block_cntr
-              @play_move = 5
-            end
-          end
-        end
+      if @board.check_block_combos != nil
+        @play_move = @board.check_block_combos
+      elsif @board.check_diag_combos != nil
+        @play_move = @board.check_diag_combos
+      else
+        @play_move = @board.first_moves #I think this is broken
       end
 
-
-      diag_block_combos.each do |key, value|
-        if cells[value[0]] == "X" && cells[value[1]] == "X" || cells[value[0]] == "O" && cells[value[1]] == "O"
-          if key == :block_up_rt
-            @play_move = 3
-          elsif key == :block_up_lft
-            @play_move = 1
-          elsif key == :block_low_rt
-            @play_move = 9
-          elsif key == :block_low_lft
-            @play_move = 7
-          else
-            ideal_moves = ["1", "3", "5", "7", "9"]
-            ideal_moves.collect! do |value|
-              @board.valid_move?(value)
-              @value = value
-            end
-            @play_move = ideal_moves.sample
-          end # close key check
-        end # close if statement
-      end # close iteration
-
-      if @board.valid_move?(@play_move)
+      #if @board.valid_move?(@play_move)
+        @valid_moves[@play_move.to_i - 1] = self.token
+        # Error: expected ["1", "2", "3", "4", "5", "6", "7", "8", "9"] to include ""
         @play_move.to_s
-      end
+      #end
 
-    end
+    end # #move
 
-  end
+  end # class
 
-end
+end # module
 
-# need to give "player_2" a starting move?
+
+####### Breaks after player 1 goes for the 2nd time ##############
+
+
+
+
+#
+# @valid_moves.collect! do |value|
+#  +        @board.valid_move?(value)
+#  +        @value = value
+#  +      end
+#  +
+#  +      input = @valid_moves.sample
+#  +      @board.update(input, self)
+#  +      @board.cells
+#  +      @valid_moves[@value.to_i - 1] = @value
+#  +      #expect(valid_moves).to include(computer_move)
+
 
 
 
@@ -116,7 +78,7 @@ end
 
 
 #   else
-#     @play_move = @valid_moves.sample
+#     choice = @valid_moves.sample
 #     # @play_move = @valid_moves[choice.to_i]
 #     # @play_move
 #   end
@@ -128,3 +90,61 @@ end
 # if @board.valid_move?(@play_move)
 #   @play_move.to_s
 # end
+
+
+
+
+# @diag_block_combos.detect do |combo|
+#   if @cells[value[0]] == "X" && @cells[value[1]] == "X" || @cells[value[0]] == "O" && @cells[value[1]] == "O"
+#     if key == :block_up_rt
+#       @play_move = 3
+#     elsif key == :block_up_lft
+#       @play_move = 1
+#     elsif key == :block_low_rt
+#       @play_move = 9
+#     elsif key == :block_low_lft
+#   #       @play_move = 7
+#       else
+#         nil
+#       end # close key check
+#     end # close if statement
+#   end # close iteration
+# end
+
+
+########
+
+# Error
+#
+# Welcome to Tic Tac Toe!
+# ---
+# Would you like to play a 0, 1, or 2-player game?
+# 1
+# The first player will be 'X'. Good luck!
+# ---
+# ---
+# Your turn, X.
+# ---
+# Please enter your move, 1-9
+# 5
+#    |   |
+# -----------
+#    | X |
+# -----------
+#    |   |
+# ---
+# Your turn, O.
+# ---
+# Please enter a valid move, 1-9.
+# ---
+# Your turn, O.
+# ---
+# Please enter a valid move, 1-9.
+# ---
+# Your turn, O.
+# ---
+# Please enter a valid move, 1-9.
+# ---
+# Your turn, O.
+# ---
+# Please enter a valid move, 1-9
