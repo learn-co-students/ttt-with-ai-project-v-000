20cc63dc0f870a36c5f6db38b365c2e95fd42705
Adjoa <adjoa.darien@gmail.com>
2018-01-14 15:41:12 -0500

    added board.display to #turn

--- lib/game.rb
+++ lib/game.rb
@@ -52,6 +52,7 @@ class Game
       input = current_player.move(board)
      if @board.valid_move?(input)
        @board.update(input,current_player)
+       @board.display
       else
         turn
        end
