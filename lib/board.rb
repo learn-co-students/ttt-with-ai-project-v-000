b65f63f5a23ef65b745921f462e749d653f373d9
funemployeddad <funemployeddad@gmail.com>
2018-01-14 15:21:45 -0700

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,16 @@
 class Board
   attr_accessor :cells
 
+  def initialize
+    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+
+  def reset!
+    @cells.clear
+    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+
+  
 
 
 
