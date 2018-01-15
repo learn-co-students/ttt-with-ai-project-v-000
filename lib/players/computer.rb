371ed8aef9b24607267066a90ffcc957707a17d6
Andy Maskiell <maskiella@gmail.com>
2018-01-15 03:37:02 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,6 +1,30 @@
 module Players
   class Computer < Player
+
+    WIN_COMBINATIONS = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6]
+    ]
+
     def move(board)
+      blocking_move = []
+      WIN_COMBINATIONS.each do |win_combo|
+        if (board.cells[win_combo[0]] == "X" && board.cells[win_combo[1]] == "X") || (board.cells[win_combo[0]] == "O" && board.cells[win_combo[1]] == "O")
+          blocking_move << win_combo[2].to_s
+        elsif (board.cells[win_combo[0]] == "X" && board.cells[win_combo[2]] == "X") || (board.cells[win_combo[0]] == "O" && board.cells[win_combo[2]] == "O")
+          blocking_move << win_combo[1].to_s
+        elsif (board.cells[win_combo[1]] == "X" && board.cells[win_combo[2]] == "X") || (board.cells[win_combo[1]] == "O" && board.cells[win_combo[2]] == "O")
+          blocking_move << win_combo[0].to_s
+        end
+      end
+      blocking_move[0]
+
       input = (Random.new.rand(9)).to_s
       if board.valid_move?(input)
         input
@@ -8,5 +32,6 @@ module Players
         move(board)
       end
     end
+
   end
 end
