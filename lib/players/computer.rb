c2751f741aa209d95abc595ba34a06dad3f4a93f
Neil <neilmarkoliverryan@gmail.com>
2018-01-15 13:05:16 -0800

    working computer logic, getting ready to use a module

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -4,24 +4,27 @@ module Players
 		WIN_THREATS = [
 			[0,1,2],
 			[1,2,0],
+			[0,2,1],
 			[3,4,5],
 			[4,5,3],
+			[3,5,4],
 			[6,7,8],
 			[7,8,6],
+			[6,8,7],
 			[0,3,6],
 			[3,6,0],
+			[0,6,3],
 			[1,4,7],
 			[4,7,1],
+			[1,7,4],
 			[2,5,8],
 			[5,8,2],
+			[2,8,5],
 			[0,4,8],
 			[4,8,0],
+			[0,8,4],
 			[2,4,6],
 			[4,6,2],
-			[0,2,1],
-			[3,5,4],
-			[6,8,7],
-			[0,8,4],
 			[2,6,4]
 		]
 
@@ -31,18 +34,24 @@ module Players
 			case board.turn_count
 
 			when 0 # i am X
+				puts "board turn count is:#{board.turn_count}"
 				"5"
+
 			when 1 # i am O
-				puts "i must move to an even numbered index"
+				puts "board turn count is:#{board.turn_count}"
+				puts "i must move to an even numbered index, preferrably 4"
 				if board.cells[4] != "X"
 					"5"
 				else
-					o_m = [0,2,6,8]
-					m = o_m[rand(o_m.length)].to_s
+					options = [0,2,6,8]
+					m = options[rand(options.length)] + 1
+					m.to_s
 				end
+
 			when 2 # i am X
+				puts "board turn count is:#{board.turn_count}"
 				closed_indices = []
-				closed_cells = board.cells.each_with_index { |item, index|
+				board.cells.each_with_index { |item, index|
  					if item == "X" || item == "O"
     					closed_indices << index
 					end
@@ -55,103 +64,188 @@ module Players
 				currently_blocked_index = 8 - closed_indices[0]
 				puts "currently blocked index is #{currently_blocked_index}"
 				available_cells.delete(currently_blocked_index)
-				puts available_cells
-				move = available_cells[rand(available_cells.length)] + 1
+				m = available_cells[rand(available_cells.length)] + 1
+				m.to_s
+
 			when 3 # i am O
+				puts "board turn count is:#{board.turn_count}"
 				win_threat = nil
 
 			    WIN_THREATS.each do |combo|
-			    	pos_1 = board[combo[0]]
-			    	pos_2 = board[combo[1]]
+			    	pos_1 = board.cells[combo[0]]
+			    	pos_2 = board.cells[combo[1]]
+			    	pos_3 = board.cells[combo[2]]
 
-			    	if pos_1 == "X" && pos_2 == "X"
+			    	if pos_1 == "X" && pos_2 == "X" && pos_3 == " "
 			    		win_threat = combo
-			    		break
-			    	elsif pos_1 == "O" && pos_2 == "O"
-			    		win_threat = combo
-			    		break
 			    	end
             	end
 
-		    	puts "The win threat is:#{win_threat}"
-		    	puts "To block the threat fill index #{win_threat[2]}"
-		    	m = win_threat[2] + 1
+            	if win_threat != nil
+					puts "The win threat is:#{win_threat}"
+			    	puts "To block the threat fill index #{win_threat[2]}"
+			    	m = win_threat[2] + 1
+			    	m.to_s
+			    else
+					remaining_cells = []
+					board.cells.each_with_index { |item, index|
+ 						if item == " "
+    						remaining_cells << index
+						end
+					}
+					m = remaining_cells[rand(remaining_cells.length)] + 1
+					m.to_s
+		    	end
 
 			when 4 # i am X
+				puts "board turn count is:#{board.turn_count}"
 				win_threat = nil
 
 			    WIN_THREATS.each do |combo|
 			    	pos_1 = board.cells[combo[0]]
 			    	pos_2 = board.cells[combo[1]]
+			    	pos_3 = board.cells[combo[2]]
 
-			    	if pos_1 == "X" && pos_2 == "X"
+			    	if pos_1 == "O" && pos_2 == "O" && pos_3 == " "
 			    		win_threat = combo
-			    		break
-			    	elsif pos_1 == "O" && pos_2 == "O"
-			    		win_threat = combo
-			    		break
 			    	end
             	end
 
-		    	puts "The win threat is:#{win_threat}"
-		    	puts "To block the threat fill index #{win_threat[2]}"
-		    	m = win_threat[2] + 1
+            	if win_threat != nil
+					puts "The win threat is:#{win_threat}"
+			    	puts "To block the threat fill index #{win_threat[2]}"
+			    	m = win_threat[2] + 1
+			    	m.to_s
+			    else
+					remaining_cells = []
+					board.cells.each_with_index { |item, index|
+ 						if item == " "
+    						remaining_cells << index
+						end
+					}
+					m = remaining_cells[rand(remaining_cells.length)] + 1
+					m.to_s
+		    	end
 
 			when 5 # i am O
+				puts "board turn count is:#{board.turn_count}"
 				win_threat = nil
 
 			    WIN_THREATS.each do |combo|
 			    	pos_1 = board.cells[combo[0]]
 			    	pos_2 = board.cells[combo[1]]
+			    	pos_3 = board.cells[combo[2]]
 
-			    	if pos_1 == "X" && pos_2 == "X"
+			    	if pos_1 == "X" && pos_2 == "X" && pos_3 == " "
 			    		win_threat = combo
-			    		break
-			    	elsif pos_1 == "O" && pos_2 == "O"
-			    		win_threat = combo
-			    		break
 			    	end
             	end
 
-		    	puts "The win threat is:#{win_threat}"
-		    	puts "To block the threat fill index #{win_threat[2]}"
-		    	m = win_threat[2] + 1
+            	if win_threat != nil
+					puts "The win threat is:#{win_threat}"
+			    	puts "To block the threat fill index #{win_threat[2]}"
+			    	m = win_threat[2] + 1
+			    	m.to_s
+			    else
+					remaining_cells = []
+					board.cells.each_with_index { |item, index|
+ 						if item == " "
+    						remaining_cells << index
+						end
+					}
+					m = remaining_cells[rand(remaining_cells.length)] + 1
+					m.to_s
+		    	end
 
-		    when 6
-				remaining_cells = []
-				board.cells.each_with_index { |item, index|
- 					if item == " "
-    					remaining_cells << index
-					end
-				}
-				m = remaining_cells[rand(remaining_cells.length)] + 1
-			when 7
+		    when 6 # i am X
+				puts "board turn count is:#{board.turn_count}"
 				win_threat = nil
 
 			    WIN_THREATS.each do |combo|
 			    	pos_1 = board.cells[combo[0]]
 			    	pos_2 = board.cells[combo[1]]
+			    	pos_3 = board.cells[combo[2]]
 
-			    	if pos_1 == "X" && pos_2 == "X"
+			    	if pos_1 == "O" && pos_2 == "O" && pos_3 == " "
 			    		win_threat = combo
-			    		break
-			    	elsif pos_1 == "O" && pos_2 == "O"
-			    		win_threat = combo
-			    		break
 			    	end
             	end
 
-		    	puts "The win threat is:#{win_threat}"
-		    	puts "To block the threat fill index #{win_threat[2]}"
-		    	m = win_threat[2] + 1
-		    when 8
-		    	remaining_cell = nil
-				board.cells.each_with_index { |item, index|
- 					if item == " "
-    					remaining_cell = index
-					end
-				}
-				m = remaining_cell + 1
+            	if win_threat != nil
+					puts "The win threat is:#{win_threat}"
+			    	puts "To block the threat fill index #{win_threat[2]}"
+			    	m = win_threat[2] + 1
+			    	m.to_s
+			    else
+					remaining_cells = []
+					board.cells.each_with_index { |item, index|
+ 						if item == " "
+    						remaining_cells << index
+						end
+					}
+					m = remaining_cells[rand(remaining_cells.length)] + 1
+					m.to_s
+		    	end
+
+			when 7 # i am O
+				puts "board turn count is:#{board.turn_count}"
+				win_threat = nil
+
+			    WIN_THREATS.each do |combo|
+			    	pos_1 = board.cells[combo[0]]
+			    	pos_2 = board.cells[combo[1]]
+			    	pos_3 = board.cells[combo[2]]
+
+			    	if pos_1 == "X" && pos_2 == "X" && pos_3 == " "
+			    		win_threat = combo
+			    	end
+            	end
+
+            	if win_threat != nil
+					puts "The win threat is:#{win_threat}"
+			    	puts "To block the threat fill index #{win_threat[2]}"
+			    	m = win_threat[2] + 1
+			    	m.to_s
+			    else
+					remaining_cells = []
+					board.cells.each_with_index { |item, index|
+ 						if item == " "
+    						remaining_cells << index
+						end
+					}
+					m = remaining_cells[rand(remaining_cells.length)] + 1
+					m.to_s
+		    	end
+
+		    when 8 #i am X
+				puts "board turn count is:#{board.turn_count}"
+				win_threat = nil
+
+			    WIN_THREATS.each do |combo|
+			    	pos_1 = board.cells[combo[0]]
+			    	pos_2 = board.cells[combo[1]]
+			    	pos_3 = board.cells[combo[2]]
+
+			    	if pos_1 == "O" && pos_2 == "O" && pos_3 == " "
+			    		win_threat = combo
+			    	end
+            	end
+
+            	if win_threat != nil
+					puts "The win threat is:#{win_threat}"
+			    	puts "To block the threat fill index #{win_threat[2]}"
+			    	m = win_threat[2] + 1
+			    	m.to_s
+			    else
+					remaining_cells = []
+					board.cells.each_with_index { |item, index|
+ 						if item == " "
+    						remaining_cells << index
+						end
+					}
+					m = remaining_cells[rand(remaining_cells.length)] + 1
+					m.to_s
+				end
 			end
 		end
 	end
