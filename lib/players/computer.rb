719fc54ef3b3947a9c820dd63f28648e6f1e6dda
PJ Frias <peejfrias@gmail.com>
2018-01-16 13:51:38 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -7,7 +7,12 @@ module Players
     end
 
     def move(board)
+      index = nil
       puts "Please enter 1-9:"
+      if [0,1].include?(board.turn_count)
+        index = 5 if board.valid_move?(5)
+        index = [1,3,7,9].sample
+      else
       index = rand(1..9).to_s
       # binding.pry
       puts "Computer player #{self.token} chooses position #{index}"
