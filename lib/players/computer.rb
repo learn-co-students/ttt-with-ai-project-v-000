94299f91407961d137aed4bfbd838c3c095e3c7f
PJ Frias <peejfrias@gmail.com>
2018-01-16 14:02:52 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -15,6 +15,10 @@ module Players
         else
           index = [1,3,7,9].sample
         end
+      elsif [2,3].include?(board.turn_count)
+        if board.position(5) == self.token
+          index = [2,4,6,8].sample
+        end
       else
         index = rand(1..9).to_s
       end
