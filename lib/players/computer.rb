a3b5e7d9c085aa6b8a4c1faadc877245f4ac4b6e
Andy Maskiell <maskiella@gmail.com>
2018-01-16 00:03:53 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -21,11 +21,11 @@ module Players
           new_move = win_combo[1].to_s if board.cells[win_combo[1]] == " "
         elsif (board.cells[win_combo[1]] == self.token && board.cells[win_combo[2]] == self.token)
           new_move = win_combo[0].to_s if board.cells[win_combo[0]] == " "
-        elsif (board.cells[win_combo[0]] == "X" && board.cells[win_combo[1]] == "X") || (board.cells[win_combo[0]] == "O" && board.cells[win_combo[1]] == "O")
+        elsif (board.taken?(win_combo[0] + 1) && board.cells[win_combo[0]] != self.token) && (board.taken?(win_combo[1] + 1) && board.cells[win_combo[1]] != self.token)
           new_move = win_combo[2].to_s if board.cells[win_combo[2]] == " "
-        elsif (board.cells[win_combo[0]] == "X" && board.cells[win_combo[2]] == "X") || (board.cells[win_combo[0]] == "O" && board.cells[win_combo[2]] == "O")
+        elsif (board.taken?(win_combo[0] + 1) && board.cells[win_combo[0]] != self.token) && (board.taken?(win_combo[2] + 1) && board.cells[win_combo[2]] != self.token)
           new_move = win_combo[1].to_s if board.cells[win_combo[1]] == " "
-        elsif (board.cells[win_combo[1]] == "X" && board.cells[win_combo[2]] == "X") || (board.cells[win_combo[1]] == "O" && board.cells[win_combo[2]] == "O")
+        elsif (board.taken?(win_combo[1] + 1) && board.cells[win_combo[1]] != self.token) && (board.taken?(win_combo[2] + 1) && board.cells[win_combo[2]] != self.token)
           new_move = win_combo[0].to_s if board.cells[win_combo[0]] == " "
         else
           new_move = last_ditch_move(board)
@@ -33,24 +33,6 @@ module Players
       end
       new_move
     end
-    #     if (board.cells[win_combo[0]] == self.token && board.cells[win_combo[1]] == self.token)
-    #       new_move = win_combo[2].to_s unless board.cells[win_combo[2]] != " "
-    #     elsif (board.cells[win_combo[0]] == self.token && board.cells[win_combo[2]] == self.token)
-    #       new_move = win_combo[1].to_s unless board.cells[win_combo[1]] != " "
-    #     elsif (board.cells[win_combo[1]] == self.token && board.cells[win_combo[2]] == self.token)
-    #       new_move = win_combo[0].to_s == unless board.cells[win_combo[0]] != " "
-    #     elsif (board.cells[win_combo[0]] == "X" && board.cells[win_combo[1]] == "X") || (board.cells[win_combo[0]] == "O" && board.cells[win_combo[1]] == "O")
-    #       new_move = win_combo[2].to_s unless board.cells[win_combo[2]] != " "
-    #     elsif (board.cells[win_combo[0]] == "X" && board.cells[win_combo[2]] == "X") || (board.cells[win_combo[0]] == "O" && board.cells[win_combo[2]] == "O")
-    #       new_move = win_combo[1].to_s unless board.cells[win_combo[1]] != " "
-    #     elsif (board.cells[win_combo[1]] == "X" && board.cells[win_combo[2]] == "X") || (board.cells[win_combo[1]] == "O" && board.cells[win_combo[2]] == "O")
-    #       new_move = win_combo[0].to_s unless board.cells[win_combo[0]] != " "
-    #     else
-    #       new_move = last_ditch_move(board)
-    #     end
-    #   end
-    #   new_move
-    # end
 
     def last_ditch_move(board)
       input = (Random.new.rand(9)).to_s
