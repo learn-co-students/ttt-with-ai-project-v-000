645487f955a7915f65c5f57b6e2a654463b8aee2
PJ Frias <peejfrias@gmail.com>
2018-01-16 14:20:02 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -18,6 +18,15 @@ module Players
       elsif [2,3].include?(board.turn_count)
         if board.position(5) == self.token
           index = [2,4,6,8].sample
+        else #go to opposite corner of current spot
+          corner = [1,3,7,9].detect{|index|board.position(index) == self.token}
+          if corner == 1 || corner == 9
+            index = 9 if corner == 1
+            index = 1 if corner == 9
+          elsif corner == 3 || corner == 7
+            index = 7 if corner == 3
+            index = 3 if corner == 7
+          end
         end
       else
         index = rand(1..9).to_s
