04e1d9f5cd9c9ed16df1ca68634e91c3cedd66eb
Adjoa <adjoa.darien@gmail.com>
2018-01-18 15:05:51 -0500

    computerO #move chooses a random square if 2 is taken

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -38,7 +38,7 @@ module Players
 
 
       
-        elsif self.token == "O"
+      elsif self.token == "O"
         case(board.turn_count)
         when 1
           if !board.taken?(5)
@@ -48,40 +48,39 @@ module Players
           end
         when 3
           taken_corners = (board.position(1) == "X" || board.position(3) == "X" || board.position(7) || board.position(9))
-         if board.position(5) == "O" && taken_corners
+          if board.position(5) == "O" && taken_corners && board.taken?("2") == false
             move = "2"
           else
             all_sq = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
             move = all_sq.delete_if { |sq| board.taken?(sq) }.sample
           end
         when 5
+          # Find combo with missing square
           possible_win = Game::WIN_COMBINATIONS.detect do |combo|
-         tokens = [
-           board.cells[combo[0]],
-           board.cells[combo[1]],
-           board.cells[combo[2]]
-         ]
-           tokens.count{ |token| token == self.token } > 1 && tokens.include?('O') == false
-           tokens.count{ |token| token == self.token } > 1 && tokens.include?('X') == false
-       end
+            tokens = [
+              board.cells[combo[0]],
+              board.cells[combo[1]],
+              board.cells[combo[2]]
+            ]
+            tokens.count{ |token| token == self.token } > 1 && tokens.include?('X') == false
+          end
 
-       # Fill the missing square
-       if possible_win.nil?
-         all_sq = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-         move = all_sq.delete_if { |sq| board.taken?(sq) }.sample
+          # Fill the missing square
+          if possible_win.nil?
+            all_sq = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+            move = all_sq.delete_if { |sq| board.taken?(sq) }.sample
+          else
+            empty_sq = possible_win.detect do |i|
+              board.cells[i] != self.token && board.taken?((i + 1).to_s) == false
+            end
 
-       else
-         empty_sq = possible_win.detect do |i|
-           board.cells[i] != self.token && board.taken?((i + 1).to_s) == false
-         end
-
-         move = (empty_sq + 1).to_s
-       end
-     else
-       all_sq = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-       move = all_sq.delete_if { |sq| board.taken?(sq) }.sample
-     end # case_end
-   end # if_end
- end # move_end
-end # class_end
+            move = (empty_sq + 1).to_s
+          end
+        else
+          all_sq = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+          move = all_sq.delete_if { |sq| board.taken?(sq) }.sample
+        end # case_end
+      end # if_end
+    end # move_end
+  end # class_end
 end # module_end
