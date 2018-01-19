6ab0792532e591b04e9a88fd2cb3a6a9a6dcf79c
rh24 <ms.rebeccahong@gmail.com>
2018-01-19 17:54:47 -0500

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -2,3 +2,9 @@ require 'bundler'
 Bundler.require
 
 require_all 'lib'
+# require_relative '../lib/cli'
+# require_relative '../lib/board'
+# require_relative '../lib/game'
+# require_relative '../lib/player'
+# require_relative '../lib/players/computer'
+# require_relative '../lib/players/human'
