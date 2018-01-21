6a57d29188981a54a7c2e4a31805412ea69ee461
Wali Rahman <wali.rahman89@yahoo.com>
2018-01-21 22:14:51 +0000

    Done.

---
+++ lib/game.rb
@@ -0,0 +1,25 @@
+class Game
+  attr_accessor :player_1, :player_2, :board
+  attr_reader
+
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+    @board = board
+    @player_1 = player_1
+    @player_2 = player_2
+  end
+
+  WIN_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8]
+
+  ]
+
+
+
+end
---
+++ lib/players/computer.rb
@@ -0,0 +1,10 @@
+module Players
+
+  class Computer < Player
+
+    def move(board)
+
+    end
+  end
+
+end
