ceb2029f0577aaff277a0a15c6e65243108f795c
rh24 <ms.rebeccahong@gmail.com>
2018-01-23 15:18:36 -0500

    Add CLIRun class

--- lib/game.rb
+++ lib/game.rb
@@ -13,26 +13,6 @@ class Game
      [6,4,2]
    ]
 
-  def self.start
-    puts "Hello! Welcome to Tic Tac Toe!"
-    puts "Please, input player mode: 0, 1, or 2"
-    player_mode = gets.strip.to_i
-    puts "Would you like to go first and be 'X'? (y/n)"
-    input = gets.strip.downcase
-    
-    if player_mode == 2 && input == "yes" || input == "y"
-      self.new(Players::Human.new("X"), Players::Human.new("O"), Board.new)
-    elsif player_mode == 2 && input == "no" || input == "n"
-      self.new(Players::Human.new("O"), Players::Human.new("X"), Board.new)
-    elsif player_mode == 1 && input == "yes" || input == "y"
-      self.new(Players::Human.new("X"), Players::Computer.new("O"), Board.new)
-    elsif player_mode == 1 && input == "no" || input == "n"
-      self.new(Players::Computer.new("X"), Players::Human.new("O"), Board.new)
-    elsif player_mode == 0
-      puts "Goodbye!"
-      exit
-    end
-  end
 
   def initialize(player_1=Players::Human.new("X"), player_2=Players::Human.new("O"), board=Board.new)
     @player_1 = player_1
