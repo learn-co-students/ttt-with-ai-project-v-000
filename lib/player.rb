af074b6c45de55dbb2a988b03f461543dc47d36c
Ingrid Wong <yw340@georgetown.edu>
2018-01-25 18:35:44 +0000

    Done.

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,11 @@
 class Player
 
+      WIN_COMBINATIONS = [
+        [0,1,2], [3,4,5],[6,7,8],
+        [0,4,8], [2,4,6],
+        [0,3,6], [1,4,7], [2,5,8]
+      ]
+      
   attr_reader :token
   def initialize(token)
     @token = token
--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,11 +1,6 @@
 module Players
   class Computer < Player
 
-    WIN_COMBINATIONS = [
-      [0,1,2], [3,4,5],[6,7,8],
-      [0,4,8], [2,4,6],
-      [0,3,6], [1,4,7], [2,5,8]
-    ]
 
   attr_accessor :my_last, :center_play
   attr_reader :token
