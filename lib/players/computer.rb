21658a14c8bd0335740c56fb4d1a37e03d75ca9a
Ingrid Wong <yw340@georgetown.edu>
2018-01-25 15:58:54 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -8,10 +8,9 @@ module Players
     end
 
     def move(board)
-      if self.token == "X"
-
-        if board.turn_count == 0 #plays first
-          edge = true
+      if @token == "X"
+#////////////// EDGE CASE //////////////
+        if board.turn_count == 0
            @my_last = "4"
 
         elsif board.turn_count == 2 && @my_last == "4"
@@ -33,8 +32,52 @@ module Players
             @my_last = "6"
           end
         end
+        #////////////// CENTER CASE //////////////
 
+        if board.turn_count == 0
+           @my_last = "5"
+
+        elsif board.turn_count == 2 && @my_last == "4"
+          if board.last_move.even? #if opponent plays 2, 4, 6, 8 => EDGE: OFFENSE MODE
+            if board.last_move == "2" || board.last_move == "8"
+              @my_last = "1" # REFACTOR: CAN ALSO BE 4, 7, 3,6,9
+            elsif board.last_move == "4" || board.last_move == "6"
+              @my_last = "3" # REFACTOR: CAN ALSO BE 1,2, 7,8,9
+            end
+          else #opponent plays corner: DEFENSE MODE
+            @my_last = "7"  #A IMPLEMENTER
+          end
+
+        elsif board.turn_count == 4
+
+          if @my_last == "1"
+            if board.last_move == "9"
+              @my_last = "7"
+            else
+              @my_last = "9" # WON!
+            end
+          end
+          if @my_last == "3"
+            if board.last_move == "7"
+              @my_last = "1"
+            else
+              @my_last = "7" # WON!
+            end
+          end
+
+        end
+
+        elsif board.turn_count == 6
+          if board.last_move == "6"
+            @my_last = "9"
+          else
+            @my_last = "6"
+          end
+        end
       end
+
+
+
     end
   end
 end
