98896766d1133939b3d3246a7f4b5d99e97cabed
Alli Tzadok <allisontzadok@gmail.com>
2018-01-26 19:48:56 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,37 +1,54 @@
 class Board
   #represents the data and logic of a Tic Tac Toe game board.
 
-  attr_accessor :cells,
+  attr_accessor :cells
 
   #has a property cells that stores the data of the state of the board in an array
 
 
-  def initialize(board)
+  def initialize()
     #use #reset to set the cells for a new game
-    @board = board
-    board.reset!
+    reset!
   end
 
   def reset!
     # #reset! method can reset the state of the cells to what a board should look like at start of game
     # an array with 9 " " elements
-    @@board = {" ", " ", " ", " ", " ", " ", " ", " ", " "}
+    @cells = Array.new(9, " ")
 
   end
 
-  def display(board)
-    @@board
-
+  def display
+    puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
+    puts "-----------"
+    puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
+    puts "-----------"
+    puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
   end
 
   def position(input)
     #takes in a users input in the form of 1-9 strings
     #looks up the value of the cells at the correct index from array
-    input -= get.strips.to_i - 1
+    cells[input.to_i - 1]
   end
 
+  def full?
+    cells.all? {|token| token == "X" || token == "O"}
+  end
 
+  def turn_count
+    cells.count {|token| token == "X" || token == "O"}
+  end
 
+  def valid_move?(input)
+    input.to_i.between?(1,9) && !taken?(input)
+  end
 
+  def taken?(input)
+    !(position(input) == " " || position(input) == "")
+  end
 
+  def update(input, player)
+    cells[input.to_i-1]= player.token
+  end
 end
