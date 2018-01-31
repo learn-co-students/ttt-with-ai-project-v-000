afa2e2ee91be831ca2b4f5bd07cc67d243f2483b
Wali Rahman <wali.rahman89@yahoo.com>
2018-01-31 12:26:09 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -13,11 +13,15 @@ module Players
       elsif board.turn_count == 2
         move = [1, 3, 7, 9].detect{|i| !board.taken?(i)}.to_s
 
+<<<<<<< HEAD
       elsif board.turn_count == 3 && (board.position(1) == board.position(9) || board.position(3) == board.position(7))
         move = "2"
 
       else
         Game::WIN_COMBINATIONS.detect do |cmb|
+=======
+      end
+>>>>>>> 5e6416091f974a49d1c619bef64acc9953ac6579
 
           if cmb.select{|i| board.position(i+1) == token}.size == 2 && cmb.any?{|i| board.position(i+1) == " "}
                move = cmb.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
