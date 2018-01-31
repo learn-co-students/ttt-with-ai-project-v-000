ddec5bcb1f4cf7a189a5a104af357ce2e3b23e8c
Emily Zhao <tong.zhao.92@gmail.com>
2018-01-31 23:00:05 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -9,20 +9,32 @@ module Players
       if @game.turn_count == 0
         a = "5" #go to middle when board is empty
       elsif @game.turn_count == 1 #if second player, first move always go to diagonal cell
-        index = [1..9].detect {|a|@game.board.taken?(a.to_s) == true}
-        if index < 6
-          a = index + 4
+        if !@game.board.taken?("5")
+          a = "5"
         else
-          a = index - 4
+          a = ["1","3","7","9"][rand(0..3)]
         end
       elsif @game.turn_count == 2 #second move if AI went first (go to diagonal corner cell)
-        a = ["1","3","7","9"][rand(0..3)]
-      elsif @game.turn_count == 3 #second move if AI went second (block)
-        index_array = [1..9].collect {|a|@game.board.taken?(a.to_s) == true}
+        a = ["1","3","7","9"].detect{|a|!@game.board.taken?(a)} #occupy the corners whichever one was not occupied
+      elsif @game.turn_count == 3 #second move beyond: first check if opponent has a winning move, if yes, block; if no, then go for a winning move
+        index_array = [1..9].select {|a|@game.board.taken?(a.to_s) && @game.board.}
         difference = index_array[1] - index_array[0]
-        if difference % 4 == 0 #if opponent's move is 2 positions diagonal
-          if @game.board.valid_move?(index_array.min.to_s)
+        if difference % 4 == 0 #if opponent's move is 2 positions diagonal to the left or right
+          new_index = index_array.min - 4
+          if @game.board.valid_move?(new_index.to_s)
+            a = new_index.to_s
           end
+          new_index = index_array.max + 4
+          if @game.board.valid_move?(new_index.to_s)
+            a = new_index.to_s
+          end
+          a = ["1","3","7","9"][rand(0..3)] #in the case that opponent is not occupying a corner (next step will be to win)
+        elsif difference % 2 == 0
+          new_index = index_array.min - 2
+          if @game.board.valid_move?(new_index.to_s)
+            a = new_index.to_s
+          end
+
         end
       end
     end
