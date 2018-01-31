c98e2e49d39ed90793e08df2b73ff47176f2c98d
Wali Rahman <wali.rahman89@yahoo.com>
2018-01-31 12:02:07 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -13,6 +13,13 @@ module Players
       elsif board.turn_count == 2
         move = [1, 2, 7, 9]
 
+      else
+        Game::WIN_COMBINATIONS.detect do |cmb|
+          if cmb.select{|i| board.position(i+1) == token}.size == 2 && cmb.any?{|i| board.position(i+1) == " "}
+               move = cmb.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
+          elsif cmb.select{|i| board.position(i+1) != " " && board.position(i+1) != token}.size == 2 && cmb.any?{|i| board.position(i+1) == " "}
+               move = cmb.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
+          end
       end
 
     end
