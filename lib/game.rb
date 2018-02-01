7a30266c27cddfdae64b27b9c34e32cacf38d32f
Ariana Berger <ariana.berger@gmail.com>
2018-02-01 18:08:49 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -67,7 +67,6 @@ class Game
       def turn
         player = current_player # 1 or 2 (object, x or o)
         current_move = player.move(@board) #this returns a string "1", which is what was entered by human
-        #  binding.pry
           if board.valid_move?(current_move) # checks if "1" is a valid move choice, if so, updates the board
             board.update(current_move, player)
             puts "Player #{player.token} moved to #{current_move}:"
@@ -89,6 +88,45 @@ class Game
         else
           puts "Cat's Game!"
         end
+    end
+
+
+    def start #evoke this in bin/tictactoe
+      # 1. Greet user
+      puts "Welcome to Tic Tac Toe! The fight to the death between X and O."
+      puts "------"
+      # 2. Ask if they want 0, 1, or 2 player
+      puts "Please enter 0 for a computer match, 1 for 1-player, and 2 for 2-player:"
+      input = gets.strip
+      # 3. Ask who is going first, they will be X
+      puts "Choose who is going first! They will be playing for X." #they can choose a number, will be rand. generated, to decide who is first
+          # If 0, then.... computer simulation
+          # If 1, use Human and Computer classes to play
+          # If 2, ask for input using Human every turn
+
+      # 4. Initialize game with player 1 and play through the Game
+
+      # 5. When over, ask if they want to play again, otherwise exit
+    end
+
+    def computer_game
+    end
+
+    def one_player
+    end
+
+    def two_player
+      game = Game.new #create new game instance
+      play #asks for number input of where to move
+      ask = puts "Do you want to play again? If yes, enter 'yes', otherwise enter 'no'." #can I do this?
+      input = gets.strip
+      if input == "yes"
+        two_player #start the method again from the top, might be a better way of doin this
+      elsif input == "no"
+        exit #how to exit the game?
+      else
+        ask
+      end
     end
 
 end
