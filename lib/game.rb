8c5dc1e411f9bfef69dedd931561abe234a041d8
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-02-02 16:55:27 +0000

    Done.

---
+++ lib/game.rb
@@ -0,0 +1,74 @@
+require 'pry'
+class Game
+  attr_accessor :board, :player_1, :player_2, :cells, :token
+  include Players  
+  
+  @board = []
+  
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+  
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+   @player_1 = player_1
+   @player_2 = player_2
+    @board = board
+  end
+  
+  def current_player
+    @board.turn_count % 2 == 0 ? @player_1 : @player_2 
+  end 
+  
+  def over?
+    @board.full? or won? or draw?
+  end 
+  
+  def won?
+    WIN_COMBINATIONS.find do |wc|
+    if @board.cells[wc[0]] == "X" && 
+       @board.cells[wc[1]] == "X" && 
+       @board.cells[wc[2]] == "X"
+      wc
+    elsif @board.cells[wc[0]] == "O" && 
+          @board.cells[wc[1]] == "O" && 
+          @board.cells[wc[2]] == "O"
+      wc
+    else 
+      false 
+    end
+    end
+  end 
+  
+  def draw?
+    !won? and @board.full?
+  end
+  
+  def winner
+    @board.cells[won?[0]] if won?
+  end 
+  
+  def turn
+    move = current_player.move(@board)
+    if @board.valid_move?(move)
+      move
+    else
+      current_player.move(@board) 
+    end 
+    @board.update(move, current_player)
+  end 
+  
+  def play
+    turn
+    over?
+    
+  end 
+
+  
+end
