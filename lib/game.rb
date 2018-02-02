a3ce27f30f408074138cfbc9fa0edb0db66131a6
Emily Zhao <tong.zhao.92@gmail.com>
2018-02-02 23:07:51 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -37,10 +37,10 @@ class Game
 
   def won?
     WIN_COMBINATIONS.detect do |combo|
-      @board.cells[combo[0]] == @board.cells[combo[1]] &&
-      @board.cells[combo[1]] == @board.cells[combo[2]] &&
-      @board.taken?(combo[0])
-    end
+       @board.cells[combo[0]] == @board.cells[combo[1]] &&
+       @board.cells[combo[1]] == @board.cells[combo[2]] &&
+       @board.taken?(combo[0]+1)
+     end
   end
 
   def full?
