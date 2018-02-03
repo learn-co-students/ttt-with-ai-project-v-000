cf46a6d4734c4bd1e882c6ea8ef673f49b5a226d
Jennifer Stoos <jstoos@bitlaw.com>
2018-02-03 01:18:08 +0000

    Done.

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,3 +1,12 @@
-class Players::Human < Player
+module Players
+  class Human < Player
 
+    def move(board)
+      puts "Where would you like to play (1-9)?"
+      input = gets.strip
+      input
+    end
+
+
+  end
 end
