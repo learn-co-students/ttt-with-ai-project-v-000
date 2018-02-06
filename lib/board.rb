a04239eaaac51aeb4f4306ac7b0cd4f92255516a
Nadia Lee <miss.nadia.lee@gmail.com>
2018-02-06 20:47:30 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,23 @@
+class Board
+  attr_accessor :cells
+
+  def reset!
+    @cells = Array.new(9, " ")
+  end
+
+  def initialize
+    reset!
+  end
+
+  def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
+
+  def position
+
+  end
+end
