5c0e8a68a2dcc07ea1b7f28764c575f22c75fa30
Ariana Berger <ariana.berger@gmail.com>
2018-02-07 22:53:48 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -9,27 +9,29 @@ module Players
         move = ""
 
         puts "Computer is making a move..."
+
         if board.turn_count == 0 || board.turn_count == 1
           move = "5"
-        #go to a corner for the first 2 turns
-        elsif board.turn_count <= 3
-          [0, 2, 6, 8].each { |x| return x + 1 if board.cells[x] == " " }
-        else #after this is needs to check the win combos! If 2 are taken in a win combo for other player, then occupy that space
-          n = random_num #pick a random number
-          if board.valid_move?(n) #check that it is a valid move (position)
-          n #return the valid position
-        end
-      end
-    end
+
+      #   #go to a corner for the first 2 turns
+        elsif board.turn_count == 2
+          win = [0, 2, 7, 8].find { |x| board.cells[x] != " " }
 
 
-    end
+      #   else #after this it needs to check the win combos!
+      #      Game::WIN_COMBINATIONS.find do |combo|
+      # #       #If 2 are taken in a win combo for other player, then occupy that space
+      #        if combo.collect { |i| board.cells[i] == token}.count == 2 && combo.any?{|i| board.cells[i] == " "}
+      #          move = [0,1,2].detect {|i| board.cells[i] != " "} #something is going wrong here!
+
+        else
+           n = random_num #pick a random number
+           if board.valid_move?(n) #check that it is a valid move (position)
+           n #return the valid position
+         end
+       end
+
+
+     end
   end
-
-
-
-  ## AI NOTES
-
-  # 1. after HU goes first, go into corner, otherwise if first go into a corner
-  # 2. if CO went first, go into another corner.
-  # 3. Continue to check winning combos to block - how to implement?
+end
