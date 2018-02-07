e67604737fcceeabe1ba9aad55926a212528e38c
Katrina Brinson <kbrinso@gmail.com>
2018-02-07 19:54:09 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -3,14 +3,47 @@ class Game
   attr_accessor :board, :player_1, :player_2
   WIN_COMBINATIONS = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2] ]
   
-  def initialize (player_1,player_2,board=nil)
+  #binding.pry
+  
+  def initialize (player_1=Players::Human.new("X"),player_2=Players::Human.new("O"),board=Board.new) 
+    #need to set the default of each player to a Players::Human
     #this method is expecting Player instances
-    
-    #@player_2 = player_2
+    @player_1 = player_1
+    @player_2 = player_2
     @board = board
   end
   
   #binding.pry
   
+  def current_player
+    if self.board.turn_count.odd?
+      @player_2
+    else 
+      @player_1
+    end
+    #binding.pry
+  end
   
+  def over?
+    self.board.full?
+    #if won?(board) || draw?(board)
+     #return true
+    #elsif !(full?(board))
+    # return false
+    #end
+  end
+  
+  def won?
+   if self.board.cells == [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+     return false
+   else
+     WIN_COMBINATIONS.any?{|combo|
+       if self.board.taken?(combo[0])
+         if self.board.cells[combo[0]] == self.board.cells[combo[1]] && self.board.cells[combo[0]] == self.board.cells[combo[2]]
+           return combo
+         end
+       end
+       }
+   end
+ end
 end
--- lib/player.rb
+++ lib/player.rb
@@ -1,26 +1,17 @@
 class Player
-  attr_accessor :player_1, :player_2
+  #attr_accessor :player_1, :player_2
   attr_reader :token 
   
   def initialize(token)
     @token = token
-    #@player_1 = "X"
-    #@player_2 = "O"
   end
   
-  #def self.player_1
- #   @player_1 = "X"
- # end
-  
-  #def self.player_2
-  #  @player_2 = "O"
+  #def player_1
+   # @player_1 = Player.new("X")
   #end
   
-  def player_1
-    @player_1 = Player.new("X")
-  end
+  #def player_2
+   # @player_2 = Player.new("O")
+  #end
   
-  def player_2
-    @player_2 = Player.new("O")
-  end
 end
