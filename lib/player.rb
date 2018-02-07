fa6eacdce21032388958b4b3c0c382d50bdca08a
Katrina Brinson <kbrinso@gmail.com>
2018-02-07 18:35:45 +0000

    Done.

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,26 @@
 class Player
-  attr_accessor 
-  attr_reader :token
+  attr_accessor :player_1, :player_2
+  attr_reader :token 
+  
   def initialize(token)
     @token = token
+    #@player_1 = "X"
+    #@player_2 = "O"
+  end
+  
+  #def self.player_1
+ #   @player_1 = "X"
+ # end
+  
+  #def self.player_2
+  #  @player_2 = "O"
+  #end
+  
+  def player_1
+    @player_1 = Player.new("X")
+  end
+  
+  def player_2
+    @player_2 = Player.new("O")
   end
 end
