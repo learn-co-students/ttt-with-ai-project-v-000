6957683eb6b4ebc5418b9e6a210e7de67e514efd
Robert Hughes <roberts.ginger.email@gmail.com>
2018-02-07 06:04:07 +0000

    automatically backed up by learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -2,7 +2,7 @@ module Players
   class Human < Player
     def move(position = nil)
       puts "Please enter 1 to 9:"
-      position = gets.chomp
+      position = gets.chomp unless position
       position
     end
   end
