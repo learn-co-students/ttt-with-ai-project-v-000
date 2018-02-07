d5ec207066baf0bd26733b8932681990fb5bb4fb
Katrina Brinson <kbrinso@gmail.com>
2018-02-07 00:14:01 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,34 @@
+class Board
+  attr_accessor :cells
+  def initialize
+    @cells = [" "," "," "," "," "," "," "," "," "]
+  end
+  
+  def reset!
+    @cells = [" "," "," "," "," "," "," "," "," "]
+  end
+  
+  def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
+  
+  def position(location)
+    @cells[location.to_i - 1]
+  end
+  
+  def full?
+    if @cells.detect{|i| i == " "} == nil
+      return true
+    else
+      return false
+    end
+  end
+  
+  def turn_count
+    @cells.count{|i| i != " "}
+  end
+end
