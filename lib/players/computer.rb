5b9496d44df5c1b37020225aa041c7965fa97ecb
Ariana Berger <ariana.berger@gmail.com>
2018-02-07 17:47:39 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -7,13 +7,34 @@ module Players
 
       def move(board)
         puts "Computer is making a move..."
-        n = random_num #pick a random number
-        if board.valid_move?(n) #check that it is a valid move (position)
-           n #return the valid position
-         else
-           random_num
+        if board.turn_count <= 4
+          [0, 2, 6, 8].each { |x| return x + 1 if board.cells[x] == " " }
+        else
+          n = random_num #pick a random number
+          if board.valid_move?(n) #check that it is a valid move (position)
+          n #return the valid position
         end
       end
+    end
+
+
+
+          ##RANDOM NUMBER
+      #   n = random_num #pick a random number
+      #   if board.valid_move?(n) #check that it is a valid move (position)
+      #      n #return the valid position
+      #    else
+      #      random_num
+      #   end
+      # end
 
     end
   end
+
+
+
+  ## AI NOTES
+
+  # 1. after HU goes first, go into corner, otherwise if first go into a corner
+  # 2. if CO went first, go into another corner. Otherwise check winning combo
+  # 3. Continue to check winning combos to block
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -3,8 +3,17 @@ module Players
 
       def move(board)
         puts ">> Make a move! Please enter 1-9:"
+        # display
         gets.strip
       end
+
+      # def display
+      #   puts " 1 | 2 | 3 "
+      #   puts "-----------"
+      #   puts " 4 | 5 | 6 "
+      #   puts "-----------"
+      #   puts " 7 | 8 | 9 "
+      # end
 
     end
   end
