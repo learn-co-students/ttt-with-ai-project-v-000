22f7949eb3a1f90d81c1f9a84590872508c645b4
nptravis <nptravis@yahoo.com>
2018-02-10 07:54:15 +0700

    Done.

---
+++ lib/players/computer.rb
@@ -0,0 +1,14 @@
+module Players
+	class Computer < Player
+		
+		def move(board)
+			valid_moves = ["1","2","3","4","5","6","7","8","9"]
+			valid_moves.each do |position|
+				if board.valid_move?(position)
+					return position
+				end
+			end
+		end
+
+	end
+end
