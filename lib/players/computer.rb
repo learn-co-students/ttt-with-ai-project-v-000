d145e9c3d08b292b0f0b1da2043328906e37a873
Tyler Pulse <tyler3321@gmail.com>
2018-02-12 20:54:23 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,7 +3,9 @@ module Players
     class Computer < Player
         
         def move(board)
-        #first_move - Pretty self-explanatory.  If 5 is open, take it
+
+            move = nil
+      
                 if !board.taken?(5)
                 move = "5"
                 
@@ -12,12 +14,23 @@ module Players
                 
                 elsif board.turn_count == 2
                 move = [1, 3, 7, 9].detect{|i| !board.taken?(i)}.to_s
-                
-                else board.turn_count == 3
+            
+                elsif board.turn_count == 3
                 move = [2, 6, 4, 8].detect{|i| !board.taken?(i)}.to_s
-            end 
+            end
         end 
+        
+        def ai_wins
+            move until move == WIN_COMBINATIONS
+        end 
+        
+        
     end
 end
 
+ WIN_COMBINATIONS = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], #Top
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], #vertical - array 1
+        [0, 4, 8], [2, 4, 6], #diagonal - array 2
+    ]
 
