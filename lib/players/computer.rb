15beb2c96d181310a20b0bba8bf0725167ef262e
Nadia Lee <miss.nadia.lee@gmail.com>
2018-02-12 22:01:32 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -10,20 +10,21 @@ module Players
         move = "5"
       # if 1st player, 2nd move, take any edge
       elsif board.turn_count == 2
-        move = [2, 4, 6, 8].detect{|m| !board.taken?(m)}.to_i
+        move = [2, 4, 6, 8].detect{|m| !board.taken?(m)}
       # if 2nd player, 2nd move, take left or right edge
       elsif board.turn_count == 3
-        move = [4, 6].detect{|m| !board.taken?(m)}.to_i
+        move = [4, 6].detect{|m| !board.taken?(m)}
       else
         Game::WIN_COMBINATIONS.detect do |combo|
           if combo.select{|input| board.position(input) == token}.size == 2 && combo.any?{|input| board.position(input) == " "}
-            move = combo.select{|input| !board.taken?(input)}.first.to_i
-          elsif combo.select{|input| board.position(input) != " " && board.position(input) != token}.size == 2 && combo.any?{|input| board.position(input) == " "}
-            move = combo.select{|input| !board.taken?(input)}.first.to_i
+           move = combo.select{|input| !board.taken?(input)}.first
+         elsif combo.select{|input| board.position(input) != " " && board.position(input) != token}.size == 2 && combo.any?{|input| board.position(input) == " "}
+           move = combo.select{|input| !board.taken?(input)}.first
           end
-          move
         end
+        move = [2, 4, 6, 8, 1, 3, 7, 9].detect{|input| !board.taken?(input)}
       end
+      move
     end
   end
 end
