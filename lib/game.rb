67af04514df418ef3f04ad84384e2c696de6362d
Judy McGoldrick <jjeanius@verizon.net>
2018-02-13 04:43:04 +0000

    Done.

---
+++ lib/game.rb
@@ -0,0 +1,24 @@
+require 'pry'
+
+class Game
+
+  attr_accessor :board, :player_1, :player_2
+
+  WIN_COMBINATIONS = [
+    [6,7,8],
+    [0,3,6],
+    [0,1,2],
+    [3,4,5],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2]
+  ]
+
+  def initialize(player_1 = Player::Human.new("X"), player_2 = Player::Human.new("O"), board = board.new)
+    @player_1 = player_1
+    @player_2 = player_2
+    @board = board
+  end
+
+end
---
+++ lib/players/computer.rb
@@ -0,0 +1,10 @@
+class Player
+
+attr_accessor :player
+
+  def initialized(token)
+    Player.new=self
+    @token
+  end
+
+end
