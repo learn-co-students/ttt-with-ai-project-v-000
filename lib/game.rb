bdfd78f2f825edffad45ef3d890c4f9b4693c991
Ryne Stajcar <rynostajcar@gmail.com>
2018-02-15 17:01:59 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,13 @@
 
 class Game
-  attr_accessor :board, :player_1, :player_2, :name
+  attr_accessor :board, :player_1, :player_2
+
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+    @player_1 = player_1
+    @player_2 = player_2
+    @board = board
+  end
+
 
   WIN_COMBINATIONS = [
 [0,1,2],
@@ -12,14 +19,6 @@ class Game
 [0,4,8],
 [2,4,6]
 ]
-
-
-  def initialize(player_1, player_2, board)
-    @player_1 = player_1
-    @player_2 = player_2
-    @board = board
-  end
-
 
   def current_player
     if @board.turn_count.even?
@@ -50,10 +49,11 @@ class Game
   end
 
   def turn
-    puts "Its #{current_player}'s turn"
+    puts "Its #{current_player.token}'s turn"
     @board.display
     input = current_player.move(@board)
     if !@board.valid_move?(input)
+      binding.pry
       puts "#{input} Invalid move!"
       turn
     elsif @board.valid_move?(input)
--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,12 +1,10 @@
-
-
 module Players
 
   class Computer < Player
 
 
     def move(board)
-      sleep 1
+      sleep 0.5
       valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
       corner_moves = ["1", "3", "7", "9"]
 
@@ -17,8 +15,7 @@ module Players
       elsif board.turn_count < 3
         corner_moves.select {|i| !board.taken?(i)}.sample
       # elsif board.turn_count == 3 &&
-      elsif board.turn_count > 2
-        Game::WIN_COMBINATIONS.detect do |combo|
+      elsif Game::WIN_COMBINATIONS.detect do |combo|
           tokens = combo.collect {|i| board.position(i+1)}
           empty_space = tokens.count(" ")
           my_tokens = tokens.count(self.token)
@@ -30,6 +27,7 @@ module Players
                 return spot.to_s
               end
             end
+
           elsif opp_tokens == 2 && empty_space == 1
             combo.detect do |i|
               if board.position(i+1) == " "
@@ -37,11 +35,12 @@ module Players
                 return spot.to_s
               end
             end
-          elsif board.turn_count == 3
-            return corner_moves.select {|i| !board.taken?(i)}.sample
          end
         end
+      elsif board.turn_count > 2
+        corner_moves.select {|i| !board.taken?(i)}.sample
       end
+
     end
 
     end
