20e3c03432b9cf7695732f2dc9d8cc08c6682966
Alexander Urizar <urizaraj@gmail.com>
2018-02-15 15:34:25 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -12,7 +12,12 @@ class Game
     [6, 4, 2]
   ].freeze
 
-  def initialize(player_1 = Players::Human.new('X'), player_2 = Players::Human.new('O'), board = Board.new)
+  def initialize(
+    player_1 = Players::Human.new('X'),
+    player_2 = Players::Human.new('O'),
+    board = Board.new
+  )
+
     self.board = board
     self.player_1 = player_1
     self.player_2 = player_2
