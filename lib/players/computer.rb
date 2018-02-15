7e1e5db541ef33a0aea21d314d1cb29887813a1b
Ryne Stajcar <rynostajcar@gmail.com>
2018-02-15 17:14:57 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -7,6 +7,7 @@ module Players
       sleep 0.5
       valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
       corner_moves = ["1", "3", "7", "9"]
+      cross_moves = ["2", "4", "6", "8"]
 
       if board.turn_count == 0
         corner_moves.sample
@@ -27,18 +28,29 @@ module Players
                 return spot.to_s
               end
             end
-
-          elsif opp_tokens == 2 && empty_space == 1
-            combo.detect do |i|
-              if board.position(i+1) == " "
-                spot = i+1
-                return spot.to_s
+          end
+        end
+        elsif Game::WIN_COMBINATIONS.detect do |combo|
+            tokens = combo.collect {|i| board.position(i+1)}
+            empty_space = tokens.count(" ")
+            my_tokens = tokens.count(self.token)
+            opp_tokens = 3 - tokens.count(self.token) - tokens.count(" ")
+            if opp_tokens == 2 && empty_space == 1
+              combo.detect do |i|
+                if board.position(i+1) == " "
+                  spot = i+1
+                  return spot.to_s
+                end
               end
             end
-         end
-        end
+          end
+
       elsif board.turn_count > 2
-        corner_moves.select {|i| !board.taken?(i)}.sample
+        if cross_moves.detect {|i| !board.taken?(i)}
+          cross_moves.select {|i| !board.taken?(i)}.sample
+        else
+          valid_moves.select {|i| !board.taken?(i)}.sample
+        end
       end
 
     end
