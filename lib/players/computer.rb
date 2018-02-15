66951c00b2683a49b1903cc4be40f6ee01279837
Jul Jen <juliajen.prog@gmail.com>
2018-02-15 00:21:09 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -4,11 +4,24 @@ module Players
   class Computer < Player
 
       def move(board)
-        test = [1,2,3,4,5,6,7,8,9]
-        if board.valid_move?(5)
+        if board.valid_move?(1)
+          "1"
+        elsif board.valid_move?(2)
+          "2"
+        elsif board.valid_move?(3)
+          "3"
+        elsif board.valid_move?(4)
+          "4"
+        elsif board.valid_move?(5)
           "5"
-        elsif board.valid_move?(1) == nil
-          test.to_s
+        elsif board.valid_move?(6)
+          "6"
+        elsif board.valid_move?(7)
+          "7"
+        elsif board.valid_move?(8)
+          "8"
+        elsif board.valid_move?(9)
+          "9"
         end
       end
 
