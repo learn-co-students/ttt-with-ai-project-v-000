4a3b12a48dbccc801d431a5b5d7a423e4b3b4df7
Chaim Reisman <crreisman@gmail.com>
2018-02-16 00:06:52 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,46 @@
+require 'pry'
+class Board
+  attr_accessor :cells
+  
+  def initialize
+    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end 
+  
+  def reset!
+    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end 
+  
+  def display
+      puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+      puts "-----------"
+      puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+      puts "-----------"
+      puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end 
+  
+  def position(input)
+    user_input = input.to_i - 1 
+    @cells[user_input]
+  end 
+
+  def full?  
+    @cells.all?{|index| index == "X" || index == "O"}
+  end 
+  
+  def turn_count
+     @cells.count{|index| index == "X" || index == "O"}
+  end 
+  
+  def taken?(input)
+    user_input = input.to_i - 1 
+    @cells[user_input] == "X" || @cells[user_input] == "O"
+  end     
+  
+  def valid_move?(input)
+    input.to_i.between?(1,9) && !taken?(input)
+  end 
+  
+  def update(index, player)
+    @cells[index.to_i - 1] = "X"
+  end
+end 
---
+++ lib/game.rb
@@ -0,0 +1,78 @@
+class Game
+  attr_accessor :board, :player_1, :player_2
+  
+  WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [2,5,8],
+  [1,4,7],
+  [0,4,8],
+  [2,4,6]
+  ]
+  
+  def initialize(player_1=Players::Human.new("X"), player_2=Players::Human.new("O"), board=Board.new)
+    @player_1 = player_1
+    @player_2 = player_2
+    @board = board
+  end
+  
+  def current_player
+  binding.pry
+    if board.turn_count.even?
+      @player_1
+    elsif board.turn_count.odd?
+      @player_2
+    end
+  end
+  
+  def full
+    board.cells.all?{|space| space =="X"|| space =="O"}
+  end
+  
+  def over?
+    draw? || won?
+  end
+  
+  def won?
+    WIN_COMBINATIONS.detect do |win_combinations|
+      # win_combinations == [3,4,5]
+      index_1 = win_combinations[0] # 3
+      index_2 = win_combinations[1] # 4
+      index_3 = win_combinations[2] # 5
+      
+      position_1 = board.cells[index_1] 
+      position_2 = board.cells[index_2]
+      position_3 = board.cells[index_3]
+      
+      position_1 == position_2 && position_2 == position_3 && board.taken?(index_1 + 1)
+    end 
+  end 
+  
+  def draw?
+    full && !won?
+  end 
+  
+  def winner
+    if won?
+      array = won?
+      board.cells[array.first]
+    end 
+  end
+  
+  def turn 
+    player = current_player
+    input = player.move(board)
+    if board.valid_move?(input)
+      board.update(input, player)
+    else
+      turn
+    end
+  end  
+  
+  def play 
+    turn until over?
+  end 
+   
+end 
