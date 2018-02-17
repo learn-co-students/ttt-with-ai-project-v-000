c5a0e31ccfe09ef9698345633776dd0451be8a52
Judy McGoldrick <jjeanius@verizon.net>
2018-02-17 04:42:23 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class Game
 
-  attr_accessor :board, :player_1, :player_2
+  attr_accessor :board, :player_1, :player_2, :token
 
   WIN_COMBINATIONS = [
     [6,7,8],
@@ -15,24 +15,31 @@ class Game
     [6,4,2]
   ]
 
-
   def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
-    #binding.pry
       @board = board
       @player_1 = player_1
       @player_2 = player_2
     end
 
-  def current_player
-
-  end
+def current_player
+  @board.turn_count % 2 == 0 ? @player_1 : @player_2
+end
 
   def over?
+    draw? || won?
+  end
+
+  def won?
+    win[3] = [0,1,2]
+    win[6] = [2,6,8]
+
+    !draw? &&
+
 
   end
 
   def draw?
-
+    !won(board) || !winner
   end
 
   def winner
@@ -40,7 +47,13 @@ class Game
   end
 
   def turn
-
+    counter = 0
+    board.each do |token|
+      if token == player_1 || token == player_2
+        counter += 1
+      end
+    end
+    turn
   end
 
   def play
