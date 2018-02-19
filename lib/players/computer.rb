537a92c632639db284d79aa767b65086552898c5
Judy McGoldrick <jjeanius@verizon.net>
2018-02-19 05:15:57 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,4 +1,4 @@
-class Player
+class Players::Computer < Player
 
 attr_accessor :player
 
@@ -7,4 +7,26 @@ attr_accessor :player
     @token
   end
 
+  def move(board)
+    position = gets.strip
+    if valid_move? (board, position)
+      move(board, position, current_player)
+    else
+      turn(board)
+    end
+  end
+
 end
+  #  computer.move(board) == valid_move
+
+
+  #    describe '#move' do
+  #      it 'returns a valid position for the computer to move' do
+  #        computer = Players::Computer.new("X")
+  #        board = Board.new
+#
+#          valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+#
+#          computer_move = computer.move(board)
+#
+#          expect(valid_moves).to include(computer_move)
