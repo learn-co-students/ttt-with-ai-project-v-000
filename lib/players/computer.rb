aca22f31fb6218099dd0f218f04b9e66d379064c
Brennan Heisler <learn@heislercreative.com>
2018-02-24 03:25:43 +0000

    computer player passing

---
+++ lib/players/computer.rb
@@ -0,0 +1,30 @@
+module Players
+  class Computer < Player
+  
+    def move(board)
+      computer_move = nil
+      
+      if !board.taken?(5)
+        computer_move = "5"
+      elsif !board.taken?(7)
+        computer_move = "7"
+      elsif !board.taken?(1)
+        computer_move = "1"
+      elsif !board.taken?(9)
+        computer_move = "9"
+      elsif !board.taken?(4)
+        computer_move = "4"
+      elsif !board.taken?(8)
+        computer_move = "8"
+      elsif !board.taken?(3)
+        computer_move = "3"
+      elsif !board.taken?(6)
+        computer_move = "6"
+      else
+        computer_move = "2"
+      end
+      computer_move
+    end
+  
+  end
+end
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,10 +1,10 @@
 module Players
   class Human < Player
   
-  def move(board = nil)
-    puts "You're up! Where would you like to move? Please pick a valid position from 1-9."
-    gets.strip
-  end
+    def move(board)
+      puts "You're up! Where would you like to move? Please pick a valid position from 1-9."
+      gets.strip
+    end
   
   end
 end
