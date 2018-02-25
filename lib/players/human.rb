31e0c9bd5ebfffa3ef8e9d891d5c10af2a1985df
Brennan Heisler <learn@heislercreative.com>
2018-02-25 00:42:52 +0000

    formatted user prompts

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -2,7 +2,8 @@ module Players
   class Human < Player
   
     def move(board)
-      puts "You're up! Where would you like to move? Please pick a valid position from 1-9."
+      puts "You're up! Where would you like to move?"
+      puts "  Please pick a valid position from 1-9."
       gets.strip
     end
   
