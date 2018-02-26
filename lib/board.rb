f28fc677bd5828e05512a7231dcc299e5811ab86
Barbara Choy <barbchoy@gmail.com>
2018-02-26 21:33:04 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,42 @@
+class Board
+
+  attr_accessor :cells
+
+  def initialize
+    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+
+  def reset!
+    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+
+  def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
+
+  def position(pos)
+    @cells[pos.to_i - 1]
+  end
+
+  def full?
+    @cells.all? {|token| token == "X" || token == "O"}
+  end
+
+  def turn_count
+    @cells.count {|token| token == "X" || token == "O"}
+  end
+
+  def taken?(pos)
+    @cells[pos.to_i - 1] == "X" || @cells[pos.to_i - 1] == "O"
+  end
+
+  def valid_move?(pos)
+    return false if [1..9].include?(pos) == false
+    @cells[pos.to_i] == " "
+  end
+
+end
