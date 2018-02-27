8e1c0db8e2981df194daadc411a3bb559efdd8b2
jaybednar <33104902+jaybednar@users.noreply.github.com>
2018-02-26 19:51:48 -0800

    Update human.rb

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -3,7 +3,12 @@ module Players
   class Human < Player
 
     def move(board = nil)
-      puts "Please enter a Position (1 - 9)"
+      puts "Please enter a Position"
+      puts "(1 | 2 | 3)"
+      puts "-----------"
+      puts "(4 | 5 | 6)"
+      puts "-----------"
+      puts "(7 | 8 | 9)"
 
       input = gets.strip
     end
