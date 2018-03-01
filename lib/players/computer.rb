5e14f46ab0efb4b16e3f9799b2417c7b3c2789ca
Branden Mosley <branden.mosley1337@gmail.com>
2018-03-01 22:46:39 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,19 +1,9 @@
 require 'pry'
 module Players
   class Computer < Player
-    WIN_COMBINATIONS = [
-      [0,1,2],
-      [3,4,5],
-      [6,7,8],
-      [0,3,6],
-      [1,4,7],
-      [2,5,8],
-      [0,4,8],
-      [6,4,2]
-    ]
     
     def initial_move(board)
-      first_moves ||= WIN_COMBINATIONS.sample #pick a random win_combo to play through
+      first_moves ||= Game::WIN_COMBINATIONS.sample #pick a random win_combo to play through
       i = first_moves.sample
       if board.cells[i] == " "
         move = i + 1
