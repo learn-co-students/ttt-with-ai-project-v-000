f2cb7d1df4bb8187c8a815d85a13d3716f2d62a8
Salma Elshahawy <salmaeng71@gmail.com>
2018-03-02 16:36:44 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -39,7 +39,8 @@ end
 
 def win_test(board)
   Game::WIN_COMBINATIONS.detect do |test|
-    cells = [
+    cells = [8
+
       board.cells[test[0]],
       board.cells[test[1]],
       board.cells[test[2]]
