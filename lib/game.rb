809c265d90d605824faf98dddbf62842194c084c
Monica Tamaru Jones <mntamaru@gmail.com>
2018-03-06 21:10:56 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -60,16 +60,21 @@ class Game
   
   def turn
     pos = current_player.move
-    until (pos.to_i > 0 && pos.to_i < 10)
+    until pos.to_i > 0 && pos.to_i < 10 && @board.valid_move?(pos)
       pos = current_player.move
     end
+
     @board.update(pos, current_player)
+   
     pos
   end
   
   def play
     until over?
       turn
+      puts "          "
+      @board.display
+      puts "          "
     end
     
     if draw?
@@ -77,8 +82,59 @@ class Game
     else
       puts "Congratulations #{winner}!"
     end
+    
+    puts "Would you like to play again? Y for Yes"
+    
   end
   
   def start
+    puts "Which game would you like to play?"
+    puts "   Enter 0 for computer vs. computer"
+    puts "   Enter 1 for 1 player vs. computer"
+    puts "   Enter 2 for player vs player"
+    
+    input = gets.strip
+    
+    until input == "0" || input == "1" || input == "2"
+      puts "Please enter number of players - 0, 1, or 2"
+      input = gets.strip
+    end
+    
+    player = ""
+    if input != "0"
+      puts "Who should go first and be X? (for 1 or 2 players)
+              Enter 1 -> player 1  
+                    2 -> player 2/computer "
+    
+      player =  gets.strip
+      
+      until  player == "1" || player == "2"
+        puts "Please enter who goes first ('X') - 1 or 2"
+        player = gets.strip
+      end
+    end
+    
+    if input == "0"
+      @player_1 = Players::Computer.new("X")
+      @player_2 = Players::Computer.new("O")
+    elsif input == "1"
+      if player == "1"
+        @player_1 = Players::Human.new("X")
+        @player_2 = Players::Computer.new("O")
+      else
+        @player_1 = Players::Computer.new("X")
+        @player_2 = Players::Human.new("O")
+      end
+    elsif input == "2"
+      if player == '1'
+        @player_1 = Players::Human.new("X")
+        @player_2 = Players::Human.new("O")
+      else
+        @player_1 = Players::Human.new("O")
+        @player_2 = Players::Human.new("X")
+      end
+    end
+    
+    play
   end
 end
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -2,7 +2,7 @@ require 'pry'
 module Players
   class Human < Player
     def move(board=nil)
-      puts "Please enter a board position to play: "
+      puts "Please enter a board position to play (1-9): "
       input = gets.strip
     end
   end
