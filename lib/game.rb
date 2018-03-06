d837cafaa31c8bf3fe3819c330d867edabbc831c
Kriti Rai <kriti.rai@gmail.com>
2018-03-06 06:13:01 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -56,31 +56,82 @@ class Game
         puts "Cat's Game!"
       end
   end
-  # def start
-    # input = gets.strip
-    #   if input == 1
-    #     game = Game.new(player_1= Players::Computer.new("X"), player_2 = Players::Computer.new("O"))
-    #     game.play
-    #   elsif input == 2
-    #     puts "Who would you like to go first and be X? For Player 1, type 1. For Player 2, type 2."
-    #     player = gets.strip
-    #       if player == 1
-    #         game = Game.new(player_1= Players::Human.new("X"), player_2 = Players::Computer.new("O"))
-    #         game.play
-    #       elsif player == 2
-    #         game = Game.new(player_1= Players::Computer.new("X"), player_2 = Players::Human.new("O"))
-    #         game.play
-    #       end
-    #   elsif input == 3
-    #     puts "Who would you like to go first and be X? For Player 1, type 1. For Player 2, type 2."
-    #     player = gets.strip
-    #       if player == 1
-    #         game = Game.new(player_1= Players::Human.new("X"), player_2 = Players::Human.new("O"))
-    #         game.play
-    #       elsif player == 2
-    #         game = Game.new(player_1= Players::Human.new("O"), player_2 = Players::Human.new("X"))
-    #         game.play
-    #       end
-    #     end
+
+  #/////////////////////starting the game //////////////
+  def start
+    input = gets.strip.to_i
+
+      if input == 1
+        puts "You have chosen Option 1: 2 Computer players."
+        puts "Let the game begin!!"
+        Board.new.display
+        game = Game.new(player_1= Players::Computer.new("X"), player_2= Players::Computer.new("O"))
+        game.play
+      elsif input == 2
+        puts "You have chosen Option 2: 1 player."
+        puts "Who would you like to go first - you or the computer? For yourself, type 1. For computer, type 2."
+        turn = gets.strip.to_i
+        puts "Would you like to be X or O?"
+        token = gets.strip.downcase
+        puts "Let the game begin!!"
+        Board.new.display
+          if turn == 1 && token == "x"
+            game = Game.new(player_1= Players::Human.new("X"), player_2= Players::Computer.new("O"))
+            game.play
+          elsif turn == 1 && token == "o"
+            game = Game.new(player_1= Players::Human.new("O"), player_2= Players::Computer.new("X"))
+            game.play
+          elsif turn == 2 && token == "x"
+            game = Game.new(player_1= Players::Computer.new("O"), player_2= Players::Human.new("X"))
+            game.play
+          elsif turn == 2 && token == "o"
+            game = Game.new(player_1= Players::Computer.new("X"), player_2= Players::Human.new("O"))
+            game.play
+          end
+      elsif input == 3
+        puts "You have chosen Option 3: 2 Human players."
+        puts "Would you like to be Player 1 or 2? For Player 1, type 1. For Player 2, type 2."
+        turn = gets.strip.to_i
+        puts "Would you like to be X or O?"
+        token = gets.strip.downcase
+        puts "Let the game begin!!"
+        Board.new.display
+          if turn == 1&& token == "x"
+            # binding.pry
+            game = Game.new(player_1= Players::Human.new("X"), player_2= Players::Human.new("O"))
+            game.play
+          elsif turn == 1 && token == "o"
+            game = Game.new(player_1= Players::Human.new("O"), player_2= Players::Human.new("X"))
+            game.play
+          elsif turn == 2 && token == "x"
+            game = Game.new(player_1= Players::Human.new("O"), player_2= Players::Human.new("X"))
+            game.play
+          elsif turn == 2 && token == "o"
+            game = Game.new(player_1= Players::Human.new("X"), player_2= Players::Human.new("O"))
+            game.play
+          end
+        end
+          #//////////////START ANOTHER GAME//////////////////
+          puts "Would you like to play another game?"
+          puts "Type Y for YES, N for NO"
+          replay = gets.strip.downcase
+          if replay == "y"
+            Game.new.start
+          elsif replay == "n"
+            puts "Good-bye. See you soon!"
+          end
+    end
+
+    # def replay
+    #   puts "Would you like to play another game?"
+    #   puts "Type Y for YES, N for NO"
+    #   input = gets.strip.to_s.downcase
+    #   if input == "y"
+    #     Game.new.start
+    #   elsif input == "n"
+    #     puts "Good-bye. See you soon!"
     #   end
+    # end
+
+
 end
--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -9,9 +9,61 @@ module Players
           i.to_s
         end
       end
-      valid_moves.find{|valid_move| valid_move}
-      # binding.pry
+      valid_moves.sample.to_s
     end
-    # binding.pry
   end
 end
+
+
+    # def center_move(board)
+    #   #if center is available always make this move
+    #   if !@board.taken?(5)
+    #     @board.update(5)
+    #   end
+    # end
+    #
+    # def corner_move(board)
+    #   #else make this one of the corner moves
+    #   corner_move = [1,3,7,9].sample
+    #   if valid_move?(corner_move)
+    #     @board.update(corner_move)
+    #   end
+    # end
+    # def block_move(board)
+    #
+    # end
+    # def opposite_corner(board)
+    #   if @board.position(1) == token && valid_move?(9)
+    #     @board.update(9,token)
+    #   elsif @board.position(9) == token && valid_move?(1)
+    #     @board.update(1,token)
+    #   elsif @board.position(3) == token && valid_move?(7)
+    #     @board.update(7,token)
+    #   elsif @board.position(7) == token && valid_move?(3)
+    #     @board.update(3,token)
+    #   end
+    # end
+    # def win_move(board)
+    #   center_move(board) ||
+    #   if !@board.taken?(7)
+    #     @board.update(7,token)
+    #   elsif !@board.taken?(3)
+    #     @board.update(7,token)
+    #   end
+    #
+    #   if
+    #
+    #
+    #   end
+    # end
+    # def move(board)
+    #   corner_move(board)
+    #   # if valid_move?(5) center_move(board)
+    #
+    #   win_move(board)
+    # end
+
+      # ||edge_move(board)||win_move(board)
+#     end
+#
+# end
