7f399b22b723e6b4fef73a8c1dda7d93c3db8a53
Kriti Rai <kriti.rai@gmail.com>
2018-03-07 06:58:19 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,19 +2,6 @@ require 'pry'
 module Players
 
   class Computer < Player
-
-    def move(board)
-      valid_moves = Board.new.cells.map.with_index(1) do |v, i|
-        if v == " "
-          i.to_s
-        end
-      end
-      valid_moves.sample.to_s
-      # valid_moves.find{|move|@board.cells[move] == " "}.to_s
-      binding.pry
-    end
-  end
-end
     #////////////AS PLAYER_1//////////////////////
     def corner_move(board)
       #first move
@@ -24,45 +11,81 @@ end
 
     def opposite_corner(board)
       #second move
-     if @board.position(1) == self.token
+     if board.position(1) == self.token
        "9"
-     elsif @board.position(3) == self.token
+     elsif board.position(3) == self.token
        "7"
-     elsif @board.position(7) == self.token
+     elsif board.position(7) == self.token
        "3"
-     elsif @board.position(9) == self.token
+     elsif board.position(9) == self.token
        "1"
      end
    end
    #<<<<<<<<<<<block_move>>>>>>>>>>>>>>
    def center_move(board)
-     if !@board.taken?(5)
+     if !board.taken?(5)
        "5"
      end
    end
    #<<<<<<<<<<<block_move>>>>>>>>>>>>>>
    def edge_move(board)
      #possibly third move
-     if !@board.taken(3) && !@board.taken(7)
+     if !board.taken?(3) && !board.taken?(7)
        #if 3 and 7 are available choose 1
        third_move = [3,7].sample.to_s
        third_move
-     elsif @board.taken(3)
+     elsif board.taken?(3)
       #if 3 is taken move to 7
        "7"
-     elsif @board.taken(7)
+     elsif board.taken?(7)
        #if 7 is taken move to 3
        "3"
      end
    end
    #<<<<<<<<<<<block_move>>>>>>>>>>>>>>
-   def fourth_move(board)
-     WIN_COMBINATIONS.find do |d|
-       d[0] = i_1
-       d[1] = i_2
-       d[2] = i_3
-       i_1
-       i_2
-       i_3
+   def win_move(board)
+     win_1 = WIN_COMBINATIONS.detect do |combo|
+    		       input_1 = combo[0]
+    		       input_2 = combo[1]
+    		       input_3 = combo[2]
+    		       board[input_1] == "X" && board[input_2] == "X" && board[input_3] == " "
+  	        end
 
-   end
+     win_2 = WIN_COMBINATIONS.detect do |combo|
+  			       input_1 = combo[0]
+  			       input_2 = combo[1]
+  			       input_3 = combo[2]
+  			       board[input_2] == "X" && board[input_3] == "X" && board[input_1] == " "
+  	        end
+   	win_3 = WIN_COMBINATIONS.detect do |combo|
+           	 input_1 = combo[0]
+           	 input_2 = combo[1]
+           	 input_3 = combo[2]
+           	 board[input_1] == "X" && board[input_3] == "X" && board[input_2] == " "
+           	end
+       if win_1
+            (win_1.detect{|i| board[i]==" "}+1).to_s
+       elsif win_2
+       	    (win_2.detect{|i| board[i]==" "}+1).to_s
+       elsif win_3
+       	    (win_3.detect{|i| board[i]==" "}+1).to_s
+       end
+    end
+    def move(board)
+        if board.turn_count == 0
+          corner_move(board)
+        elsif board.turn_count == 2
+          opposite_corner(board)
+        elsif board.turn_count == 4
+          edge_move(board) || win_move(board)
+        else
+          valid_moves = board.cells.map.with_index(1) do |v, i|
+              if v == " "
+                i.to_s
+              end
+            end
+          valid_moves.sample.to_s
+        end
+    end
+  end
+end
