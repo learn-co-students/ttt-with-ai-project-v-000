108aeb30c98588fdf399d35fb35ea0a0bea7746e
Joshua Owens <joshuaowens011@gmail.com>
2018-03-07 09:39:08 -0500

    move #over? spec below #won? and #draw? specs

--- spec/04_game_spec.rb
+++ spec/04_game_spec.rb
@@ -75,29 +75,6 @@ describe 'Game' do
     end
   end
 
-  describe '#over?' do
-    it 'returns true for a draw' do
-      game = Game.new
-      game.board.cells = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
-
-      expect(game.over?).to be_truthy
-    end
-
-    it 'returns true for a won game' do
-      game = Game.new
-      game.board.cells = ["X", "O", "X", "O", "X", "X", "O", "O", "X"]
-
-      expect(game.over?).to be_truthy
-    end
-
-    it 'returns false for an in-progress game' do
-      game = Game.new
-      game.board.cells = ["X", " ", "X", " ", "X", " ", "O", "O", " "]
-
-      expect(game.over?).to be_falsey
-    end
-  end
-
   describe '#won?' do
     it 'returns false for a draw' do
       game = Game.new
@@ -145,6 +122,29 @@ describe 'Game' do
       game.board.cells = ["X", " ", "X", " ", "X", " ", "O", "O", "X"]
 
       expect(game.draw?).to be_falsey
+    end
+  end
+
+  describe '#over?' do
+    it 'returns true for a draw' do
+      game = Game.new
+      game.board.cells = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
+
+      expect(game.over?).to be_truthy
+    end
+
+    it 'returns true for a won game' do
+      game = Game.new
+      game.board.cells = ["X", "O", "X", "O", "X", "X", "O", "O", "X"]
+
+      expect(game.over?).to be_truthy
+    end
+
+    it 'returns false for an in-progress game' do
+      game = Game.new
+      game.board.cells = ["X", " ", "X", " ", "X", " ", "O", "O", " "]
+
+      expect(game.over?).to be_falsey
     end
   end
 
