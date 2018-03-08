44b592e68dc7b9b6b4d56ab06bfb3906afd18f7f
Kriti Rai <kriti.rai@gmail.com>
2018-03-08 06:00:24 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,23 +2,30 @@ require 'pry'
 module Players
 
   class Computer < Player
+    WIN_COMBINATIONS = [
+      [0,1,2], [3,4,5], [6,7,8],
+      [0,3,6], [1,4,7], [2,5,8],
+      [0,4,8], [2,4,6]
+    ]
     #////////////AS PLAYER_1//////////////////////
     def corner_move(board)
-      #first move
       first_move = [1,3,7,9].sample.to_s
       first_move
     end
 
     def opposite_corner(board)
-      #second move
      if board.position(1) == self.token
-       "9"
+       "9" unless (board.position(9) != self.token && board.taken?(9))
+       ["7","3"].sample
      elsif board.position(3) == self.token
-       "7"
+       "7" unless (board.position(7) != self.token && board.taken?(7))
+       ["1","9"].sample
      elsif board.position(7) == self.token
-       "3"
+       "3" unless (board.position(3) != self.token && board.taken?(3))
+       ["7","3"].sample
      elsif board.position(9) == self.token
-       "1"
+       "1"unless (board.position(1) != self.token && board.taken?(1))
+       ["1","9"].sample
      end
    end
    #<<<<<<<<<<<block_move>>>>>>>>>>>>>>
@@ -42,49 +49,64 @@ module Players
        "3"
      end
    end
-   #<<<<<<<<<<<block_move>>>>>>>>>>>>>>
    def win_move(board)
      win_1 = WIN_COMBINATIONS.detect do |combo|
     		       input_1 = combo[0]
     		       input_2 = combo[1]
     		       input_3 = combo[2]
-    		       board[input_1] == "X" && board[input_2] == "X" && board[input_3] == " "
+    		       board.cells[input_1] == self.token && board.cells[input_2] == self.token && board.cells[input_3] == " "
   	        end
-
      win_2 = WIN_COMBINATIONS.detect do |combo|
   			       input_1 = combo[0]
   			       input_2 = combo[1]
   			       input_3 = combo[2]
-  			       board[input_2] == "X" && board[input_3] == "X" && board[input_1] == " "
+  			       board.cells[input_2] == self.token && board.cells[input_3] == self.token && board.cells[input_1] == " "
   	        end
    	win_3 = WIN_COMBINATIONS.detect do |combo|
            	 input_1 = combo[0]
            	 input_2 = combo[1]
            	 input_3 = combo[2]
-           	 board[input_1] == "X" && board[input_3] == "X" && board[input_2] == " "
+           	 board.cells[input_1] == self.token && board.cells[input_3] == self.token && board.cells[input_2] == " "
            	end
        if win_1
-            (win_1.detect{|i| board[i]==" "}+1).to_s
+            (win_1.detect{|i| board.cells[i]==" "}+1).to_s
        elsif win_2
-       	    (win_2.detect{|i| board[i]==" "}+1).to_s
+       	    (win_2.detect{|i| board.cells[i]==" "}+1).to_s
        elsif win_3
-       	    (win_3.detect{|i| board[i]==" "}+1).to_s
+       	    (win_3.detect{|i| board.cells[i]==" "}+1).to_s
        end
     end
+    def valid_move(board)
+      valid_move = board.cells.map.with_index(1) do |v, i|
+          if v == " "
+            i.to_s
+          end
+        end
+      end
     def move(board)
         if board.turn_count == 0
           corner_move(board)
+        elsif board.turn_count == 1
+          corner_move(board) || center_move(board)
         elsif board.turn_count == 2
           opposite_corner(board)
+        elsif board.turn_count == 3
+          "8" unless board.taken?(8) || "2" unless board.taken?(2)
         elsif board.turn_count == 4
-          edge_move(board) || win_move(board)
+          moves = [win_move(board),edge_move(board),valid_move(board)]
+          # binding.pry
+          moves.find{|move| board.valid_move?(move)}
+        elsif board.turn_count == 5
+          "3" unless board.taken?(3) || "7" unless board.taken?(7)
+        elsif board.turn_count == 6
+          binding.pry
+          win_move(board) unless board.valid_move?(win_move(board)) ||
+          valid_move(board) unless board.valid_move?(valid_move(board))
+        elsif board.turn_count == 7
+          "3" unless board.taken?(3) || "7" unless board.taken?(7)
         else
-          valid_moves = board.cells.map.with_index(1) do |v, i|
-              if v == " "
-                i.to_s
-              end
-            end
-          valid_moves.sample.to_s
+          win_move(board) unless board.valid_move?(win_move(board)) ||
+          valid_move(board) unless board.valid_move?(valid_move(board))
         end
     end
   end
