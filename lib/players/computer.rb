76455cfb7701caffddcf3a06d92896849cf162bc
Kriti Rai <kriti.rai@gmail.com>
2018-03-09 03:35:54 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,4 +1,3 @@
-require 'pry'
 module Players
 
   class Computer < Player
@@ -8,130 +7,142 @@ module Players
     end
     def opposite_corner(board)
      if board.position(1) == self.token
-       "9" unless (board.position(9) != self.token && board.taken?(9))
-       ["7","3"].sample
-     elsif board.position(3) == self.token
-       "7" unless (board.position(7) != self.token && board.taken?(7))
-       ["1","9"].sample
-     elsif board.position(7) == self.token
-       "3" unless (board.position(3) != self.token && board.taken?(3))
-       ["7","3"].sample
-     elsif board.position(9) == self.token
-       "1"unless (board.position(1) != self.token && board.taken?(1))
-       ["1","9"].sample
-     end
-   end
-   def center_move(board)
-     if !board.taken?(5)
-       "5"
-     end
-   end
-   def edge_move(board)
-     if !board.taken?(3) && !board.taken?(7)
-       [3,7].sample.to_s
-     elsif board.taken?(3)
-       "7"
-     elsif board.taken?(7)
-       "3"
-     end
-   end
-   def win_move(board)
-     win_1 = WIN_COMBINATIONS.detect do |combo|
-    		       input_1 = combo[0]
-    		       input_2 = combo[1]
-    		       input_3 = combo[2]
-    		       board.cells[input_1] == self.token && board.cells[input_2] == self.token && board.cells[input_3] == " "
-  	        end
-     win_2 = WIN_COMBINATIONS.detect do |combo|
-  			       input_1 = combo[0]
-  			       input_2 = combo[1]
-  			       input_3 = combo[2]
-  			       board.cells[input_2] == self.token && board.cells[input_3] == self.token && board.cells[input_1] == " "
-  	        end
-   	win_3 = WIN_COMBINATIONS.detect do |combo|
-           	 input_1 = combo[0]
-           	 input_2 = combo[1]
-           	 input_3 = combo[2]
-           	 board.cells[input_1] == self.token && board.cells[input_3] == self.token && board.cells[input_2] == " "
-           	end
-       if win_1
-            (win_1.detect{|i| board.cells[i]==" "}+1).to_s
-       elsif win_2
-       	    (win_2.detect{|i| board.cells[i]==" "}+1).to_s
-       elsif win_3
-       	    (win_3.detect{|i| board.cells[i]==" "}+1).to_s
+       if !board.taken?(9)
+         "9"
+       elsif board.position(9) != self.token && board.taken?(9)
+         ["7","3"].sample
        end
-    end
-    def defend(board)
-      defend_1 = WIN_COMBINATIONS.detect do |combo|
-                input_1 = combo[0]
-                input_2 = combo[1]
-                input_3 = combo[2]
-                board.cells[input_1] != self.token && board.cells[input_1] != " " && board.cells[input_2] != self.token && board.cells[input_2] != " " && board.cells[input_3] == " "
-             end
-      defend_2 = WIN_COMBINATIONS.detect do |combo|
-                input_1 = combo[0]
-                input_2 = combo[1]
-                input_3 = combo[2]
-                board.cells[input_2] != self.token && board.cells[input_2] != " " && board.cells[input_3] != self.token && board.cells[input_3] != " " && board.cells[input_1] == " "
-             end
-     defend_3 = WIN_COMBINATIONS.detect do |combo|
-              input_1 = combo[0]
-              input_2 = combo[1]
-              input_3 = combo[2]
-              board.cells[input_1] != self.token && board.cells[input_1] != " " && board.cells[input_3] != self.token && board.cells[input_3] != " " && board.cells[input_2] == " "
-             end
-        if defend_1
-             (defend_1.detect{|i| board.cells[i]==" "}+1).to_s
-        elsif defend_2
-             (defend_2.detect{|i| board.cells[i]==" "}+1).to_s
-        elsif defend_3
-             (defend_3.detect{|i| board.cells[i]==" "}+1).to_s
-        end
+     elsif board.position(3) == self.token
+       if !board.taken?(7)
+         "7"
+       elsif board.position(7) != self.token && board.taken?(7)
+         ["1","9"].sample
+       end
+     elsif board.position(7) == self.token
+       if !board.taken?(3)
+         "3"
+       elsif board.position(3) != self.token && board.taken?(3)
+         ["7","3"].sample
+       end
+     elsif board.position(9) == self.token
+       if !board.taken?(1)
+         "1"
+       elsif board.position(1) != self.token && board.taken?(1)
+         ["1","9"].sample
+       end
      end
-    def valid_move(board)
-      valid_moves = []
-      board.cells.map.with_index(1) do |v, i|
-          if v == " "
-            valid_moves << i.to_s
-          end
-        end
-        valid_moves.sample
+     end
+     def center_move(board)
+       if !board.taken?(5)
+         "5"
+       end
+     end
+     def edge_move(board)
+       if !board.taken?(3) && !board.taken?(7)
+         [3,7].sample.to_s
+       elsif board.taken?(3)
+         "7"
+       elsif board.taken?(7)
+         "3"
+       end
+     end
+     def win_move(board)
+       win_1 = WIN_COMBINATIONS.detect do |combo|
+      		       input_1 = combo[0]
+      		       input_2 = combo[1]
+      		       input_3 = combo[2]
+      		       board.cells[input_1] == self.token && board.cells[input_2] == self.token && board.cells[input_3] == " "
+    	        end
+       win_2 = WIN_COMBINATIONS.detect do |combo|
+    			       input_1 = combo[0]
+    			       input_2 = combo[1]
+    			       input_3 = combo[2]
+    			       board.cells[input_2] == self.token && board.cells[input_3] == self.token && board.cells[input_1] == " "
+    	        end
+     	win_3 = WIN_COMBINATIONS.detect do |combo|
+             	 input_1 = combo[0]
+             	 input_2 = combo[1]
+             	 input_3 = combo[2]
+             	 board.cells[input_1] == self.token && board.cells[input_3] == self.token && board.cells[input_2] == " "
+             	end
+         if win_1
+              (win_1.detect{|i| board.cells[i]==" "}+1).to_s
+         elsif win_2
+         	    (win_2.detect{|i| board.cells[i]==" "}+1).to_s
+         elsif win_3
+         	    (win_3.detect{|i| board.cells[i]==" "}+1).to_s
+         end
       end
-    def move(board)
-        if board.turn_count == 0
-          corner_move(board)
-        elsif board.turn_count == 1
-          sleep(1)
-          if !board.taken?("5")
-            center_move(board)
-          else
+      def defend(board)
+        defend_1 = WIN_COMBINATIONS.detect do |combo|
+                  input_1 = combo[0]
+                  input_2 = combo[1]
+                  input_3 = combo[2]
+                  board.cells[input_1] != self.token && board.cells[input_1] != " " && board.cells[input_2] != self.token && board.cells[input_2] != " " && board.cells[input_3] == " "
+               end
+        defend_2 = WIN_COMBINATIONS.detect do |combo|
+                  input_1 = combo[0]
+                  input_2 = combo[1]
+                  input_3 = combo[2]
+                  board.cells[input_2] != self.token && board.cells[input_2] != " " && board.cells[input_3] != self.token && board.cells[input_3] != " " && board.cells[input_1] == " "
+               end
+       defend_3 = WIN_COMBINATIONS.detect do |combo|
+                input_1 = combo[0]
+                input_2 = combo[1]
+                input_3 = combo[2]
+                board.cells[input_1] != self.token && board.cells[input_1] != " " && board.cells[input_3] != self.token && board.cells[input_3] != " " && board.cells[input_2] == " "
+               end
+          if defend_1
+            (defend_1.detect{|i| board.cells[i]==" "}+1).to_s
+          elsif defend_2
+            (defend_2.detect{|i| board.cells[i]==" "}+1).to_s
+          elsif defend_3
+            (defend_3.detect{|i| board.cells[i]==" "}+1).to_s
+          end
+       end
+       def valid_move(board)
+         valid_moves = []
+         board.cells.map.with_index(1) do |v, i|
+            if v == " "
+              valid_moves << i.to_s
+            end
+          end
+          valid_moves.sample
+       end
+      def move(board)
+          if board.turn_count == 0
             corner_move(board)
+          elsif board.turn_count == 1
+            sleep(1)
+              if !board.taken?("5")
+                center_move(board)
+              else
+                corner_move(board)
+              end
+          elsif board.turn_count == 2
+            opposite_corner(board)
+          elsif board.turn_count == 3
+            sleep(1)
+            defend(board) || win_move(board) ||
+            if !board.taken?(8)
+              "8"
+            elsif !board.taken?(2)
+              "2"
+            end
+          elsif board.turn_count == 4
+            defend(board) || win_move(board) || valid_move(board)
+          elsif board.turn_count == 5
+            sleep(1)
+            defend(board) || win_move(board) ||
+            if !board.taken?(3)
+              "3"
+            elsif !board.taken?(7)
+              "7"
+            end
+          else
+            sleep(1)
+            defend(board) || win_move(board) || valid_move(board)
           end
-        elsif board.turn_count == 2
-          opposite_corner(board)
-        elsif board.turn_count == 3
-          sleep(1)
-          defend(board) || win_move(board) ||
-          if !board.taken?(8)
-            "8"
-          elsif !board.taken?(2)
-            "2"
-          end
-        elsif board.turn_count == 4
-          defend(board) || win_move(board) || valid_move(board)
-        elsif board.turn_count == 5
-          sleep(1)
-          defend(board) || win_move(board) ||
-          if !board.taken?(3)
-            "3"
-          elsif !board.taken?(7)
-            "7"
-          end
-        else
-          sleep(1)
-          defend(board) || win_move(board) || valid_move(board)
         end
     end
-  end
 end
