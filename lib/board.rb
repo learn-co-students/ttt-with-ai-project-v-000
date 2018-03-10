0f1ed2328423c6e17101870ebf2b83410a576c7c
Ron Sala <ron.sala@gmail.com>
2018-03-10 10:40:30 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,43 @@
 class Board
   attr_accessor :cells
 
-  def reset
+  def initialize
+    reset!
+  end
+
+  def reset!
+    @cells = Array.new(9, " ")
+  end
+
+  def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
+
+  def position
+
+  end
+
+  def update(position, token)
+
+  end
+
+  def token
+
+  end
+
+  def full?
+    @cells.all?{|token| token == "X" || token == "O"}
+  end
+
+  def turn_count
+
+  end
+
+  def valid_move
 
   end
 end
---
+++ lib/player.rb
@@ -0,0 +1,6 @@
+class Player
+
+  def initialize(token)
+    @token = token
+  end
+end
