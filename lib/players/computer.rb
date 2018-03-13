0c3dcd4c607763f9c5caf2a03081d2c77f901e4c
Ryan Manchester <ryanm17@gmail.com>
2018-03-13 21:05:22 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,28 +3,15 @@ require 'pry'
 module Players
   class Computer < Player
 
-#valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
     def move(board)
-binding.pry
+      valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+      board.valid_move?("5") ? "5" : other_moves(board)
 
-      #   if !board.taken?("5")
-      #    "5"
-      #    binding.pry
-      #  elsif board.taken?("5") == token
-    # elsif board.taken?("5") && board.taken?("3")
-    #      "7"
-      #  elsif board.taken?("5") && board.taken?("7")
-      #    "3"
-      #  elsif board.taken?("5") && board.taken?("1")
-      #    "9"
-
-      #  else
-      #    valid_moves.each {|move| board.valid_move?(move)}.sample
-      #end
     end
 
   def corner(board)
-    corners = [1, 5, 7, 9]
+    corners = [1, 3, 7, 9]
     corners.detect {|cell| board.valid_move?(cell)}
   end
 
@@ -33,14 +20,8 @@ binding.pry
     valid_moves.each {|move| board.valid_move?(move)}.sample
   end
 
-  def block(board)
-
-    valid_moves = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    valid_moves.detect do |move|
-      if !board.valid_move?(move)
-        board.valid_move?(move + 1)
-      end
-    end
+  def other_moves(board)
+    corner(board) || random(board)
   end
 
 
