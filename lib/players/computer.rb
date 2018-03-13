ae15deddbc534b9ad087db02bf92a57644828b2c
Ryan Manchester <ryanm17@gmail.com>
2018-03-13 04:05:28 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -4,17 +4,25 @@ module Players
   class Computer < Player
 
     def move(board)
-binding.pry
-      valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-      valid_moves.detect {|move| board.valid_move?(move)}
+      binding.pry
+        valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        if !board.taken?("5")
+         "5"
+       elsif board.taken?("3") && board.taken?("5") == !self.token
+          "7"
+    # elsif board.taken?("5") && board.taken?("3")
+    #      "7"
+      #  elsif board.taken?("5") && board.taken?("7")
+      #    "3"
+      #  elsif board.taken?("5") && board.taken?("1")
+      #    "9"
+
+       else
+         valid_moves.each {|move| board.valid_move?(move)}.sample
+      end
     end
 
-    # def best_move(board)
-    #   if !board.taken?("5")
-    #     board.position("5")
-    #   end
-    #
-    # end
+
 
 
   end
