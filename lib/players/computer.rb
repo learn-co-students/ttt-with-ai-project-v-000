06fcfecc90a864178905316015967415e2eba481
Zac Willmington <zac.willmington@gmail.com>
2018-03-20 02:01:00 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -15,7 +15,6 @@ class Players
              else
                 rand(1..9).to_s
             end
-
         end
 
         def quickest_win_combo(board, win_combos)
@@ -30,17 +29,24 @@ class Players
                     elements == combos[0] || elements == combos[1] || elements == combos[2]
                 end
             end
+                #I think it's recognizing both tokens qui
 
             combos_most_tokens = win_combo_match.find do |best_combo|
-                best_combo.include?(places_of_tokens[0] && places_of_tokens[1])
+                # best_combo.include?(places_of_tokens[0] && places_of_tokens[1])
+                var = best_combo - (@places_of_tokens[0]..@places_of_tokens[1]).to_a
+                var
             end
             combos_most_tokens
+            #binding.pry
         end
 
         def winning_square(board, win_combos)
-            winning_place = quickest_win_combo(board, win_combo)
-                val = winning_place.select do |i|
-                    !@places_of_tokens.include?(i)
+            winning_place = quickest_win_combo(board, win_combos)
+
+                val = winning_place.reject do |i|
+                    #binding.pry
+                    @places_of_tokens.include?(i)
+                    #binding.pry
                 end
             w_i_n = val[0] + 1
             w_i_n.to_s
