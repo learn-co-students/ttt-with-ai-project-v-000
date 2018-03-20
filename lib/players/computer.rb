b00c9c6427055b4a6a6406636494f6dae6efe1d6
Denise Pendleton <dpendl1@hotmail.com>
2018-03-20 00:30:44 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,15 +1,8 @@
 module Players
     class Computer < Player
-        #  input = nil
+
       def move(board)
       input = rand(1..9).to_s
-      # board.current_player = self
-      # if board.valid_move?(input)
-      #   board.update(input, player)
-      # else
-      #   move(board)
-      #  end
-
-    end
+      end
   end
 end
