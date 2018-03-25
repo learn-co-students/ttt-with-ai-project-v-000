6b74abf9b53b97f2992178a89a53669eb8369367
Zac Willmington <zac.willmington@gmail.com>
2018-03-25 18:21:35 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -4,16 +4,17 @@ class Players
 
     class Computer < Player
 
-        attr_accessor :places_of_tokens, :places_available, :combos_available, :final_combo
+        attr_accessor :places_of_tokens, :places_available, :combos_available, :final_combo, :challenger_places
 
         def move(board, win_combos)
             puts "AI is thinking....."
             sleep 3
-            if board.turn_count >= 4
+            if board.turn_count >= 2
                 find_tokens(board)
+                places_taken_by_challenger(board)
                 find_available_places(board)
                 all_combos_available(win_combos)
-                final_combo
+                final_combos
                 quickest_win_combo(board, win_combos)
              else
                 rand(1..9).to_s
@@ -25,6 +26,13 @@ class Players
                 board.cells[i] == self.token
             end
             @places_of_tokens = places_of_tokens
+       end
+
+       def places_taken_by_challenger(board)
+           challenger_places = board.cells.each_index.select do|i|
+               board.cells[i] != self.token && board.cells[i] != " "
+           end
+           @challenger_places = challenger_places
        end
 
         def find_available_places(board)
@@ -45,7 +53,7 @@ class Players
         end
 
         def final_combos
-            final_combo = combos_available.detect do |b|
+            final_combo = @combos_available.detect do |b|
                         if b.include?(@places_of_tokens[0]) &&  b.include?(@places_of_tokens[1]) ||  b.include?(@places_of_tokens[1]) &&  b.include?(@places_of_tokens[0])
                              b
                         end
@@ -54,23 +62,25 @@ class Players
         end
 
         def quickest_win_combo(board, win_combos)
-
                     if @final_combo == nil
                     	second_best_option = @combos_available.detect do |b|
-                    	 b.include?(@places_of_tokens[0]) ||  b.include?(@places_of_tokens[1])
+                    	  b.include?(@places_of_tokens[0]) || !b.all?{|e| @challenger_places.include?(e)}
+
+
                     	 end
-                         binding.pry
+
                     	 next_move = second_best_option[0] + 1
-                         next_move
+                         next_move.to_s
                     else
-                    best_option = final_combo.reject do |i|
-                        @places_of_tokens.include?(i)
-                      end
-                      binding.pry
-                    final_place = best_option[0] + 1
-                    final_place
+                        best_option = final_combo.reject do |i|
+                            @places_of_tokens.include?(i)
+                        end
+
+                      final_place = best_option[0] + 1
+                      final_place.to_s
                     end
-		    #    binding.pry
+
         end
+
     end
 end
