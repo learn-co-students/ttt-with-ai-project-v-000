c7f1136f87bb3d9ed9f7b6e035543537c0a74246
Joshua Needham <joshuabneedham@gmail.com>
2018-03-26 14:17:09 -0600

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,42 +1,45 @@
 module Players
   class Computer < Player
+    attr_accessor :board
+
     def move(board)
-      move = nil
-
-      # When going first, take the middle square. When going second, take the middle square if it isn't yet taken.
-      if !board.taken?(5)
-        move = "5"
-
-      # If going second and the middle square is taken, take the upper-left corner square.
-      elsif board.turn_count == 1
-        move = "1"
-
-      # If you went first (and took the middle), take a corner square with your second move.
-      elsif board.turn_count == 2
-        move = [1, 3, 7, 9].detect{|i| !board.taken?(i)}.to_s
-
-      # If you went second (and took the middle) and the other player has occupied opposing corner squares, blow up the attempted trap by taking a side square.
-      elsif board.turn_count == 3 && (board.position(1) == board.position(9) || board.position(3) == board.position(7))
-        move = "2"
-
-      # From here on, run through the WIN_COMBINATIONS array, checking whether any of the combinations have two squares filled with the same token and a third, empty square.
-      else
-        Game::WIN_COMBINATIONS.detect do |cmb|
-
-          # First, check whether you have any chances to win, since it doesn't matter whether the opponent has a chance to win if you can win first.
-          if cmb.select{|i| board.position(i+1) == token}.size == 2 && cmb.any?{|i| board.position(i+1) == " "}
-            move = cmb.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
-
-          # If you can't play any winning moves, play a move to block the opponent from winning.
-          elsif cmb.select{|i| board.position(i+1) != " " && board.position(i+1) != token}.size == 2 && cmb.any?{|i| board.position(i+1) == " "}
-            move = cmb.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
-          end
-        end
-
-        # If none of the WIN_COMBINATIONS patterns have two squares taken by the same token and a third empty square, play into the first open square you find, first checking corners and then checking sides.
-        move = [1, 3, 7, 9, 2, 4, 6, 8].detect{|i| !board.taken?(i)}.to_s if move == nil
-      end
-      move
+      ( !board.taken?("5") ? "5" : possible_move(board) + 1)
     end
+
+    def possible_move(board)
+      win_or_block(board) || corner_move(board) || edge_move(board)
+    end
+
+    def corner_move(board)
+      corners = [0,2,6,8]
+      corners.shuffle.detect { |spot| !board.taken?(spot+1) }
+    end
+
+    def edge_move(board)
+      [1,3,5,7].shuffle.detect { |spot| !board.taken?(spot+1) }
+    end
+
+    def open_spot(board, token)  #returns winning combination that must be blocked or won
+      Game::WIN_COMBINATIONS.detect do |spot|
+        ((board.cells[spot[0]] == token && board.cells[spot[1]] == token) && !board.taken?(spot[2]+1)) ||
+          ((board.cells[spot[1]] == token && board.cells[spot[2]] == token) && !board.taken?(spot[0]+1)) ||
+          ((board.cells[spot[2]] == token && board.cells[spot[0]] == token) && !board.taken?(spot[1]+1))
+      end
+    end
+
+    def opp_token
+      ( self.token == "X" ? "O" : "X")
+    end
+
+    # if there is a winning combination with one spot not taken, the next move is to pick this spot -
+    # whether it's to block the opponent or to try to take it for the win
+    def win_or_block(board)
+      if open_spot(board, self.token)
+        open_spot(board, self.token).detect { |index| !board.taken?(index+1) }
+      elsif open_spot(board, self.opp_token)
+        open_spot(board, self.opp_token).detect { |index| !board.taken?(index+1) }
+      end
+    end
+
   end
 end
