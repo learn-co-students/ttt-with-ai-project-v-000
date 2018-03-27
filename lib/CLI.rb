e23f3b9ddc0f030c701c59be17240e690be6afe8
Beth Schofield <bethmschofield@gmail.com>
2018-03-27 06:29:21 +0000

    GoT Edition updated with bug fix

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -1,5 +1,6 @@
 class CLI
-
+@@x = ""
+@@o = ""
   def initialize
     2.times{puts''}
     puts "Welcome to Tic Tac Toe (that's Noughts & Crosses to the rest of us)!"
@@ -27,45 +28,69 @@ class CLI
     puts 'or for a cheeky extra try... Winter is coming!..'
     puts ""
     user_input = gets.strip
-    # if user_input == "wargames"
-    #   wargames
-    # else
-      case user_input.downcase
-        when "1"
-          Game.new().play
-          again_again?
-        when "2"
-          Game.new(Players::Human.new("X"), Players::Computer.new("O")).play
-          again_again?
-        when "3"
-          Game.new(Players::Computer.new("X"), Players::Computer.new("O")).play
-          again_again?
-        when /winter is coming/
-          wargames
-          Game.play_again?
-        when /bye/
-          puts "See you next time!"
-          sleep 1
-          exit!
-        else
-          2.times{puts ""}
-          puts "Sorry, what did you say?"
-          continue
-      # end
+    case user_input.downcase
+      when "1"
+        Game.new().play
+        again_again?
+      when "2"
+        Game.new(Players::Human.new("X"), Players::Computer.new("O")).play
+        again_again?
+      when "3"
+        Game.new(Players::Computer.new("X"), Players::Computer.new("O")).play
+        again_again?
+      when /winter is coming/
+        wargames
+        Game.play_again?
+      when /bye/
+        puts "See you next time!"
+        sleep 1
+        exit!
+      else
+        2.times{puts ""}
+        puts "Sorry, what did you say?"
+        continue
     end
+    # interpret(user_input)
   end
+
+  # def self.interpret(user_input)
+  #   case user_input.downcase
+  #     when "1"
+  #       Game.new().play
+  #       again_again?
+  #     when "2"
+  #       Game.new(Players::Human.new("X"), Players::Computer.new("O")).play
+  #       again_again?
+  #     when "3"
+  #       Game.new(Players::Computer.new("X"), Players::Computer.new("O")).play
+  #       again_again?
+  #     when /winter is coming/
+  #       wargames
+  #       Game.play_again?
+  #     when /bye/
+  #       puts "See you next time!"
+  #       sleep 1
+  #       exit!
+  #     else
+  #       2.times{puts ""}
+  #       puts "Sorry, what did you say?"
+  #       continue
+  #   end
+  # end
 
   def self.wargames
     winners = []
     10.times{puts ""}
     puts 'Winter is here! A fight to the death between the Night King and our saviour, Jon Snow!'
-    x = ""
-    while !valid_warrior?(x)
+    while !valid_warrior?
       puts 'Who will make the first move? Jon Snow or The Night King?'
       10.times{puts ""}
-      x = gets.strip
+      @@x = gets.strip
     end
-    x.downcase == "jon snow" ? o = "The Night King" : o = "Jon Snow"
+
+    define_warriors
+
+
     sleep 1
     puts ""
     puts "Preparing for Winter..."
@@ -76,27 +101,36 @@ class CLI
       winners << current_game.winner
     end
     6.times{puts''}
-    wargames_over(x, o, winners)
+    wargames_over(winners)
   end
 
-  def self.valid_warrior?(warrior_x)
-    warrior_x.downcase == "jon snow" || warrior_x == "the night king"
+  def self.valid_warrior?
+    @@x.match(/jon|snow|king of the north/) || @@x.match(/night/)
   end
 
-  def self.wargames_over(x, o, winners)
+  def self.define_warriors
+    if @@x.match(/jon|snow/)
+      @@x = "Jon Snow"
+      @@o = "The Night King"
+    else
+      @@x = "The Night King"
+      @@o = "Jon Snow"
+    end
+  end
+
+  def self.wargames_over(winners)
     puts 'Winter is over!'
     3.times{puts''}
     x_wins = winners.find_all{ |winner| winner == 'X'}
     o_wins = winners.find_all{ |winner| winner == 'O'}
-    puts "After 100 battles, #{x.split.map(&:capitalize).join(' ')} won #{x_wins.length} times and #{o} won #{o_wins.length} times. There were #{100 - o_wins.length - x_wins.length} draws."
+    puts "After 100 battles, #{@@x} won #{x_wins.length} times and #{@@o} won #{o_wins.length} times. There were #{100 - o_wins.length - x_wins.length} draws."
     again_again?
   end
-
 
   def self.again_again?
     3.times{puts ""}
     puts "Would you like to play again? y/n"
-    input = gets.strip
+    input = gets.strip.downcase
     input == "y" ? play_again : exit!
   end
 
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -4,9 +4,8 @@ module Players
     def move(user_input = nil)
       puts ""
       puts "Where would you like to go? Pick an empty space between 1 and 9"
-      puts ""
-      gets.strip
       2.times {puts ""}
+      gets.strip
     end
   end
 end
