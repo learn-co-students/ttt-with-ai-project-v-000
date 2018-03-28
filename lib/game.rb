c4ecf5aae519618cd136a39c21cf6df2f3ad551d
Dee Race <deerace12@gmail.com>
2018-03-28 17:07:10 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,13 +1,15 @@
+require 'pry'
+
 class Game
   attr_accessor :board, :player_1, :player_2
 
   WIN_COMBINATIONS = [
-    [0, 1, 2],
+    [2, 5, 8],
     [3, 4, 5],
     [6, 7, 8],
     [0, 3, 6],
     [1, 4, 7],
-    [2, 5, 8],
+    [0, 1, 2],
     [0, 4, 8],
     [2, 4, 6]
   ]
@@ -23,9 +25,10 @@ class Game
   end
 
   def won?
-    WIN_COMBINATIONS.detect do |winner|
-      @board.cells[winner[0]] == "X" && @board.cells[winner[1]] == "X" && @board.cells[winner[2]] == "X" ||
-      @board.cells[winner[0]] == "O" && @board.cells[winner[1]] == "O" && @board.cells[winner[2]] == "O"
+    WIN_COMBINATIONS.detect do |winning_combo|
+     @board.cells[winning_combo[0]] == @board.cells[winning_combo[1]] &&
+     @board.cells[winning_combo[1]] == @board.cells[winning_combo[2]] &&
+     @board.taken?(winning_combo[0] + 1)
     end
   end
 
@@ -44,14 +47,17 @@ class Game
   end
 
   def turn
-    player = current_player
-    current_move = player.move(board)
-      if board.valid_move?(current_move)
-        board.update(current_move, player)
-      else
-        player.move(board)
-      end
+    puts " "
+    puts "Player #{current_player.token}, please enter 1-9:"
+    input = current_player.move(board)
+    if board.valid_move?(input)
+      board.update(input, current_player)
+      board.display
+    else
+      turn
     end
+  end
+
 
   def play
     until over?
--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,9 +1,13 @@
 module Players
   class Computer < Player
+    attr_reader :game, :board
 
     def move(board)
-      
+      if board.cells[4] == " "
+        5.to_s
+      else
+        rand(1..9).to_s
+      end
     end
-
   end
 end
