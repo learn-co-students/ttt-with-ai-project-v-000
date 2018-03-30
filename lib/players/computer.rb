cbf9edefc155c6160fd0545d376e1d05a040bbab
Christine Cua <cloudycua@gmail.com>
2018-03-30 17:47:46 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,40 +1,34 @@
 class Players
-
   class Computer < Player
 
     WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [2,5,8], [1,4,7], [0,4,8], [6,4,2]]
 
-    def best_move(board)
-      WIN_COMINATIONS.each do |combo|
-        # Sees if two cells of winning combination matches player token and third is empty
-        if board.cells[combo[0]] == player.token && board.cells[combo[0]] == board.cells[combo[1]] && board.cells[combo[2]] == " "
-          combo[2]
-        elsif
-          board.cells[combo[0]] == player.token && board.cells[combo[0]] == board.cells[combo[2]] && board.cells[combo[1]] == " "
-          combo[1]
-        elsif
-          board.cells[combo[1]] == player.token && board.cells[combo[1]] == board.cells[combo[2]] && board.cells[combo[0]] == " "
-          combo[0]
-        # Sees if opponent has two of three spots in winning combination and blocks.
-        elsif board.cells[combo[0]] != player.token && board.cells[combo[0]] == board.cells[combo[1]] && board.cells[combo[2]] == " "
-          combo[2]
-        elsif
-          board.cells[combo[0]] != player.token && board.cells[combo[0]] == board.cells[combo[2]] && board.cells[combo[1]] == " "
-          combo[1]
-        elsif
-          board.cells[combo[1]] != player.token && board.cells[combo[1]] == board.cells[combo[2]] && board.cells[combo[0]] == " "
-          combo[0]
-        end
-      end
-    end
-
     def move(board)
-      if winning_move(board) > 0 ? winning_move(board) : random_move(board)
+#      WIN_COMBINATIONS.each do |combo|
+#        # Sees if two cells of winning combination matches player token and third is empty
+#        if board.cells[combo[0]] == self.token && board.cells[combo[0]] == board.cells[combo[1]] && board.cells[combo[2]] == " "
+#          best_move = combo[2]
+#        elsif
+#          board.cells[combo[0]] == self.token && board.cells[combo[0]] == board.cells[combo[2]] && board.cells[combo[1]] == " "
+#          best_move = combo[1]
+#        elsif
+#          board.cells[combo[1]] == self.token && board.cells[combo[1]] == board.cells[combo[2]] && board.cells[combo[0]] == " "
+#          best_move = combo[0]
+#        # Sees if opponent has two of three spots in winning combination and blocks.
+#        elsif board.cells[combo[0]] != self.token && board.cells[combo[0]] == board.cells[combo[1]] && board.cells[combo[2]] == " "
+#          best_move = combo[2]
+#        elsif
+#          board.cells[combo[0]] != self.token && board.cells[combo[0]] == board.cells[combo[2]] && board.cells[combo[1]] == " "
+#          best_move = combo[1]
+#        elsif
+#          board.cells[combo[1]] != self.token && board.cells[combo[1]] == board.cells[combo[2]] && board.cells[combo[0]] == " "
+#          best_move = combo[0]
+#        else
+          best_move = ["1", "2", "3", "4", "5", "6", "7", "8", "9"].sample
+#        end
+#      end
+      best_move
     end
 
-    def random_move(board)
-      ["1", "2", "3", "4", "5", "6", "7", "8", "9"].sample
-    end
   end
-
 end
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -2,7 +2,7 @@ class Players
 
   class Human < Player
     def move(board)
-      puts "#{self.token} please enter 1-9:"
+      puts "Player '#{self.token}' please enter 1-9:"
       gets.strip
     end
   end
