23a1e4fc12d2bc1ef20bab36566bc54dc7b30a60
Christine Cua <cloudycua@gmail.com>
2018-03-31 00:13:27 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -26,10 +26,10 @@ class Players
         # If player in center and opponent does not block win in lower right corner, WIN
         else
           WIN_COMBINATIONS.each |combo|
-            if if [board.cells.values_at(combo_values.at(0,1,2)].count(self.token) == 2
+            if [board.cells.values_at(combo[0], combo[1], combo[2])].count(self.token) == 2
               @best_move = 9 if board.cells[combo[8]] == " "
         # If opponent blocking win and opponent can win, block win
-            elsif if [board.cells.values_at(combo_values.at(0,1,2)].count(opp_token) == 2 == 2
+            elsif [board.cells.values_at(combo[0], combo[1], combo[2])].count(opp_token) == 2
               block_win
         # If opponent blocking win and can't win, setup two way win
             else
@@ -37,6 +37,7 @@ class Players
             end
           end
         end
+      end
       #Move 7:
       win if board.turn_count == 6
       #Best move or random
@@ -44,29 +45,28 @@ class Players
         best_move
 #      else
 #        [1,2,3,4,5,6,7,8,9].sample
-#      end
+      end
     end
 
     def win
       WIN_COMBINATIONS.each do |combo|
-        if [board.cells.values_at(combo_values.at(0,1,2)].count(self.token) == 2
-          @best_move = 1 if board.cells[combo[0]] == " "
-          @best_move = 2 if board.cells[combo[1]] == " "
-          @best_move = 3 if board.cells[combo[2]] == " "
+        if [board.cells.values_at(combo[0], combo[1], combo[2])].count(self.token) == 2
+          @best_move = combo[0]+1 if board.cells[combo[0]] == " "
+          @best_move = combo[1]+1 if board.cells[combo[1]] == " "
+          @best_move = combo[2]+1 if board.cells[combo[2]] == " "
         end
       end
     end
 
     def block_win
       WIN_COMBINATIONS.each do |combo|
-        if [board.cells.values_at(combo_values.at(0,1,2)].count(opp_token) == 2
-          @best_move = 1 if board.cells[combo[0]] == " "
-          @best_move = 2 if board.cells[combo[1]] == " "
-          @best_move = 3 if board.cells[combo[2]] == " "
+        if [board.cells.values_at(combo[0], combo[1], combo[2])].count(opp_token) == 2
+          @best_move = combo[0]+1 if board.cells[combo[0]] == " "
+          @best_move = combo[1]+1 if board.cells[combo[1]] == " "
+          @best_move = combo[2]+1 if board.cells[combo[2]] == " "
         end
       end
     end
 
   end
-
 end
