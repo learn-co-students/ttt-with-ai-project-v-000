6ab4c27992d2292387b814d3370cde44407773c9
Christine Cua <cloudycua@gmail.com>
2018-03-31 01:26:15 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -6,6 +6,7 @@ class Players
 
     def move(board)
       @best_move = 10
+      current_board = board.cells
       self.token == "X" ? @opp_token = "O" : @opp_token = "X"
       # FIRST PLAYER
       # Move 1: Top left corner
@@ -17,29 +18,35 @@ class Players
         else
           @best_move = 9
         end
-      end
       # Move 5:
-      if board.turn_count == 4
+      elsif board.turn_count == 4
         # If opponent in center, block win
         if board.cells[4] == opp_token
-          block_win
-        # If player in center and opponent does not block win in lower right corner, WIN
-        else
-          WIN_COMBINATIONS.each |combo|
-            if [board.cells.values_at(combo[0], combo[1], combo[2])].count(self.token) == 2
-              @best_move = 9 if board.cells[combo[8]] == " "
-        # If opponent blocking win and opponent can win, block win
-            elsif [board.cells.values_at(combo[0], combo[1], combo[2])].count(opp_token) == 2
-              block_win
-        # If opponent blocking win and can't win, setup two way win
-            else
-              @best_move = 3
-            end
-          end
+          block_win(current_board)
+#        # If player in center and opponent does not block win in lower right corner, WIN
+#        else
+#          WIN_COMBINATIONS.each |combo|
+#            if [board.cells[combo[0]], board.cells[combo[1]], board.cells[combo[2]]].count(self.token) == 2
+#              @best_move = 9 if board.cells[combo[8]] == " "
+#        # If opponent blocking win and opponent can win, block win
+#            elsif [board.cells[combo[0]], board.cells[combo[1]], board.cells[combo[2]]].count(opp_token) == 2
+#              WIN_COMBINATIONS.each do |combo|
+#                if [board.cells[combo[0]], board.cells[combo[1]], board.cells[combo[2]]].count(opp_token) == 2
+#                  @best_move = combo[0]+1 if board.cells[combo[0]] == " "
+#                  @best_move = combo[1]+1 if board.cells[combo[1]] == " "
+#                  @best_move = combo[2]+1 if board.cells[combo[2]] == " "
+#                end
+#              end
+#              block_win
+#        # If opponent blocking win and can't win, setup two way win
+#            else
+#              @best_move = 3
+#            end
+#          end
         end
       end
       #Move 7:
-      win if board.turn_count == 6
+#      win if board.turn_count == 6
       #Best move or random
       if best_move < 10
         best_move
@@ -48,22 +55,23 @@ class Players
       end
     end
 
-    def win
-      WIN_COMBINATIONS.each do |combo|
-        if [board.cells.values_at(combo[0], combo[1], combo[2])].count(self.token) == 2
-          @best_move = combo[0]+1 if board.cells[combo[0]] == " "
-          @best_move = combo[1]+1 if board.cells[combo[1]] == " "
-          @best_move = combo[2]+1 if board.cells[combo[2]] == " "
-        end
-      end
-    end
 
-    def block_win
+#    def win
+#      WIN_COMBINATIONS.each do |combo|
+#        if [board.cells.values_at(combo[0], combo[1], combo[2])].count(self.token) == 2
+#          @best_move = combo[0]+1 if board.cells[combo[0]] == " "
+#          @best_move = combo[1]+1 if board.cells[combo[1]] == " "
+#          @best_move = combo[2]+1 if board.cells[combo[2]] == " "
+#        end
+#      end
+#    end
+
+    def block_win(current_board)
       WIN_COMBINATIONS.each do |combo|
-        if [board.cells.values_at(combo[0], combo[1], combo[2])].count(opp_token) == 2
-          @best_move = combo[0]+1 if board.cells[combo[0]] == " "
-          @best_move = combo[1]+1 if board.cells[combo[1]] == " "
-          @best_move = combo[2]+1 if board.cells[combo[2]] == " "
+        if [current_board[combo[0]], current_board[combo[1]], current_board[combo[2]]].count(opp_token) == 2
+          @best_move = combo[0]+1 if current_board[combo[0]] == " "
+          @best_move = combo[1]+1 if current_board[combo[1]] == " "
+          @best_move = combo[2]+1 if current_board[combo[2]] == " "
         end
       end
     end
