a59b1900e58ada84c9f3fe591711d4361e3538f4
Trevor Seitz <trevinla@gmail.com>
2018-04-04 10:21:34 -0400

    cleaning

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,9 +1,56 @@
-module Players 
-  class Computer < Player  
+class Players
 
-def move(board) 
-  
+  class Computer < Player
 
-end 
-  end 
-end 
+    attr_reader :token, :index
+
+    def initialize(token)
+      @token = token
+    end
+
+    def move(board)
+      available_moves = []
+      board.cells.each_with_index do |cell, index|
+        if cell == " "
+          available_moves << (index + 1).to_s
+        end
+      end
+      available_moves.sample
+    end
+
+  end
+
+  # def best_possible (board, game.WIN_COMBINATIONS)
+  #   best_moves = []
+  #   WIN_COMBINATIONS.each do |combo|
+  #     if board[combo[0]] == board[combo[1]] && board[combo[1]] != " "
+  #       if board[combo[1]] == computer.token
+  #         # make move
+  #       else
+  #         best_moves << board[combo[2]]
+  #       end
+  #     elsif if board[combo[0]] == board[combo[2]] && board[combo[2]] != " "
+  #       if board[combo[2]] == computer.token
+  #         # make move
+  #       else
+  #         best_moves << board[combo[1]]
+  #       end
+  #     elsif if board[combo[1]] == board[combo[1]] && board[combo[2]] != " "
+  #       if board[combo[1]] == computer.token
+  #         # make move
+  #       else
+  #         best_moves << board[combo[0]]
+  #       end
+  #     end
+  #   end
+  #   best_moves
+  #   # use best_moves to make random choice from
+  #   # if best_moves == [] us available moves
+  # end
+
+  def best_possible (board, game.WIN_COMBINATIONS)
+    best_moves = []
+    WIN_COMBINATIONS.each do |combo|
+
+
+end
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,12 +1,19 @@
+class Players
 
-module Players
+  class Human < Player
 
-class Human < Player
+    attr_reader :token, :index
+
+    def initialize(token)
+      @token = token
+    end
 
     def move(board)
-        puts "Where would you like to move?"
-        gets.strip
+      puts "Please enter your move.  Enter an open cell between 1 - 9."
+      cell_num = gets.chomp
+      cell_num
     end
 
   end
+
 end
