c77f1e647a822a5f91e0ba6dfb029e16acac21e2
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-04-06 07:08:57 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,27 +1,12 @@
 module Players
   class Computer < Player
+    attr_reader :game, :board
     def move(board)
-      move = nil
-      if !board.taken?(5)
-        move = "5"
-      elsif board.turn_count == 1
-        move = "1"
-      elsif board.turn_count == 2
-        move = [1, 3, 7, 9].detect{|i| !board.taken?(i)}.to_s
-      elsif board.turn_count == 3 && (board.position(1) == board.position(9) || board.position(3) == board.position(7))
-        move = "2"
+      if board.cells[3] == " "
+        4.to_s
       else
-
-        Game::WIN_COMBINATIONS.detect do |cmb|
-          if cmb.select{|i| board.position(i+1) == token}.size == 2 && cmb.any?{|i| board.position(i+1) == " "}
-          move = cmb.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
-        elsif cmb.select{|i| board.position(i+1) != " " && board.position(i+1) != token}.size == 2 && cmb.any?{|i| board.position(i+1) == " "}
-          move = cmb.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
-        end
-      end
-      move = [1, 3, 7, 9, 2, 4, 6, 8].detect{|i| !board.taken?(i)}.to_s if move == nil
+        rand(1..9).to_s
     end
-    move
     end
   end
 end 
