875911f0b6eed9b1220a87702a932450d9960a18
David Markel <davidgmarkel@gmail.com>
2018-04-09 21:12:33 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -12,10 +12,11 @@ class Game
   [6,4,2]
   ]
 
-  def initialize(player_1 = Players::Human.new("X".yellow), player_2 = Players::Human.new("O".blue), board = Board.new)
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
     @player_1 = player_1
     @player_2 = player_2
     @board = board
+    binding.pry
   end
 
   def current_player
--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,9 +3,42 @@ module Players
   class Computer < Player
 
     def move(board)
-      "#{rand(1..9)}"
-    end
+      if self.token == "X"
+        other_player = "O"
+      else
+        other_player = "X"
+      end
+      #defines first move if computer is Player 1
+      if board.cells.all? {|cell| cell == " "}
+        "5"
+      #defines first move if computer is Player 2
+      elsif board.cells.count(" ") == 8
+        binding.pry
+        if !board.taken?(board.cells[0])
+          "1"
+        else
+          "3"
+        end
+      #defines computer's second move
+
+      elsif board.cells.count(" ") < 8
+        examined_row = []
+        WIN_COMBINATIONS.each do |win_combination|
+
+          win_combination.each do |board_position|
+            examined_row << board.cells[board_position]
+          end
+
+          if examined_row.include?(self.token) && !examined_row.include?(other_player)
+            empty_cell = examined_row.index(" ")
+            "#{[win_combination[empty_cell]]}"
+            break
+          else
+            examined_row.clear
+          end
+       end
+     end
+   end
 
   end
-
 end
