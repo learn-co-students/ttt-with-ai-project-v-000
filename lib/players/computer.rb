035e85f9ab6b4eaa7c171f5c4f935796be69d9e9
Vanessa Toro <vanetoro84@gmail.com>
2018-04-11 23:19:56 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,39 +3,41 @@ module Players
         attr_accessor :combos
         
  WIN_COMBO = [
-        [1,2,3],
-        [4,5,6],
-        [7,8,9],
-        [1,4,7],
-        [2,5,8],
-        [3,6,9],
-        [1,5,9],
-        [3,5,7],
-        ]
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+  ]
         
       def move(board)
         num = '5'
           if  board.valid_move?(num)
             board.valid_move?(num) ? num.to_s : move(board)
-          elsif
-             win_array_check(board)
+          elsif win_array_check(board)
+          
+            win_array_check(board).each do |num|
+              binding.pry
+              board.valid_move?(num) ? num.to_s : move(board)
+            end
           else
             num = 1 + rand(9) 
-            board.valid_move?(num) ? num.to_s : move(board)
+            board.valid_move?(num) ? (num-1).to_s : move(board)
           end
     end
     
 
 
     def win_array_check(board)
-      WIN_COMBO.find do |combo|
-          if board.cells[combo[0]] == board.cells[combo[1]] || board.cells[combo[1]] == board.cells[combo[2]] || board.cells[combo[0]] == board.cells[combo[2]] &&        (board.cells[combo[0]] != ' ' || board.cells[combo[1]] != ' ')
-                combo.each do |num|
-                binding.pry
-                # board.valid_move?(num) ? num.to_s : move(board)
-             end
-          end
+     WIN_COMBO.find do |combo|
+        #binding.pry
+        (board.cells[combo[0]] == board.cells[combo[1]] || board.cells[combo[1]] == board.cells[combo[2]] || board.cells[combo[0]] == board.cells[combo[2]]) && ([board.cells[combo[0]], board.cells[combo[1]], board.cells[combo[2]]].select { |a| a == " "}.length == 1)
+            
       end
+   
     end  
     
     
