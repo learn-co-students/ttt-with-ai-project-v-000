3756d3919b6f7a850494916cd90a4c9bdcbc2c98
Trever Andersen <trever.r.andersen@gmail.com>
2018-04-11 21:02:15 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -3,11 +3,10 @@ class Board
   attr_accessor :cells
 
   def initialize
-    @cells = Array.new(9, " ")
+    reset!
   end
 
   def reset!
-    cells.clear
     @cells = Array.new(9, " ")
   end
 
@@ -15,9 +14,28 @@ class Board
     puts " #{cells[0]} | #{cells[1]} | #{cells[2]} \n-----------\n #{cells[3]} | #{cells[4]} | #{cells[5]} \n-----------\n #{cells[6]} | #{cells[7]} | #{cells[8]} "
   end
 
-  def position(space)
-    space = space.to_i-1
-    cells[space].map!{|i| i = "O"}
+  def position(input)
+    cells[input.to_i-1]
+  end
+
+  def full?
+    cells.all?{|i| i == "X" || i == "O"}
+  end
+
+  def turn_count
+    cells.count{|i| i == "X" || i == "O"}
+  end
+
+  def taken?(input)
+    position(input) == "X" || position(input) == "O"
+  end
+
+  def valid_move?(input)
+    input.to_i.between?(1, 9) && !taken?(input)
+  end
+
+  def update(input, player)
+    cells[input.to_i-1] = player.token
   end
 
 end
---
+++ lib/players/computer.rb
@@ -0,0 +1,8 @@
+module Players
+  class Computer < Player
+
+    def move
+      
+    end
+  end
+end
