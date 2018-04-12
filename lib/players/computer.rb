ca40387e8510ee75110c507004e3405db9cdb831
Victoria Meng <vym205@nyu.edu>
2018-04-12 09:30:16 -0400

    Finished Computer#move

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,8 +1,14 @@
 module Players
   class Computer < Player
 
-    def move
-      
+    def move(board)
+    	input = ""
+    	until board.valid_move?(input)
+      		input = (1..9).to_a.sample
+      	end
+      	input.to_s
     end
+
   end
+
 end
