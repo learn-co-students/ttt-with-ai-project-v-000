c5465797b29c0dde42b2388df31ab9a316cc81ac
Trevor Seitz <trevinla@gmail.com>
2018-04-12 19:51:06 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -6,9 +6,9 @@ attr_accessor :player_1, :player_2, :board
 [0, 1, 2], [3, 4, 5],
 [6, 7, 8], [0, 4, 8],
 [2, 4, 6], [0, 3, 6],
-[1, 4, 7], [2, 5, 8],
+[2, 5, 8], [1, 4, 7],
 ]
- 
+
 def initialize(player_1 = Players::Human.new("X") , player_2 = Players::Human.new("O"), board = Board.new)
   @player_1 = player_1
   @player_2 = player_2
@@ -57,9 +57,10 @@ end
 
 def won?
   WIN_COMBINATIONS.detect { |win_combination|
-          @board.cells[win_combination[0]] == @board.cells[win_combination[1]] &&
-          @board.cells[win_combination[1]] == @board.cells[win_combination[2]] &&
-          @board.taken?(win_combination[0]+1)}
+    @board.cells[win_combination[0]] == @board.cells[win_combination[1]] &&
+    @board.cells[win_combination[1]] == @board.cells[win_combination[2]] &&
+    @board.taken?(win_combination[0]+1)}
+
   end
 
   def draw?
