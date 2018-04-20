7357f5cf4895809bca835f5ce474a1d029c48a62
Katie Cassell <kcassell944@gmail.com>
2018-04-20 20:33:26 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,29 @@
+class Board
+  attr_accessor :cells
+
+  def initialize
+    reset!
+  end
+
+  #def reset! 
+  #   @cells = Array.new(9, " ")
+  # end
+  def reset!
+    @cells = Array.new(9, " ")
+  end
+
+  def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
+
+  def position(user_input)
+    cells[user_input.to_i - 1]
+  end
+
+  def full?
+  end
+end
