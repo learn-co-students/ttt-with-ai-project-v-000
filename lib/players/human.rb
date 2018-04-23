370a2549e6d05e345a1660073e62992350393e39
Chris Dundon <chris.dundon@gmail.com>
2018-04-23 20:58:50 +0000

    Done.

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -5,7 +5,13 @@ module Players
 
     def initialize(token)
       super
-
     end
+
+    def move
+      puts "Please select a cell (1 though 9)."
+      rtn = gets.strip
+      rtn
+    end
+
   end
 end
