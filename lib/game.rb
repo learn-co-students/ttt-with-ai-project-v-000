7165b484bd14c22f6c2ab416138f0a8d2a9accdc
Chris Dundon <chris.dundon@gmail.com>
2018-04-23 21:08:52 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,6 @@
 
 class Game
-  attr_accessor :board
+  attr_accessor :board, :player_1, :player_2
 
   WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
 
@@ -8,5 +8,5 @@ class Game
     self.board = Board.new
   end
 
-  
+
 end
