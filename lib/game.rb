8a6201b3c9ff4d7ddf1f43a0075cc7cd53a6e601
Chris Dundon <chris.dundon@gmail.com>
2018-04-23 21:10:01 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -4,8 +4,10 @@ class Game
 
   WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
 
-  def initialize
-    self.board = Board.new
+  def initialize(board, p1, p2)
+    self.board = board
+    self.player_1 = p1
+    self.player_2 = p2
   end
 
 
