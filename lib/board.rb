fb7ec047d3bb2837150694bdf9cacf04d10c3276
Katie Cassell <kcassell944@gmail.com>
2018-04-24 19:25:39 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -25,5 +25,29 @@ class Board
   end
 
   def full?
+    @cells.all? {|cell| cell == "X" || cell == "O"}
   end
+
+  def turn_count
+  @cells.count("X") + @cells.count("O")
+  end
+
+  def taken?(number)
+    if @cells[number.to_i - 1] == " "
+      false
+    else
+      true
+    end
+  end
+
+  def valid_move?(index)
+  cells = index.to_i
+  cells.between?(1, 9) && !taken?(cells)
+end
+
+def update(input, player)
+  @cells[input.to_i - 1] = player.token
+
+end
+
 end
