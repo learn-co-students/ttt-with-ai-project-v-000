a751eb05ad9872c58afedbf7e2765ff135f56886
Diego Yanez <diegosyanez92@gmail.com>
2018-04-24 22:34:46 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,14 @@
 class Game
 
-
+  WIN_COMBINATIONS = [
+    [1,2,3], #<- Top-Row Win
+    [4,5,6], #<- Middle-Row Win
+    [7,8,9], #<- Bottom-Rown Win
+    [1,4,7], #<- Left-Column Win
+    [2,5,8], #<- Middle-Column Win
+    [3,6,9], #<- Right-Column Win
+    [1,5,9], #<- Left-Diagnal Win
+    [3,5,7]] #<- Right-Diagnal Win
 
 
 end
