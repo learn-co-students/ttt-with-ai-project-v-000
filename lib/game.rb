7c077edbd73200c3f46981ee65f188848952dd16
Katie Cassell <kcassell944@gmail.com>
2018-04-24 21:40:49 +0000

    Done.

---
+++ lib/game.rb
@@ -0,0 +1,23 @@
+class Game
+  attr_accessor :board, :player_1, :player_2
+
+  def initialize(board = Board.new)
+    @board = board
+    @player_1 = player_1
+    @player_2 = player_2
+  end
+
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2]
+  ]
+
+
+
+end
