bfcccba1472c6faaa6674bc5fa0beac8778833a2
Diego Yanez <diegosyanez92@gmail.com>
2018-04-24 16:53:53 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,31 @@
+require 'pry'
+class Board
+  attr_accessor :cells
+
+    def reset!
+      @cells.clear
+      @cells = Array.new(9, " ")
+    end
+
+    def initialize
+      @cells = Array.new(9, " ")
+    end
+
+    def display
+      puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+      puts "-----------"
+      puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+      puts "-----------"
+      puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+    end
+
+    def position(input)
+      binding.pry
+    end
+
+
+
+
+
+
+end
---
+++ lib/players/computer.rb
@@ -0,0 +1,8 @@
+module Players
+  class Computer
+
+
+
+
+  end
+end
