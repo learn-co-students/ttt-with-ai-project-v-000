50da1df83e5f7ed5c01aaffe90ea6d0f708b5718
Diego Yanez <diegosyanez92@gmail.com>
2018-04-25 15:22:17 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -11,8 +11,18 @@ class Game
     [0,4,8], #<- Left-Diagnal Win
     [6,4,2]] #<- Right-Diagnal Win
 
-    def initialize(player_1 = Players::Human, player_2 = Players::Human, board)
+    def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
       @player_1 = player_1
       @player_2 = player_2
+      @board = board
     end
+
+    def current_player
+      @board.turn_count % 2 == 0? player_1 : player_2
+    end
+
+    def won?
+      
+    end
+
 end
