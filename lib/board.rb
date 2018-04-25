e3e6e897a6a2a0c4db61023002b61c097efba091
Katleiah Ramos <katleiahramos@gmail.com>
2018-04-25 01:28:54 +0000

    Board.position added

--- lib/board.rb
+++ lib/board.rb
@@ -10,13 +10,20 @@ class Board
   end
 
   def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
 
-  puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
-  puts "-----------"
-  puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
-  puts "-----------"
-  puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  def input_to_index(user_input)
+    user_input.to_i - 1
+  end
 
+  def position(user_input)
+    i = input_to_index(user_input)
+    self.cells[i]
   end
 
 end
