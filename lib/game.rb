43210ca81dcbe8da14ae08932847c3b07e958a8d
Katie Cassell <kcassell944@gmail.com>
2018-04-25 18:38:28 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,7 @@
 class Game
   attr_accessor :board, :player_1, :player_2
 
-  def initialize(board = Board.new)
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
     @board = board
     @player_1 = player_1
     @player_2 = player_2
@@ -18,6 +18,44 @@ class Game
     [6,4,2]
   ]
 
+def current_player
+  #if the turn count is even, then it's going to be player 1's turn
+  if self.board.turn_count.even?
+    player_1
+  else
+    player_2
 
+  end
+  #else is player 2's
+end
 
+def won?
+  WIN_COMBINATIONS.find do |win_combo|
+    #win_combo will be an array
+    index_1 = win_combo[0]
+    index_2 = win_combo[1]
+    index_3 = win_combo[2]
+    position_1 = self.board.cells[index_1]
+    position_2 = self.board.cells[index_2]
+    position_3 = self.board.cells[index_3]
+    position_1 == position_2 && position_3 == position_1 && position_2 != " "
+
+    end
+  end
+
+  def draw?
+    self.board.full? && !won?
+  end
+
+  def over?
+    self.won? || self.draw?
+  end
+
+  def winner
+    if self.won?
+      index = won?[0]
+      self.board.cells[index]
+
+    end
+  end
 end
