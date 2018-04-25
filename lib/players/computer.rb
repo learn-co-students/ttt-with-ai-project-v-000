40b6a909682c8fb99f38f67e150de473941790e3
Chris Dundon <chris.dundon@gmail.com>
2018-04-25 18:51:01 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -15,26 +15,34 @@ module Players
     def move(board)
       return "4" if board.turn_count == 0
 
-      t = self.token
-      winners = self.win_combo
-      df = {0 => 3, 1 => 2, 2 => 3, 3 => 2, 4 => 4, 5 => 2, 6 => 3, 7 => 2, 8 => 3}
-
-      win_weight = []
-      winners.each do |e|
-        e0 = position_value(e[0])
-        e1 = position_value(e[1])
-        e2 = position_value(e[2])
-
-        val = e0 + e1 + e2
-
-        win_weight << val
+      #find max win_weight
+      max_win = [-1, -1]
+      (0..8).each do |e|
+        tmp_board = board
+        tmp_board[e] = self.token if tmp_board[e].strip == ""
+        w_w = win_weight(tmp_board)
+        max_win = [e, w_w] if w_w > max_win[1]
       end
 
-
+      (max_win[1] + 1).to_s
     end
+    
   private
       def win_weight(new_board)
-        
+        t = self.token
+        winners = self.win_combo
+
+        win_weight = []
+        winners.each do |e|
+          e0 = position_value(new_board[e[0]])
+          e1 = position_value(new_board[e[1]])
+          e2 = position_value(new_board[e[2]])
+
+          val = e0 + e1 + e2
+
+          win_weight << val^2
+        end
+
       end
 
       def position_value(occupant)
