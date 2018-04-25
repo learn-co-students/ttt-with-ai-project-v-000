a420f262977b65504e6ee0180e0b17019eee26ac
Diego Yanez <diegoyanez@MacBook-Pro-2.local>
2018-04-25 16:24:54 -0400

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -58,8 +58,16 @@ class Game
     end
 
     def play
+      until over?
+        turn
+      end
 
+      if won?
+        puts "Congratulations #{winner}!"
+      elsif draw?
+        puts "Cat's Game!"
+      end
     end
-    
+
 
 end
