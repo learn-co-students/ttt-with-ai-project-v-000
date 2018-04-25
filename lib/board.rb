352e0c4072cd1ff9a2cfa51075639e609a972dc3
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-04-25 02:12:28 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,28 @@
+class Board
+
+  attr_accessor :cells
+
+  def initialize
+    reset!
+  end
+
+  def reset!
+    # @cells.clear
+    @cells = Array.new(9, " ")
+  end
+
+  def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+
+  end
+
+  def position
+    @cells[input.to_i - 1]
+
+  end
+
+end
