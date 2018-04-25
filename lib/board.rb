1735030c8bd1ec3c7c48332fe59f1163051ff78e
Katleiah Ramos <katleiahramos@gmail.com>
2018-04-25 01:26:29 +0000

    Board.display added

--- lib/board.rb
+++ lib/board.rb
@@ -8,4 +8,15 @@ class Board
   def reset!
     self.cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   end
+
+  def display
+
+  puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+  puts "-----------"
+  puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+  puts "-----------"
+  puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+
+  end
+
 end
