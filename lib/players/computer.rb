bdd7bf8faa9b8da95ce903fbc359770e95978760
Chris Dundon <chris.dundon@gmail.com>
2018-04-26 00:50:43 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -19,7 +19,6 @@ module Players
       #find max win_weight
       max_win = [-1, -1]
       (0..8).each do |e|
-        #puts "Board spot: " + e.to_s
 
         tmp_board = []
         tmp_board.replace(board.cells)
@@ -36,7 +35,6 @@ module Players
   private
       def win_weight(new_board)
         winners = self.win_combo
-        puts "---"
 
         win_wei = 0
         winners.each do |e|
@@ -44,20 +42,27 @@ module Players
           e1 = position_value(new_board[e[1]])
           e2 = position_value(new_board[e[2]])
 
-          puts "#{e0} #{e1} #{e2}"
-
-          val = e0 + e1 + e2
-
-          win_wei += val**2
-        end
+          if (e0 == e1) && (e1 == e2) && (e0 != 0)
+            win_wei = 32 #Winner
+          elsif (e0 + e1 + e2 == 4) && (e0 != 0) && (e1 != 0) && (e2 != 0)
+            win_wei = 16 #Need to block opponent
+          elsif (e0 + e1 + e2 == 5) && (e0 != 0) && (e1 != 0) && (e2 != 0)
+            win_wei = 0 #Blocked path
+          elsif (e0 + e1 + e2 == 3) && (e0 != e1) && (e1 != e2) && (e2 != e0)
+            win_wei = 4 #One and One - blocked opponent
+          elsif (e0 + e1 + e2 == 4) && (e0 != 1) && (e1 != 1) && (e2 != 1)
+            win_wei = 8 #Two of ours and an open space - has potential
+          else
+            win_wei = 0 #No value here.
+          end
         win_wei
       end
 
       def position_value(occupant)
         occ = occupant.strip
         return 0 if occ == ""
-        return 1 if occ == self.token
-        return -1 #otherwise
+        return 2 if occ == self.token
+        return 1 #otherwise
       end
 
   end
