12771383f60043ef7e3f2d11d439ad2c31037f47
Diego Yanez <diegoyanez@MacBook-Pro-2.local>
2018-04-26 12:22:50 -0400

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -13,7 +13,25 @@ module Players
     #    [6,4,2]] #<- Right-Diagnal Win
 
     def move(board)
-    
+        if board.cells[4] == " "
+            "5"
+            elsif board.cells[0] == " "
+            "1"
+            elsif board.cells[2] == " "
+            "3"
+            elsif board.cells[6] == " "
+            "7"
+            elsif board.cells[8] == " "
+            "9"
+            elsif board.cells[1] == " "
+            "2"
+            elsif board.cells[3] == " "
+            "4"
+            elsif board.cells[5] == " "
+            "6"
+            elsif board.cells[7] == " "
+            "8"
+        end
     end
 
   end
