0f200e5bd291f0e595350cb7f011d3032877d77b
Katleiah Ramos <katleiahramos@gmail.com>
2018-04-27 17:35:46 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -8,13 +8,21 @@ module Players
       open = []
 
       board.cells.each_with_index {|cell, index|
-        open << index if cell == " "
+
+        cell == " "? open << index : nil
       }
 
       best_move = open.select {|open_i| open_i == 4 || open_i ==  0 || open_i == 2 || open_i == 6 || open_i == 8}.first
 
+      #first two turns
+      if best_move
       #returns position 5 if open
-      open.include?(4) ? "5" : "#{best_move + 1}" #takes best move index and converts to position string
+        open.include?(4) ? "5" : "#{best_move + 1}" #takes best move index and converts to position string
+      else
+
+        open.first + 1
+      end
+
     end
 
 
