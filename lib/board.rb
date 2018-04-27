0b83abb83f46050ad377032453ad4907e6595de4
Ally Netzel <netzelally@gmail.com>
2018-04-27 02:01:12 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,48 @@
+require "pry"
+class Board
+  attr_accessor :cells, :board
+  
+  def reset!
+    @cells = Array.new(9, " ") 
+  end
+  
+  def initialize(board = nil)
+    @cells = Array.new(9," ")
+  end
+  
+  def display
+  puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} \n-----------\n #{@cells[3]} | #{@cells[4]} | #{@cells[5]} \n-----------\n #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
+  
+  def position(input)
+    value = (input.to_i)-1
+    cell = @cells[value]
+    cell
+  end
+  
+  def full?
+    @cells.all? do |box|
+      box == "X" || box == "O"
+    end
+  end
+  
+  def turn_count
+    count = 0 
+    @cells.each do |i|
+      if i == "X" || i == "O"
+       count += 1 
+      end
+    end
+   return count  
+  end
+  
+  def taken?(index)
+    if position(index) == "X" || position(index) == "O"
+      return true
+    end
+    if position(index) == " " || position(index) == ""
+      return false
+    end
+  end
+  
+end
