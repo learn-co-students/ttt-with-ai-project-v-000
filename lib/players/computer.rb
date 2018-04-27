a3f8690c121afd96092c2c2e5f81443ca0b78c87
Katleiah Ramos <katleiahramos@gmail.com>
2018-04-27 13:22:43 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,8 +1,20 @@
+require 'pry'
+
 module Players
   class Computer < Player
 
     def move(board)
-      input = gets.strip
+      #get open indicies
+      open = []
+
+      board.cells.each_with_index {|cell, index|
+        open << index if cell == " "
+      }
+
+      best_move = open.select {|open_i| open_i == 4 || open_i ==  0 || open_i == 2 || open_i == 6 || open_i == 8}.first
+
+      #returns position 5 if open
+      open.include?(4) ? "5" : "#{best_move + 1}" #takes best move index and converts to position string
     end
 
 
