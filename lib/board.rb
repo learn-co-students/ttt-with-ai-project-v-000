e861f58ff938fbb1f14ffebb72f6c6a17d21d33f
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-04-29 01:27:40 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Board
 
   attr_accessor :cells
@@ -20,9 +22,29 @@ class Board
 
   end
 
-  def position
-    @cells[input.to_i - 1]
+  def position(input)
+    cells[input.to_i - 1]
+  end
 
+  def full?
+    cells.all? {|char| char == "X" || char == "O"}
+  end
+
+  def turn_count
+    cells.count{|char| char == "X" || char == "O"}
+  end
+
+  def taken?(input)
+    position(input) == "X" || position(input) == "O"
+  end
+
+  def valid_move?(input)
+    # binding.pry
+    input.to_i.between?(1, 9) && !taken?(input)
+  end
+
+  def update(input, player)
+    cells[input.to_i - 1] = player.token # stands for X or O
   end
 
 end
