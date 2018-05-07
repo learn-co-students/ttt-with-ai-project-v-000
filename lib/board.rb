428a7e859c69617ee29b9f9e97640940e95c8449
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-05-07 22:41:55 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,56 @@
+
+
+class Board
+  attr_accessor :cells
+
+  def initialize
+    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+
+  def reset!
+    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+
+  def display
+    puts  " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts  "-----------"
+    puts  " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts  "-----------"
+    puts  " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
+
+  def position(pos_num)
+    value_in_cell = @cells[pos_num.to_i-1] if pos_num.to_i != 0
+  end
+
+  def full?
+    @cells.all? {|position| position == "X" or position == "O"}
+  end
+
+  def turn_count
+    @cells.count {|position| position == "X" or position == "O"}
+  end
+
+  def taken?(pos_num)
+    if self.position(pos_num) == "X" or self.position(pos_num) == "O" and pos_num.to_i != 0
+      true
+    else
+      false
+    end
+  end
+
+  def valid_move?(pos_num)
+    if self.taken?(pos_num) == false and pos_num.to_i.between?(1,9) == true
+      true
+    else
+      false
+    end
+  end
+
+  def update(pos_num, player)
+    if self.valid_move?(pos_num)
+      @cells[pos_num.to_i-1] = player.token
+    end
+  end
+
+end
