52e187970e7960f7becf50d3f666870e874ce60c
Trac Nguyen <nguyendtrac@gmail.com>
2018-05-13 01:59:36 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,6 @@
 class Game
   include Players
-  attr_accessor :board, :player_1
+  attr_accessor :board, :player_1, :player_2
   WIN_COMBINATIONS = [
     [0,1,2],
     [3,4,5],
@@ -12,12 +12,12 @@ class Game
     [2,4,6]
     ]
 
-  def board
-    @board = []
-  end
 
-  def initialize(player_1 = Players::Human.new("X"))
+
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
     @player_1 = player_1
+    @player_2 = player_2
+    @board = board
   end
 
 
