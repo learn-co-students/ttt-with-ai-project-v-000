a353d14806843201044d9844a8aaf435c8ed85e7
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-05-16 19:01:03 +0000

    Done.

--- lib/player.rb
+++ lib/player.rb
@@ -1,15 +1,7 @@
 class Player
   attr_reader :token
 
-  def initialize
-    if turn_count % 2 == 0
-			return "X"
-		else
-			return "O"
-		end
-  end
-
-  def token
-
+  def initialize(token)
+    @token = token
   end
 end
