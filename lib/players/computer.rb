6ac4e525a24d3a9372bcbe6eca83def7a44c5bf4
Hiroki Kato <hkato@sas.upenn.edu>
2018-05-17 17:11:17 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,43 +1,31 @@
+require 'pry'
 module Players
   class Computer < Player
     def move(board)
       move = nil
-
-      #Go for the middle
       if !board.taken?(5)
         move = "5"
-
-      #Go for the corners, if the computer is second
       elsif board.turn_count == 1
         move = "1"
-
-      # If you went first (and took the middle), take a corner square with your second move.
       elsif board.turn_count == 2
-        move = "1"
+        move = [1, 3, 7, 9].detect {|s| !board.taken?(s)}.to_s
+      elsif board.turn_count == 3 && (board.position(1) == board.position(9) || board.position(3) == board.position(7))
+        move = "2"
+      else
+       Game::WIN_COMBINATIONS.detect do |combo|
+         # First, check whether you have any chances to win, since it doesn't matter whether the opponent has a chance to win if you can win first.
+         if combo.select{|i| board.position(i+1) == token}.size == 2 && combo.any?{|i| board.position(i+1) == " "}
+           move = combo.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
 
-      elsif board.turn_count == 3
-        move = "3"
-
-      elsif board.turn_count == 4
-        move = "3"
-
-      elsif board.turn_count == 5
-        move = "7"
-
-      elsif board.turn_count == 6
-        move = "7"
-
-      elsif board.turn_count == 7
-        move = "9"
-
-      elsif board.turn_count == 8
-        move = "9"
-
-      elsif board.turn_count == 9
-        move = (1..9).to_a.detect do |open_spot|
-          !board.taken?(open_spot)
-        end
-      end
-    end
+         # If you can't play any winning moves, play a move to block the opponent from winning.
+         elsif combo.select{|i| board.position(i+1) != " " && board.position(i+1) != token}.size == 2 && combo.any?{|i| board.position(i+1) == " "}
+           move = combo.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
+         end
+       end
+       # If none of the WIN_COMBINATIONS patterns have two squares taken by the same token and a third empty square, play into the first open square you find, first checking corners and then checking sides.
+       move = [1, 3, 7, 9, 2, 4, 6, 8].detect{|i| !board.taken?(i)}.to_s if move == nil
+     end
+     move
+   end
   end
 end
