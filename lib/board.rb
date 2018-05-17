2c398c5e4e2dd7d43dccd75085f0a1d2cae466c5
pcherian10 <pcherian10@gmail.com>
2018-05-17 17:10:03 -0400

    updating board.rb

---
+++ lib/board.rb
@@ -0,0 +1,32 @@
+
+class Board
+
+  attr_accessor :cells
+
+  def initialize
+    @cells = Array.new(9) {" "}
+  end
+
+  def reset!
+    @cells = Array.new(9) {" "}
+  end
+
+  def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
+
+  def position (input)
+    @cells[input.to_i - 1]
+  end
+
+  def full
+
+
+  end
+
+
+end
