0a408d3bc1be1e4fef304a39d475ca353a9c98ce
Adam Kalt <adam.kalt.1@gmail.com>
2018-05-18 05:05:39 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,24 @@
 class Game
 
+attr_accessor :board
+
+def initialize
+  board = []
+end
+
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5], # Middle row
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6],
+]
+
+def board
+board = []
+end
+
 end
