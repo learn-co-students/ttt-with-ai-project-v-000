284b972f795e29239426f093ee75a42ad084fdd7
Ashley Schultz <ashleyeschultz@gmail.com>
2018-05-18 03:52:11 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,6 @@
 class Game 
   
-  attr_accessor :board
+  attr_accessor :board, :player_1
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
   
   def board
@@ -8,7 +8,7 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4
   end
   
   def player_1
-    
+    @player_1 = Player.new("X")
   end
 end 
 
