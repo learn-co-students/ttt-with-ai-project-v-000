7e26c35679bb7c101f48f8caff92d8d22162afd0
Robert Watson <spartan83@gmail.com>
2018-05-20 20:24:48 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -19,4 +19,43 @@ def position(input)
   @cells[input.to_i-1]
 end
 
+def full?
+  empties = []
+  empties =  @cells.select{|cell| cell== " "}
+  if empties.length > 0
+    return false
+  else
+    return true
+  end
+end
+
+def turn_count
+  turns = @cells.select{|cell| cell != " "}
+  turns.length
+end
+
+def taken?(cell)
+  if position(cell)=="X" || position(cell)=="O"
+    return true
+  else
+    return false
+  end
+end
+
+def valid_move?(cell)
+  if cell.length > 1
+    false
+  elsif cell =~ /[^0-9]/
+    false
+  elsif taken?(cell)
+    return false
+  else
+    return true
+  end
+end
+
+def update(cell, player)
+  @cells[cell.to_i-1]=player.token
+end
+
 end
---
+++ lib/game.rb
@@ -0,0 +1,56 @@
+class Game
+  WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]]
+  attr_accessor :board, :player_1, :player_2
+
+  def initialize(player_1 =Players::Human.new("X"), player_2=Players::Human.new("O"), board = Board.new)
+    @player_1 =player_1
+    @player_2= player_2
+    @board= board
+  end
+
+  def current_player
+    if @board.turn_count.even?
+      @player_1
+    else
+      @player_2
+    end
+  end
+
+  def won?
+    WIN_COMBINATIONS.detect do |win_combo|
+      @board.cells[win_combo[0]] == @board.cells[win_combo[1]] &&
+      @board.cells[win_combo[1]] == @board.cells[win_combo[2]] &&
+      @board.taken?(win_combo[0]+1)
+    end
+  end
+
+  def draw?
+    @board.full? && !won?
+  end
+
+  def over?
+    draw? || won?
+  end
+
+  def winner
+    if winning_combo = won?
+      @winner = @board.cells[winning_combo.first]
+    end
+  end
+
+  def turn
+    player = current_player
+    current_move = player.move(@board)
+    if !@board.valid_move?(current_move)
+      turn
+    else
+      puts "Turn: #{@board.turn_count+1}\n"
+      @board.display
+      @board.update(current_move, player)
+      puts "#{player.token} moved #{current_move}"
+      @board.display
+      puts "\n\n"
+    end
+  end
+
+end
