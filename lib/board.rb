bd1b50ac74cd54cbdc80dbcb09ca8c1574a9f351
Wasif Khan <waasif.khaan@gmail.com>
2018-05-21 21:55:23 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,12 +1,13 @@
 class Board
   attr_accessor :cells
   def initialize
-    @cells = [" "," "," "," "," "," "," "," "," "]
+    @cells = Array.new(9, " ")
   end 
   
   def reset!
-    @cells = [" "," "," "," "," "," "," "," "," "]
+    @cells = Array.new(9, " ")
   end 
+  
   def display 
     puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
     puts "-----------"
@@ -14,22 +15,35 @@ class Board
     puts "-----------"
     print " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
   end 
+  
   def position(input)
     index = input.to_i - 1
     @cells[index]
   end 
-  def update(posittion_user,player_token) 
+  
+  def update(input,player_token)
+    @cells[input.to_i - 1]= player_token
   end 
+  
   def token 
   end 
-  def taken?
+  
+  def taken?(input)
+    position(input) == "X" || position(input) == "O"  ? true : false 
+    
   end 
+  
   def turn_count
+    positions_taken = @cells.select {|position| position != " "}
+    positions_taken.length
   end 
+  
   def full?
-    if @cells[index]
+    @cells.all? {|position| position != " "} ? true : false
   end 
-  def valid_move?
+  
+  def valid_move?(input)
+    input.to_i >= 1 && input.to_i <= 9 && taken?(input) == false  ? true : false  
   end 
     
   
