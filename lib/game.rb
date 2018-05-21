d31decd34b9beb89997b3b4a651bcf6ac2097620
Anu Khambete <anukhambete@gmail.com>
2018-05-21 01:32:52 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -30,4 +30,16 @@ class Game
     end
   end
   
+  def won?
+  WIN_COMBINATIONS.each do |wc_oa|     
+    #win comb outer array wc_oa
+   w_x = wc_oa.all?{|i| @board[i] =="X"}
+   w_o = wc_oa.all?{|i| @board[i] =="O"}
+     if w_x == true || w_o == true
+       return wc_oa
+     end
+  end
+  return false
+  end
+  
 end
