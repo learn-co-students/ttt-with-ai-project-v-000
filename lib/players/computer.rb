4878e6f3e63e5c300363b52e6cd9127fde4a67b6
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-05-23 16:49:02 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,9 +1,42 @@
 module Players
   class Computer < Player
+    attr_accessor :board
+
+    WIN_COMBINATIONS = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 4, 6],
+    [2, 5, 8],
+    [3, 4, 5],
+    [6, 7, 8]
+    ]
+    CORNERS = [0, 2, 6, 8]
 
     def move(board)
-      rand(1..9)
+      "1"
     end
+  end
+
+  def win_moves
+    #define win for the computer
+  end
+
+  def corners
+    #define corner moves
+  end
+
+  def block
+
+  end
+
+  def tie
+
+  end
+
+  def center_move
+
   end
 end
 
