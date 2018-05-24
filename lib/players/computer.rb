037950763652f2032cf5c33f9d80e8e422046120
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-24 14:34:24 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -18,33 +18,9 @@ module Players
       end
       move
     end
-  end
-end
-
-
-
-    #   elsif board.valid_move?(input)
-    #     move = input
-    #   elsif !board.valid_move?(input)
-    #     move(board)
-    #   elsif board.full?
-    #     nil
-    #   end
-    # end
-
-    # def move(board)
-    #   move = nil
-    #   if (board.turn_count == 0 || board.turn_count == 1) && !board.taken?(5)
-    #       --> don't need this because it would happen 1st round no matter what and nobody can win with only 1 move completed
-    #     move = "5"
-    #   end
-
-    # if win_combinations is possible, i.e. 1 move off --> not sure how to state this in code
-    #   move = index of open move to win  --> to either win or block
-
-    # will need to incorporate win_combinations somehow
-
     # to block:
     # check if there are any two positions that are taken with the same opposing token that is
     # lands in the win_combinations and block the third position that has not yet been
     # taken in the winning combination
+  end
+end
