36fc182c41963f3a118793f2afc1b6ca923a5b0a
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-05-24 23:03:32 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,18 +2,6 @@ module Players
   class Computer < Player
     attr_accessor :board
 
-    WIN_COMBINATIONS = [
-    [0, 1, 2],
-    [0, 3, 6],
-    [0, 4, 8],
-    [1, 4, 7],
-    [2, 4, 6],
-    [2, 5, 8],
-    [3, 4, 5],
-    [6, 7, 8]
-    ]
-    CORNERS = [0, 2, 6, 8]
-
 
     def move(board)
       if board.cells[4] == " "
