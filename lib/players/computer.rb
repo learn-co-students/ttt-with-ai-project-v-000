4d878b34b88c8dd93967201b4957aa98dc6268db
Chrissa Constantine <chrissa1@mac.com>
2018-05-24 23:18:27 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,18 +1,43 @@
 module Players
   class Computer < Player
 
-
+    attr_accessor :board
     #played perfectly is unwinnable. logic - like on turn 1 always pic middle or go to corner. min/max algorithm may not work.
     def move(board)
-      @board = board
-      #case input
-        #input = gets.strip
-      #when condition
-
-    #  end
-    #it accepts board. it returns the move from the computer in a 1-9 string. must make a valid move.
-    "1"
+      if board.cells[4] == " "
+        timer
+         "5"
+      elsif board.cells[0] == " "
+         timer
+         "1"
+      elsif board.cells[2] == " "
+         timer
+         "3"
+      elsif board.cells[6] == " "
+         timer
+         "7"
+      elsif board.cells[8] == " "
+         timer
+         "9"
+      elsif board.cells[1] == " "
+         timer
+         "2"
+      elsif board.cells[3] == " "
+         timer
+         "4"
+      elsif board.cells[5] == " "
+         timer
+         "6"
+      elsif board.cells[7] == " "
+         timer
+         "8"
+      end
     end
+
+    def timer
+      sleep(2)
+    end
+
 
   end
 end
