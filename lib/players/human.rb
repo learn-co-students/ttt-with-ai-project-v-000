45eb0d6cbfd5c7fef4e370e4bc75c25a9d677790
Alex Wilson <fonzmp@gmail.com>
2018-05-25 01:42:55 +0000

    Done.

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -2,5 +2,11 @@ module Players
   class Human < Player
     # your code here
 
+    def move(board)
+      puts "Please pick a position:"
+      user_input = gets
+      user_input
+    end
+
   end
 end
