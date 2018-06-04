2458fa9c9f860bd8e818bce20e79cd7e33de2435
taryn yaeger <tsyaeger@gmail.com>
2018-06-04 22:10:36 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -64,70 +64,45 @@ module Players
               @offense << combo[ix]
             end
           end
-
         end
       end
     end
 
       
-      
     def find_best_move
-      
-      puts "off #{@offense.inspect}"
-      puts "def #{@defense.inspect}"
       
        @offense.each do |i| 
         if @defense.include?(i)
           @combo << i
         end
       end
-          
-      puts "combo #{combo.inspect}"
       
       if @win != nil
-        puts "win #{win}"
         move = @win
         
       elsif @block != nil
-        puts "block#{block}"
         move = @block
         
       elsif @board.all?{|cell| cell == " "}
-        puts "all ' ' "
         no_5 = @board.uniq.reject{|cell| cell == 4}
-        puts "no_5#{no_5.inspect}"
         move = randomizer(no_5)
         
       elsif @board.count(@opp_token) == 1 && @board[4] == " "
-        puts "mid"
         move = 4
     
       elsif @combo.length > 0
-        puts "combo"
         move = randomizer(@combo)
         
       elsif @offense.length > 0
-        puts "offense"
         move = randomizer(@offense.uniq)
-        move
         
       elsif @defense.length > 0
-        puts "defense"
         move = randomizer(@defense.uniq)
-        puts "else move #{move}"
-        move
         
       else
-        puts "last move"
-        puts "@board: #{@board.inspect}"
         move = @board.index(" ")
-        puts "last move #{move}"
-        move
       end
-      
-      puts "move: #{move}"
       (move.to_i + 1).to_s
     end
-    
   end
 end
