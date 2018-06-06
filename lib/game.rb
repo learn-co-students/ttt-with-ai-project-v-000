c72479cba172a6e889521e962826be95be9ba937
Ayana Bando <ayanaflax@yahoo.com>
2018-06-06 22:36:16 +0000

    Done.

---
+++ lib/game.rb
@@ -0,0 +1,37 @@
+class Game
+  attr_accessor :board
+
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2]
+  ]
+  def player_1
+  end
+
+  def player_2
+  end
+
+  def current_player
+  end
+
+  def won?
+  end
+
+  def draw?
+  end
+
+  def over?
+  end
+
+  def play
+  end
+
+  def turn
+  end
+end
