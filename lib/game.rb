12f31a069b483e2869fd658c96eede2e4e344ea4
Nancy Jo <enciie@gmail.com>
2018-06-06 01:12:25 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,6 @@
 class Game
 
-  attr_accessor :board
+  attr_accessor :board, :player_1, :player_2
 
   WIN_COMBINATIONS = [
     [0,1,2],[3,4,5],[6,7,8],
@@ -8,10 +8,36 @@ class Game
     [0,4,8],[6,4,2]
   ]
 
-  def player_1
+
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+    @player_1 = player_1
+    @player_2 = player_2
+    @board = board
   end
 
-  def player_2
+  def current_player
+    board.turn_count.even? ? player_1 : player_2
+  end
+
+  def won?
+    WIN_COMBINATIONS.each do |combo|
+      if @board.cells[combo[0]] == @board.cells[combo[1]] &&
+         @board.cells[combo[1]] == @board.cells[combo[2]] &&
+         @board.taken?(combo[0])
+        return combo
+      end
+    end
+    return false
+  end
+
+  def draw?
+    !won? && @board.full?
+  end
+
+  def over?
+    won? || draw? ? true : false
+    #if game is won, game is over true
+    #if draw is true, game is over is true
   end
 
 end
