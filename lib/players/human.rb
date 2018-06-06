b57b2c12dd85b4c08aa647d1d477924c1f591507
Brennan Fulmer <brennan.flmr@gmail.com>
2018-06-06 05:47:34 +0000

    Done.

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -3,9 +3,8 @@ module Players
   class Human < Player
     
     def move(board)
-      # puts "Please enter 1-9:"
-      # input = gets.strip
-      
+      puts "Please enter 1-9:"
+      input = gets.strip
     end
     
   end
