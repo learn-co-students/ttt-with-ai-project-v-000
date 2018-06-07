576ef9e84b5d9074dcf38fc9915de23139a9234f
Brennan Fulmer <brennan.flmr@gmail.com>
2018-06-07 01:28:39 +0000

    Done.

---
+++ lib/players/computer.rb
@@ -0,0 +1,21 @@
+
+module Players
+  class Computer < Player
+    
+    def move(board)
+      
+      if board.turn_count < 4
+        
+        if board.position("6") != ' '
+          # prioritizecenter
+          "6"
+        elsif 
+        end
+        
+      else
+      end
+      
+    end
+    
+  end
+end
