71fd4a97f40c0feb2344e204639a522fda67ed87
Brennan Fulmer <brennan.flmr@gmail.com>
2018-06-07 05:23:20 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,37 +2,42 @@
 module Players
   class Computer < Player
     
-def move(board)
-  binding.pry
-  two_row = [
-              ['1','2'], ['2','3'], ['1','4'], ['2','5'], ['3','6'], ['1','5'],
-              ['3','5'], ['2','4'], ['2','6'], ['4','5'], ['5','6'], ['4','7'],
-              ['5','8'], ['6','9'], ['4','8'], ['6','8'], ['5','7'], ['5','9'],
-              ['7','8'], ['8','9']
-            ]
-  edges = ['2','4','6','8']
-  corners = ['1','3','7','9']
-
-  if token == 'O'
-    enemy = 'X'
-  else
-    enemy = 'O'
-  end
+    def move(board)
+      binding.pry
+    
+      corners = ['1','3','7','9']
+      edges = ['2','4','6','8']
+    
+      if token == 'O'
+        enemy = 'X'
+      else
+        enemy = 'O'
+      end
 
 =begin
-  if # win / block - use two_row for conditional logic
-  elsif # (block)fork - this one may require a giant array of adjacent spots
-  elsif board.position("6") == ' ' # center
-    '6'
-  elsif # opposite corner
-  elsif # corner
-    corners.detect { |area| board.position(area) == ' '}
-  else # edge
-    edges.detect { |area| board.position(area) == ' '}
-  end
+  # under construction
+      p1 = false
+      Game.win_combinations.each { |set|
+        if board.cells[set[0]]
+      }
+    
+      if # win / block - use Game.win_combinations for conditional logic
+      elsif # (block)fork - this one may require a giant array of adjacent spots
+      elsif board.position('6') == ' ' # center
+        '6'
+      elsif board.position('1') == enemy && board.position('9') == ' ' # opposite corner 1
+        '9'
+      elsif board.position('3') == enemy && board.position('7') == ' ' # opposite corner 3
+        '7'
+      elsif board.position('7') == enemy && board.position('3') == ' ' # opposite corner 7
+        '3'
+      elsif board.position('9') == enemy && board.position('1') == ' ' # opposite corner 9
+        '1'
+      else # corner / edge
+        corners.detect { |area| board.position(area) == ' '} || edges.detect { |area| board.position(area) == ' '}
+      end
 =end
-end
-
+    end
     
   end
 end
