123170133c5f5a909536db4a81f07934c60731f1
Brennan Fulmer <brennan.flmr@gmail.com>
2018-06-07 02:25:55 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,19 +3,16 @@ module Players
   class Computer < Player
     
     def move(board)
-      
-      if board.turn_count < 4
-        
-        if board.position("6") != ' '
-          # prioritize center
-          "6"
-        elsif 
-        end
-        
-      else
+      moves = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+      if board.turn_count < 3
+        "6" if board.position("6") != ' ' # center
+    
+    
       end
-      
+      # conditionals for 2 in a row, etc
+    
     end
+
     
   end
 end
