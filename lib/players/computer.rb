0bcc8976dc74066fc2ed2e11f52fa8ac8312c9ec
Brennan Fulmer <brennan.flmr@gmail.com>
2018-06-07 17:17:03 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,28 +1,28 @@
 
 module Players
   class Computer < Player
+    attr_reader :urgent_move
+    
+    def win_or_block(board)
+      Game.win_combinations.each { |set|
+        if board.cells[set[0]] != ' ' && board.cells[set[0]] == board.cells[set[1]] && board.cells[set[2]] == ' '
+          @urgent_move = "#{set[2] + 1}"
+          return true
+        elsif board.cells[set[1]] != ' ' && board.cells[set[1]] == board.cells[set[2]] && board.cells[set[0]] == ' '
+          @urgent_move = "#{set[0] + 1}"
+          return true
+        end
+      }
+      false
+    end
     
     def move(board)
       corners = ['1','3','7','9']
       edges = ['2','4','6','8']
+      enemy = token == 'O' ? 'X' : 'O'
     
-      if token == 'O'
-        enemy = 'X'
-      else
-        enemy = 'O'
-      end
-    
-      urgent_move = [false, '0']
-      Game.win_combinations.each { |set|
-        if board.cells[set[0]] != ' ' && board.cells[set[0]] == board.cells[set[1]] && board.cells[set[2]] == ' '
-          urgent_move = [true, set[2].to_s]
-        elsif board.cells[set[1]] != ' ' && board.cells[set[1]] == board.cells[set[2]] && board.cells[set[0]] == ' '
-          urgent_move = [true, set[0].to_s]
-        end
-      }
-    
-      if urgent_move[0] # win / block - use Game.win_combinations for conditional logic
-        urgent_move[1]
+      if win_or_block(board) # win / block - use Game.win_combinations for conditional logic
+        urgent_move
       # elsif # (block)fork - this one may require a giant array of adjacent spots
       elsif board.position('6') == ' ' # center
         '6'
