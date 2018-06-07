069af8dab4d271cd7511fe993893e37c0e0ec659
Brennan Fulmer <brennan.flmr@gmail.com>
2018-06-07 19:39:11 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,7 +1,7 @@
 
 module Players
   class Computer < Player
-    attr_reader :urgent_move
+    attr_reader :urgent_move, :corner_move
     
     def win_or_block?(board)
       Game.win_combinations.any? { |set|
@@ -15,26 +15,36 @@ module Players
       }
     end
     
+    def counter_corner?(board)
+      enemy = token == 'O' ? 'X' : 'O'
+      
+      if board.position('1') == enemy && board.position('9') == ' '
+        @corner_move = '9'
+        true
+      elsif board.position('9') == enemy && board.position('1') == ' '
+        @corner_move = '1'
+        true
+      elsif board.position('3') == enemy && board.position('7') == ' '
+        @corner_move = '7'
+        true
+      elsif board.position('7') == enemy && board.position('3') == ' '
+        @corner_move = '3'
+        true
+      end
+    end
+    
     def move(board)
       corners = ['1','3','7','9']
       edges = ['2','4','6','8']
-      enemy = token == 'O' ? 'X' : 'O'
     
-      if win_or_block?(board) # win / block
+      if win_or_block?(board)
         urgent_move
-      # elsif # (block)fork - this one may require a giant array of adjacent spots
-      elsif board.position('6') == ' ' # center
-        '6'
-      elsif board.position('1') == enemy && board.position('9') == ' ' # opposite corner 1
-        '9'
-      elsif board.position('3') == enemy && board.position('7') == ' ' # opposite corner 3
-        '7'
-      elsif board.position('7') == enemy && board.position('3') == ' ' # opposite corner 7
-        '3'
-      elsif board.position('9') == enemy && board.position('1') == ' ' # opposite corner 9
-        '1'
-      else # corner / edge
-        corners.detect { |area| board.position(area) == ' '} || edges.detect { |area| board.position(area) == ' '}
+      elsif board.position('5') == ' ' # center
+        '5'
+      elsif counter_corner?(board)
+        corner_move
+      else
+        corners.detect { |area| board.position(area) == ' ' } || edges.detect { |area| board.position(area) == ' ' }
       end
     end
     
