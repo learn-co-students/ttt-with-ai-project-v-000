eb6da1c26e198519b99b10bbd01cf39c559b1ffc
Brent Bauman <brentbauman86@gmail.com>
2018-06-08 15:47:10 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,48 @@
+class Board
+
+attr_writer :cells
+
+@@all = []
+
+  def initialize(cells)
+    @cells = cells
+  end
+
+  def cells
+
+    puts "#{board}"
+
+  end
+
+  def reset!
+
+  end
+
+  def display
+
+  end
+
+  def position
+
+  end
+
+  def full?
+
+  end
+
+  def turn_count
+
+  end
+
+  def taken?
+
+  end
+
+  def valid_move?
+
+  end
+
+  def update
+
+  end
+end
