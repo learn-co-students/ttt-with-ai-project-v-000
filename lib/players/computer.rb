002af7c014ab3d9b587bd8ee8807d5485998d983
Jebediah Elliott <jebediahelliott@gmail.com>
2018-06-08 00:58:46 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,5 +1,26 @@
 module Players
   class Computer < Player
+    def display
+      puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+      puts "-----------"
+      puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+      puts "-----------"
+      puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+    end
+    WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8],
+    [2,4,6],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8]
+    ]
+
+    def move(board)
+      5
+    end
 
   end
 end
