bf46828b4bc38e8b54d8fa7fc7457b77a686c9a1
Jebediah Elliott <jebediahelliott@gmail.com>
2018-06-08 12:54:30 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -8,6 +8,9 @@ module Players
     #   puts "-----------"
     #   puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
     # end
+    # move_arr = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    # # binding.pry
+    # move_arr.sample
     WIN_COMBINATIONS = [
     [0,1,2],
     [3,4,5],
@@ -20,9 +23,16 @@ module Players
     ]
 
     def move(board)
-      move_arr = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-      # binding.pry
-      move_arr.sample
+      corners = [1, 3, 7, 9]
+      edges = [2, 4, 6, 8]
+      center = 5
+      if Game.player_1 == self
+        if board.cells.empty?
+          corners.sample
+        end
+      elsif Game.player_2 == self
+
+      end
     end
 
    end
