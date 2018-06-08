c7f72312cf2df07d8a58cf33aafc0e41ae86f435
Carrie Savinell <carrielane89@gmail.com>
2018-06-08 14:15:31 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,31 @@
+require 'pry'
+
 class Board
-end   
+
+  attr_accessor :cells
+
+  def initialize
+    reset!
+  end
+
+  def reset!
+    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+
+  def display
+    puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
+    puts "-----------"
+    puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
+    puts "-----------"
+    puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
+  end
+
+  def position(user_input)
+    input = user_input.to_i
+    @cells[input - 1]
+  end
+
+  def full?
+    @cells.none? |cell| cell == " " || cell == nil
+  end
+end
