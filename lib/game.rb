fc3f7f4c79cd42a51d27177f109d5d0aa527d44b
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-06-08 20:03:40 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -12,5 +12,10 @@ class Game
     [2, 4, 6]
     ]
     
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+    @board = board
+    @player_1 = player_1
+    @player_2 = player_2
+  end
   
 end
