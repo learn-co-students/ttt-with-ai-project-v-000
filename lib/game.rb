50338f3ff078a4c38a3f4d9173972010b6e4900e
Richard Melchior <raustin.melchior@gmail.com>
2018-06-09 16:16:50 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,5 @@
 class Game
-  attr_accessor :board, :player_1, :player_2
+  attr_accessor :board, :player_1, :player_2, :num_players
 
   def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
     @player_1 = player_1;
@@ -17,6 +17,38 @@ class Game
     [0, 4, 8], # Left to Right diagonal
     [2, 4, 6]  # Right to Left diagonal
   ];
+
+  ##
+  # This method asks the first player which token he would like and assigns both player tokens
+  ##
+  def define_players
+    get_player_input = "Would you like to be 'X's or 'O's?";
+    case @num_players
+      when 0
+        self.new(Players::Computer.new("X"), Players::Computer.new("O"));
+      when 1
+        puts get_player_input;
+        input = gets.strip;
+
+        input == "X" || input == "x" ? self.new(Players::Human("X"), Players::Computer("O")) :
+                                       self.new(Players::Human("O"), Players::Computer("X"));
+      when 2
+        puts get_player_input;
+        input = gets.strip;
+        input == "X" || input == "x" ? self.new(Players::Human("X"), Players::Human("O")) :
+                                       self.new(Players::Human("O"), Players::Human("X"));
+      else
+        puts "Invalid input, please select a valid option.";
+        define_players(@num_players);
+      end
+  end #end #define_players
+
+
+  def replay
+    puts "Would you like to play again?";
+    input = gets.strip #I need to ask about this method.
+    input == "yes" || input == "Yes" ? start : puts("Goodbye!")
+  end
 
   def current_player
     @board.turn_count % 2 == 0 ? @player_1 : @player_2;
@@ -39,46 +71,57 @@ class Game
         (position_1 == o && position_2 == o && position_3 == o))
           return win_combination;
         else
-          false
+          false;
       end
     end
     false; #explicitly tell ruby to return false if we've cycled through the board and no win combinations can be found
   end
 
   def draw?
-    @board.full? && !won?
+    @board.full? && !won?;
   end
 
   def over?
-    won? || draw?
+    won? || draw?;
   end
 
   def winner
-    winner = won?
-    won? == false ? nil : @board.cells[winner[0]]
+    winner = won?;
+    won? == false ? nil : @board.cells[winner[0]];
   end
 
   def turn
     puts "Please enter 1-9:"
-    move = current_player.move(@board)
+    move = current_player.move(@board);
     if @board.valid_move?(move)
-      @board.update(move, current_player)
-      @board.display
+      @board.update(move, current_player);
+      @board.display;
     else
-      turn
+      turn;
     end
   end
 
   def play
     until over? do
-      turn
+      turn;
     end
 
     if won?
-      puts "Congratulations #{winner}!"
+      puts "Congratulations #{winner}!";
+      replay
     elsif draw?
-      puts "Cat's Game!"
+      puts "Cat's Game!";
+      replay
     end
+  end
+
+  def start
+    puts "Welcome to Tic Tac Toe!"
+    puts ""
+    puts "How many players want to play? (0, 1, or 2)"
+    @num_players = gets.strip
+    define_players
+    play
   end
 
 end
