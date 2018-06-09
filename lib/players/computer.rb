b6bb331ead978988ca7167bf4a3755ca79303bba
Nancy Jo <enciie@gmail.com>
2018-06-09 02:34:01 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -26,49 +26,5 @@ module Players
         "6"
       end
     end
-
-# (1..9.to_a.sample) randomly selects a number
-
-# def move(board, game=Game.new)
-#   return_value = nil
-#   current_token = game.current_player.token
-#   opponent_token = current_token == "O" ? "X" : "O"
-#   edges_array = [1, 3, 5, 7]
-#   corners_array = [0, 2, 6, 8]
-#
-#  if board.turn_count == 0
-#    return 5
-#  end
-#
-#  if board.turn_count == 2
-#    edges_array.each do |index|
-#      if board.cells[index] == opponent_token
-#        ##index is the edge index at which the opponent went
-#        case index
-#        when 1
-#          return_value = [7, 9].sample
-#        when 3
-#          return_value = [3, 7].sample
-#        when 5
-#          return_value = [1, 3].sample
-#        else
-#          return_value = [1, 9].sample
-#        end
-#      end
-#    end
-#
-#  end
-#
-#
-#   resultsArr = []
-#   board.cells.each_with_index do |cell, i|
-#     if cell == " "
-#       resultArr << i + 1
-#     end
-#    end
-#    resultsArr.sample
-#  end
-# end
-
   end
 end
