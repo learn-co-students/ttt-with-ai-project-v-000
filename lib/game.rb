0ee5965977cd9496c7e4ee8512cb334021e623b0
Ayana Bando <ayanaflax@yahoo.com>
2018-06-11 15:46:44 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,5 @@
 class Game
-  attr_reader :board, :player_1, :player_2
+  attr_accessor :board, :player_1, :player_2
 
   WIN_COMBINATIONS = [
     [0,1,2],
@@ -12,23 +12,11 @@ class Game
     [6,4,2]
   ]
 
-  def player_1=(player_1)
-    @player_1 = player_1
-  end
-
-  def player_2=(player_2)
-    @player_2 = player_2
-  end
-
-  def board=(board)
-    @board = []
-  end
-
-  def initialize(player_1=Players::Human.new("X"), player_2=Players::Human.new("O"), board=(Board.new))
+    def initialize(player_1=Players::Human.new("X"), player_2=Players::Human.new("O"), board=(Board.new))
     @player_1 = player_1
     @player_2 = player_2
     @board = board
-  end
+    end
 
   def current_player
     board.turn_count % 2 == 0 ? player_1 : player_2
@@ -36,40 +24,39 @@ class Game
 
   def won?
     WIN_COMBINATIONS.any? do |combo|
-      if taken?(combo[0]) && @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]]
+      if board.taken?(combo[0]) && @board.cells[combo[0]] == @board.cells[combo[1]] && @board.cells[combo[1]] == @board.cells[combo[2]]
         return combo
       end
     end
   end
 
   def draw?
-      !won?(board) && full?(board)
+      !won? && board.full?
   end
 
   def over?
-    won?(board) || draw?(board)
+    won? || draw?
   end
 
   def play
-    while !over?(board)
-      turn(board)
-    end
-    if won?(board)
-      puts "Congratulations #{winner(board)}!"
-    elsif draw?(board)
-      puts "Cat's Game!"
-    end
+    turn until over?
+    puts winner ? "Congratulations #{winner}!" : "Cat's Game!"
+  end
+
+  def winner
+   if combo = won?
+     @board.cells[combo[0]]
+   end
   end
 
   def turn
     puts "Please enter 1-9:"
     user_input = gets.strip
-    index = input_to_index(user_input)
-    if valid_move?(board, index)
-      move(board, index, current_player(board))
-      display_board(board)
+    if valid_move?(input)
+      board.position(user_input)
+      turn
     else
-      turn(board)
+      display
     end
   end
 end
