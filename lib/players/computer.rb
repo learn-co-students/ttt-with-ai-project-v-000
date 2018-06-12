3c47f2f5bafa0cdbeeb0b823a37007c4bc4eae1b
Scott Weinstein <sweinstein27@gmail.com>
2018-06-12 14:50:14 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -36,9 +36,11 @@ module Players
     sleep(@timer)
     input.to_s
   end
+
   def other
     token == "X" ? "O" : "X"
   end
+
   def winning_move
     winning_row = WIN_COMBINATIONS.find do |combo|
       (board.cells[combo[0]] == token && board.cells[combo[1]] == token && board.cells[combo[2]] == " ") || (board.cells[combo[2]] == token && board.cells[combo[1]] == token && board.cells[combo[0]] == " ") || (board.cells[combo[0]] == token && board.cells[combo[2]] == token && board.cells[combo[1]] == " ")
@@ -47,6 +49,7 @@ module Players
       winning_cell = winning_row.find {|cell| board.cells[cell] == " "}
     end
   end
+
   def blocking_move
     winning_row = WIN_COMBINATIONS.find do |combo|
       (board.cells[combo[0]] == other && board.cells[combo[1]] == other && board.cells[combo[2]] == " ") || (board.cells[combo[1]] == other && board.cells[combo[2]] == other && board.cells[combo[0]] == " ") || (board.cells[combo[2]] == other && board.cells[combo[0]] == other && board.cells[combo[1]] == " ")
@@ -55,15 +58,19 @@ module Players
       winning_cell = winning_row.find {|cell| board.cells[cell] == " "}
     end
   end
+
   def center?
     board.cells[4] == " "
   end
+
   def corners
     CORNERS.shuffle!
   end
+
   def corner
     corners.find {|corner| board.cells[corner] == " "}
   end
+
   def opposite_corner
     case
       when board.taken?(1) && !board.taken?(9)
@@ -78,4 +85,6 @@ module Players
         nil
     end
   end
+
+end
 end
