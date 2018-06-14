9022ed16fe0021e8bd4943236025c9bb8b0ae251
Elaine Anderson <eparie@bu.edu>
2018-06-14 18:15:18 +0000

    Done.

---
+++ lib/game.rb
@@ -0,0 +1,30 @@
+
+
+  class Game
+  attr_accessor :board, :player_1, :player_2, :token
+
+
+  WIN_COMBINATIONS = [
+[0,1,2],
+[3,4,5],
+[6,7,8],
+[0,3,6],
+[1,4,7],
+[2,5,8],
+[0,4,8],
+[2,4,6]
+]
+
+def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+  @player_1 = player_1
+  @player_2 = player_2
+  @board = board
+end
+
+def current_player
+  if Board.turn_count.odd?
+  player_1
+
+end
+
+end
---
+++ lib/players/human.rb
@@ -0,0 +1,11 @@
+
+module Players
+  class Human < Player
+
+    def move(input)
+      input = gets.strip
+      input
+    end
+  end
+
+end
