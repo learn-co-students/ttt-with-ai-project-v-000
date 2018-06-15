ca9509efa51ee25eec419e297cc543c2c9317a72
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-06-15 15:37:44 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,40 +1,3 @@
-# The `Board` class represents the data and logic
-# of a Tic-tac-toe game board. It has a property, `cells`,
-# that stores the data of the state of the board in an array.
-# The `#reset!` method can reset the state of the cells
-# to what a board should look like at the start of a game,
-# an array with 9 `" "` elements.
-#
-# When a board is initialized, it should start with
-# cells for a new game of Tic-tac-toe.
-# You can and should use `#reset!`.
-#
-# A board can print its current state with the `#display` method.
-#
-# You'll also build a `#position` method that takes in
-# the user's input in the form of 1-9 strings like "2" or "9"
-# and looks up the value of the cells at the correct index
-# from the array's perspective. All other methods will take
-# input in the user's perspective of 1-9 strings and use `#position`
-# to look up the value according to the cells' array index.
-#
-# Similarly, you're going to build an `#update` method that
-# represents updating the board when a player makes a move.
-# This method will take two arguments, the first will be the
-# position the user wants to occupy in the form of 1-9 strings that
-# you will need to convert to the board cells' array index,
-# along with the player object making the move.
-# When you update the appropriate index in the cells,
-# you will set it equal to the token of the player object
-# by calling the `#token` method on the player.
-#
-# Finally, a board can return values based on its state such as
-# `#full?` when entirely occupied with "X" and "O"s and a
-# `#turn_count` based on how many positions in the cells array
-# are filled. `#taken?` will return true or false for an individual
-# position. `#valid_move?` will ensure that moves are between
-# 1-9 and not taken.
-
 require 'pry'
 
 class Board
@@ -93,6 +56,13 @@ class Board
         true
       end
     end
+  end
+
+  def update(cell_number, player)
+    if valid_move?(cell_number)
+      @cells[cell_number.to_i - 1] = player.token
+    end
+    @cells
   end
 
 end
