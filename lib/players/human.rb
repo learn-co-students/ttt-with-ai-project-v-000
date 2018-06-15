5ad91fbbf30ad364205d9fbadb0b081a8c8135a9
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-06-15 15:46:35 +0000

    Done.

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -5,8 +5,6 @@ module Players
     def move(board)
       # Even though the method accepts a `board` argument,
       # it does not need to use it.
-      puts "Welcome to Tic Tac Toe!"
-      board.display
       puts "Enter a number from 1-9:"
       input = gets.strip
     end
