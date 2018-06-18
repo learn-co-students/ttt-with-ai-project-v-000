9764fced787fb256724574a71cee3f4d4eb00b41
Monica Seaman <monica.l.seaman@gmail.com>
2018-06-18 19:24:03 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,20 +2,37 @@ module Players
   class Computer < Player
 
     def move(board)
-      #corners
-      #middle
-      (rand(9) + 1).to_s
+      if board.turn_count == 0
+        binding.pry
+        1
+      elsif board.turn_count == 1
+        if !board.taken?(5)
+          5
+        elsif board.taken?(5)
+          corners
+        end
+      elsif board.turn_count == 2
+        9 unless board.taken?(9)
+        corners
+      elsif board.turn_count == 3
+        edges
+      else
+        (rand(9) + 1).to_s
+      end
     end
 
     def corners
-      0 unless taken?
-      2 unless taken?
-      6 unless taken?
-      8 unless taken?
+      1 unless taken?(1)
+      3 unless taken?(3)
+      7 unless taken?(7)
+      9 unless taken?(9)
     end
 
-    def middle
-      4
+    def edges
+      2 unless taken?(2)
+      4 unless taken?(4)
+      6 unless taken?(6)
+      8 unless taken?(8)
     end
 
   end
