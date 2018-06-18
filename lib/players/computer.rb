2412ca8f13076eddf62bf714520ca99b01a55c59
Brent Bauman <brentbauman86@gmail.com>
2018-06-18 19:05:55 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,8 +2,40 @@ require "pry"
 module Players
   class Computer < Player
 
-    def move(input)
-      (rand(9) + 1).to_s
+    def move(board)
+      if board.turn_count == 0
+        1
+      elsif board.turn_count == 1
+        if !board.taken?(5)
+          5
+        elsif taken?(5)
+          corners
+        end
+
+
+elsif board.turn_count == 2
+  9 unless taken?(9)
+  corners
+elsif board.turn_count == 3
+        edges
+      else
+        (rand(9) + 1).to_s
     end
   end
-end 
+
+    def corners
+      1 unless taken?(1)
+      3 unless taken?(3)
+      6 unless taken?(6)
+      9 unless taken?(9)
+    end
+
+    def edges
+      2 unless taken?(2)
+      4 unless taken?(4)
+      6 unless taken?(6)
+      8 unless taken?(8)
+    end
+
+  end
+end
