9777807f1f5dcd8fbcd8fc5ea445b689865ad6a1
Ariel Zhen <zhenpingping@live.com>
2018-06-21 01:47:01 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,57 @@
+class Board
+
+  attr_accessor :cells
+
+  def initialize
+    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+
+  def reset!
+    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+
+  def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
+
+  def position(input)
+    @cells[input.to_i - 1]
+  end
+
+  def update(input,player)
+    @cells[input.to_i-1] = player.token
+  end
+
+  def full?
+    @cells.all? {|ele| ele != " "}
+  end
+
+  def turn_count
+    counter = 9
+    @cells.each do |ele|
+      counter -= 1 if ele == " "
+    end
+    counter
+  end
+
+  def taken?(input)
+    if (position(input) == " " || position(input) == nil)
+       false
+    else
+      true
+    end
+  end
+
+  def valid_move?(input)
+    if ((input.to_i >= 1 && input.to_i <= 9) && !taken?(input))
+      true
+    else
+      false
+    end
+  end
+
+end
