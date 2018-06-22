f942555d5fef25ae53826338f1ac67c18e1129ab
Gabriel Valle <gvalle91@gmail.com>
2018-06-22 22:43:29 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -18,11 +18,45 @@ class Game
     @player_2 = player_2
   end
 
+  def turn
+    puts "Enter an empty position (1-9)"
+    input = gets
+    while !@board.valid_move?(input)
+      puts "Enter a empty position (1-9)"
+      input = gets
+    end
+    input
+  end
+
   def current_player
     if @board.turn_count.odd?
       @player_2
     else
       @player_1
+    end
+  end
+
+  def won?
+    winner = WIN_COMBINATIONS.find do |combo|
+      @board.cells[combo[0]] == @board.cells[combo[1]] && @board.cells[combo[0]] == @board.cells[combo[2]]
+        #winner = "#{combo[0]}, #{combo[1]}, #{combo[2]}"
+      end
+    winner
+  end
+
+  def draw?
+    !self.won? && @board.full?
+  end
+
+  def over?
+    self.draw? || self.won? || @board.full?
+  end
+
+  def winner
+    if @board.cells[self.won?[0]] == " "
+      return nil
+    else
+      return @board.cells[self.won?[0]]
     end
   end
 
