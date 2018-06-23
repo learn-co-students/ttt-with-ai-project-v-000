17504ebd7caf71ea1f68752a57900ac7edea4d97
Amar Chatman <amarjerome@gmail.com>
2018-06-23 03:47:17 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,24 @@
+require 'pry'
+class Board
+
+attr_accessor :cells
+
+def initialize
+reset!
+end
+
+
+
+def reset!
+  @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+end
+
+def display
+  puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+  puts "-----------"
+  puts " #{@cells[3]} | #{@cells[4]}  | #{@cells[5]} "
+  puts "-----------"
+  puts "  #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+end
+
+end
