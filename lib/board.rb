a41256050718282cf5d2b4e34bb1e98b83b6d30f
Sheila Wittman <szandora789@gmail.com>
2018-06-25 20:40:14 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,47 @@
+class Board
+  attr_accessor :cells
+
+  def initialize
+    reset!
+  end
+
+  def reset!
+    @cells = Array.new(9, " ")
+  end
+
+  def display
+    puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
+    puts "-----------"
+    puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
+    puts "-----------"
+    puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
+  end
+
+  def position(input)
+    cells[input.to_i - 1]
+  end
+
+  def full?
+    cells.all? {|cell| cell != " "}
+  end
+
+  def turn_count
+    cells.count {|cell| cell == "X" || cell == "O"}
+  end
+
+  def taken?(input)
+    position(input) == "X" || position(input) == "O"
+  end
+
+  def valid_move?(input)
+    input = input.to_i
+    input.between?(1,9) && !taken?(input)
+  end
+
+  def update(input, player)
+    input = input.to_i
+    cells[input - 1] = player.token
+  end
+end
+
+  
---
+++ lib/players/human.rb
@@ -0,0 +1,10 @@
+module Players
+  class Human < Player
+
+    def move(board)
+      puts "Please enter 1-9:"
+      gets.chomp
+    end
+  end
+
+end
