4cfb3e1357c80c2e84a94b45dc7caa8456dda929
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-06-27 15:24:33 +0000

    human

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,1 +1,11 @@
+<<<<<<< HEAD
 
+=======
+module Players
+  class Human < Player
+    def move(board)
+      input = gets.strip
+    end
+  end
+end
+>>>>>>> faf445fdfba6cace0267507d3f666713b24a5fdc
