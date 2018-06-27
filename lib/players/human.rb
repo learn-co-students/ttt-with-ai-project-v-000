bf226cda4c210440d7e7926e4d1433256eb6f23b
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-06-27 15:27:42 +0000

    removed the extra github stuff" xit

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
-=======
->>>>>>> faf445fdfba6cace0267507d3f666713b24a5fdc
 module Players
   class Human < Player
     def move(board)
@@ -11,7 +5,3 @@ module Players
     end
   end
 end
-<<<<<<< HEAD
->>>>>>> faf445fdfba6cace0267507d3f666713b24a5fdc
-=======
->>>>>>> faf445fdfba6cace0267507d3f666713b24a5fdc
