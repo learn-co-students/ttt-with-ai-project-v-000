faf445fdfba6cace0267507d3f666713b24a5fdc
Richard Cho <richardcho100@gmail.com>
2018-06-27 03:38:46 +0000

    Finished Player class and Players::Human.

---
+++ lib/game.rb
@@ -0,0 +1,46 @@
+class Game
+  attr_accessor :board, :player_1, :player_2
+
+  WIN_COMBINATIONS = [
+    [0,1,2], #Top row
+    [3,4,5], #Middle row
+    [6,7,8], #Bottom row
+    [0,3,6], #Left column
+    [1,4,7], #Middle column
+    [2,5,8], #Right column
+    [0,4,8], #Diagonal downward right
+    [2,4,6] #Diagonal downward left
+  ]
+
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+    @board = board
+    @player_1 = player_1
+    @player_2 = player_2
+  end
+
+  def current_player
+    @board.turn_count % 2 == 0 ? player_1 : player_2
+  end
+
+  def over?
+    won? || draw?
+  end
+
+  def won?
+    WIN_COMBINATIONS.detect do |winner|
+      @board.cells[winner[0]] == @board.cells[winner[1]] &&
+      @board.cells[winner[1]] == @board.cells[winner[2]] &&
+      (@board.cells[winner[0]] == 'X') ||   (@board.cells[winner[0]] == 'O')
+    end
+  end
+
+  def draw?
+    @board.full? && !won?
+  end
+
+  def winner
+    if a win_combination = won?
+      @winner = @board.cells[winning_combo.first]
+    end
+  end
+end
