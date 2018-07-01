1ddaed290b2539738fb7f8c937bbe18501d52be2
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-07-01 03:55:17 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -3,16 +3,7 @@ class Game
   attr_accessor :board, :player_1, :player_2, :winner
 
   #Define WIN_COMBINATIONS constant
-  WIN_COMBINATIONS = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-  ]
+  WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
   def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
     @board = board
@@ -41,10 +32,24 @@ class Game
   def winner
     if winXO = won?
       @winner = @board.cells[winXO.first]
-    #elsif !won?
-    #   nil
     end
-    #binding.pry
   end
+
+  def move(index, current_player)
+    @board[index] = current_player
+  end
+
+  def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input.to_i - 1
+    if @board.valid_move?(index)
+      move(index, current_player)
+      @board.display
+    else
+      turn
+    end
+  end
+
 
 end
