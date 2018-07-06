6aba698ce98a8376d263dac5e1f35a7f452c8429
Trevor Russell <truss.42@gmail.com>
2018-07-06 20:34:29 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -15,21 +15,51 @@ class Players::Computer < Player
       if cell == nil
         cell = 1 if (board.cells[2] == other_token || board.cells[3] == other_token || board.cells[6] == other_token || board.cells[1] == other_token)
         cell = 3 if (board.cells[0] == other_token || board.cells[5] == other_token || board.cells[8] == other_token || board.cells[4] == other_token)
+        cell = 9 if ((board.cells[2] == other_token && board.cells[7] == other_token) || (board.cells[5] == other_token && board.cells[6] == other_token))
+        cell = 7 if ((board.cells[0] == other_token && board.cells[7] == other_token) || (board.cells[3] == other_token && board.cells[8] == other_token))
+        cell = 4 if ((board.cells[0] == other_token && board.cells[8] == other_token) || (board.cells[2] == other_token && board.cells[6] == other_token))
       end
     when 5
       cell = self.pair(board)
+      if cell == nil
+        cell = 4 if (board.cells[1] == self.token || board.cells[7] = self.token)
+        cell = 2 if (board.cells[3] == self.token || board.cells[5] = self.token)
+        cell = 3 if board.cells[0] == self.token
+        cell = 1 if board.cells[2] == self.token
+      end
     when 7
       cell = self.pair(board)
+      if cell == nil
+        cell = board.cells.find_index{|mark| mark == " "}
+        cell += 1
+      end
     when 0 
       cell = 1
     when 2
       board.taken?(5) ? cell = 3 : cell = 5
     when 4
       cell = self.pair(board)
+      if cell == nil
+        cell = 3 if board.cells[3] == other_token
+        cell = 7 if board.cells[1] == other_token
+      end
     when 6
       cell = self.pair(board)
+      if cell == nil
+        if board.cells[6] == other_token
+          cell = 6
+        elsif board.cells[3] == other_token
+          cell = 8
+        else
+          cell = 4
+        end
+      end
     when 8
       cell = self.pair(board)
+      if cell == nil
+        cell = board.cells.find_index{|mark| mark == " "}
+        cell += 1
+      end
     end
     cell.to_s
   end
@@ -104,4 +134,5 @@ class Players::Computer < Player
     end
     cell
   end
+  
 end
