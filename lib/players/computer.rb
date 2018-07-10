0f8d78ae7a67d225c38e2ee3d4907d2becb26616
Amar Chatman <amarjerome@gmail.com>
2018-07-10 15:08:55 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,8 +2,14 @@ module Players
   class Computer < Player
 
   def move(board)
-    "#{rand(1...9)}"
+    if   board.turn_count == 0
+      4
+    elsif board.turn_count == 1 && board.taken?(4)
+      0
+    end
   end
+
+
 
 end
 end
