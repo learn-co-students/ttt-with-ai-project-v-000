34e4bcfdf152dbec1f838721e977125a1afecc04
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-07-13 14:18:46 +0000

    Done.

--- lib/player.rb
+++ lib/player.rb
@@ -1,4 +1,4 @@
-class Player 
+class Player
   
   attr_reader :token 
   
---
+++ lib/players/human.rb
@@ -0,0 +1,12 @@
+module Players
+  class Human < Player
+    
+    def move(board) 
+      puts ("1-9:")
+      gets.chomp
+      
+  end 
+    
+  
+  end
+end
