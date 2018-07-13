0d4edb5f701cef6642f95deba6fa0fc32856702f
Maurice Argoetti <maurice.argoetti@gmail.com>
2018-07-13 13:00:42 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -18,5 +18,13 @@ end
 
 # Using if/else/elsif statements:
 
-# 1) if AI plays first, then move to center cell
-# 2) elsif AI plays second, and the center cell is taken, then move to upper-left corner cell
+##### AI's FIRST MOVE #####
+# 1) if AI plays first, then move to center cell (4)
+# 2) elsif AI plays second, and the center cell is taken, then move to upper-left corner cell (0)
+# 3) elsif AI plays second, and the center cell is not taken, then move to center cell
+
+##### AI's SECOND MOVE #####
+# 4) elsif AI plays
+
+
+##### AI's THIRD MOVE #####
