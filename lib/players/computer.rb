45409feb64ef21aba1a8cd07f3af5948c92e5909
Maurice Argoetti <maurice.argoetti@gmail.com>
2018-07-13 12:49:31 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,6 +2,21 @@ module  Players
 
   class Computer < Player
 
+    def move(board)
+
+
+
+    end
+
+
+
   end
 
 end
+
+################## AI PLAY LOGIC ##################
+
+# Using if/else/elsif statements:
+
+# 1) if AI plays first, then move to center cell
+# 2) elsif AI plays second, and the center cell is taken, then move to upper-left corner cell
