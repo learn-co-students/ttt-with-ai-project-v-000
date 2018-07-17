a01eb2c01daa93aeaa865fc15e46b1b9b4518fb1
Leslie Stevens <flylady@mail.utexas.edu>
2018-07-17 00:21:37 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,9 +1,15 @@
 module Players
   class Computer < Player
 
+
     def move(user_input)
       user_input = rand(1..9)
-      binding.pry
+      choice = user_input.to_i - 1
+      if choice.between?(1, 9)
+        choice.to_s
+      end
+
+
     end
 
   end
