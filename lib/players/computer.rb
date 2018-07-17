b09acff7c23e62f9c31460238d88080394cd7567
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-07-17 23:35:31 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,13 +2,30 @@ module Players
   class Computer < Player
 
    
-def move(board)
-if   board.cells[0] == " "
-  "1"
+def move(board) 
+ if  board.cells[1] == " "
+   "1"
+      board.cells[2] == " "
+      "2"
+       board.cells[3] == " "
+       "3"
+        board.cells[4] == " "
+        "4"
+         board.cells[5] == " "
+         "5"
+          board.cells[6] == " "
+          "6"
+           board.cells[7] == " "
+           "7"
+            board.cells[8] == " "
+            "8"
+             board.cells[9] == " "
+             "9"
+  end
 end 
-end 
+
   
   
-  
+ 
   end
 end
