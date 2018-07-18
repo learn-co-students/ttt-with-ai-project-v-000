3092d0ac01d70e9b7a91b06ada072de7c9c05d93
Leticia Molina <letimolina@gmail.com>
2018-07-18 01:34:42 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -6,11 +6,15 @@ class Board
   end
   
   def initialize 
-    @cells = Array.new(9," ")
+    reset!
   end
   
   def display
-  
+    puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
+    puts "-----------"
+    puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
+    puts "-----------"
+    puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
   end
   
 end
