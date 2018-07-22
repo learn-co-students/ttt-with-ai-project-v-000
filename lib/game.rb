4c8cf23139a2de76e18a3deaa791b5abcafba2f6
Leticia Molina <letimolina@gmail.com>
2018-07-22 21:58:33 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -24,7 +24,7 @@ class Game
   def won?
     WIN_COMBINATIONS.detect do |combo|
     @board.cells[combo[0]] == @board.cells[combo[1]] &&
-    @board.cells[combo[1]] == @board.cells[combo[2]] &&
+    @board.cells[combo[1]] == @board.cells[combo[2]] 
     end
   end
   
@@ -36,4 +36,6 @@ class Game
     won? || draw?
   end
   
+  def winner
+  end
 end
