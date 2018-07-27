2ac4d9558f8f4f8ab5874fb4352ff15f4c14b060
Jason Mei <jason.mei403@gmail.com>
2018-07-27 03:30:20 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -48,7 +48,7 @@ class Game
   end
 
   def turn
-    puts "Please enter 1-9:"
+    puts "Please enter 1 - 9:"
     move = current_player.move(board)
     if board.valid_move?(move)
       board.update(move, current_player)
