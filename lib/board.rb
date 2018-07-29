b7dd3ad447062ccd7ce0cf9b1d0058f23d61296b
Trisha Garrett <trisha.garrett@gmail.com>
2018-07-29 22:50:49 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -3,7 +3,6 @@ require 'pry'
 class Board
   def cells=(cells)
     @cells = cells
-    #@cells = []
   end
 
   def cells
@@ -24,5 +23,16 @@ class Board
     puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
     puts " ----------- "
     puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
+
+  def position(user_input)
+    @cells[user_input.to_i - 1]
+  end
+
+  def full
+    @cells.all? do |cell|
+      if cell != " "
+
+    end
   end
 end
