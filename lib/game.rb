9b2c99e01feca1df24c24c0ccbfb8c1f90f89111
Brenden Thornton <thorntonbrenden@gmail.com>
2018-07-31 15:17:10 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -22,15 +22,15 @@ class Game
       
   end 
   
-  def turn_count(board)
-    counter = 0 
-    @board.each do |space|
-      if space == "X" || space == "O"
-        counter += 1 
-      end 
-    end 
-    return counter 
-  end 
+  # def turn_count(board)
+  #   counter = 0 
+  #   @board.each do |space|
+  #     if space == "X" || space == "O"
+  #       counter += 1 
+  #     end 
+  #   end 
+  #   return counter 
+  # end 
   
   def won?
     WIN_COMBINATIONS.detect do |combo|
@@ -60,7 +60,6 @@ class Game
       turn 
     else
       puts "Turn:#{board.turn_count+1}"
-      @board.display
       @board.update(current_move, player)
       puts "#{player.token} moved #{current_move}"
       @board.display
