fe9e4bd1b80e2f3c66d5b3c8ce8dc802bca82242
Ethel C <cofresi.ethel@gmail.com>
2018-08-03 20:49:11 +0000

    Done.

--- spec/05_computer_player_spec.rb
+++ spec/05_computer_player_spec.rb
@@ -9,9 +9,8 @@ describe 'Players::Computer' do
     it 'returns a valid position for the computer to move' do
       computer = Players::Computer.new("X")
       board = Board.new
-
+      
       valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-
       computer_move = computer.move(board)
 
       expect(valid_moves).to include(computer_move)
