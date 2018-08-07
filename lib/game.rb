24559f36b4b4eb0ff32c30102047a9a5d7c8e9e7
Adam Barton <adam.barton333@gmail.com>
2018-08-07 03:32:47 +0000

    Done.

---
+++ lib/game.rb
@@ -0,0 +1,30 @@
+class Game
+
+  WIN_COMBINATIONS = [
+   [0, 1, 2],
+   [3, 4, 5],
+   [6, 7, 8],
+   [0, 3, 6],
+   [1, 4, 7],
+   [2, 5, 8],
+   [0, 4, 8],
+   [2, 4, 6]
+  ]
+
+  attr_accessor :board
+  attr_reader :player_1, :player_2
+
+  def initialize#(player_1, player_2, board = Board.new)
+
+  end
+
+  def player_1=(player_1)
+    @player_1 = player_1
+  end
+
+  def player_2=(player_2)
+    @player_2 = player_2
+  end
+
+
+end
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,9 +1,9 @@
 module Players
   class Human < Player
 
-    def move
-    board.user_input
-
+    def move(token)
+      game = Board.new(token)#.user_input
+      input = gets.strip
     end
 
 
