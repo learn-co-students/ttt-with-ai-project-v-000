b14e34a6b4f3991842c408f2baae245d53c321d7
Adam Barton <adam.barton333@gmail.com>
2018-08-11 04:35:50 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,5 @@
 class Game
 
-  #include Countable
-
   WIN_COMBINATIONS = [
    [0, 1, 2],
    [3, 4, 5],
@@ -14,23 +12,12 @@ class Game
   ]
 
 
-  # attr_accessor :board
   attr_reader :player_1, :player_2, :board
 
    def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
      @player_1 = player_1
      @player_2 = player_2
      @board = board
-  end
-
-  def one_player_game
-    one_player = Game.new(player_1 = Players:: Human.new("X"), player_2 = Players::Computer.new("O"), board = Board.new)
-    one_player.play
-  end
-
-  def no_player_game
-    no_players = Game.new(player_1 = Players::Computer.new("X"), player_2 = Players::Computer.new("O"), board = Board.new)
-    no_plyers.play
   end
 
   def player_1=(player_1)
@@ -60,15 +47,13 @@ class Game
   end
 
   def won?
-    WIN_COMBINATIONS.detect do |win_index|
+    WIN_COMBINATIONS.detect do |winner|
 
-     position_1 = board.cells[win_index[0]]
-     position_2 = board.cells[win_index[1]]
-     position_3 = board.cells[win_index[2]]
 
-     position_1 == "X" && position_2 == "X" && position_3 == "X" ||
-     position_1 == "O" && position_2 == "O" && position_3 == "O"
-   end
+     board.cells[winner[0]] == board.cells[winner[1]] &&
+     board.cells[winner[1]] == board.cells[winner[2]] &&
+     (board.cells[winner[0]] == "X" || board.cells[winner[0]] == "O")
+    end
   end
 
   def draw?
@@ -101,22 +86,6 @@ class Game
     elsif
       draw?
         puts "Cat's Game!"
-    end
-  end
-
-  def game_type
-    selection = STDIN.gets.strip
-    until selection == "1" || "2" || "3"
-      puts "Please enter 1, 2 or 3."
-    end
-      case selection
-      when "1"
-        one_player_game
-      when "2"
-        game_1 = Game.new
-        game_1.play
-      when "3"
-        no_player_game
     end
   end
 
