f1ccad74389fa9df57fcddd02c4743a9ca9c4aa9
Przemyslaw Dabrowski <bluh@outlook.com>
2018-08-19 04:14:27 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,16 +1,36 @@
 module Players
   class Computer < Player
-    attr_accessor :game
-    def initialize(game)
-      super
-      @game = game
-    end
+    attr_accessor :board
 
-    def move
-      if @game.board.valid_move?(5)
-        return 5
+
+    def move(board)
+      @board = board
+      if @board.valid_move?(5)
+        return 5.to_s
       end
-      @game.WIN_COMBINATIONS.each 
+      Game.WIN_COMBINATIONS.each do |wc|
+        ar = [@board.cells[wc[0]], @board.cells[wc[1]], @board.cells[wc[2]]]
+        tkn = ar.uniq.delete(" ")
+        if tkn.length == 1 && ar.delete(" ").length == 2
+          wc.each do |val|
+            if @board.valid_move?(val + 1)
+              return (val + 1).to_s
+            end
+          end
+        end
+      end
+      corners = [1, 3, 7, 9]
+      corners.each do |corner|
+        if @board.valid_move?(corner)
+          return corner.to_s
+        end
+      end
+      edges = [2, 4, 6, 8]
+      edges.each do |edge|
+        if @board.valid_move?(edge)
+          return edge.to_s
+        end
+      end
+      end
     end
   end
-end
