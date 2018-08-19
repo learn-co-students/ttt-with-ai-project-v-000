a2f2859d4f08bf85c58087acd483336fc04b8108
Nathalie Santana <nathalie.scruz@gmail.com>
2018-08-19 17:28:22 +0000

    Done.

---
+++ lib/game.rb
@@ -0,0 +1,36 @@
+require 'pry'
+
+class Game
+  attr_accessor :board, :player_1, :player_2
+
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2]
+  ]
+
+  def initialize(player_1=Players::Human.new("X"), player_2=Players::Human.new("O"), board = Board.new)
+    @board = board
+    @player_1 = player_1
+    @player_2 = player_2
+  end
+
+  def current_player
+    if @board.turn_count.odd?
+      player_2
+    else
+      player_1
+    end
+  end
+
+  def won?
+    @board.cells.detect do |wc|
+
+  end
+
+end
