99ca7ceb812a0d38011e33478abb4d44a939db98
Nathalie Santana <nathalie.scruz@gmail.com>
2018-08-20 03:36:25 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,13 +2,9 @@ require 'pry'
 module Players
   class Computer < Player
 
-    def move(board)
+    def move(board) #computers game
       n = ["1", "2", "3", "4", "5", "6", "7", "8", "9"].sample
       move = n unless board.taken?(n)
     end
-
-
-
-
   end
 end
