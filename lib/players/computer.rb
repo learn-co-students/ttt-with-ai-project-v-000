c582cd0dde9de5ba130368a1ddce48a023901651
Katrina Pritchard <pritckat@gmail.com>
2018-08-24 21:23:39 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -5,7 +5,7 @@ module Players
       valid_indeces = []
       board.cells.each_with_index do |cell, index|
         if cell == " "
-          valid_indeces << index
+          valid_indeces << index + 1
         end
       end
       valid_indeces.sample.to_s
