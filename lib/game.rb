bf2f2487b508e5282913b72b003110f4690ce061
Kitty Davies <kittyfdavies@gmail.com>
2018-08-25 00:35:48 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Game
 
   attr_accessor :board, :player_1, :player_2
@@ -15,7 +17,7 @@ class Game
     [2, 4, 6] # r-to-l diagonal
   ]
 
-  def initialize(p1=Players::Human.new("X"), p2=Player.new("O"), b=Board.new)
+  def initialize(p1=Players::Human.new("X"), p2=Players::Human.new("O"), b=Board.new)
     @player_1 = p1
     @player_2 = p2
     @board = b
@@ -31,6 +33,40 @@ class Game
 
   def player_2
     @player_2
+  end
+
+  def current_player
+    if board.turn_count.odd?
+      @player_2
+    elsif board.turn_count.even?
+      @player_1
+    end
+  end
+
+  def won?
+    WIN_COMBINATIONS.detect do |win_combo|
+      if @board.cells[win_combo[0]] == @board.cells[win_combo[1]] && @board.cells[win_combo[0]] == @board.cells[win_combo[2]]
+        if @board.taken?(win_combo[0]) == true
+          return win_combo
+        end
+      end
+    end
+  end
+
+  def draw?
+    not won? and @board.full?
+  end
+
+  def over?
+    won? or draw?
+  end
+
+  def winner
+    if draw?
+      nil
+    elsif won?
+      @board.cells[won?[0]]
+    end
   end
 
 end
