0a990d743feae8d3157c28346327d27690631f3b
Mika88 <mikaspivak@gmail.com>
2018-08-28 22:27:05 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,25 +1,20 @@
 require 'pry'
 module Players
   class Computer < Player
-    def find_win(board)
-       @input = nil
-       Game::WIN_COMBINATIONS.each do |w|
-       if board.cells[w[0]] == board.cells[w[1]] && board.cells[w[0]] != " " && board.valid_move?("#{w[2] + 1}")
-            @input = "#{w[2] + 1}"
-       elsif board.cells[w[0]] == board.cells[w[2]] && board.cells[w[0]] != " " && board.valid_move?("#{w[1] + 1}")
-            @input = "#{w[1] + 1}"
-       elsif board.cells[w[1]] == board.cells[w[2]] && board.cells[w[1]] != " " && board.valid_move?("#{w[0] + 1}")
-             @input = "#{w[0] + 1}"
-       end
-     end
-     @input
-   end
-   def moves(board)
-      moves = ["5", "1", "3", "7", "9", "2", "4", "6", "8"]
-      moves.find{|p| board.valid_move?(p)}
-   end
-     def move(board)
-          moves(board)
+    def move(board)
+       input = nil
+       Game::WIN_COMBINATIONS.detect do |w|
+         if w.select{|i| board.position(i + 1) == token}.size == 2 && w.any?{|i| board.position(i + 1) == " "}
+            input = w.select{|i| !board.taken?(i + 1)}.first.to_i+(1).to_s
+         elsif w.select{|i| board.position(i + 1) != " " && board.position(i + 1) != token}.size == 2 && w.any?{|i| board.position(i + 1) == " "}
+           input = w.select{|i| !board.taken?(i + 1)}.first.to_i.+(1).to_s
+         else
+           input = ["5", "1", "3", "7", "9", "2", "4", "6", "8"].find{|i| board.valid_move?(i)} if input == nil
+         end
+       input
      end
    end
  end
+end
+
+      
