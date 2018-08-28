df324aeb1147a6500fed14a9e427ae22cb81590d
Daniel Etter <etter.dt@gmail.com>
2018-08-28 16:55:25 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,7 @@
-require 'players'
+# require 'players'
 
 class Game
-  extend Players
+  # extend Players
   attr_accessor :board, :player_1, :player_2
   
   WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
@@ -13,12 +13,11 @@ class Game
   end
   
   def current_player
-    if board.cells.count("X") == 1 && board.cells.count("O") == 1
-      return player_1
-    elsif board.cells.count("X").even?
-      return player_2
-    else board.cells.count("O").odd?
-      return player_1
+    if @board.turn_count % 2 == 0 
+      @player_1
+    else
+      @board.turn_count % 2 == 1
+        @player_2
     end
   end
   
@@ -58,19 +57,17 @@ class Game
   end
   
   def turn
-    binding.pry
+    # binding.pry
     current_move = current_player.move(board)
-    # super
-    # puts "Please enter 1-9"
-    # user_input = gets.strip
-    # user_input_index = board.user_input_to_index(user_input)
     if board.valid_move?(current_move)
-      board.update(current_move, current_player) && board.display
+      board.update(current_move, current_player)
+      board.display
     else
       puts "invalid move"
       turn
     end
   end
+  
 end
 
 Game.start
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,7 +1,7 @@
 module Players
   class Human < Player 
     
-    def move(board)
+    def move(user_input)
       puts "Please enter 1-9:"
       gets.strip
     end
