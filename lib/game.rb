dbc6ef8461823aa1902b264045bf77ca673a3ce6
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-28 19:02:06 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,5 @@
-# require 'players'
-
 class Game
-  # extend Players
+  
   attr_accessor :board, :player_1, :player_2
   
   WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
