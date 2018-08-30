7e9bcca9d40642b3f176708d6dc0f5816db56845
github username <github email address>
2018-08-29 21:37:35 -0400

    Done.

---
+++ lib/game.rb
@@ -0,0 +1,15 @@
+class Game
+
+  # Win combinations constant
+  WIN_COMBINATIONS = [
+    [0, 1, 2], # Top row
+    [3, 4, 5], # Middle row
+    [6, 7, 8], # Bottom row
+    [0, 3, 6], # Left column
+    [1, 4, 7], # Middle column
+    [2, 5, 8], # Right column
+    [0, 4, 8], # Left Diagonal
+    [2, 4, 6]  # Right Diagonal
+  ]
+
+end
