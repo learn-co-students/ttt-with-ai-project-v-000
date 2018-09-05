c8a4c418b47977211ccf8536c96762ac28809b46
Melissa Real <melissa.real@hotmail.com>
2018-09-05 17:11:53 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,7 +2,7 @@ module Players
   class Computer < Player
 
     def move(board)
-      #works but yes, its pretty dumb. To revisit.
+      #works but yes, its dumb not strategic. To revisit.
 
       range = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
       meh = range.sample
