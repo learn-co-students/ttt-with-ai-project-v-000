50b51ddc77cff98cff008a187c7ed34772b926a7
Kevin W <kjoewill@gmail.com>
2018-09-06 22:32:51 +0000

    Done.

--- lib/players/computer_brain.rb
+++ lib/players/computer_brain.rb
@@ -1,28 +1,26 @@
 class ComputerBrain
   
-  WINs = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ]
+  WINS = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ]
   
   CENTERS = [4]
   CORNERS = [0,2,6,8]
   SIDES = [1,3,5,7]  
   
-  def move (board = nil)
-    
-    return "1"
+  def move (board)
     
     if (m = find_winning_move)
       return m
     end
     
-    if (m = find_blocking_move)
+    if (m = find_blocking_move(board))
       return m
     end
     
-    if (m = find_center_move)
+    if (m = find_center_move(board))
       return m
     end
     
-    if (m = find_corner_move)
+    if (m = find_corner_move(board))
       return m
     end
     
@@ -38,33 +36,44 @@ class ComputerBrain
     nil
   end
   
-  def find_blocking_move
-    nil
+  def find_blocking_move(board)
+    combo = WINS.find { |e| near_win?(board, e) }
+    if combo.nil?
+      return nil
+    end
+    to_pos(combo.find { |e| board.cells[e] == " " })
   end
   
-  def find_center_move
-    nil
+  def find_center_move(board)
+    to_pos(CENTERS.find { |i| board.cells[i] == " " })
   end
   
-  def find_corner_move
-    nil
+  def find_corner_move(board)
+    to_pos(CORNERS.find { |i| board.cells[i] == " " })
   end
   
   def find_side_move(board)
-    to_pos([1, 3, 5, 7].find { |i| board.cells[i] == " " })
+    to_pos(SIDES.find { |i| board.cells[i] == " " })
   end
 
+  #utilities
   
+  def near_win?(board, combo)
+    combo.find_all { |i| board.cells[i] == foe_token(board) }.size == 2 &&
+    combo.find_all { |i| board.cells[i] == " " }.size == 1
+  end
   
   def to_pos(index)
-    if index.between?(0,8)
-      return (index + 1).to_s
-    else
+    if index.nil?
       return nil
+    else
+      return (index + 1).to_s
     end
   end
   
-  
+  def foe_token(board)
+    board.cells.find_all {|t| t == "X" || t == "O"}.size.even? ? "O" : "X"
+  end
   
   
 end
--- spec/computer_brain_spec.rb
+++ spec/computer_brain_spec.rb
@@ -2,30 +2,94 @@ require 'spec_helper'
 
 describe "ComputerBrain" do
   let(:brain){ComputerBrain.new}
-
-  describe '#move' do
-    it 'returns a position string "1" - "9"' do
-      expect(brain).to respond_to(:move)
-      expect(["1", "2", "3" , "4", "5", "6", "7", "8", "9"]).to include(brain.move)
-    end
-  end
+  let(:board){Board.new}
+  
 
   describe '#find_side_move' do
-    let(:board){Board.new}
-    
+
     it 'finds an open "side" cell on an empty board' do
       expect(["2", "4", "6", "8"]).to include(brain.find_side_move(board))
     end
     
     
     it 'finds an open "side" cell on a board with one side left' do
-      board.cells = ["X", "X", "X", "X", "O", "O", "X", " ", "O"]
-      
+      board.cells = [" ", "X", " ", "O", " ", "X", " ", " ", " "]
       expect(brain.find_side_move(board)).to eq("8")
     end
     
-    
+    it 'returns nil if sides already taken' do
+      board.cells = [" ", "X", " ", "X", "O", "X", "O", "X", "O"]
+      expect(brain.find_side_move(board)).to eq(nil)
+    end
+
   end
 
+  describe '#find_corner_move' do
+
+    it 'finds an open "corner" cell on an empty board' do
+      expect(["1", "3", "7", "9"]).to include(brain.find_corner_move(board))
+    end
+    
+    it 'finds an open "corner" cell on a board with one corner left' do
+      board.cells = [" ", "X", "X", "X", " ", "O", "X", " ", "O"]
+      expect(brain.find_corner_move(board)).to eq("1")
+    end
+    
+    it 'returns nil if corners already taken' do
+      board.cells = ["X", "O", "X", "O", "X", "O", "X", " ", "O"]
+      expect(brain.find_corner_move(board)).to eq(nil)
+    end
+
+
+  end
+  
+  describe '#find_center_move' do
+    it 'finds the open"center" cell on an empty board' do
+      expect(["5"]).to include(brain.find_center_move(board))
+    end
+    
+    it 'returns nil if center already taken' do
+      board.cells = [" ", "X", "X", "X", "X", "O", "X", " ", "O"]
+      expect(brain.find_center_move(board)).to eq(nil)
+    end
+  end
+  
+  describe '#find_blocking_move' do
+    
+    it 'finds move to block an opponent win if such exists' do
+      board.cells = ["X", " ", "X", " ", "O", " ", " ", " ", " "]
+      expect(brain.find_blocking_move(board)).to eq("2")
+    end
+    
+    it 'finds move to block an opponent win if such exists' do
+      board.cells = ["X", " ", "O", " ", "O", " ", "X", " ", "X"]
+      expect(brain.find_blocking_move(board)).to eq("8")
+    end
+    
+    it 'finds move to block an opponent win if such exists' do
+      board.cells = ["X", " ", "O", " ", "X", " ", " ", " ", "O"]
+      expect(brain.find_blocking_move(board)).to eq("6")
+    end
+
+  end
+  
+  #utilities
+  describe '#foe_token' do
+    it 'determines from an empty board that foe token is "O" (since current player must be "X" cause "X" goes first!)' do
+      board.cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+      expect(brain.foe_token(board)).to eq("O")
+    end
+    
+    it 'determines from a board that foe token is "X" (since current play must be "O")' do
+      board.cells = ["X", " ", " ", " ", " ", " ", " ", " ", " "]
+      expect(brain.foe_token(board)).to eq("X")
+    end
+    
+    it 'determines from a board that foe token is "X" (since its "O" turn )' do
+      board.cells = ["X", "O", "X", "O", "X", " ", " ", " ", " "]
+      expect(brain.foe_token(board)).to eq("X")
+    end
+  end
+  
   
 end
