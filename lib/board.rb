e915bfa65a666f97420a897a70152793be7f0023
CSavi <carrielane89@gmail.com>
2018-09-07 10:09:35 -0400

    finished lab

--- lib/board.rb
+++ lib/board.rb
@@ -27,6 +27,34 @@ class Board
 
   def full?
     @cells.none? {|cell| cell == " " || cell == nil} #added brackets
-
   end
+
+  def turn_count
+    count = 0 
+    @cells.each do |cell| 
+      count += 1 if cell == 'X' || cell == 'O'
+    end 
+    return count
+  end 
+
+  def taken?(x)
+    if position(x) == 'X' || position(x) == 'O'
+      return true 
+    else
+      return false
+    end 
+  end 
+
+  def valid_move?(x)
+    if x.to_i.between?(1, 9) && !taken?(x)
+      return true
+    else
+      return false 
+    end 
+  end 
+
+  def update(input, player)
+    @cells[input.to_i - 1] = player.token
+  end 
+
 end
--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,6 @@
 class Game
 
-attr_accessor :board, :player_1, :player_2
+attr_accessor :board, :player_1, :player_2, :user_input, :winner
 
 WIN_COMBINATIONS = [
   [0, 1, 2],
@@ -13,7 +13,7 @@ WIN_COMBINATIONS = [
   [2, 4, 6]
 ]
 
-  def initialize(player_1 = "X", player_2 = "O", board= Board.new)
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board= Board.new)
     @board = board
     @player_1 = player_1
     @player_2 = player_2
@@ -46,5 +46,27 @@ WIN_COMBINATIONS = [
       @winner = @board.cells[winning_combo.first]
    end
   end
+
+  def turn 
+    puts "Please enter a number 1-9"
+    @user_input  = current_player.move(@board)
+    if @board.valid_move?(@user_input)
+      @board.update(@user_input, current_player)
+    else 
+      puts "Please enter a number 1-9"
+      @board.display 
+      turn 
+    end 
+    @board.display
+  end 
+
+  def play 
+    turn until over?
+      if won?
+        puts "Congratulations #{winner}!"
+      elsif draw?
+        puts "Cat's Game!"
+      end 
+  end 
 
   end
