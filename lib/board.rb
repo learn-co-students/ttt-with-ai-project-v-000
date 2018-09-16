df0dc65debc54cf26180dd443011e03d08c90ca8
Rafael Lopez <lpz.raf@gmail.com>
2018-09-16 20:38:52 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -4,10 +4,18 @@ class Board
 
   
   def initialize
-    @cells = Array.new(9, " ")
+    reset!
   end
   
+  def display_board
+  puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+  puts "-----------"
+  puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+  puts "-----------"
+  puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+end
 
-  
-  
+def reset!
+  @cells = Array.new(9, " ")
+end
 end
