eb59c8adc1ffcf205c9f9b8791059f0b836657ea
Sharmaine Requerme <srqrme@gmail.com>
2018-09-19 21:14:28 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -41,7 +41,7 @@ class Board
    input.to_i.between?(1,9) && !taken?(input)
   end
 
-  def update(position, player)
-
+  def update(input, player)
+    cells[input.to_i - 1] = player.token
   end
 end
