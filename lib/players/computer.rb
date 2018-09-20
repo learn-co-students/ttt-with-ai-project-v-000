7a2fbd518a2c1aadc2d886a672d484a71aa50632
Marc Mills <mdmills1990@gmail.com>
2018-09-20 21:30:45 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -12,22 +12,45 @@ module Players
         move = [1,3,7,9].detect{|cell| !board.taken?(cell)}.to_s
       elsif board.turn_count == 3 && (board.position(1) == board.position(9)  || board.position(3) == board.position(7)) #human setting trap
         move = "2" #spring_trap
-      else
-        Game::WIN_COMBINATIONS.detect do |combo_wombos|
-          if combo_wombos.select{|i| board.position(i+1) == token}.size == 2 && combo_wombo.any?{|i| board.position(i+1) == " "}
-            move = combo_wombos.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
-          elsif combo_wombos.select{|i| board.position(i+1) != " " && board.position(i+1) != token}.size == 2 && combo_wombos.any?{|i| board.position(i+1) == " "}
-            move = combo_wombos.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
-          end
-        end
-        move = [1,2,3,4,5,6,7,8,9].detect{|i| !board.taken(i)}.to_s if move == nil
+      elsif board.turn_count == 3
+        move = [1,3,7,9].detect{|cell| !board.taken?(cell)}.to_s
+      elsif board.turn_count == 4 && (board.position(5) == board.position(2))
+        move = "8"
+      elsif board.turn_count == 4 && (board.position(1) == board.position(2))
+        move = "3"
+      elsif board.turn_count == 5 && (board.position(1) == board.position(7))
+        move = "4"
+      elsif board.turn_count == 5 && (board.position(2)  == board.position(5))
+        move = "8"
+      elsif board.turn_count == 5 && (board.position(3) == board.position(5))
+        move = "7"
+      elsif board.turn_count == 6 && (board.position(8) == board.position(9))
+        move = "7"
+      elsif board.turn_count == 7 && (board.position(5) == board.position(1))
+
+
+
+
+#bin/tictactoe
+        ##now I need blockers and win_combo attempts
+
       end #end of big ass conditional
     end #end of def
+    binding.pry
   end
 end
 #
 
 
+#Game::WIN_COMBINATIONS.detect do |combo|
+  #WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+
+#  if combo.select{|i| board.position(i+1) == token}.size == 2 && combo.any?{|i| board.position(i+1) == " "}
+#    move = combo.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
+#  elsif combo.select{|i| board.position(i+1) != " " && board.position(i+1) != "X"}.size == 2 && combo.any?{|i| board.position(i+1) == " "}
+#    move = combo.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
+#  end
+#end
 "Ok fucking aweseome i just lost ALL of my pseudo code writing that I had worked on for the last two hours
 but don't worry a lot of it was repetitive and got you toward a final step tree, from what I can remember it looked like this:
 
