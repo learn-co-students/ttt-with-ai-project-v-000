8d665cbbe7fa85ddedd1be056829a2a4331bfd72
Sharmaine Requerme <srqrme@gmail.com>
2018-09-25 16:47:33 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,7 +1,18 @@
+require 'pry'
+
 module Players
   class Players::Computer < Player
     def move(board)
-      computer_move = computer.move(board)
+      move = nil
+      if !board.taken?(1)
+        move = "1"
+      elsif board.turn_count == 1
+        move = "5"
+      elsif board.turn_count == 2
+        move "3"
+      elsif board.turn_count == 3
+        move = [2, 4, 6, 8].detect { |i| !board.taken?(i)}.to_s
+      end
     end
   end
 end
