de7ec8066c3d4efcae6ae9dad75a100306f7330e
Sharmaine Requerme <srqrme@gmail.com>
2018-09-27 01:47:09 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,15 +3,43 @@ require 'pry'
 module Players
   class Players::Computer < Player
     def move(board)
+      possible_win_pairs = [
+        [1, 2],
+        [2, 3],
+        [4, 5],
+        [5, 6],
+        [7, 8],
+        [8, 9],
+        [1, 4],
+        [4, 7],
+        [2, 5],
+        [5, 8],
+        [3, 6],
+        [6, 9],
+        [1, 5],
+        [5, 9],
+        [3, 5],
+        [5, 7]
+      ]
       move = nil
-      if !board.taken?('1')
-        move = '1'
+      if !board.taken?('5')
+        move = '5'
       elsif board.turn_count == 1
-        move = "5"
+        move = [1, 3, 7, 9].sample { |i| !board.taken?(i) }.to_s
       elsif board.turn_count == 2
-        move = "3"
+        move = [2, 4, 6, 8].sample { |i| board.taken?(i) }.to_s
       elsif board.turn_count == 3
-        move = [2, 4, 6, 8].detect { |i| !board.taken?(i)}.to_s
+        move = [1, 3, 4, 6, 7, 8].sample { |i| board.taken?(i) }.to_s
+      elsif board.turn_count == 4
+        move = [2, 4, 7].sample { |i| board.taken?(i) }.to_s
+      elsif board.turn_count == 5
+        move = [1, 2, 3, 7].sample { |i| board.taken?(i) }.to_s
+      elsif board.turn_count == 6
+        move = [2, 3, 9, 7].sample { |i| board.taken?(i) }.to_s
+      elsif board.turn_count == 7
+        move = [4, 6].sample { |i| board.taken?(i) }.to_s
+      elsif board.turn_count == 8
+        move = [1, 2, 3, 4, 6, 8, 9].sample { |i| board.taken?(i) }.to_s
       end
     end
   end
