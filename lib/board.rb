729f739558a8a4941381b4293052eab1bee2ff0c
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-09-29 04:50:57 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -25,27 +25,24 @@ class Board
 	end
 
 	def full?
-    cells.all? {|cell| cell == 'X' || cell == 'O'}
+    cells.all? {|char| char == 'X' || char == 'O'}
 	end
 
-	def turn_count
-    cell.count{|char| char == "X"  || char =="O"}
-	end
+  def turn_count
+    cells.count{|char| char == "X"  || char =="O"}
+  end
 
-	def taken?(input)
-	  position(input) == 'X' || position(input) == 'O'
-	end
+  def taken?(input)
+    position(input ) == 'X' || position(input) == 'O'
+  end
 
-	 def valid_move?(input)
-	   input.to_i.between(1, 9)  && taken?(inputs)
-	 end
+   def valid_move?(input)
+     input.to_i.between?(1, 9)  && !taken?(input)
 
-   def input_to_index(input)
-	   input = input.to_i   - 1
    end
 
-	 def update(input, player)
-	   cells[input.to_i -1 ] = player.token  # stands for either X OR O
-	 end
+   def update(input, player)
+     cells[input.to_i - 1 ] = player.token  # stands for either X OR O
+   end
 
    end
