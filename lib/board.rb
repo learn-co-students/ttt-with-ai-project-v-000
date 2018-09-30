030064ef11931fabd12879650650eb01b2f86ab1
meg-gutshall <meghan.gutshall@gmail.com>
2018-09-30 18:21:33 -0400

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,46 @@
+class Board
+  attr_accessor :cells
+
+  def initialize
+    @cells = Array.new(9, " ")
+  end
+
+  def reset!
+    self.cells = Array.new(9, " ")
+  end
+
+  def display
+    puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
+    puts "-----------"
+    puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
+    puts "-----------"
+    puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
+  end
+
+  def position(input)
+    position = input.to_i - 1
+    cells[position]
+  end
+
+  def full?
+    !self.cells.include?(" ")
+  end
+
+  def turn_count
+    9 - self.cells.count(" ").to_i
+  end
+
+  def taken?(input)
+    input_position = self.position(input)
+    input_position == "X" || input_position == "O"
+  end
+
+  def valid_move?(move)
+    !self.taken?(move) if move.match(/\d/)
+  end
+
+  def update(input, player)
+    cells[input.to_i - 1] = player.token
+  end
+
+end
--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,5 @@
 class Game
+  attr_accessor :board, :player_1, :player_2
 
   # Win combinations constant
   WIN_COMBINATIONS = [
@@ -11,5 +12,61 @@ class Game
     [0, 4, 8], # Left Diagonal
     [2, 4, 6]  # Right Diagonal
   ]
+
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+    @board = board
+    @player_1 = player_1
+    @player_2 = player_2
+  end
+
+  def current_player
+    board.turn_count.odd? ? player_2 : player_1
+  end
+
+  def won?
+    # THIS FUCKING SUCKEDDDD
+    win = false
+    WIN_COMBINATIONS.each do |combo|
+      win = combo if check_combo(combo)
+    end
+    win
+  end
+
+  def check_combo(combo)
+    win = false
+    poss_win = []
+    combo.each {|i| poss_win << board.cells[i]}
+    win = combo if poss_win == ["X", "X", "X"] || poss_win == ["O", "O", "O"]
+    win
+  end
+
+  def draw?
+    board.full? && !self.won?
+  end
+
+  def over?
+    board.full?
+  end
+
+  def winner
+    board.cells[self.won?&.first] if won?
+  end
+  
+  def turn
+    puts "Make your move."
+    input = current_player.move(board)
+    if !board.valid_move?(input)
+      puts "Choose a different move."
+      input = current_player.move(board)
+    else
+      board.update(input, current_player)
+    end
+  end
+
+  def play
+    turn
+  
+
+  end
 
 end
