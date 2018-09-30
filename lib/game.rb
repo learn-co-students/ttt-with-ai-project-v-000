31245e94cf579dc10cfc93507cd704683368184b
Heshie Brody <4hbrody@gmail.com>
2018-09-30 16:27:29 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Game
   WIN_COMBINATIONS  = [
     [0,1,2],
@@ -5,6 +6,39 @@ class Game
     [6,7,8],
     [0,3,6],
     [1,4,7],
-    [2,5,8]
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
   ]
+
+  attr_accessor :board, :player_1, :player_2
+
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+    @player_1 = player_1
+    @player_2 = player_2
+    @board = board
+  end
+
+  def current_player
+    board.turn_count.even? ? player_1 : player_2
+  end
+
+  def won?
+    winner = nil
+    # step through win combinations
+    WIN_COMBINATIONS.each do |x|
+
+      if board.cells[x[0]] == "X" && board.cells[x[1]] == "X" && board.cells[x[2]] == "X"
+
+        winner = x.join(",")
+      elsif board.cells[x[0]] == "O" && board.cells[x[1]] == "O" && board.cells[x[2]] == "O"
+        winner = x.join(",")
+      elsif winner != nil
+        break
+      end
+    end
+    binding.pry
+    winner
+  end
+
 end
