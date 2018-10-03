5b94a382c45be99053b1135ddc0fa4b9dd300489
Sherry Martin <sm674@cornell.edu>
2018-10-03 19:59:27 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -2,13 +2,26 @@ class Board
   
   attr_accessor
   
-  @@cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  @@cells = Array.new(9, " ")
   
   def reset!
     @@cells.clear
   end
   
-  def initialize
+  def initialize(@@cells = nil)
+    @display = display || Array.new(9, " ")
     self.reset!
+  end
   
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def position(input)
+    input.to_i - 1
+  end
 end
