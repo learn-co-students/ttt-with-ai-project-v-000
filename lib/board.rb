79cfbb7aa01e356b2c93544fff7bb524c5946fc5
Sherry Martin <sm674@cornell.edu>
2018-10-03 21:36:12 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -22,11 +22,11 @@ class Board
   end
   
   def full?
-     @cells.all?{|token| token != " " }
+     cells.all?{ |token| token != " " }
   end
   
   def turn_count
-    @cells.count{|token| token == "X" || token == "O"}
+    cells.count{ |token| token == "X" || token == "O" }
   end
   
   def taken?(input)
@@ -37,4 +37,7 @@ class Board
     input.to_i.between?(1,9) && !taken?(input)
   end  
     
+  def update(input, player)
+    cells[input.to_i-1] = player.token
+  end
 end
