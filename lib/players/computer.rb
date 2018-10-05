c862f7da5177d777d3f705296bddd26f44db3eff
meg-gutshall <meghan.gutshall@gmail.com>
2018-10-05 14:02:37 -0400

    First and second moves if the computer plays first.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,15 +2,42 @@ require_relative "../player.rb"
 
 module Players
   class Computer < Player
-    def move(board)
-      computer_move = (1..9).to_a.sample.to_s
+    @@corner_move = [1, 3, 7, 9].sample.to_s
+    @@edge_move = [2, 4, 6, 8].sample.to_s
+    @@center_move = "5"
 
-      # strategic moves
+    def move(board)
+      random_move = (1..9).to_a.sample.to_s
+      binding.pry
+
+      strategic moves
       if self.token == "X"
-        # first player strategic moves
+        first_player(board)
       else
-        # second player strategic moves
+        second_player(board)
       end
     end
+
+    def self.first_player(board)
+      # first player strategic moves
+      first_move = "5"
+      if board.cells == [" ", "O", " ", " ", "X", " ", " ", " ", " "]
+        second_move = [7, 9].sample.to_s
+      elsif board.cells == [" ", " ", " ", "O", "X", " ", " ", " ", " "]
+        second_move = [3, 9].sample.to_s
+      elsif board.cells == [" ", " ", " ", " ", "X", "O", " ", " ", " "]
+        second_move = [1, 7].sample.to_s
+      elsif board.cells == [" ", " ", " ", " ", "X", " ", " ", "O", " "]
+        second_move = [1, 3].sample.to_s
+      end
+      second_move
+
+      
+    end
+
+    def self.second_player(board)
+      # second player strategic moves
+    end
+
   end
 end
