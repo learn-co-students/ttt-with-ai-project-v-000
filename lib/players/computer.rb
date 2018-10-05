cd3f48b5302a81307375a5d2412fe10a994935fe
meg-gutshall <meghan.gutshall@gmail.com>
2018-10-05 16:34:12 -0400

    First few moves done

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -7,49 +7,42 @@ module Players
     @@center_move = "5"
     @@random_move = (1..9).to_a.sample.to_s
 
+
     def move(board)
       # strategic moves
-      if self.token == "X"
-        first_player(board)
-      elsif self.token == "O"
-        second_player(board)
-      end
+ 
+    end
+    
+    
+    def first_move_x(board)
+      "5"
     end
 
-    def first_player(board)
-      # first player strategic moves
-      first_move = "5"
+    def first_move_y(board)
+      board.cells[4] == "X" ? first_move_y = @@corner_move : first_move_y = "5"
+    end
+
+    def second_move_x(board)
       if board.cells[1] == "O"
-        second_move = [7, 9].sample.to_s
+        second_move_x = [7, 9].sample.to_s
       elsif board.cells[3] == "O"
-        second_move = [3, 9].sample.to_s
+        second_move_x = [3, 9].sample.to_s
       elsif board.cells[5] == "O"
-        second_move = [1, 7].sample.to_s
+        second_move_x = [1, 7].sample.to_s
       elsif board.cells[9] == "O"
-        second_move = [1, 3].sample.to_s
+        second_move_x = [1, 3].sample.to_s
       elsif board.cells[0] == "O"
-        second_move = "9"
+        second_move_x = "9"
       elsif board.cells[2] == "O"
-        second_move = "7"
+        second_move_x = "7"
       elsif board.cells[6] == "O"
-        second_move = "3"
+        second_move_x = "3"
       elsif board.cells[8] == "O"
-        second_move = "1"
+        second_move_x = "1"
       end
-      second_move
-      @@random_move
+      second_move_x
     end
 
-    def second_player(board)
-      # second player strategic moves
-      if board.cells[5] == "X"
-        first_move = @@corner_move
-      else
-        first_move = "5"
-      end
-      first_move
-      @@random_move
-    end
 
   end
 end
