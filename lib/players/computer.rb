4f860590f3ccc6379b16022a74c8ad6632840445
Heshie Brody <4hbrody@gmail.com>
2018-10-05 14:25:59 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,21 +2,19 @@ module Players
   class Computer < Player
 
     def move(board)
-      binding.pry
-      new_move = (1..9).to_a.sample.to_s
 
-      # new_move = [1,3,7,9].reject {|x| board.taken?(x)}
+      if board.turn_count = 0
+          new_move = ["1","3","7","9"].sample
+        elsif [0,3,6,8].detect{|x| board.cells[x] == self.token}
+
+        else
+          new_move = (1..9).to_a.sample.to_s
+          
+        end
 
       board = new_move
     end
 
-    if board.turn_count = 0
-        if [0,3,6,8].detect{|x| board.cells[x] == self.token}
-    
-        end
-      end
 
-    **if first**
-    start in a corner
   end
 end
