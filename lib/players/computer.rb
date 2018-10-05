fda6de3c7db6a5368b75e3b747bed4e1a6fbf728
meg-gutshall <meghan.gutshall@gmail.com>
2018-10-05 13:34:49 -0400

    Start coding strategic moves

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -4,6 +4,13 @@ module Players
   class Computer < Player
     def move(board)
       computer_move = (1..9).to_a.sample.to_s
+
+      # strategic moves
+      if self.token == "X"
+        # first player strategic moves
+      else
+        # second player strategic moves
+      end
     end
   end
 end
