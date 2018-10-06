434ac5c25f1a8fe3194f71ed945092cc07052b6c
Dan Hintz <dhintz@live.com>
2018-10-06 02:50:56 +0000

    Done.

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -2,7 +2,7 @@ module Players
   class Human < Player
   
   def move(board)
-    puts "Please enter a move (1-9):"
+    puts "#{@token}, please enter a move (1-9):"
     input = gets.strip
   end
   
