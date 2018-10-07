be839f9481aba0f7ae4541ae6e2db8ff2845e2c6
Dan Hintz <dhintz@live.com>
2018-10-07 18:30:22 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -5,11 +5,16 @@ module Players
     
     WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
+
+# choose the next move based on current state of the board
     def move(board)
       puts "Computer:"
       case 
 #check for winning space
-
+      when victory(board) != nil
+        if board.valid_move?(victory(board))
+          victory(board)
+        end
 
 #check for spaces to block
       when block(board) != nil
@@ -29,14 +34,29 @@ module Players
       end
     end
 
-  # choose the next move based on current state of the board
 
+#Checks for imminent victory and takes it
+    def victory(board)
+      opponent_token = find_opponent_token
+      victory_options = []
+      WIN_COMBINATIONS.each do |combo| 
+        if combo.reject {|i| board.cells[i] == @token}.length == 1
+          victory_options << combo.detect {|i| board.cells[i] == " "}
+        end
+      end
+      victory_move = victory_options.first
+      if victory_move != nil
+        (victory_move+=1).to_s
+      end
+      victory_move
+    end
+
+#Checks for imminent loss and attempts to block
     def block(board)
       opponent_token = find_opponent_token
       block_options = []
       WIN_COMBINATIONS.each do |combo| 
         if combo.reject {|i| board.cells[i] == opponent_token}.length == 1
-  # try reject if this causes problems
           block_options << combo.detect {|i| board.cells[i] == " "}
         end
       end
