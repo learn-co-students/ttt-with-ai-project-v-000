6a33bd5a975eca5208961b887744a48606086068
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-10-09 22:10:40 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -88,6 +88,25 @@ class Game
   end
 
 
+  def play
+
+    until over? == true
+      turn
+    end
+
+    if draw? == true
+      puts "Cat's Game!"
+    elsif winner == "X"
+      puts "Congratulations X!"
+    elsif winner == "O"
+      puts "Congratulations O!"
+    else
+      puts "Thanks for playing"
+    end
+
+  end
+
+
   WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]]
 
 end
