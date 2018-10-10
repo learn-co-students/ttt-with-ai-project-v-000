9be253e878bf07edbca9552c9c83534492dcbeec
Kalpana Poosarla <joyci09@gmail.com>
2018-10-10 21:15:28 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,51 @@
+class Board
+
+  attr_accessor :cells
+
+  def initialize
+   @cells = Array.new(9, " ")
+  end
+
+  def reset!
+   @cells = Array.new(9, " ")
+  end
+
+  def display
+        puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+        puts "-----------"
+        puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+        puts "-----------"
+        puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
+
+    def position(user_input)
+    cells[user_input.to_i-1]
+   end
+
+   def full?
+    cells.all?  {|index| index == "X" || index == "O"}
+   end
+
+  def turn_count
+  cells.count {|cell| cell == "X" || cell == "O"}
+  end
+
+   def taken?(num) #Returns false if the position is empty
+     num = num.to_i - 1 if num.class == String
+     !(cells[num].nil? || cells[num] == " ")
+   end
+
+   def valid_move?(input)
+     input.to_i.between?(1,9) && !taken?(input)
+   end
+
+   def update(input, player)
+    input = index(input)
+    cells[input] = player.token
+  end
+
+  def index(input)
+    input = input.to_i - 1
+  end
+
+end
---
+++ lib/game.rb
@@ -0,0 +1,79 @@
+class Game
+  attr_accessor :board, :player_1, :player_2
+
+  WIN_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+    ]
+
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+    @board = board
+    @player_1 = player_1
+    @player_2 = player_2
+  end
+
+  def current_player
+    if @board.turn_count % 2 == 0
+      player_1
+    else
+      player_2
+    end
+  end
+
+  def won?
+    WIN_COMBINATIONS.detect do |winner|
+      @board.cells[winner[0]] == @board.cells[winner[1]] && @board.cells[winner[0]] == @board.cells[winner[2]] &&
+      (@board.cells[winner[0]] == "X" || @board.cells[winner[0]] == "O")
+    end
+  end
+
+  def draw?
+    @board.full? && !won?
+  end
+
+  def over?
+    won? || draw?
+  end
+
+  def winner
+    @board.cells[won?[0]] if won?
+  end
+
+  def turn
+    player = current_player
+    current_move = player.move(@board)
+    if !@board.valid_move?(current_move)
+      turn
+    else
+      puts "Turn: #{@board.turn_count+1}\n"
+      @board.display
+      puts "Choose a number between 1-9"
+      @board.update(current_move, player)
+      puts "#{player.token} moved to position #{current_move}"
+      @board.display
+    end
+
+    def play
+      while !over?
+        turn
+      end
+      if won?
+        puts "Congratulations #{winner}!"
+      elsif draw?
+        puts "Cat's Game!"
+      end
+    end
+
+  end
+
+    #makes valid moves
+    #asks for input again after a failed validation
+    #changes to player 2 after the first turn
+
+end 
