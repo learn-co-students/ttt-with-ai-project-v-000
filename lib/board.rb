0dee22b7b8d418b5a584a8ee12cd64e1d689fb64
Nick Romans <nicholas.romans@gmail.com>
2018-10-16 23:30:01 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -2,12 +2,26 @@
 class Board
   attr_accessor :cells
   
-  def cells
-    @cells = []
+  def initialize
+    reset!
   end
   
   def reset!
-    @cells.clear
+    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   end
+  
+  def display
+  puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+  puts "-----------"
+  puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+  puts "-----------"
+  puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
+  
+  def position(user_input)
+   input = user_input.to_i - 1
+    @cells[input]
+  end
+    
   
 end
