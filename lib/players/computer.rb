9c50d95c35d2a0a116a96d7fa186b0cbb80817a8
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-10-17 00:22:25 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -7,59 +7,77 @@ module Players
     [0,3,6], [1,4,7], [2,5,8],
     [0,4,8], [2,4,6]]
 
+    CORNERS = [0, 2, 8, 6]
+
     def move(board)
       @board = board
+
       case
-      # when first_move? == true
-      #   input = 5
-    # when counter_attack != nil
-    #     input = counter_attack + 1
-      when center
-        input = 5
-      when corner
-        input = corner + 1
-      else
-        until !board.taken?(input)
-          input = (1..9).to_a.sample
-        end
+        when winning_move != nil
+          input = winning_move + 1
+        when blocking_move != nil
+          input = blocking_move + 1
+        when center?
+          input = 5
+        when opposite_corner != nil && board.taken?(opposite_corner) == false
+          input = opposite_corner
+        when corner
+          input = corner + 1
+        else
+          until !board.taken?(input)
+            input = (1..9).to_a.sample
+          end
       end
       input.to_s
     end
 
-    def first_move?
-      board.cells == []
+    def other
+      token == "X" ? "O" : "X"
     end
 
-    def counter_attack # when player has 2 of 3 win combination, move to block
-# binding.pry
-      block_now = WIN_COMBINATIONS.detect do |win_com| #win_com is an array of indicies... make an array. iterate over win_com and .collect to be able to compare arrays
-        # binding.pry
-        (board.cells[win_com[0..2]] == ["X","X"," "] || board.cells[win_com[0..2]] == ["X"," ","X"] || board.cells[win_com[0..2]] == [" ","X","X"]) ||
-         (board.cells[win_com[0..2]] == ["O","O"," "] || board.cells[win_com[0..2]] == ["O"," ","O"] || board.cells[win_com[0..2]] == [" ","O","O"])
+    def winning_move
+      winning_row = WIN_COMBINATIONS.find do |win_com|
+        (board.cells[win_com[0]] == token && board.cells[win_com[1]] == token && board.cells[win_com[2]] == " ") || (board.cells[win_com[2]] == token && board.cells[win_com[1]] == token && board.cells[win_com[0]] == " ") || (board.cells[win_com[0]] == token && board.cells[win_com[2]] == token && board.cells[win_com[1]] == " ")
       end
-      if block_now != nil
-        block_move = block_now.detect {|w_c_p| board.cells[w_c_p] == " "}
+      if winning_row != nil
+        winning_cell = winning_row.find {|cell| board.cells[cell] == " "}
       end
-      # block_move
     end
 
-    def center # if empty, or first_move take center square unless counter_attack is needed
-      board.cells[4] == " " || first_move?
+    def blocking_move
+      winning_row = WIN_COMBINATIONS.find do |win_com|
+        (board.cells[win_com[0]] == other && board.cells[win_com[1]] == other && board.cells[win_com[2]] == " ") || (board.cells[win_com[1]] == other && board.cells[win_com[2]] == other && board.cells[win_com[0]] == " ") || (board.cells[win_com[2]] == other && board.cells[win_com[0]] == other && board.cells[win_com[1]] == " ")
+      end
+      if winning_row != nil
+        winning_cell = winning_row.find {|cell| board.cells[cell] == " "}
+      end
     end
 
-    def edge # if corner = false, move to any edge space
+    def center?
+      board.cells[4] == " "
     end
 
-    def corner # if center = false, move to any corner space
-      corners = [0, 2, 6, 8]
-      corners.detect {|corner| board.cells[corner] == " "}
+    def corners
+      CORNERS.shuffle!
     end
 
+    def corner
+      corners.find {|corner| board.cells[corner] == " "}
+    end
 
-
-
-
-
-
+    def opposite_corner
+      case
+        when board.taken?(1) && !board.taken?(9)
+          9
+        when board.taken?(9) && !board.taken?(1)
+          1
+        when board.taken?(3) && !board.taken?(7)
+          7
+        when board.taken?(7) && !board.taken?(3)
+          3
+        else
+          nil
+      end
+    end
   end
 end
