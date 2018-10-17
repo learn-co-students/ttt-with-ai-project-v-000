a30d8d9a5d57556a1297fc10fb405061e9b5cc2c
Nick Romans <nicholas.romans@gmail.com>
2018-10-17 16:38:44 +0000

    Done.

---
+++ lib/game.rb
@@ -0,0 +1,15 @@
+
+class Game
+  
+  WIN_COMBINATIONS = [
+  [0,1,2], # top row
+  [3,4,5], # middle row
+  [6,7,8], # bottom row
+  [0,3,6], # left row
+  [1,4,7], # vertical middle row
+  [2,5,8], # right row
+  [0,4,8], # top left diag
+  [2,4,6]  # top right diag
+  ]
+  
+end
