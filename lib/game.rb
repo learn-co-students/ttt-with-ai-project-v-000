dc1d6377035270fbf45a3e8b80a64ea979c2433d
Samantha brake <sammy.brake@gmail.com>
2018-10-17 13:10:56 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -99,17 +99,21 @@ def position_taken?(index)
   !(@board.cells[index].nil? || @board.cells[index] == " ")
 end
 
-def turn
+def turn 
   puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
+end 
+
+# def turn
+#   puts "Please enter 1-9:"
+#   input = gets.strip
+#   index = input_to_index(input)
   
-  if valid_move?(index)
-    move(index, current_player)
-    display_board
-  else
-    turn
-  end
-end
+#   if valid_move?(index)
+#     move(index, current_player)
+#     display_board
+#   else
+#     turn
+#   end
+# end
 
 end 
