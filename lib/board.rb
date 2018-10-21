4c38d22a36c4b0213e4ebaafcd7d93ffdd88aebe
Ashley Wong <ashwong04@gmail.com>
2018-10-21 23:44:36 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -11,4 +11,20 @@ class Board
     9.times {@cells << " "}
   end
   
+  def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end  
+  
+  def position(input)
+    @cells[input.to_i-1]
+  end
+  
+  def full?
+    
+  end
+
 end
