8273ebe2f799cbe78cae2056f5ab74c09e6d8973
Ashley Bland <ashleyjbland@gmail.com>
2018-10-22 21:49:10 +0000

    Done.

---
+++ lib/players/computer.rb
@@ -0,0 +1,11 @@
+class Players
+  class Computer < Player
+
+    def move(board)
+      @board = board
+      "3"
+
+    end
+
+  end
+end
---
+++ lib/players/human.rb
@@ -0,0 +1,9 @@
+class Players
+  class Human < Player
+
+  def move(board)
+    puts "Please enter 1-9:"
+    gets.chomp
+  end
+end
+end
