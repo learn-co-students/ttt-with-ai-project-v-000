44f8b2fbd78b74ee3591623a2b3454e681eb6b44
Ashley Wong <ashwong04@gmail.com>
2018-10-23 13:11:57 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require_relative './players/human.rb'
 
 class Game
@@ -13,6 +14,48 @@ class Game
   end
   
   def current_player
+    counter = 0
+    @board.cells.each do |count|
+      if count == "O" || count == "X"
+      counter += 1
+      end
+    end
+    if counter.even?
+      @player_1
+   elsif !counter.even?
+      @player_2
+    end
+  end
+  
+  def won?
+    WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+    
+    position_1 = @board.cells[win_index_1]
+    position_2 = @board.cells[win_index_2]
+    position_3 = @board.cells[win_index_3]
+    
+      if  (position_1 == "X" || position_1 == "O") && position_1 == position_2 && position_1 == position_3
+         return win_combination
+      end     
+    end
+    return false
+  end
+  
+  def draw?
+    ongoing_game = @board.cells.any? {|cell| cell == " "}
+    if !ongoing_game && !won?
+      return "Cat's game!"
+    end
+  end
+  
+  def over?
+    draw? || won?
+  end
+  
+  def winner
     
   end
   
