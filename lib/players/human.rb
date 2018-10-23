2c9ade17e977839214b21aa26075eec57d347497
Ashley Wong <ashwong04@gmail.com>
2018-10-23 00:55:05 +0000

    Done.

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,10 +1,12 @@
+require 'pry'
 require_relative '../player.rb'
 
 module Players
   class Human < Player
   
-    def move(board)
-      puts board
+    def move(input)
+      puts "Please enter 1-9:"
+      input = gets.strip
     end
   
   end
