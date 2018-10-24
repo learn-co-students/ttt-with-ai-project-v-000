c621ddf1493b265b605306de4138cfd26cc3a5df
Ashley Wong <ashwong04@gmail.com>
2018-10-24 13:19:21 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,2 +1,14 @@
-require_relative '../player.rb.rb'
+require_relative '../player.rb'
 
+module Players
+  class Computer < Player
+    
+     def move(position)
+      position = rand(1..9)
+      if valid_move?(position)
+        position
+      end
+    end
+    
+  end
+end
