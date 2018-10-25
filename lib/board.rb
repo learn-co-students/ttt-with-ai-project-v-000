f2f7bcebd400c58f8ba776bc3c95c91180d4d15d
Cheryl K <cherylkelson@icloud.com>
2018-10-25 18:09:29 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -21,9 +21,30 @@ class Board
     puts " #{cells[3]} | #{cells[4]} | #{cells[5]} " 
     puts "-----------"
     puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
-    
   end 
   
+  def position(input) 
+    cells[input.to_i-1]
+  end 
   
+  def full? 
+    cells.all? { |token| token == "X" ||  token == "O"}
+  end 
   
-end 
+  def turn_count 
+    count = 9 - @cells.count(" ")
+  end 
+  
+  def taken?(input)
+    !(position(input) == " " || position(input) == " ")
+  end
+  
+  def valid_move?(input) 
+    input.to_i.between?(1,9) && !taken?(input)
+  end 
+  
+  def update(input, player)  
+    cells[input.to_i-1] = player.token
+  end 
+  
+end
