e6f1126213a7bbd9feab2f3b9dcfd22f1c044898
sharonrieger <sharonarieger@gmail.com>
2018-10-26 07:22:07 -0400

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -3,6 +3,11 @@ require 'pry'
 class Game 
     attr_accessor :board, :player_1, :player_2
 
+    #----WIN_COMBINATIONS---------------
+       WIN_COMBINATIONS = [
+        [0,1,2], [3,4,5], [6,7,8], [0,3,6],[1,4,7], [2,5,8], [0,4,8], [6,4,2]
+    ]
+    
     # --INITIALIZE--
     # new games gets a new board and 2 human players with default values of "X" and "O".
     def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
@@ -11,7 +16,7 @@ class Game
         @board = board
     end
 
-    # --START--
+    #-----START---------------
     def start
         puts "Welcome to Tic Tac Toe!"
         
@@ -37,77 +42,14 @@ class Game
            new_game.play
 
         # 0 == COMPUTER against COMPUTER.
-        else 
-            input == 0
+        elsif input == 0
             new_game = Game.new(Players::Computer.new("X"), Players::Computer.new("O"))
             new_game.play
-        end 
-        
+        end    
     end
 
-    # --WIN_COMBINATIONS--
-    WIN_COMBINATIONS = [
-        [0,1,2], [3,4,5], [6,7,8], [0,3,6],[1,4,7], [2,5,8], [0,4,8], [6,4,2]
-    ]
-    
-    # --CURRENT_PLAYER--
-    # returns the correct player. We use modulo to see which player is odd or even
-    def current_player
-        board.turn_count % 2 == 0 ? @player_1 : @player_2
-    end
-
-     #--WON?--
-    # iterates over board to detect a WIN_COMBINATIONS match and increments by 1 to the next array in WIN_COMBINATIONS until we DETECT a match.
-    def won?
-        WIN_COMBINATIONS.detect do |win_combo|
-          @board.cells[win_combo[0]] == @board.cells[win_combo[1]] &&
-          @board.cells[win_combo[1]] == @board.cells[win_combo[2]] && 
-          @board.taken?(win_combo[0] + 1)
-        end
-    end
-
-    # --WINNER--
-    # check board to see if game has been won. If so, show the winning combination and winner's token.
-    def winner
-       if won?
-            @board.cells[won?[0]]
-       else
-            nil
-       end
-    end
-
-    # --DRAW?--
-    # if board is full and no winner, it's a draw
-    def draw?
-       @board.full? && !won? 
-    end
-
-    # --OVER--
-    # game is over if there is a draw or if someone has won.
-    def over?
-        draw? || won?
-    end
-
-    # --TURN--
-    def turn
-        current_move = current_player.move(board)
-
-        if !board.valid_move?(current_move)
-            turn
-            # when a #valid_move is made, execute what's below. 
-        else
-            #
-            puts "Turn: #{board.turn_count+1}\n"
-                board.display
-             board.update(current_move, current_player)
-            puts "#{current_player.token} moved #{current_move}\n"
-             board.display
-            puts "\n\n"
-        end
-    end
-
-    # --PLAY--
-    # continue to play by executing #turn until game is #over and if someone has won, puts a "congratulations". Or if #draw, puts "Cat's game"
+     # -----PLAY----------------
+    # continue with #turn until game is #over by #draw or #won
     def play 
         until over?
         turn
@@ -117,6 +59,63 @@ class Game
         elsif draw?
             puts "Cat's Game!"
         end
+    end
+
+    # -----CURRENT_PLAYER-----------
+    # returns the correct player. We use modulo to see which player is odd or even
+    def current_player
+        board.turn_count % 2 == 0 ? @player_1 : @player_2
+    end
+
+    #-------TURN-----------
+    def turn
+        player_current = current_player
+        current_move = current_player.move(board)
+        
+        if !board.valid_move?(current_move)
+            turn
+            # when a #valid_move is made, execute what's below. 
+        else
+            #
+            puts "Turn: #{board.turn_count+1}\n"
+                board.display
+             board.update(current_move, current_player)
+            puts "#{player_current.token} moved #{current_move}\n"
+             board.display
+            puts "\n\n"
+        end
+    end
+
+     #------WON?----------
+    # iterates over board to detect a WIN_COMBINATIONS match and increments by 1 to the next array in WIN_COMBINATIONS until we DETECT a match.
+    def won?
+        WIN_COMBINATIONS.detect do |win_combo|
+          @board.cells[win_combo[0]] == @board.cells[win_combo[1]] &&
+          @board.cells[win_combo[1]] == @board.cells[win_combo[2]] && 
+          @board.taken?(win_combo[0] + 1)
+        end
+    end
+
+    # -----DRAW?---------
+    # if board is full and no winner, it's a draw
+    def draw?
+        @board.full? && !won? 
+     end
+
+    #-----OVER----------
+    # game is over if there is a draw or if someone has won.
+    def over?
+        draw? || won?
+    end
+
+    #----WINNER--------------
+    # check board to see if game has been won. If so, winning comb and winning token.
+    def winner
+       if won?
+            @board.cells[won?[0]]
+       else
+            nil
+       end
     end
 
 end
--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,18 +3,39 @@ require 'pry'
 module Players
 
     class Computer < Player
+        # BEST MOVE (center square)
+        # computer == [4], human position == "5" 
 
-        corners = [1, 3, 7, 9]
+        # SECOND_BEST_MOVES (corner squares)
+        # computer == [0, 2, 6, 8], humman == "1", "3", "7" , "9"
+      
+        # LEAST_FAVORABLE_MOVES
+        # computer == [1, 3, 5, 7], humman == "2", "4", "6" , "8"
 
-        # --MOVE--
-        #selects a random number between 1 - 9, not 0 - 8.
-        def move(position)
-            position = rand(1..9).to_s 
+          #---VALID_MOVE?-------
+        def valid_move?(input)
+            input.to_i.between?(1,9) && !taken?(board)
         end
+        
+        #----MOVE----------
+        def move(board)
+            squares = [[5],[1, 3, 7, 9], [2, 4, 6, 8]]
 
-         # --VALID_MOVE?--
-         def valid_move?(input)
-            input.to_i.between?(1,9) && !taken?(input)
+            #center square
+            best_move = "#{squares[0][0]}" #using interpolation here is 
+            #the same as converting to_s
+            # corners
+            second_best_moves = "#{squares[1].sample}" 
+            least_favorable_moves = "#{squares[2].sample}"
+       
+            if !board.taken?(best_move)
+                best_move
+            elsif !board.taken?(second_best_moves)
+                second_best_moves
+            else 
+                "#{rand(1..9)}" #same as converting to_s
+            end
         end
     end
 end
+
