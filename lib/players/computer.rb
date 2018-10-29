d3977931d6fbbe106a8042cce743dc4b87c95483
Cheryl K <cherylkelson@icloud.com>
2018-10-29 18:27:25 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,21 +3,14 @@ module Players
   class Computer < Player 
     
   def move(board, game = nil)
-      if board.turn_count = 0 
-        return "5" 
-      end 
+    my_answer = (1..9).to_a.sample.to_s 
     
+    until board.valid_move?(my_answer)
       my_answer = (1..9).to_a.sample.to_s 
-  
-      until board.valid_move?(my_answer) 
-        my_answer = (1..9).to_a.sample.to_s 
-      
-      end 
-    
-      my_answer
-      
     end 
-  end 
-end
-  
-  
+  my_answer
+      end
+    end 
+end 
+    
+     
