a8b8786dc9aa93ceb3f0109169eadd58b73275fb
Vince Andrew Pimentel <contact@vincep.me>
2018-11-01 09:09:39 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,11 +1,12 @@
 module Players
   class Computer < Player
     def move(board)
-      #Random.new.rand(10)
-      #AI Rules
-      #Always start at center (4) if possible, else any corner (0, 2, 6, 8). Edges lowest priority (1, 3, 5, 7).
-
 
     end
   end
 end
+
+
+#Center: 4
+#Edge: 1, 3, 5, 7
+#Corner: 0, 2, 6, 8
