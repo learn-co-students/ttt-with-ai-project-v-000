b943770bf42df76515c2ba0499f9273e3058a2a0
Vince Andrew Pimentel <contact@vincep.me>
2018-11-01 21:41:25 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,33 +3,60 @@ module Players
     def move(board)
       #If player_1 (X)
       if self.token == "X"
-        #Take CENTER on first turn
-        if board.turn_count == 0
-          "5"
-        end
+        #Save player_2's moves
+        o_first = nil
+        o_second = nil
+        o_third = nil
+        o_fourth = nil
+        #On first turn, take CENTER
+        if board.turn_count == 0 then "5"; end
 
         if board.turn_count == 2
-          #If any CORNER contains "O"
-          if board.cells.values_at(0, 2, 6, 8).include?("O")
+          #EDGE CASES
+          if board.cells.values_at(1, 3, 5, 7).include?("O")
             case board.cells.find_index("O")
-            when 0 then "9"
-            when 2 then "7"
-            when 6 then "3"
-            when 8 then "1"
+            #If player_2 takes EDGE then take a far CORNER
+            when 1 then o_first = 1; "7"
+            when 3 then o_first = 3; "9"
+            when 5 then o_first = 5; "1"
+            when 7 then o_first = 7; "3"
             end
           end
 
-          #If any EDGE contains "O"
-          if board.cells.values_at(1, 3, 5, 7).include?("O")
+          #CORNER CASES
+          if board.cells.values_at(0, 2, 6, 8).include?("O")
             case board.cells.find_index("O")
-            when 1 then "7"
-            when 3 then "9"
-            when 5 then "1"
-            when 7 then "3"
+            #If player_2 takes CORNER then take opposite CORNER
+            when 0 then o_first = 0; "9"
+            when 2 then o_first = 2; "7"
+            when 6 then o_first = 6; "3"
+            when 8 then o_first = 8; "1"
             end
           end
         end
 
+        if board.turn_count == 4
+          case o_first
+          #EDGE CASES (if player_2 DEFENDs then COUNTER-ATK else WIN)
+          when 1 then board.cells[2] == "O" ? "1" : "3"
+          when 3 then board.cells[0] == "O" ? "7" : "1"
+          when 5 then board.cells[8] == "O" ? "3" : "9"
+          when 7 then board.cells[6] == "O" ? "9" : "7"
+          #CORNER CASES
+          #CORNER-TO-CLOSE-EDGE CASE
+          #If player_2 takes close EDGE then COUNTER-ATK)
+          when 0
+            if    board.cells[1] == "O"; o_second = 1; "3"
+            elsif board.cells[3] == "O"; o_second = 3; "7"; end
+          when 2
+            if    board.cells[1] == "O"; o_second = 1; "1"
+            elsif board.cells[5] == "O"; o_second = 5; "9"; end
+          when 6
+          when 8
+          end
+
+
+        end
       end
     end
   end
