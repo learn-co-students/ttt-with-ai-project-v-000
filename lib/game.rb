df5bbd501161c37d90ddc897963ed3c7938e4b1e
Vince Andrew Pimentel <contact@vincep.me>
2018-11-01 06:34:08 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -21,6 +21,36 @@ class Game
   end
 
   def current_player
+    board.turn_count.even? ? player_1 : player_2
   end
 
+  def won?
+    WIN_COMBINATIONS.each do |combo|
+      if board.cells[combo[0]] == board.cells[combo[1]] && board.cells[combo[0]] == board.cells[combo[2]] && board.taken?(combo[0])
+        return combo
+      end
+    end
+    return false
+  end
+
+  def draw?
+    !won? && board.full? ? true : false
+  end
+
+  def over?
+    won? || draw? || board.full? ? true : false
+  end
+
+  def winner
+    if board.cells[won?[0]].include?(player_1.token)
+      return player_1.token
+    else
+      return player_2.token
+    end
+    binding.pry
 end
+
+#LOSE               WIN
+#["X", "O", "X"]    ["X", "O", "X"]
+#["O", "X", "X"]    ["O", "O", "X"]
+#["O", "X", "O"]    ["O", "X", "X"]
