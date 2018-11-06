447513a087668cbed604a59e2ea5a5be2318d785
Stef Claus <stef.claus@gmail.com>
2018-11-06 03:28:01 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,31 +1,43 @@
 class Board
-  
-  def cells 
-  end 
-  
+
+  attr_accesor :board
+
+  def board=(board)
+     @board =  [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+   end
+
+   def cells
+     @board
+  end
+
   def reset!
   end
-  
-  def display #shows current state 
-  end 
-  
+
+  def display #shows current state
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
   def position #takes uers input inform of 1-9
-  end 
-  
-  def update 
-  end 
+  end
+
+  def update
+  end
 
   def token
-  end 
-  
+  end
+
   def full?
-  end 
-  
-  def turn_count 
-  end   
+  end
+
+  def turn_count
+  end
 
   def valid_move?
-  end 
+  end
 
-  
-end #ends class 
+
+end #ends class
