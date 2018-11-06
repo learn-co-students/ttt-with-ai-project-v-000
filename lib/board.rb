76b2ac2dddb8d5813601590393d757a4baa65b18
Stef Claus <stef.claus@gmail.com>
2018-11-06 03:10:53 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,31 @@
+class Board
+  
+  def cells 
+  end 
+  
+  def reset!
+  end
+  
+  def display #shows current state 
+  end 
+  
+  def position #takes uers input inform of 1-9
+  end 
+  
+  def update 
+  end 
+
+  def token
+  end 
+  
+  def full?
+  end 
+  
+  def turn_count 
+  end   
+
+  def valid_move?
+  end 
+
+  
+end #ends class 
