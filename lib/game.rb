4eddd9e80ceb5be717532ebfe35c7b96bcb752ec
Stef Claus <stef.claus@gmail.com>
2018-11-17 23:58:38 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -31,6 +31,22 @@ WIN_COMBINATIONS = [
    end
  end
 
+ def won?
+   WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
 
+    position_1 = @board[win_index_1]
+    position_2 = @board[win_index_2]
+    position_3 = @board[win_index_3]
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"
+         return win_combination
+      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+         return win_combination
+   	  end # end if
+      end # end each
+    false
+ end
 
 end
