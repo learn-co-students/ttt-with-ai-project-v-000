98b8e3f64c43f140bc411c1a69726f13e8db32d6
tgray017 <tgray017@gmail.com>
2018-11-23 20:17:05 +0000

    Done.

---
+++ lib/player.rb
@@ -0,0 +1,9 @@
+class Player
+  attr_reader :token
+  
+  def initialize
+    @token = 'X'
+  end
+  
+  
+end
---
+++ lib/players/computer.rb
@@ -0,0 +1,10 @@
+module Players
+  class Computer < Player 
+    
+    def move(board)
+      #position = something
+      #position
+    end
+    
+  end 
+end 
