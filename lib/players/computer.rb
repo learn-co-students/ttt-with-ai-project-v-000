99738298602d7d6a0401aebc142541e906cb1ab6
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-11-25 17:22:21 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,28 +1,28 @@
 module Players
   class Computer < Player
-    
-    def move(board) 
-      if board.cells[4] = " "
+
+    def move(board)
+      if board.cells[4] == " "
         return "5"
-      elsif board.cells[0] = " "
+      elsif board.cells[0] == " "
         return "1"
-      elsif board.cells[2] = " "
+      elsif board.cells[2] == " "
         return "3"
-      elsif board.cells[6] = " "
+      elsif board.cells[6] == " "
         return "7"
-      elsif board.cells[8] = " "
+      elsif board.cells[8] == " "
         return "9"
-      elsif board.cells[1] = " "
+      elsif board.cells[1] == " "
         return "2"
-      elsif board.cells[3] = " "
+      elsif board.cells[3] == " "
         return "4"
-      elsif board.cells[5] = " "
+      elsif board.cells[5] == " "
         return "6"
-      elsif board.cells[7] = " "
+      elsif board.cells[7] == " "
         return "8"
-      end 
-    end 
-    
-    
-  end 
+      end
+    end
+
+
+  end
 end 
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,6 +1,7 @@
 module Players
   class Human < Player
     
+    
     def move(board) 
       x = gets.strip 
     end 
