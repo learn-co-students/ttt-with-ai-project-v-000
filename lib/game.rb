6edf672dc283611f693554b80be998775c4ffb16
tgray017 <tgray017@gmail.com>
2018-11-30 12:23:04 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -62,7 +62,47 @@ class Game
   end
   
   def start
-    
+    puts "Welcome to Tic Tac Toe!"
+    puts "Which game would you like to play?"
+    puts "Enter 0 for the computer to play itself."
+    puts "Enter 1 for you to play the computer."
+    puts "Enter 2 for a two-player game."
+    game_type = gets.strip
+    while game_type != "1" && game_type != "2" && game_type != "3"
+      puts "Invalid input."
+      game_type = gets.strip
+    end
+    puts "Which player should go first--X or O?"
+    first_token = gets.strip.upcase
+    while first_token != "X" && first_token != "O"
+      puts "Invalid input."
+      first_token = gets.strip.upcase
+    end
+    if first_token == "X"
+      second_token = "O"
+    else
+      second_token = "X"
+    end
+    case game_type
+      when "0" then 
+        game = self.new(player_1 = Players::Computer.new(first_token), player_2 = Players::Computer.new(second_token), board = Board.new)
+      when "1" then
+        puts "Who should go first--you or the computer?"
+        puts "Enter '1' to go first or '2' for the computer to go first."
+        first_player = gets.strip.upcase
+        while first_player != "1" && first_player != "2"
+          puts "Invalid input."
+          first_player = gets.strip.upcase
+        end
+        if first_player == "1"
+          game = self.new(player_1 = Players::Human.new(first_token), player_2 = Players::Computer.new(second_token), board = Board.new)
+        else
+          game = self.new(player_1 = Players::Computer.new(first_token), player_2 = Players::Human.new(second_token), board = Board.new)
+        end
+      when "2" then
+        game = self.new(player_1 = Players::Human.new(first_token), player_2 = Players::Human.new(second_token), board = Board.new)
+    end
+    game.play
   end
   
   def play
@@ -75,6 +115,10 @@ class Game
     elsif draw?
       puts "Cat's Game!"
     end
+  end
+  
+  def start
+    
   end
   
   
@@ -90,6 +134,7 @@ class Game
 
   def multiple_losing_combos
     #need a method for computer to take the position on the board where there is > 1 losing combination if the opposing player were to take that position
+    #maybe refactor the #almost_lost method to #collect all losing combos and use this method to #detect which one to use and which position to take
   end
   
   def multiple_winning_combos
