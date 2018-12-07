d0957816ff2b25fbb37bd048aec5eb4e8691ad32
Paul Fell-Cinoman <swellpf@gmail.com>
2018-12-07 18:02:08 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,9 @@
 class Game
   
-  attr_accessor :board, :player_1, :player_2
+  attr_accessor :board, :player_1, :player_2, :token
   
   WIN_COMBINATIONS=[
-    [0,1,2], [3,4,5], [6,7,8],[0,3,6], [1,4,7], [2,5,8],[0,4,8], [2,4,6]
+    [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]
     ]
   
   def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
@@ -13,6 +13,46 @@ class Game
   end
 
   def current_player
-  
+    if board.turn_count.even? 
+        current_player = player_1
+      else
+        current_player = player_2
+      end
   end 
+  
+  def won?
+    WIN_COMBINATIONS.each do |combination|
+         if board.cells[combination[0]] == board.cells[combination[1]] && board.cells[combination[1]] == board.cells[combination[2]] 
+              if board.cells[combination[2]] != " "
+                  return combination
+                else
+                  return nil
+         end
+       end
+    end
+     !board.full?
+    end
+    
+    def draw?
+      won? == false && board.full? 
+    end
+    
+    def over?
+      draw? == true || won?.kind_of?(Array) == true
+    end
+    
+    def winner
+      win_combo = won?
+      board.cells[win_combo[0]] if win_combo != nil
+    end
+    
+    def turn
+    # binding.pry
+      current_player.move(move)
+     # binding.pry
+    #  if board.valid_move?(move)
+     #   board.update(input, current_player) 
+    #  else self.turn
+     # end
+    end
 end
