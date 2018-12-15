ffed2452f3f493b72627227bd0a5c1f887f28ef6
Paul Fell-Cinoman <swellpf@gmail.com>
2018-12-15 01:57:33 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -8,13 +8,13 @@ module Players
         move = "1"
       elsif board.turn_count == 2
         move = [1, 3, 7, 9].detect{|i| !board.taken?(i)}.to_s
-      elsif board.turn_count == 3 && board.position(1) == board.position(9)  || board.position(3) == board.position(7)
+      elsif board.turn_count == 3 && (board.position(1) == board.position(9)  || board.position(3) == board.position(7))
         move = "2"
       else
         Game::WIN_COMBINATIONS.detect do |combo|
         if combo.select{ |i| board.position(i+1) == token}.size == 2 && combo.any?{ |i| board.position(i+1) == " " }
           move = combo.select{ |i| !board.taken?(i+1)}.first.to_i.+(1).to_s
-          elsif combo.select{ |i| board.position(i+1) !=" " && board.position(i+1) != token}.size == 2 && combo.any?{ |i| board.position(i+1) == " "}
+          elsif combo.select{|i| board.position(i+1) !=" " && board.position(i+1) != token}.size == 2 && combo.any?{ |i| board.position(i+1) == " "}
           move = combo.select{ |i| !board.taken?(i+1)}.first.to_i.+(1).to_s
         end
         end
