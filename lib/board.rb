fdbf61cf023196d98dfddcf38762187cefd8fc09
Lois Lee <lois.lee@me.com>
2019-01-02 21:16:33 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -11,23 +11,23 @@ class Board
   end
 
   def display
-    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
     puts "-----------"
-    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
     puts "-----------"
-    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+    puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
   end
 
   def full?
-    !@cells.include?(" ")
+    !cells.include?(" ")
   end
 
   def turn_count
-    @cells.count{|token| token == "X" || token == "O"}
+    cells.count{|token| token == "X" || token == "O"}
   end
 
   def position(input)
-    @cells[input.to_i-1]
+    cells[input.to_i-1]
   end
 
   def taken?(input)
@@ -41,4 +41,5 @@ class Board
   def update(input, player)
     cells[input.to_i-1] = player.token
   end
+
 end
