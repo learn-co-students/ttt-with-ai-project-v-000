15325431c84042297e033b5c2c0edc8b1ebbc2de
Ashleigh Chun <ashleigh.chun@wework.com>
2019-01-11 00:26:38 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -5,7 +5,7 @@ module Players
     def move(board)
       
       valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-      computer_move = board.valid_move?(valid_moves.to_i)
+      computer_move = rand(1..9)
       
     end 
   end
