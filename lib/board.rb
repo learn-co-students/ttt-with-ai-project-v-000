893c3dbefa28910108da02357187737c1ecdacaf
Sara Walker <sarawall1187@gmail.com>
2019-01-22 23:31:44 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,6 @@
 require "pry"
 class Board 
-  attr_accessor :cells
+  attr_accessor :cells, :token, :player
   
   def initialize
     reset!
@@ -19,29 +19,32 @@ class Board
   end
   
   def position(user_input)
-   @cells[user_input.to_i - 1] 
+   cells[user_input.to_i - 1] 
   end
   
   def full?
-  @cells.all? {|cell| cell == "X" || cell == "O"}
+  cells.all? {|cell| cell == "X" || cell == "O"}
   end
   
   def turn_count
   turns = 0
-  @cells.each do |cell|
+  cells.each do |cell|
     if cell == "X" || cell == "O"
       turns += 1
     end
    end
    turns
   end
-   
+  
    def taken?(user_input)
-     @cells[user_input.to_i - 1] == "X" || @cells[user_input.to_i - 1] == "O"
+    cells[user_input.to_i - 1] == "X" || cells[user_input.to_i - 1] == "O"
    end
    
-   def valid_move?(user_input) 
-    @cells[user_input.to_i - 1].between?(0,8) && !taken?(user_input)
+   def valid_move?(user_input)
+    !taken?(user_input.to_i) && user_input.to_i.between?(1, 9)  
   end
   
+  def update(user_input, player)
+   cells[user_input.to_i - 1]
+  end
  end
