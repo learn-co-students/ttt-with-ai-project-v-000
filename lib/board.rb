23cb66e131c69c6ac3a1c6d282d223a6e4507ec0
Sara Walker <sarawall1187@gmail.com>
2019-01-22 22:28:25 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,47 @@
+require "pry"
+class Board 
+  attr_accessor :cells
+  
+  def initialize
+    reset!
+  end
+  
+  def reset!
+   @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+  
+  def display
+    puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
+    puts "-----------"
+    puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
+    puts "-----------"
+    puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
+  end
+  
+  def position(user_input)
+   @cells[user_input.to_i - 1] 
+  end
+  
+  def full?
+  @cells.all? {|cell| cell == "X" || cell == "O"}
+  end
+  
+  def turn_count
+  turns = 0
+  @cells.each do |cell|
+    if cell == "X" || cell == "O"
+      turns += 1
+    end
+   end
+   turns
+  end
+   
+   def taken?(user_input)
+     @cells[user_input.to_i - 1] == "X" || @cells[user_input.to_i - 1] == "O"
+   end
+   
+   def valid_move?(user_input) 
+    @cells[user_input.to_i - 1].between?(0,8) && !taken?(user_input)
+  end
+  
+ end
