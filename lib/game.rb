e3fd8941d80d2c71ef5f13d76303a35f90f97874
J Fern <jfern20@gmail.com>
2019-02-28 14:29:34 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,26 @@
 class Game 
+  attr_accessor :board, :player_1, :player_2
   
-  
+  WIN_COMBINATIONS = [
+  [0,1,2], #Top row
+  [3,4,5], #Middle row
+  [6,7,8], #Bottom row
+  [0,3,6], #Left column
+  [1,4,7], #Middle column
+  [2,5,8], #Right column
+  [0,4,8], #Left start diagonal
+  [6,4,2]  #Right start diagonal
+]
 
+
+
+  def initialize(player_1 = Players::, player_2, board)
+    
+    
+  end 
+  
 
 end 
+
+
+
