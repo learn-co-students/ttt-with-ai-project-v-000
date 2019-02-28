c2161fa7eff85a6c0d8804d76bce95a28df16637
J Fern <jfern20@gmail.com>
2019-02-28 06:15:10 +0000

    Done.

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,12 @@
 class Player 
+  require 'pry'
   
+  attr_reader :token   #cannot be changed so needs to be just a reader 
+  
+  def initialize(token)
+    @token = token
+    
+  end 
   
   
   
