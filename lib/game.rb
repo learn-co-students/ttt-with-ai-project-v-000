1e33b7316dd3a13dbfeead7e9fb3e4d7026dcb5c
J Fern <jfern20@gmail.com>
2019-02-28 14:49:50 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -15,7 +15,9 @@ class Game
 
 
   def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
-    
+    @board = board
+    @player_1 = player_1
+    @player_2 = player_2 
     
   end 
   
