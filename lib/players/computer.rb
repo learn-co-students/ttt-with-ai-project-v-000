18a35c20956d28c86e1668546da3a07236824cfd
Mat Anderson <matandersonlv@hotmail.com>
2019-03-23 17:35:57 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,10 +2,13 @@ module Players
   class Computer < Player
     
     def move(board)
-      move = nil 
+      move = " " 
       if !board.taken?(5)
         move = "5"
+      else
+        move = "5" += 1
       end
     end
+    
   end
 end
