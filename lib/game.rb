6b95d152da9d8140111d9b8cf723019c7ad882b3
jamieberrier <berrier.jamie@gmail.com>
2019-04-04 20:31:29 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,9 @@
+#The Game class is the main model of the application and represents a singular instance of a Tic-tac-toe session.
+  #A game has one Board through its board property.
+  #A game has two Players stored in a player_1 and player_2 property.
+#Board and Player do not directly relate to the Game but do collaborate with each other through arguments.
+
+#Beyond providing relationships with players and a board, the Game instance must also provide the basic game runtime and logic. These methods relate to the state of the game such as #current_player, #won?, and #winner. The other methods relate to managing a game, like #start, #play, and #turn. The test suite describes the method requirements.
 class Game
   attr_accessor :board, :player_1, :player_2
 
@@ -6,7 +12,7 @@ class Game
 
   #accepts 2 players and a board
   #defaults to two human players, X and O, with an empty board
-  def initialize(player_1=Player.new("X"), player_2=Player.new("O"), board=[])
+  def initialize(player_1=Players::Human.new("X"), player_2=Players::Human.new("O"), board=[])
     @player_1 = player_1
     @player_2 = player_2
     @board = board
