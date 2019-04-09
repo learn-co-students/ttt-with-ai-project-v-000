0d5842555141ce773d224f02666b5e88640be1e8
jamieberrier <berrier.jamie@gmail.com>
2019-04-09 18:35:37 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,7 +2,10 @@ module Players
   class Computer < Player
     #returns a valid position for the computer to move
     def move(board)
-
+      computer_move = rand(1..9).to_s
+      if board.valid_move?(computer_move)
+        computer_move
+      end
     end
   end
 end
