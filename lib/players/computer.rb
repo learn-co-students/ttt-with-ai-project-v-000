4ade85129dbd241cf7913e6b7b6de060e7dd5462
jamieberrier <berrier.jamie@gmail.com>
2019-04-09 20:39:56 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,10 +3,35 @@ module Players
     #returns a valid position for the computer to move
     def move(board)
       puts "My turn! I'm #{token}"
-      computer_move = rand(1..9).to_s
-      if board.valid_move?(computer_move)
-        computer_move
+      if board.cells == [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+        '5'
+      elsif board.cells == [" ", " ", " ", " ", "X", " ", " ", " ", " "] || [" ", " ", " ", " ", "O", " ", " ", " ", " "]
+        '1'
+      else
+        computer_move = rand(1..9).to_s
+        if board.valid_move?(computer_move)
+          computer_move
+        end
       end
-    end
-  end
+    end #method
+  end #class
+end #module
+
+=begin
+      if board.cells == [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+        '5'
+      elsif board.cells == [" ", " ", " ", " ", "X", " ", " ", " ", " "] || [" ", " ", " ", " ", "O", " ", " ", " ", " "]
+        '1'
+      else
+        computer_move = rand(1..9).to_s
+        if board.valid_move?(computer_move)
+          computer_move
+        end
+      end
+=end
+=begin
+computer_move = rand(1..9).to_s
+if board.valid_move?(computer_move)
+  computer_move
 end
+=end
