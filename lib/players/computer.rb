06ff2bee6edc9da9159ebe2b77956d5577a9e647
jamieberrier <berrier.jamie@gmail.com>
2019-04-09 20:46:29 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,16 +3,17 @@ module Players
     #returns a valid position for the computer to move
     def move(board)
       puts "My turn! I'm #{token}"
+      #binding.pry
       if board.cells == [" ", " ", " ", " ", " ", " ", " ", " ", " "]
         '5'
-      elsif board.cells == [" ", " ", " ", " ", "X", " ", " ", " ", " "] || [" ", " ", " ", " ", "O", " ", " ", " ", " "]
-        '1'
+      #elsif board.cells == [" ", " ", " ", " ", "X", " ", " ", " ", " "] || [" ", " ", " ", " ", "O", " ", " ", " ", " "]
+      #  '1'
       else
         computer_move = rand(1..9).to_s
         if board.valid_move?(computer_move)
           computer_move
         end
-      end
+      end #AI strategy
     end #method
   end #class
 end #module
