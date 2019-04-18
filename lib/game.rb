318cd802e353f837d4c58c919303546cb04dda65
Kimberly Aviles <01010110.v@gmail.com>
2019-04-18 06:01:50 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -48,9 +48,20 @@ class Game
   end
 
   def turn
-    # cp = self.current_player
-     m = self.current_player.move
-    self.current_player.move unless !board.valid_move?
+    cpm = self.current_player.move # stores the user's inputted desired move <--- calls GETS
+      if board.valid_move?(cpm) # checks if the user's desired position is available
+        board.update(cpm, current_player) # changes the board's space to the user's playertoken if valid
+      else
+        puts "invalid"
+        self.turn # if the inputted desired position is not available, go back to TURN's 1st line and try for a different input
+      end
+  end
+
+  def play
+    self.turn
+    self.over?
+
+    self.play
   end
 
 end #<---CLASSend
