260a64b6df11f506fc3f9bd47b0871867d387ae9
Diapause <diapause@protonmail.com>
2019-04-23 21:17:47 -0700

    computer takes center first

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -8,17 +8,41 @@ module Players
 
     def move(board)
       #move returns a valid position for the computer to move
-      open_cells(board).sample + 1
-      #this is where AI would change input
+
+      #take center if open
+      if board.cells[4] == " "
+        5
+      else
+        #pick sample of remaing open cells
+        open_cells(board).sample + 1
+      end
+      # until corner_cells(board) == nil
+      #   #check for winning combo
+      #      check_for_win
+      #   #take corner if open
+      #      corner_cells(board)
+      # end
     end
 
-    def open_cells(board)
-      #weird nil behavior from map
-      arr = []
-      board.cells.each_with_index do |i, index|
-        arr << index if i == " "
-      end
-      arr
-    end
+    # def open_cells(board)
+    #   #weird nil behavior from map
+    #   arr = []
+    #   board.cells.each_with_index do |i, index|
+    #     arr << index if i == " "
+    #   end
+    #   arr
+    # end
+    #
+    # def corner_cells(board)
+    #   corners = [0,2,5,7]
+    #     corners.detect do |corner|
+    #       open_cells(board)[corner] == corner
+    #     end
+    #   end
+    # end
+
+    # def check_for_win
+    #
+    # end
   end
 end
