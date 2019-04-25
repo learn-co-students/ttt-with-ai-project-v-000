40095b70dc0d4ee559de068e953d5648d3064184
Niusha H <niusha.hadziomerovic@gmail.com>
2019-04-25 18:58:57 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -18,7 +18,42 @@ class Board
   end
   
   def position
+    #look up the value according to the cells' array index
     
   end
+  
+  def update(position, player)
+    #represents updating the board when player makes a move
+    
+    #position user wants to occupy in the form of 1-9 strings that you will need to convert to the board cells' array index
+    
+    #player object of who's making the move
+    
+    #update the token method of the player
+    
+  end 
 
+  def token
+    #invoked when saving the update method
+    
+  end
+  
+  def full?
+    #when entirely occupied by "X"" and "O"s
+    
+  end
+  
+  def turn_count
+    #how many positions in cells array are filled
+    
+  end
+  
+  def taken?
+    #returns true or false for a position
+  end
+
+  def valid_move?
+    #ensures that moves are between 1-9 and not taken(i.e. available)
+    
+  end
 end
