e37fe302d07aa1c1395655d381616ede94bc6605
Niusha H <niusha.hadziomerovic@gmail.com>
2019-04-25 17:51:21 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,24 @@
+class Board
+
+
+
+  def cells
+    #stores data of the state of the board in an Array
+    board_data = []
+  end
+
+  def reset!
+    #reset the state of the cells to what a board should look like at game start with 9 " " elements
+    new_board = 
+  end
+
+  def display
+    #print current state of board
+    print board + board_data
+  end
+  
+  def position
+    
+  end
+
+end
