d769bd607e79f4c866bac5393edddfb7637211a4
Kimberly Aviles <01010110.v@gmail.com>
2019-04-30 21:22:12 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,9 +1,10 @@
 module Players
  class Computer < Player
 
-
-
-
+   def move(board)
+    move_array = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    move_array.sample
+   end
 
  end #<--CLASSend
 end
