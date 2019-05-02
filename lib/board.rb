e6fe312211140b3a67be7e0c9d4da7e025f2e0a5
Niusha H <niusha.hadziomerovic@gmail.com>
2019-05-02 19:45:41 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -2,16 +2,22 @@ class Board
   attr_accessor :cells
 
 
-  def reset!
-    @cells = Array.new(9, " ")
-  end
-
   def initialize
     reset!
   end
 
+
+  def reset!
+    @cells = Array.new(9, " ")
+  end
+
   def display
-    print board.cells
+    puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
+    puts "---------"
+    puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
+    puts "---------"
+    puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
+    puts "---------"
   end
   
   def position
