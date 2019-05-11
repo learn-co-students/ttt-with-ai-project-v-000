ba23925b3c96f5c7d2ff918a492c212efbf36896
Niusha H <niusha.hadziomerovic@gmail.com>
2019-05-11 18:45:42 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -41,26 +41,30 @@ class Board
     cells.count { |character| character == "X" || character == "O"}
   end
 
-  def update(position, player)
-    #represents updating the board when player makes a move
-    
-    #position user wants to occupy in the form of 1-9 strings that you will need to convert to the board cells' array index
-    
-    #player object of who's making the move
-    
-    #update the token method of the player - call it with name space Players::Human or Players::Computer
-    
-  end 
+  def taken?
+    #returns true or false for a position
+    if cells.each { |character| character == "X" || character == "O"}
+      true
+    else
+      false
+    end 
+  end
 
+
+  # def update(position, player)
+  #   #represents updating the board when player makes a move
+    
+  #   #position user wants to occupy in the form of 1-9 strings that you will need to convert to the board cells' array index
+    
+  #   #player object of who's making the move
+    
+  #   #update the token method of the player - call it with name space Players::Human or Players::Computer
+    
+  # end 
   # def token
   #   #invoked when saving the update method
     
   # end
-
-  def taken?
-    #returns true or false for a position
-    cells.any? { |character| character == "X" || character == "O"}
-  end
 
   # def valid_move?
   #   #ensures that moves are between 1-9 and not taken(i.e. available)
