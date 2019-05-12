14daf36d250495bb0c1d3872238d8f3d36208d5d
Niusha H <niusha.hadziomerovic@gmail.com>
2019-05-12 03:27:46 +0000

    Done.

--- lib/player.rb
+++ lib/player.rb
@@ -9,5 +9,4 @@ class Player
     @token = token
   end
 
-  
 end
--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,11 +3,10 @@ module Players
   class Computer < Player
   
   
-    def move
+    def move(input)
       #represents how this type of player makes a TTT move
-      
+      move_array = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+      move_array.sample
     end
-  
-  
   end
 end
