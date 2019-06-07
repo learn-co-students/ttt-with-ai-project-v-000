1a009a32022e8eb15a42ad917e7be417b22eda92
Kimberly Aviles <01010110.v@gmail.com>
2019-06-07 19:12:05 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -4,6 +4,8 @@ module Players
    def move(board)
     move_array = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
     move_array.sample
+    # chooses a random number - ****but creates invalid space choices - spaces that are already taken -
+    # PLZ: FIX : check to see if sampled space is taken or not- if it is taken - try sample again until valid space is sampled
    end
 
  end #<--CLASSend
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -3,7 +3,9 @@ module Players
 
     def move(board_argument)
       puts "please choose a space 1-9"
+
       board_argument.display
+
       user_input = gets.strip
     end
 
