176cbc79102c0807f5cec194de6317ba74959147
David Forer <david.j.forer@gmail.com>
2019-06-10 14:03:05 +0000

    Done.

---
+++ lib/players/computer.rb
@@ -0,0 +1,16 @@
+module Players
+  class Computer < Player
+    attr_accessor :board
+
+     def move(board)
+      @board = board
+      @valid_moves = []
+      @board.cells.each.with_index(1) do |cell, index|
+        if cell == " "
+          @valid_moves << "#{index}"
+        end
+      end
+      computer_move = @valid_moves[rand(@valid_moves.length)]
+    end
+end
+end
