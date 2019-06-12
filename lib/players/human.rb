272ce995c3b4182a004ef1da032888a2e034baab
emanuelbierman <emanuel.bierman@gmail.com>
2019-06-12 12:16:03 -0400

    Done.

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -3,8 +3,6 @@ module Players
   class Human < Player
 
     def move(board)
-      # Even though the method accepts a `board` argument,
-      # it does not need to use it.
       puts "Enter a number from 1-9:"
       input = gets.strip
     end
