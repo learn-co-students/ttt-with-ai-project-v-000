2face6d4b84960ae99c09cd6434447e53302cf39
alejandra cantu <alejandra.cantu13@gmail.com>
2019-09-06 22:15:17 +0000

    Done.

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,5 @@
 class Player
 
-  attr_accessor
   attr_reader :token
 
   def initialize(token)
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,10 +1,12 @@
-class Players
+module Players
   class Human < Player
 
+
+
   def move(board)
+    puts "Please enter your selection"
     input = gets.chomp
     input
-
   end
 
 
