7857ff11743b8b47ff9c4cb41814dad424403b94
Dan Suarez <suarez.daniel101@gmail.com>
2019-09-10 23:08:59 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Board
   attr_accessor :cells
 
@@ -13,11 +14,45 @@ class Board
   end
 
   def display
+
+    # input = " "
+    #  if input = "X" || input = "O"
+    #    @cells.fill(" ", 0, 9).each do |a|
+    #   print " #{a} | #{a} | #{a} "
+    #   print "-----------"
+    # end
+
+
+    # @cells.collect do |a|
+    #
+    #   print " #{a} | #{a} | #{a} "
+    #   print "-----------"
+    # end
+
+  #
+    # @cells.fill("X", 0, 3).each do |a|
+    #   print " #{a} | #{a} | #{a} "
+    #   print "-----------"
+    # end
+  #
+    # @cells.replace(a).each do |b|
+    #     print " #{b} | #{b} | #{b} "
+    #     print "-----------"
+    #   end
+  #
+    # @cells.each do |a|
+    #   a.replace(" ")
+    #   print " #{a} | #{a} | #{a} "
+    #   print "-----------"
+    #   end
+
     @cells.each do |a|
-      print "  |  |  "
-    end
-    # # print "  |  |  "
-    # print "  |  |  "
+      # a = "X"
+        # a[b].replace(@cells[0..9])
+      puts a[0][1] 
+      puts "-----------"
+      end
+
   end
 
 end
