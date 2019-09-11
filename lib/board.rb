09ea641aa2172d1f10ec8b6e5987e9235f46f7ac
Dan Suarez <suarez.daniel101@gmail.com>
2019-09-11 02:23:53 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -14,45 +14,11 @@ class Board
   end
 
   def display
-
-    # input = " "
-    #  if input = "X" || input = "O"
-    #    @cells.fill(" ", 0, 9).each do |a|
-    #   print " #{a} | #{a} | #{a} "
-    #   print "-----------"
-    # end
-
-
-    # @cells.collect do |a|
-    #
-    #   print " #{a} | #{a} | #{a} "
-    #   print "-----------"
-    # end
-
-  #
-    # @cells.fill("X", 0, 3).each do |a|
-    #   print " #{a} | #{a} | #{a} "
-    #   print "-----------"
-    # end
-  #
-    # @cells.replace(a).each do |b|
-    #     print " #{b} | #{b} | #{b} "
-    #     print "-----------"
-    #   end
-  #
-    # @cells.each do |a|
-    #   a.replace(" ")
-    #   print " #{a} | #{a} | #{a} "
-    #   print "-----------"
-    #   end
-
-    @cells.each do |a|
-      # a = "X"
-        # a[b].replace(@cells[0..9])
-      puts a[0]  
+      puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
       puts "-----------"
-      end
-
+      puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+      puts "-----------"
+      puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
   end
 
 end
