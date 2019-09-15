bf1d2ea867380a3c2d7a0117edd8286842647ef9
Dan Suarez <suarez.daniel101@gmail.com>
2019-09-15 23:00:40 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,12 +1,18 @@
 class Game
-  attr_accessor :board, :player_1, :player_2
+  # attr_accessor :board, :player_1, :player_2
   # attr_reader :board, :player_1, :player_2
+  attr_accessor :player_1, :player_2
+  attr_reader :board
 
-  # def initialize(board, player_1, player_2)
-  #   @board = board
+  # def initialize(player_1, player_2, board)
   #   @player_1 = player_1
   #   @player_2 = player_2
+  #   @board = board
   # end
+
+  def initialize
+
+  end
 
   WIN_COMBINATIONS = [
     [0,1,2],
@@ -19,5 +25,15 @@ class Game
     [6,4,2],
   ]
 
+  # def board
+  #   @board
+  # end
 
+  def board=(board)
+    @board = []
+  end
+
+  # def player_1=(player)
+  #   @player = Player.new(token)
+  # end
 end
