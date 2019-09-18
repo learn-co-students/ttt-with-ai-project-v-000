d349b80276320b28e98586f5634f2ce06172b74f
Dan Suarez <suarez.daniel101@gmail.com>
2019-09-18 19:30:02 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -14,14 +14,21 @@ class Game
     [6,4,2],
   ]
 
-  # def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = [])
   def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new())
     @player_1 = player_1
     @player_2 = player_2
-    # @board = @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
     @board = board
-
     # binding.pry
   end
+
+  def current_player
+    if board.turn_count % 2 == 0
+      return player_1
+    else
+      return player_2
+    end
+  end
+
+
 
 end
