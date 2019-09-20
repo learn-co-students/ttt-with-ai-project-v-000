409df015727c28eea6bc7ea6ab70d2d4377900ae
Dan Suarez <suarez.daniel101@gmail.com>
2019-09-20 14:53:05 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -23,14 +23,39 @@ class Game
 
   def current_player
     if board.turn_count % 2 == 0
-      return player_1
+      player_1
     else
-      return player_2
+      player_2
     end
   end
 
-  def won?
+  # def won?
+  #   WIN_COMBINATIONS[[5]]
+  #   binding.pry
+  #
+  # end
 
+  def won?
+    WIN_COMBINATIONS.each do |win_combination|
+        win_index_1 = win_combination[0]
+        win_index_2 = win_combination[1]
+        win_index_3 = win_combination[2]
+
+           if win_index_1 == "X" && win_index_2 == "X" && win_index_3 == "X"
+            #  win_combination.each {|a| a.to_i }
+            # win_combination.join("")
+             win_combination.join("").to_i(10)
+           elsif win_index_1 == "O" && win_index_2 == "O" && win_index_3 == "O"
+            #  win_combination.to_s
+             win_combination.join("").to_i(10)
+           end
+          #  Integer(win_combination.join(", "), 10)
+          #  win_combination.join(", ")
+          #  Integer("0461990540002606", 10)
+          #  binding.pry
+         end
+      false
   end
 
 end
+# Or convert it to a string and add the base: $> "0023".to_i(10) # 23 $> "0023.to_i(8) # 8
