c832c0a7440277e7a5684f9e9d64c1a18ef5cc99
Lauren Days <lmchambers0420@gmail.com>
2019-09-21 00:37:06 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -25,6 +25,24 @@ class Board
   end 
   
   def full?
-      
-  end
+    @cells.all? do |full|
+      full == "X" || full == "O"
+  end 
 end
+
+def turn_count
+  @cells.count {|turn| turn == "X" || turn == "O"}
+end
+
+def taken?(input)
+  position(input) == "X" || position(input) =="O"
+end
+
+def valid_move?(input)
+  input.to_i >= 1 && input.to_i <= 9 && !taken?(input)
+end
+
+def update(input, player)
+  @cells[input.to_i - 1] = player.token
+end 
+end 
---
+++ lib/game.rb
@@ -0,0 +1,10 @@
+class Game
+  
+  
+  
+  
+  
+  
+  
+  
+end 
