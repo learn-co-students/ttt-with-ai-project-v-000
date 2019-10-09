9134d82215f5070f3fd24b429ad3e6adb3756c30
Dan Suarez <suarez.daniel101@gmail.com>
2019-10-09 05:28:41 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -13,7 +13,7 @@ class Game
     [1,4,7],
     [2,5,8],
     [0,4,8],
-    [6,4,2],
+    [2,4,6],
   ]
 
   def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new())
