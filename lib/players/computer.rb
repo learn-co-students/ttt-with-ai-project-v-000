453500a23ab0cd09f5a5ed88c3f308722ea491fc
Dan Suarez <suarez.daniel101@gmail.com>
2019-10-13 01:07:54 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,10 +3,14 @@ module Players
     def move(board)
       # puts "Please enter 1-9:"
       # move(@board)
-      computer_move = computer.move(board)
+      # computer_move = computer.move(board)
       # board.valid_move?
 
     end
     binding.pry
   end
 end
+
+# Implement a `#move` method that accepts a board and returns the move the computer
+# wants to make in the form of a 1-9 string. How the computer decides to make that
+# move is up to you but it must be capable of returning a valid move at some point.
