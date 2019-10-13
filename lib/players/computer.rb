975008915c85c48f67a903eec7baff826aa30772
Dan Suarez <suarez.daniel101@gmail.com>
2019-10-13 00:56:28 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,8 +2,8 @@ module Players
   class Computer < Player
     def move(board)
       # puts "Please enter 1-9:"
-      move(@board)
-      valid_move?(computer_move)
+      # move(@board)
+      valid_move?
 
     end
     binding.pry
