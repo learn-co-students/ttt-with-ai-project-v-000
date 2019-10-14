ae571e98b98f8cdb294d58ba71351f432bda1343
Dan Suarez <suarez.daniel101@gmail.com>
2019-10-14 01:20:54 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,28 +1,29 @@
 module Players
   class Computer < Player
-    # attr_accessor :game, :board
-
-    # def turn
-    #   input = current_player.move(@board)
-    #   if @board.valid_move?(input)
-    #     @board.update(input, current_player)
-    #   else
-    #     turn
-    #   end
 
     def move(board)
       # puts "Please enter 1-9:"
       # game.current_player.move(@board)
       # move(@board)
-      # computer_move = board.position(input)
+      computer_move = board.position(input)
       # board.valid_move?(computer_move)
-      @board
+      # @board
       # board.valid_move?
       # computer_move = game.current_player.move(@board)
     end
     binding.pry
   end
 end
+
+# attr_accessor :game, :board
+
+# def turn
+#   input = current_player.move(@board)
+#   if @board.valid_move?(input)
+#     @board.update(input, current_player)
+#   else
+#     turn
+#   end
 
 # Implement a `#move` method that accepts a board and returns the move the computer
 # wants to make in the form of a 1-9 string. How the computer decides to make that
