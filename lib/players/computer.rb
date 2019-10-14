22c2dbfbb9b7b9e765368cfce451c15b85741bc3
Dan Suarez <suarez.daniel101@gmail.com>
2019-10-14 01:18:45 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,16 +2,25 @@ module Players
   class Computer < Player
     # attr_accessor :game, :board
 
+    # def turn
+    #   input = current_player.move(@board)
+    #   if @board.valid_move?(input)
+    #     @board.update(input, current_player)
+    #   else
+    #     turn
+    #   end
+
     def move(board)
       # puts "Please enter 1-9:"
       # game.current_player.move(@board)
       # move(@board)
       # computer_move = board.position(input)
-      board.valid_move?(computer_move)
+      # board.valid_move?(computer_move)
+      @board
       # board.valid_move?
       # computer_move = game.current_player.move(@board)
     end
-    # binding.pry
+    binding.pry
   end
 end
 
