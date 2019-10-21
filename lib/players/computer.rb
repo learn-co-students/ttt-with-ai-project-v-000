ebb12d02442c53178602e693489f76a5628181cc
Dan Suarez <suarez.daniel101@gmail.com>
2019-10-21 00:43:48 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -7,15 +7,11 @@ module Players
       # binding.pry
       valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
       # computer_move = valid_moves.select do |move|
-        # valid_moves.select {rand(0..10)}
         board.cells.select do |comp_move|
           comp_move = rand(1..9)
           if board.full? == false
              if board.valid_move?(comp_move)
               board.update(comp_move, game.current_player)
-      #     #  computer_move = board.position(@input)
-      # #       computer_move.to_s
-      # #       # board.valid_move?(computer_move)
               board.display
               comp_move.to_s
             else
