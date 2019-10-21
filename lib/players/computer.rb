cd20414146e1f525062ad10e986220e47ab8a609
Dan Suarez <suarez.daniel101@gmail.com>
2019-10-21 02:07:00 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -13,11 +13,12 @@ module Players
           # binding.pry
           comp_move = rand(1..9)
           # comp_move = game.current_player.move(rand(1..9))
-          if board.full? 
+          if board.full?
              if board.valid_move?(comp_move)
               board.update(comp_move, game.current_player)
               board.display
               comp_move.to_s
+              
             else
               move(board)
             end
