ef04b070f41b29226cfdaed0e0b277df327f8aa1
Dan Suarez <suarez.daniel101@gmail.com>
2019-10-21 01:56:31 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,14 +2,14 @@ module Players
 
   class Computer < Player
     def move(board)
-      # game = Game.new()
+      game = Game.new()
       # game.current_player
       # computer_move = game.current_player
         # binding.pry
       valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
       # computer_move = valid_moves.select do |move|
         board.cells.select do |comp_move|
-          game = Game.new()
+          # game = Game.new()
           # binding.pry
           comp_move = rand(1..9)
           # comp_move = game.current_player.move(rand(1..9))
