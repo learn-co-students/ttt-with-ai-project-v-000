7e00cb35ba74a7bf29a49128a0ef30f6e2fe9235
Dan Suarez <suarez.daniel101@gmail.com>
2019-10-23 22:13:49 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -8,34 +8,24 @@ module Players
         # binding.pry
       valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
       # computer_move = valid_moves.select do |move|
-      # def play
-      #   until over?
-      #     turn
-      #   end
-      #   if won?
-      #     puts "Congratulations #{winner}!"
-      #   elsif draw?
-      #     puts "Cat's Game!"
-      #   end
-      # end
         board.cells.select do |comp_move|
           # binding.pry
           comp_move = rand(1..9)
+          if board.full?
+           board.valid_move?(comp_move)
+            board.update(comp_move, game.current_player)
+            board.display
+            comp_move.to_s
+          # if game.won?
+          #   puts "Congratulations #{winner}!"
+          # elsif game.draw?
+          #   puts "Cat's Game!"
+          # end
           # comp_move = game.current_player.move(rand(1..9))
           # if board.full?
             # game.turn
-             if board.valid_move?(comp_move)
-              board.update(comp_move, game.current_player)
-              board.display
-                comp_move.to_s
-                if game.won?
-                  puts "Congratulations #{winner}!"
-                elsif game.draw?
-                  puts "Cat's Game!"
-                end
-             else
-              move(board)
-             end
+          else
+            move(board)
           end
           binding.pry
         end
@@ -49,6 +39,17 @@ end
 #   else
 #     turn
 #   end
+
+# def play
+#   until over?
+#     turn
+#   end
+#   if won?
+#     puts "Congratulations #{winner}!"
+#   elsif draw?
+#     puts "Cat's Game!"
+#   end
+# end
 
 # Implement a `#move` method that accepts a board and returns the move the computer
 # wants to make in the form of a 1-9 string. How the computer decides to make that
