83f051c0fc5d3b49be79bffcb1b945930b47eff0
Paris Dolfman <parisdolfman@gmail.com>
2019-10-25 21:30:36 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -12,7 +12,11 @@ def initialize
 end
 
 def  display
-  puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]}"
+  puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+  puts "-----------"
+  puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+  puts "-----------"
+  puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
 end
 
 
