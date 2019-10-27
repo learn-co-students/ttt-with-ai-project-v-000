f81591aec2cd5da6bd1aadd933115eb2bfc23f08
Dan Suarez <suarez.daniel101@gmail.com>
2019-10-27 00:27:55 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,11 +3,12 @@ module Players
     def move(board)
       game = Game.new()
         board.cells.each do |comp_move|
-          comp_move = rand(1..9)
+          binding.pry
+          rand(comp_move)
           while board.full?
             if board.valid_move?(comp_move)
               board.update(comp_move, game.current_player)
-              comp_move.to_s
+              # comp_move.to_s
             else
               move(board)
             end
@@ -17,6 +18,7 @@ module Players
   end
 end
 
+# computer_move = computer.move(board)
 # Implement a `#move` method that accepts a board and returns the move the computer
 # wants to make in the form of a 1-9 string. How the computer decides to make that
 # move is up to you but it must be capable of returning a valid move at some point.
