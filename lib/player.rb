5fec2e078a0d0e4145ed4ecd36d6abac37a3c0fc
Paris Dolfman <parisdolfman@gmail.com>
2019-10-27 21:12:17 +0000

    Done.

--- lib/player.rb
+++ lib/player.rb
@@ -1,11 +1,10 @@
 class Player
 
+attr_reader :token
+
 def initialize(token)
 @token = token
 end
 
-def token
-  @token
-end
 
 end
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,5 +1,9 @@
 module Players
   class Human < Player
 
+  def move(input)
+    Board.new(input)
+  end
+
   end
 end
