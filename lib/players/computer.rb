86057a39ed96d2210852c4fc50d162023635287e
Dan Suarez <suarez.daniel101@gmail.com>
2019-10-28 01:28:04 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -8,39 +8,41 @@ module Players
         # item = myArray[rand(myArray.length)]
         # comp_move = board.cells[rand(board.cells.length)]
         # board.cells[rand(board.cells.length)].select do |comp_move|
+          while board.full?
         board.cells.map do |cell|
           cell = rand(1..9)
-          board
-            binding.pry
-        end
-
-        board.cells.select do |computer_move|
-          # computer_move = board.cells[rand(board.cells.length)]
-          board.cells[rand(board.cells.length)]
-
-
-          # comp_move = rand(1..9)
-
-          board.cells[computer_move] = rand(1..9)
-
-          # board.cells[computer_move] = comp_move
-
-          while board.full?
-
-            if board.valid_move?(computer_move)
-
-              board.update(computer_move, game.current_player)
-              # board.cells.sort
-              computer_move.to_s
-            else
-              move(board)
-            end
+          board.cells
           end
-          # board.cells.sort
-          # board.reset!
-          # comp_move.to_s
-          binding.pry
         end
+        binding.pry
+
+        # board.cells.select do |computer_move|
+        #   # computer_move = board.cells[rand(board.cells.length)]
+        #   board.cells[rand(board.cells.length)]
+        #
+        #
+        #   # comp_move = rand(1..9)
+        #
+        #   board.cells[computer_move] = rand(1..9)
+        #
+        #   # board.cells[computer_move] = comp_move
+        #
+        #   while board.full?
+        #
+        #     if board.valid_move?(computer_move)
+        #
+        #       board.update(computer_move, game.current_player)
+        #       # board.cells.sort
+        #       computer_move.to_s
+        #     else
+        #       move(board)
+        #     end
+        #   end
+        #   # board.cells.sort
+        #   # board.reset!
+        #   # comp_move.to_s
+        #   # binding.pry
+        # end
     end
   end
 end
