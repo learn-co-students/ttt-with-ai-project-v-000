edb4fbcea61f81d200ce30bb32439dbc0f943d3a
Dan Suarez <suarez.daniel101@gmail.com>
2019-10-28 01:49:16 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -9,41 +9,41 @@ module Players
         # comp_move = board.cells[rand(board.cells.length)]
         # board.cells[rand(board.cells.length)].select do |comp_move|
 
-        board.cells.each do |comp_move|
-          while board.full?
-          comp_move = rand(1..9)
-
-          end
-          board.cells << comp_move
-        end
-        binding.pry
-
-        # board.cells.select do |computer_move|
-        #   # computer_move = board.cells[rand(board.cells.length)]
-        #   board.cells[rand(board.cells.length)]
-        #
-        #
-        #   # comp_move = rand(1..9)
-        #
-        #   board.cells[computer_move] = rand(1..9)
-        #
-        #   # board.cells[computer_move] = comp_move
-        #
+        # board.cells.each do |comp_move|
         #   while board.full?
-        #
-        #     if board.valid_move?(computer_move)
-        #
-        #       board.update(computer_move, game.current_player)
-        #       # board.cells.sort
-        #       computer_move.to_s
-        #     else
-        #       move(board)
-        #     end
+        #   comp_move = rand(1..9)
+        #   board.cells << comp_move
         #   end
-        #   # board.cells.sort
-        #   # board.reset!
-        #   # comp_move.to_s
-        #   # binding.pry
+        #
+        # end
+        # binding.pry
+
+        board.cells.select do |computer_move|
+          # computer_move = board.cells[rand(board.cells.length)]
+          # board.cells[rand(board.cells.length)]
+
+
+          computer_move = rand(1..9)
+
+          # board.cells[computer_move] = rand(1..9)
+
+          # board.cells[computer_move] = comp_move
+
+          if board.full?
+            board.cells << computer_move
+            # if board.valid_move?(computer_move)
+
+              board.update(computer_move, game.current_player)
+              # board.cells.sort
+              computer_move.to_s
+            else
+              move(board)
+            end
+          end
+          # board.cells.sort
+          # board.reset!
+          # comp_move.to_s
+          binding.pry
         # end
     end
   end
