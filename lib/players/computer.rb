dbf0ac5972a061fdacc549ce61ac42264c0b84ab
Dan Suarez <suarez.daniel101@gmail.com>
2019-10-30 22:30:29 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,9 +2,9 @@ module Players
   class Computer < Player
     def move(board)
       valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-      valid_moves.each do |space|
-        if board.valid_move?(valid_moves[space.to_i - 1]) == "1"
-          valid_moves[space.to_i - 1]
+      # valid_moves.each do |space|
+        if board.valid_move?(valid_moves[0]) == "1"
+          valid_moves[0]
         # elsif board.valid_move?(valid_moves[space.to_i - 1]) == "1" || board.valid_move?(valid_moves[space.to_i - 1]) == "3" ||
         #   board.valid_move?(valid_moves[space.to_i - 1]) == "7" || board.valid_move?(valid_moves[space.to_i - 1]) == "9"
         #   valid_moves[space.to_i - 1]
@@ -13,7 +13,7 @@ module Players
         #   valid_moves[space.to_i - 1]
         end
         binding.pry
-      end
+      # end
     end
   end
 end
