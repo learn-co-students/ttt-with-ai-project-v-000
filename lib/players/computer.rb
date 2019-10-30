af7e34eaf320cd34e1774e0ed11d3ba369b6846b
Dan Suarez <suarez.daniel101@gmail.com>
2019-10-30 21:04:29 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -5,10 +5,13 @@ module Players
       # num.to_s
       valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
       valid_moves.each do |space|
-        if board.valid_move?(space[0]) == "4"
+        # if board.valid_move?(space[0]) == "5"
+        #   space[0]
+        if rand(board.valid_move?(space[0]) == "1" || board.valid_move?(space[0]) == "3"
+          || board.valid_move?(space[0]) == "7"  || board.valid_move?(space[0]) == "9")
           space[0]
-        else
-          move(board)
+
+        #   move(board)
         #   rand(board.valid_move?(space[0]) || board.valid_move?(space[2]) ||
         #   board.valid_move?(space[6]) || board.valid_move?(space[8]))
 
