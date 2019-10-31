6e718855bb969e09365cc2d42a1f0a0e2d7ea68f
Dan Suarez <suarez.daniel101@gmail.com>
2019-10-31 14:54:07 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,17 +2,17 @@ module Players
   class Computer < Player
     def move(board)
       valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-      # valid_moves.each do |space|
-        # if board.valid_move?(valid_moves[0]) == "1"
-        #   valid_moves[0]
-        # elsif board.valid_move?(valid_moves[space.to_i - 1]) == "1" || board.valid_move?(valid_moves[space.to_i - 1]) == "3" ||
-        #   board.valid_move?(valid_moves[space.to_i - 1]) == "7" || board.valid_move?(valid_moves[space.to_i - 1]) == "9"
-        #   valid_moves[space.to_i - 1]
-        # elsif board.valid_move?(valid_moves[space.to_i - 1]) == "2" || board.valid_move?(valid_moves[space.to_i - 1]) == "4" ||
-        #   board.valid_move?(valid_moves[space.to_i - 1]) == "6" || board.valid_move?(valid_moves[space.to_i - 1]) == "8"
-        #   valid_moves[space.to_i - 1]
+      valid_moves.each do |space|
+        if board.valid_move?(valid_moves[0]) == "1"
+          valid_moves[0]
+        elsif board.valid_move?(valid_moves[space.to_i - 1]) == "1" || board.valid_move?(valid_moves[space.to_i - 1]) == "3" ||
+          board.valid_move?(valid_moves[space.to_i - 1]) == "7" || board.valid_move?(valid_moves[space.to_i - 1]) == "9"
+          valid_moves[space.to_i - 1]
+        elsif board.valid_move?(valid_moves[space.to_i - 1]) == "2" || board.valid_move?(valid_moves[space.to_i - 1]) == "4" ||
+          board.valid_move?(valid_moves[space.to_i - 1]) == "6" || board.valid_move?(valid_moves[space.to_i - 1]) == "8"
+          valid_moves[space.to_i - 1]
         binding.pry
-      # end
+      end
     end
   end
 end
