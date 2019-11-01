0c30c13bbbe4c2f037c5cdda3d0e8c973ed20143
Dan Suarez <suarez.daniel101@gmail.com>
2019-11-01 22:17:43 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -5,10 +5,10 @@ module Players
       board.cells.each do |space|
         if board.valid_move?(valid_moves[space.to_i - 1])
           valid_moves[4]
-        elsif board.valid_move?(space)
+        elsif board.valid_move?(valid_moves[space.to_i - 1])
           valid_moves[0] || valid_moves[2] ||
           valid_moves[6] || valid_moves[8]
-        elsif board.valid_move?(space)
+        elsif board.valid_move?(valid_moves[space.to_i - 1])
           valid_moves[1] || valid_moves[3] ||
           valid_moves[5] || valid_moves[7]
         end
@@ -17,3 +17,5 @@ module Players
     end
   end
 end
+
+# a.insert(index, obj)
