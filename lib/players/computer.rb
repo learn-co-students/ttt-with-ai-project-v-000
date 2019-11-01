f8789a2dfd18b25a6cf40d8911886a63ce0ceb93
Dan Suarez <suarez.daniel101@gmail.com>
2019-11-01 21:22:05 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,19 +2,8 @@ module Players
   class Computer < Player
     def move(board)
       valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-      valid_moves.each do |space|
-        # valid_moves.clear
-        # if board.valid_move?(valid_moves[space.to_i - 1])
-        #   "5"
-        # #   if board.valid_move?(valid_moves[0])
-        #     space
-
-        # if valid_moves[space.to_i - 1] == "5"
-        #   if board.valid_move?(valid_moves[space])
-        #     space
-        # elsif valid_moves[0] == "1"
-
-        if board.valid_move?(space)
+      board.cells.each do |space|
+        if board.valid_move?(valid_moves[space.to_i - 1])
           valid_moves[4]
         elsif board.valid_move?(space)
           valid_moves[0] || valid_moves[2] ||
@@ -22,54 +11,9 @@ module Players
         elsif board.valid_move?(space)
           valid_moves[1] || valid_moves[3] ||
           valid_moves[5] || valid_moves[7]
-        # elsif board.valid_move?(valid_moves[space.to_i - 1]) == "2" || board.valid_move?(valid_moves[space.to_i - 1]) == "4" ||
-        #   board.valid_move?(valid_moves[space.to_i - 1]) == "6" || board.valid_move?(valid_moves[space.to_i - 1]) == "8"
-        #   valid_moves[space.to_i - 1]
-          end
-        # end
+        end
         binding.pry
       end
     end
   end
 end
-
-# module Players
-#   class Computer < Player
-#     def move(board)
-#       # num = rand(1..9)
-#       # num.to_s
-#     end
-#   end
-# end
-# This code right above passes all the tests
-
-
-
-# module Players
-#   class Computer < Player
-#     def move(board)
-#       board.cells.each do |space|
-#
-#           if board.valid_move?(space)
-#         # board.update(space, game.current_player)
-#           num = rand(1..9)
-#             board.cells[num].to_s
-#           # else
-#           #   move(board)
-#           end
-#         end
-#       end
-#     end
-#     # space.to_s
-#   end
-
-# human move method
-# def move(board)
-#   puts "Please enter 1-9:"
-#   gets.strip
-# end
-##### 'players/computer.rb' - `Players::Computer`
-# Define a class `Players::Computer` that represents a computer player of Tic-tac-toe.
-# Implement a `#move` method that accepts a board and returns the move the computer wants
-# to make in the form of a 1-9 string. How the computer decides to make that move is up to you
-# but it must be capable of returning a valid move at some point.
