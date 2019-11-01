6386cf76853ba04042a21582fc90141948451ac6
Dan Suarez <suarez.daniel101@gmail.com>
2019-11-01 22:52:47 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,21 +1,42 @@
+# module Players
+#   class Computer < Player
+#     def move(board)
+#       valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+#       board.cells.each do |space|
+#         if board.valid_move?(valid_moves[space.to_i - 1])
+#           valid_moves[4]
+#         elsif board.valid_move?(valid_moves[space.to_i - 1])
+#           valid_moves[0] || valid_moves[2] ||
+#           valid_moves[6] || valid_moves[8]
+#         elsif board.valid_move?(valid_moves[space.to_i - 1])
+#           valid_moves[1] || valid_moves[3] ||
+#           valid_moves[5] || valid_moves[7]
+#         end
+#         # binding.pry
+#       end
+#     end
+#   end
+# end
+
 module Players
   class Computer < Player
     def move(board)
-      valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+      # valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
       board.cells.each do |space|
-        if board.valid_move?(valid_moves[space.to_i - 1])
-          valid_moves[4]
-        elsif board.valid_move?(valid_moves[space.to_i - 1])
-          valid_moves[0] || valid_moves[2] ||
-          valid_moves[6] || valid_moves[8]
-        elsif board.valid_move?(valid_moves[space.to_i - 1])
-          valid_moves[1] || valid_moves[3] ||
-          valid_moves[5] || valid_moves[7]
+        if board.valid_move?(board.cells.find_index(space) + 1)
+          space
+        # elsif board.valid_move?(valid_moves[space.to_i - 1])
+        #   valid_moves[0] || valid_moves[2] ||
+        #   valid_moves[6] || valid_moves[8]
+        # elsif board.valid_move?(valid_moves[space.to_i - 1])
+        #   valid_moves[1] || valid_moves[3] ||
+        #   valid_moves[5] || valid_moves[7]
         end
-        # binding.pry
+        binding.pry
       end
     end
   end
 end
 
+# a.find_index(obj)
 # a.insert(index, obj)
