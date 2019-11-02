628733be2c70c34f8251920e3cdd68fdcd9c2e74
Dan Suarez <suarez.daniel101@gmail.com>
2019-11-02 20:03:14 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -23,21 +23,30 @@ module Players
     def move(board)
       # valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
       board.cells.each do |space|
-        if board.valid_move?(space.to_i + 1)
+        board.cells.include?(board.position(space))
           space.to_i + 1
-        #
-        # # elsif board.valid_move?(valid_moves[space.to_i - 1])
-        # #   valid_moves[0] || valid_moves[2] ||
-        # #   valid_moves[6] || valid_moves[8]
-        # # elsif board.valid_move?(valid_moves[space.to_i - 1])
-        # #   valid_moves[1] || valid_moves[3] ||
-        # #   valid_moves[5] || valid_moves[7]
-        # end
+        # board.position(space)
         binding.pry
+        # if board.valid_move?(space.to_i + 1)
+        #   space.to_i + 1
+        # else
+        #   move(board)
+        # # # elsif board.valid_move?(valid_moves[space.to_i - 1])
+        # # #   valid_moves[0] || valid_moves[2] ||
+        # # #   valid_moves[6] || valid_moves[8]
+        # # # elsif board.valid_move?(valid_moves[space.to_i - 1])
+        # # #   valid_moves[1] || valid_moves[3] ||
+        # # #   valid_moves[5] || valid_moves[7]
+        # end
+        # binding.pry
       end
     end
   end
 end
+
+# a = [ "a", "b", "c" ]
+# a.include?("b")   #=> true
+# a.include?("z")   #=> false
 
 # describe '#move' do
 #     it 'returns a valid position for the computer to move' do
