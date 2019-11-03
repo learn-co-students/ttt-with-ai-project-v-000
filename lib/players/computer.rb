0e92f51f3c5e9d358c350c07aa20f740eb6edfdd
Paris Dolfman <parisdolfman@gmail.com>
2019-11-03 22:22:52 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,8 +2,17 @@ module Players
   class Computer < Player
 
   def move(board)
-
+    if board.cells[4] == " "
+      "5"
+    elsif board.cells[6] == " "
+      "7"
+    elsif board.cells[7] == " "
+      "8"
+    elsif board.cells[0] == " "
+      "1"
+    elsif board.cells[2] == " "
+      "3"
   end
-
+end
   end
 end
