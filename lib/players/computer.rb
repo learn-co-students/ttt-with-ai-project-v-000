7b64db84e7ea716432da67e6da7c761a31d23322
Dan Suarez <suarez.daniel101@gmail.com>
2019-11-06 23:12:39 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,11 +1,11 @@
 module Players
   class Computer < Player
     def move(board)
-       board.cells.each do |space|
-       space = board.cells.index(space)
-        if board.cells.include?(space)
-          space.to_s
-        end
+       board.cells.map do |space|
+       computer_move = board.cells.index(space)
+      #   if board.cells.include?(space)
+      #     space.to_s
+      #   end
         binding.pry
       end
     end
