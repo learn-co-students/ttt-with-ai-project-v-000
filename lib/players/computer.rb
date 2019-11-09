b388601b6f4189b70e15065eeaf888a3dafd7f13
Dan Suarez <suarez.daniel101@gmail.com>
2019-11-09 04:58:19 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,8 +3,14 @@ module Players
     def move(board)
        board.cells.each do |space|
        computer_move = board.cells.index(space) + 1
-        if board.valid_move?(computer_move)
+        if board.valid_move?(computer_move) == "5"
           return computer_move.to_s
+        elsif board.valid_move?(computer_move) == "1" || board.valid_move?(computer_move) == "3" ||
+          board.valid_move?(computer_move) == "7" || board.valid_move?(computer_move) == "9"
+          return computer_move.to_s
+        elsif board.valid_move?(computer_move) == "2" || board.valid_move?(computer_move) == "4" ||
+          board.valid_move?(computer_move) == "6" || board.valid_move?(computer_move) == "8"
+          computer_move.to_s
         end
         # binding.pry
       end
