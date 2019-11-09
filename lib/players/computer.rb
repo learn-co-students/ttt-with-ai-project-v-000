9c0522e93719b46f2ecd426efd96a8190129d286
Dan Suarez <suarez.daniel101@gmail.com>
2019-11-09 05:31:58 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,14 +3,20 @@ module Players
     def move(board)
        board.cells.each do |space|
        computer_move = board.cells.index(space) + 1
-        if board.valid_move?(computer_move) == "5"
-          return computer_move.to_s
-        elsif board.valid_move?(computer_move) == "1" || board.valid_move?(computer_move) == "3" ||
-          board.valid_move?(computer_move) == "7" || board.valid_move?(computer_move) == "9"
-          return computer_move.to_s
-        elsif board.valid_move?(computer_move) == "2" || board.valid_move?(computer_move) == "4" ||
-          board.valid_move?(computer_move) == "6" || board.valid_move?(computer_move) == "8"
-          computer_move.to_s
+        if computer_move.to_s == "5"
+          if board.valid_move?(computer_move)
+            return computer_move.to_s
+          end
+        elsif computer_move.to_s == "1" || computer_move.to_s == "3" || computer_move.to_s == "7" ||
+            computer_move.to_s == "9"
+          if board.valid_move?(computer_move)
+            return computer_move.to_s
+          end
+        elsif computer_move.to_s == "2" || computer_move.to_s == "4" || computer_move.to_s == "6" ||
+            computer_move.to_s == "8"
+          if board.valid_move?(computer_move)
+            return computer_move.to_s
+          end
         end
         # binding.pry
       end
