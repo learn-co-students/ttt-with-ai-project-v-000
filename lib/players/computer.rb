bb23bb62732067eca566c220d70ec433e0ae57e0
Dan Suarez <suarez.daniel101@gmail.com>
2019-11-12 05:32:06 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,22 +1,36 @@
+# module Players
+#   class Computer < Player
+#     def move(board)
+#        board.cells.each do |space|
+#        computer_move = board.cells.index(space) + 1
+#         if computer_move.to_s == "5"
+#           if board.valid_move?(computer_move)
+#             return computer_move.to_s
+#           end
+#         elsif computer_move.to_s == "1" || computer_move.to_s == "3" || computer_move.to_s == "7" ||
+#             computer_move.to_s == "9"
+#           if board.valid_move?(computer_move)
+#             return computer_move.to_s
+#           end
+#         elsif computer_move.to_s == "2" || computer_move.to_s == "4" || computer_move.to_s == "6" ||
+#             computer_move.to_s == "8"
+#           if board.valid_move?(computer_move)
+#             return computer_move.to_s
+#           end
+#         end
+#         # binding.pry
+#       end
+#     end
+#   end
+# end
+
 module Players
   class Computer < Player
     def move(board)
        board.cells.each do |space|
        computer_move = board.cells.index(space) + 1
-        if computer_move.to_s == "5"
-          if board.valid_move?(computer_move)
-            return computer_move.to_s
-          end
-        elsif computer_move.to_s == "1" || computer_move.to_s == "3" || computer_move.to_s == "7" ||
-            computer_move.to_s == "9"
-          if board.valid_move?(computer_move)
-            return computer_move.to_s
-          end
-        elsif computer_move.to_s == "2" || computer_move.to_s == "4" || computer_move.to_s == "6" ||
-            computer_move.to_s == "8"
-          if board.valid_move?(computer_move)
-            return computer_move.to_s
-          end
+        if board.valid_move?(computer_move)
+          return computer_move.to_s
         end
         # binding.pry
       end
