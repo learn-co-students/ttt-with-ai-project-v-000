d6a646fda01fc6927d9b811c681e5d9ba86bd7d9
Dan Suarez <suarez.daniel101@gmail.com>
2019-11-14 22:24:41 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -24,15 +24,42 @@
 #   end
 # end
 
+# module Players
+#   class Computer < Player
+#     def move(board)
+#        board.cells.each do |space|
+#        computer_move = board.cells.index(space) + 1
+#         if board.valid_move?(computer_move)
+#           return computer_move.to_s
+#         end
+#         # binding.pry
+#       end
+#     end
+#   end
+# end
+
 module Players
   class Computer < Player
     def move(board)
-       board.cells.each do |space|
-       computer_move = board.cells.index(space) + 1
-        if board.valid_move?(computer_move)
-          return computer_move.to_s
-        end
-        # binding.pry
+       board.cells.select do |space|
+        computer_move1 = board.cells.index(space) + 5
+          if board.valid_move?(computer_move1)
+          # binding.pry
+          return computer_move1.to_s
+          end
+
+          elsif computer_move2 = board.cells.index(space) + 1 || board.cells.index(space) + 3 ||
+          board.cells.index(space) + 7 || board.cells.index(space) + 9
+            if board.valid_move?(rand(computer_move2))
+              return computer_move2.to_s
+            end
+
+          elsif computer_move3 =  computer_move3 = board.cells.index(space) + 2 ||board.cells.index(space) + 4 ||
+          board.cells.index(space) + 6 || board.cells.index(space) + 8
+            if board.valid_move?(rand(computer_move3))
+              return computer_move3.to_s
+            end
+          end
       end
     end
   end
