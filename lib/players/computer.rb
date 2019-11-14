48b4bf0c7b7c9df8c5464c86a57ca27ff9092192
Dan Suarez <suarez.daniel101@gmail.com>
2019-11-14 22:26:16 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -45,8 +45,7 @@ module Players
         computer_move1 = board.cells.index(space) + 5
           if board.valid_move?(computer_move1)
           # binding.pry
-          return computer_move1.to_s
-          end
+            return computer_move1.to_s
 
           elsif computer_move2 = board.cells.index(space) + 1 || board.cells.index(space) + 3 ||
           board.cells.index(space) + 7 || board.cells.index(space) + 9
@@ -54,7 +53,7 @@ module Players
               return computer_move2.to_s
             end
 
-          elsif computer_move3 =  computer_move3 = board.cells.index(space) + 2 ||board.cells.index(space) + 4 ||
+          elsif computer_move3 = board.cells.index(space) + 2 ||board.cells.index(space) + 4 ||
           board.cells.index(space) + 6 || board.cells.index(space) + 8
             if board.valid_move?(rand(computer_move3))
               return computer_move3.to_s
