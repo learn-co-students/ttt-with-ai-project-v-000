06571942a4ca9a6512eb2f72465d34a5f106b5a8
Dan Suarez <suarez.daniel101@gmail.com>
2019-11-15 21:22:33 +0000

    Done.

--- lib/player.rb
+++ lib/player.rb
@@ -5,4 +5,3 @@ class Player
     @token = token
   end
 end
-# binding.pry
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,11 +1,8 @@
 module Players
   class Human < Player
-    # your code here
     def move(board)
-      # binding.pry  
       puts "Please enter 1-9:"
       gets.strip
-      # binding.pry
     end
   end
 end
