9f9fb572038cec582a8c353e5a1b9e1077fdb181
Dan Suarez <suarez.daniel101@gmail.com>
2019-11-15 21:06:45 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,3 +1,31 @@
+module Players
+  class Computer < Player
+    def move(board)
+       board.cells.each do |space|
+        computer_move1 = board.cells.index(space) + 5
+          if board.valid_move?(computer_move1)
+          # binding.pry
+            return computer_move1.to_s
+
+          elsif computer_move2 = board.cells.index(space) + 1 || board.cells.index(space) + 3 ||
+          board.cells.index(space) + 7 || board.cells.index(space) + 9
+            rand(computer_move2)
+            if board.valid_move?(computer_move2)
+              return computer_move2.to_s
+            end
+          elsif computer_move3 = board.cells.index(space) + 2 || board.cells.index(space) + 4 ||
+          board.cells.index(space) + 6 || board.cells.index(space) + 8
+            rand(computer_move3)
+            if board.valid_move?(computer_move3)
+              return computer_move3.to_s
+            end
+          end
+      end
+    end
+  end
+end
+
+
 # module Players
 #   class Computer < Player
 #     def move(board)
@@ -37,29 +65,3 @@
 #     end
 #   end
 # end
-
-module Players
-  class Computer < Player
-    def move(board)
-       board.cells.select do |space|
-        computer_move1 = board.cells.index(space) + 5
-          if board.valid_move?(computer_move1)
-          # binding.pry
-            return computer_move1.to_s
-
-          elsif computer_move2 = rand(board.cells.index(space) + 1 || board.cells.index(space) + 3 ||
-          board.cells.index(space) + 7 || board.cells.index(space) + 9)
-            if board.valid_move?(computer_move2)
-              return computer_move2.to_s
-            end
-
-          elsif computer_move3 = rand(board.cells.index(space) + 2 || board.cells.index(space) + 4 ||
-          board.cells.index(space) + 6 || board.cells.index(space) + 8)
-            if board.valid_move?(computer_move3)
-              return computer_move3.to_s
-            end
-          end
-      end
-    end
-  end
-end
