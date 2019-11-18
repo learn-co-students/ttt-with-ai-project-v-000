d8765cebf0050b6bf2eb76f69bada9eedce8f796
Dan Suarez <suarez.daniel101@gmail.com>
2019-11-18 16:42:21 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,23 +1,33 @@
 module Players
   class Computer < Player
     def move(board)
-       board.cells.each do |space|
-
+      #  board.cells.each_index do |space|
+      board.cells.map do |space|
         # computer_move1 = board.cells.index(space) + 5
-        #   if board.valid_move?(computer_move1)
-        #     return computer_move1.to_s
-        computer_move2 = []
-          computer_move2 << board.cells.index(space) + 1 || board.cells.index(space) + 2 ||
-          board.cells.index(space) + 3 || board.cells.index(space) + 4 || board.cells.index(space) + 5 ||
-          board.cells.index(space) + 6 || board.cells.index(space) + 7 || board.cells.index(space) + 8 ||
-          board.cells.index(space) + 9
+        computer_move1 = board.cells.index(space) + 1
+        # if computer_move1 = space + 5
+          if board.valid_move?(computer_move1)
+            return computer_move1.to_s
+          end
+
+      # if computer_move2 = space
+      #   # rand(computer_move2)
+      #     if board.valid_move?(computer_move2)
+      #       return computer_move2.to_s
+      #     end
+
+        # binding.pry
+          # computer_move2 << board.cells.index(space) + 1 || board.cells.index(space) + 2 ||
+          # board.cells.index(space) + 3 || board.cells.index(space) + 4 || board.cells.index(space) + 5 ||
+          # board.cells.index(space) + 6 || board.cells.index(space) + 7 || board.cells.index(space) + 8 ||
+          # board.cells.index(space) + 9
           #
           #   rand(computer_move2)
             # binding.pry
             # computer_move2
-            if board.valid_move?(computer_move2)
-              return computer_move2.to_s
-            end
+            # if board.valid_move?(computer_move2)
+            #   return computer_move2.to_s
+            # end
           # elsif computer_move3 = board.cells.index(space) + 2 || board.cells.index(space) + 4 ||
           # board.cells.index(space) + 6 || board.cells.index(space) + 8
           #   rand(computer_move3)
