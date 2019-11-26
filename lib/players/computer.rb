f36820aa915074552497aa770bb476b3db16d7cd
Dan Suarez <suarez.daniel101@gmail.com>
2019-11-26 21:04:14 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -20,37 +20,37 @@ module Players
   end
 end
 
-def won?
-  WIN_COMBINATIONS.find do |winner|
-    @board.cells[winner[0]] == @board.cells[winner[1]] ||
-    @board.cells[winner[3]] == @board.cells[winner[4]] ||
-    @board.cells[winner[6]] == @board.cells[winner[7]] ||
-    @board.cells[winner[1]] == @board.cells[winner[2]] ||
-    @board.cells[winner[4]] == @board.cells[winner[5]] ||
-    @board.cells[winner[7]] == @board.cells[winner[8]] ||
-    @board.cells[winner[0]] == @board.cells[winner[3]] ||
-    @board.cells[winner[1]] == @board.cells[winner[4]] ||
-    @board.cells[winner[3]] == @board.cells[winner[5]] ||
-    @board.cells[winner[3]] == @board.cells[winner[6]] ||
-    @board.cells[winner[4]] == @board.cells[winner[7]] ||
-    @board.cells[winner[5]] == @board.cells[winner[8]] ||
-    @board.cells[winner[2]] == @board.cells[winner[4]] ||
-    @board.cells[winner[4]] == @board.cells[winner[6]] ||
-    @board.cells[winner[0]] == @board.cells[winner[4]] ||
-    @board.cells[winner[4]] == @board.cells[winner[8]] ||
-    @board.cells[winner[0]] == @board.cells[winner[2]] ||
-    @board.cells[winner[2]] == @board.cells[winner[8]] ||
-    @board.cells[winner[2]] == @board.cells[winner[6]] ||
-    @board.cells[winner[3]] == @board.cells[winner[5]] ||
-    @board.cells[winner[1]] == @board.cells[winner[7]] ||
-    @board.cells[winner[0]] == @board.cells[winner[8]] ||
-    @board.cells[winner[6]] == @board.cells[winner[8]] ||
-    @board.cells[winner[0]] == @board.cells[winner[6]] ||
-    (@board.cells[winner[0]] == "X" || @board.cells[winner[0]] == "O")
-  end
-end
-
-1. Check to see if two of the same tokens are next to each other and if so occupy the third spot next to them.
-
-2. Check to see if two of the same tokes are in corner spots diagonally across from
-each other and if so occupy the middle spot.
+# def won?
+#   WIN_COMBINATIONS.find do |winner|
+#     @board.cells[winner[0]] == @board.cells[winner[1]] ||
+#     @board.cells[winner[3]] == @board.cells[winner[4]] ||
+#     @board.cells[winner[6]] == @board.cells[winner[7]] ||
+#     @board.cells[winner[1]] == @board.cells[winner[2]] ||
+#     @board.cells[winner[4]] == @board.cells[winner[5]] ||
+#     @board.cells[winner[7]] == @board.cells[winner[8]] ||
+#     @board.cells[winner[0]] == @board.cells[winner[3]] ||
+#     @board.cells[winner[1]] == @board.cells[winner[4]] ||
+#     @board.cells[winner[3]] == @board.cells[winner[5]] ||
+#     @board.cells[winner[3]] == @board.cells[winner[6]] ||
+#     @board.cells[winner[4]] == @board.cells[winner[7]] ||
+#     @board.cells[winner[5]] == @board.cells[winner[8]] ||
+#     @board.cells[winner[2]] == @board.cells[winner[4]] ||
+#     @board.cells[winner[4]] == @board.cells[winner[6]] ||
+#     @board.cells[winner[0]] == @board.cells[winner[4]] ||
+#     @board.cells[winner[4]] == @board.cells[winner[8]] ||
+#     @board.cells[winner[0]] == @board.cells[winner[2]] ||
+#     @board.cells[winner[2]] == @board.cells[winner[8]] ||
+#     @board.cells[winner[2]] == @board.cells[winner[6]] ||
+#     @board.cells[winner[3]] == @board.cells[winner[5]] ||
+#     @board.cells[winner[1]] == @board.cells[winner[7]] ||
+#     @board.cells[winner[0]] == @board.cells[winner[8]] ||
+#     @board.cells[winner[6]] == @board.cells[winner[8]] ||
+#     @board.cells[winner[0]] == @board.cells[winner[6]] ||
+#     (@board.cells[winner[0]] == "X" || @board.cells[winner[0]] == "O")
+#   end
+# end
+#
+# 1. Check to see if two of the same tokens are next to each other and if so occupy the third spot next to them.
+#
+# 2. Check to see if two of the same tokes are in corner spots diagonally across from
+# each other and if so occupy the middle spot.
