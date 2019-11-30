864421da1eef4296c80d27dab8898da21abffff8
Dan Suarez <suarez.daniel101@gmail.com>
2019-11-30 20:50:27 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -17,51 +17,51 @@ module Players
       WIN_COMBINATIONS.find do |drawer|
         if @board.cells[drawer[0]] == @board.cells[drawer[1]]
           computer_move = @board.cells[drawer[2]] ||
-        if @board.cells[drawer[3]] == @board.cells[drawer[4]]
+        elsif @board.cells[drawer[3]] == @board.cells[drawer[4]]
           computer_move = @board.cells[drawer[5]] ||
-        if @board.cells[drawer[6]] == @board.cells[drawer[7]]
+        elsif @board.cells[drawer[6]] == @board.cells[drawer[7]]
           computer_move = @board.cells[drawer[8]] ||
-        if @board.cells[drawer[1]] == @board.cells[drawer[2]]
+        elsif @board.cells[drawer[1]] == @board.cells[drawer[2]]
           computer_move = @board.cells[drawer[0]] ||
-        if @board.cells[drawer[4]] == @board.cells[drawer[5]]
+        elsif @board.cells[drawer[4]] == @board.cells[drawer[5]]
           computer_move = @board.cells[drawer[3]] ||
-        if @board.cells[drawer[7]] == @board.cells[drawer[8]]
+        elsif @board.cells[drawer[7]] == @board.cells[drawer[8]]
           computer_move = @board.cells[drawer[6]] ||
-        if @board.cells[drawer[1]] == @board.cells[drawer[4]]
+        elsif @board.cells[drawer[1]] == @board.cells[drawer[4]]
           computer_move = @board.cells[drawer[7]] ||
-        if @board.cells[drawer[0]] == @board.cells[drawer[3]]
+        elsif @board.cells[drawer[0]] == @board.cells[drawer[3]]
           computer_move = @board.cells[drawer[6]] ||
-        if @board.cells[drawer[2]] == @board.cells[drawer[5]]
+        elsif @board.cells[drawer[2]] == @board.cells[drawer[5]]
           computer_move = @board.cells[drawer[8]] ||
-        if @board.cells[drawer[3]] == @board.cells[drawer[6]]
+        elsif @board.cells[drawer[3]] == @board.cells[drawer[6]]
           computer_move = @board.cells[drawer[0]] ||
-        if @board.cells[drawer[4]] == @board.cells[drawer[7]]
+        elsif @board.cells[drawer[4]] == @board.cells[drawer[7]]
           computer_move = @board.cells[drawer[1]] ||
-        if @board.cells[drawer[5]] == @board.cells[drawer[8]]
+        elsif @board.cells[drawer[5]] == @board.cells[drawer[8]]
           computer_move = @board.cells[drawer[2]] ||
-        if @board.cells[drawer[2]] == @board.cells[drawer[4]]
+        elsif @board.cells[drawer[2]] == @board.cells[drawer[4]]
           computer_move = @board.cells[drawer[6]] ||
-        if @board.cells[drawer[4]] == @board.cells[drawer[6]]
+        elsif @board.cells[drawer[4]] == @board.cells[drawer[6]]
           computer_move = @board.cells[drawer[2]] ||
-        if @board.cells[drawer[0]] == @board.cells[drawer[4]]
+        elsif @board.cells[drawer[0]] == @board.cells[drawer[4]]
           computer_move = @board.cells[drawer[8]] ||
-        if @board.cells[drawer[4]] == @board.cells[drawer[8]]
+        elsif @board.cells[drawer[4]] == @board.cells[drawer[8]]
           computer_move = @board.cells[drawer[0]] ||
-        if @board.cells[drawer[0]] == @board.cells[drawer[2]]
+        elsif @board.cells[drawer[0]] == @board.cells[drawer[2]]
           computer_move = @board.cells[drawer[1]] ||
-        if @board.cells[drawer[3]] == @board.cells[drawer[5]]
+        elsif @board.cells[drawer[3]] == @board.cells[drawer[5]]
           computer_move = @board.cells[drawer[4]] ||
-        if @board.cells[drawer[6]] == @board.cells[drawer[8]]
+        elsif @board.cells[drawer[6]] == @board.cells[drawer[8]]
           computer_move = @board.cells[drawer[7]] ||
-        if @board.cells[drawer[0]] == @board.cells[drawer[6]]
+        elsif @board.cells[drawer[0]] == @board.cells[drawer[6]]
           computer_move = @board.cells[drawer[3]] ||
-        if @board.cells[drawer[1]] == @board.cells[drawer[7]]
+        elsif @board.cells[drawer[1]] == @board.cells[drawer[7]]
           computer_move = @board.cells[drawer[4]] ||
-        if @board.cells[drawer[2]] == @board.cells[drawer[8]]
+        elsif @board.cells[drawer[2]] == @board.cells[drawer[8]]
           computer_move = @board.cells[drawer[5]] ||
-        if @board.cells[drawer[2]] == @board.cells[drawer[6]]
+        elsif @board.cells[drawer[2]] == @board.cells[drawer[6]]
           computer_move = @board.cells[drawer[4]] ||
-        if @board.cells[drawer[0]] == @board.cells[drawer[8]]
+        elsif @board.cells[drawer[0]] == @board.cells[drawer[8]]
           computer_move = @board.cells[drawer[4]] ||
 
         (@board.cells[drawer[0]] == "X" || @board.cells[drawer[0]] == "O")
