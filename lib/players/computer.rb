c34617703e4f31e4885533dfdf353ea2041e4c28
Dan Suarez <suarez.daniel101@gmail.com>
2019-12-01 00:38:37 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,38 +1,12 @@
-# module Players
-#   class Computer < Player
-#     def move(board)
-#       computer_move = rand(0..9)
-#       until board.valid_move?(computer_move)
-#           computer_move = rand(0..9)
-#       end
-#        return computer_move.to_s
-#     end
-#   end
-# end
-
 module Players
   class Computer < Player
-
-    # WIN_COMBINATIONS = [
-    #   [0,1,2],
-    #   [3,4,5],
-    #   [6,7,8],
-    #   [1,4,7],
-    #   [0,3,6],
-    #   [2,5,8],
-    #   [2,4,6],
-    #   [0,4,8],
-    # ]
-
     def move(board)
-     if draw_move?
-       @board.cells[wincombo[0]]
-     else computer_move = rand(0..9)
-       board.valid_move?(computer_move)
+      computer_move = rand(0..9)
+      until board.valid_move?(computer_move)
+          computer_move = rand(0..9)
+      end
        return computer_move.to_s
-     end
     end
-
   end
 end
 
