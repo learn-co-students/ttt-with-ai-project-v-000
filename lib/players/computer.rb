bcc52f7d5ae5e7e941170b129170728922f4c7e1
Erica Murdock-Waters <ericahpa@gmail.com>
2020-04-15 22:52:18 +0000

    added layer of strategy to Players::Computer #move method

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,7 +1,14 @@
 module Players
   class Computer < Player
+
     def move(board)
-      rand(1..9).to_s
+      puts "Computer select move 1 - 9"
+      if !board.taken?(5)
+        "5"
+      else
+       rand(1..9).to_s
+      end
+
     end
   end
 
