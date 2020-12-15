47c0629c7515a472908a0df74cafc53e0714f95e
Jaime Johnsen <drivetraindiva@gmail.com>
2020-12-15 04:05:33 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -11,3 +11,15 @@ class Board
 
   end
 end
+
+def initialize
+  @board = Array.new(9, " ")
+end
+
+def display_board
+  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+  puts "-----------"
+  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+  puts "-----------"
+  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+end
