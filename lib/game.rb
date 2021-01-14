cd8ff2c7d57a2a23b28e5c832798684fd8977fb5
Jaime Johnsen <drivetraindiva@gmail.com>
2021-01-14 19:09:45 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,14 @@
 class Game
+attr_accessor :board, :player_1, :player_2
 
+WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
+                    [0,3,6], [1,4,7], [2,5,8],
+                  [0,4,8], [6,4,2]]
+
+def initialize(player1=Players::Human.new("X"), player2=Players::Human.new("O"), brd=Board.new)
+@player_1 = player1
+@player_2 = player2
+@board = brd
+
+end
 end
