60a6a2205ebea2556cad707cc84e8e57de95d801
Jaime Johnsen <drivetraindiva@gmail.com>
2021-01-20 03:38:30 +0000

    Done.

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,6 +1,6 @@
-module Players #overwriting module that wraps the Computer class
-  class Human < Player # Human class inherits all functionality form Player module
-
+module Players #overwriting module that wraps the Human class
+  class Human < Player # Human class inherits all functionality from Player module
+                        #? IS the note in line 2 wrong, it should by from Player class
     def move(input)
       puts "Please enter your selection: [X or O]"
       gets.strip
