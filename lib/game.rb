3a2bbb470cbfd464556f4b9ecd24c3fde959bc4c
Jaime Johnsen <drivetraindiva@gmail.com>
2021-01-22 02:19:36 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,9 @@
 class Game
   attr_accessor :board, :player_1, :player_2
 
-  WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
-                    [0,3,6], [1,4,7], [2,5,8],
-                  [0,4,8], [6,4,2]]
+  WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8],
+                      [0,3,6], [1,4,7], [2,5,8],
+                      [0,4,8], [6,4,2]]
 
   def initialize(player1=Players::Human.new("X"), player2=Players::Human.new("O"), brd=Board.new)
     @player_1 = player1
@@ -11,12 +11,39 @@ class Game
     @board = brd
   end
 
-  def current_player
-
+  def current_player(board)
+    if turn_count(board).even?
+      return "X"
+    else
+      return "O"
+    end
   end
 
-  def won?
+#  def won?
+#   if board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
+#   false
+#   else
+#  end
 
-  end
+#def draw?
+
+#end
+
+#def over?
+
+#end
+
+#def turn
+
+#end
+
+#def play
+
+#end
+
+#def start
+
+#end
+
 
 end
