9ec13d75a881d3c152a6786eacece1b14cea8a30
Jaime Johnsen <drivetraindiva@gmail.com>
2021-02-02 04:11:07 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -2,8 +2,8 @@ class Game
   attr_accessor :board, :player_1, :player_2
 
   WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8],
-                      [0,3,6], [1,4,7], [2,5,8],
-                      [0,4,8], [6,4,2]]
+  [0,3,6], [1,4,7], [2,5,8],
+  [0,4,8], [6,4,2]]
 
   def initialize(player1=Players::Human.new("X"), player2=Players::Human.new("O"), brd=Board.new)
     @player_1 = player1
@@ -19,37 +19,43 @@ class Game
     end
   end
 
- def won?
-   WIN_COMBINATIONS.each do |wins|              #values_at method accepts a variable number of arguments
-    tokens = @board.cells.values_at(*wins).uniq # splat(*) take the array and take each element in the array
-     if tokens.count == 1 && tokens[0] != " "   # and feed it's individual to the values_at method as if we had
-       return wins                              # a comma seperated list or fed them as individual parameters.
-     end
-   end
-   false
- end
-
- def draw?
-   WIN_COMBINATIONS.each do |wins|
-     tokens = @board.cells.values_at(*wins).uniq
-     if tokens.count
+  def won?
+    WIN_COMBINATIONS.each do |wins|              #values_at method accepts a variable number of arguments
+      tokens = @board.cells.values_at(*wins).uniq # splat(*) take the array and take each element in the array
+      if tokens.count == 1 && tokens[0] != " "   # and feed it's individual to the values_at method as if we had
+        return wins                              # a comma seperated list or fed them as individual parameters.
+      end                                        #wins is the variable representing win combinations at index level
+    end
+    false
   end
 
-#def over?
+  def draw?
+    true unless won? or @board.cells.include?(" ")
+    #  return false if won? or @board.cells.include?(" ") # modifier version of the if. ?unique to Ruby. code before if will not run if false.
+    #  return true #rework logic - into one line ## true unless won? or @board.cells.include?(" ")
+  end
 
-#end
+  def over?
+    won? or draw?
+  end
 
-#def turn
+  def winner
+     if there is a winner won? is true
+       returns winning token x or o
+     else there is no winner
+        (a draw) returns nil
+  end
+  #def turn
 
-#end
+  #end
 
-#def play
+  #def play
 
-#end
+  #end
 
-#def start
+  #def start
 
-#end
+  #end
 
 
 end
