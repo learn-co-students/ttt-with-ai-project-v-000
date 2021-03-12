4c7c531d8bfbf1af1f53ed73f038dae418ebf093
Valencia Caldwell <vkcaldwell@gmail.com>
2021-03-12 17:31:50 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,14 @@
 class Game
+attr_accessor :board, :player
+
+  WIN_COMBINATIONS = [
+ [0,1,2],
+ [3,4,5],
+ [6,7,8],
+ [0,3,6],
+ [1,4,7],
+ [2,5,8],
+ [0,4,8],
+ [2,4,6] ]
 
 end
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,5 +1,11 @@
 module Players
   class Human < Player
 
+  #Human will give make a move by inputting an integer that corresponds with the board
+  #The move should return the value of the user's input
+
+  def move(board)
+    user_input = gets.strip
+  end
   end
 end
