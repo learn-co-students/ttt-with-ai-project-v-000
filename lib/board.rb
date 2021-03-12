799d0b2e4d0750bd6fc1b0c0ddaad83c3615c16e
Valencia Caldwell <vkcaldwell@gmail.com>
2021-03-12 17:11:10 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -17,30 +17,44 @@ class Board
    puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
    puts "-----------"
    puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
- end
- def position(input)
-   cells[input.to_i-1]
- end
+  end
 
- def full?
-  # cells.each(&.include?("X"||"O"):true))
-  cells.all? do |character|
-    if character == "X" || character == "O"
-        true
+  def position(user_input)
+    self.cells[user_input.to_i-1]
+  end
+
+  def full?
+    # cells.each(&.include?("X"||"O"):true))
+    self.cells.all? do |char|
+      if char == "X" || char == "O"
+          true
       else
-        false
+          false
       end
     end#if position is X or O then return true
   end
 
-  def turn_count # count all of the cells that have X or O
-    cells.count{|char| char == "X" || char == "O"}# cells.count("X") + cells.count("O")# cells.all.count(cells) # if its not full => the number of X or O
-    # binding.pry
-  end
-  def taken?(input) #returns true if the positon is "X" or "O"
-    position.full?(input)
 
+
+  def turn_count # count all of the cells that have X or O
+    self.cells.count("X") + self.cells.count("O")# cells.count("X") + cells.count("O")# cells.all.count(cells) # if its not full => the number of X or O
   end
+
+  def taken?(position) #returns true if the positon is "X" or "O"
+    self.cells[position.to_i-1] == "X" ||self.cells[position.to_i-1] == "O"
+  end
+
+  def valid_move?(position) #returns true for user input 1-9 that is NOT take
+    position.to_i.between?(1,9) && !taken?(position)
+  end
+
+  def update(position,player) #gets the users input "X" or "O"
+    # self.position(user_input.to_i-1) == "#{player.token}"# put in token in correct position
+    self.cells[position.to_i-1] = player.token
+    # binding.pry
+    # self.position(user_input) = "#{player.token}" end
+  end
+
 
 
 end
