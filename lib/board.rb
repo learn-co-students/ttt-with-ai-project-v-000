b81794d0e3b10461c6bcb4caf645574aa534c3cb
atxrenegade <harleighabel@gmail.com>
2021-06-21 20:53:02 -0500

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -10,9 +10,9 @@ class Board
 
   def display
     puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
-    puts "-----------"
+    puts "-------------------------------------------"
     puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
-    puts "-----------"
+    puts "-------------------------------------------"
     puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
   end 
 
